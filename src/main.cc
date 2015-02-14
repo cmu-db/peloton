12dd6aa39cf048178861b1fd300e3f330552408f
jarulraj <jarulraj@cs.cmu.edu>
2015-02-14 17:53:23 -0500

    Added main template

---
+++ src/main.cc
@@ -0,0 +1,21 @@
+/*-------------------------------------------------------------------------
+*
+* main.cc
+* Entry point for n-store
+*
+* Copyright(c) 2015, CMU
+*
+* /n-store/src/main.cc
+*
+*-------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+
+  return EXIT_SUCCESS;
+}
