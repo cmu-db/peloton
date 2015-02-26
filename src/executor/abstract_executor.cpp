6b32e4db02284cd9d204fca6a80e0cf3d568dbfa
Eddy Yeo <eddy.yeo@gmail.com>
2015-02-26 17:35:57 -0500

    Added abstract executor class.

---
+++ src/executor/abstract_executor.cpp
@@ -0,0 +1,22 @@
+#include "abstract_executor.h"
+
+namespace nstore {
+namespace executor {
+
+bool AbstractExecutor::init() {
+    //TODO Insert any code common to all executors here.
+    p_init();
+}
+
+storage::Tile *AbstractExecutor::getNextTile() {
+    //TODO Insert any code common to all executors here.
+    return p_getNextTile();
+}
+
+void AbstractExecutor::cleanup() {
+    //TODO Insert any code common to all executors here.
+    p_cleanup();
+}
+
+} // namespace executor
+} // namespace nstore
--- src/executor/abstract_executor.h
+++ src/executor/abstract_executor.h
@@ -5,15 +5,20 @@
 namespace nstore {
 namespace executor {
 
-class AbstractPlanNode {
+class AbstractExecutor {
   public:
     bool init();
     storage::Tile *getNextTile();
     void cleanup();
   protected:
+    AbstractExecutor(AbstractPlanNode *abstract_node) {
+        plan_node_ = abstract_node;
+    }
     virtual bool p_init() = 0;
     virtual storage::Tile *p_getNextTile() = 0;
     virtual void p_cleanup() = 0;
+  private:
+    plannode::AbstractPlanNode plan_node_;
 };
 
 } // namespace executor
