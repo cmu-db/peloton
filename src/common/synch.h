6a5a12695c3d86aa08fb8fdd92864e0525d08ded
jarulraj <jarulraj@cs.cmu.edu>
2015-03-15 05:06:13 -0400

    Added templates and removed color support for gcc

--- src/common/synch.h
+++ src/common/synch.h
@@ -20,7 +20,7 @@ namespace nstore {
 
 template <typename T>
 inline bool atomic_cas(T* object, T old_value, T new_value) {
-	return __sync_bool_compare_and_swap(object, old_value, new_value);
+  return __sync_bool_compare_and_swap(object, old_value, new_value);
 }
 
 } // End nstore namespace
