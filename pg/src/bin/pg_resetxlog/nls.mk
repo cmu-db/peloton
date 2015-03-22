747d7e9bca7d7a182ca2aa5df1a12c6672da48e2
jarulraj <jarulraj@cs.cmu.edu>
2015-03-22 01:52:47 -0400

    Moved pg outside

--- pg/src/bin/pg_resetxlog/nls.mk
+++
@@ -1,4 +0,0 @@
-# src/bin/pg_resetxlog/nls.mk
-CATALOG_NAME     = pg_resetxlog
-AVAIL_LANGUAGES  = cs de es fr it ja pl pt_BR ru sv zh_CN
-GETTEXT_FILES    = pg_resetxlog.c
--- pg/src/config/test_quiet_include.h
+++
@@ -1,9 +0,0 @@
-/*
- * For the raison d'etre of this file, check the comment above the definition
- * of the PGAC_C_INLINE macro in config/c-compiler.m4.
- */
-static inline int
-fun()
-{
-	return 0;
-}
