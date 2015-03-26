2287437dfa0d3ac65929eaa5eab25a0a026a839f
jarulraj <jarulraj@cs.cmu.edu>
2015-03-26 03:14:47 -0400

    Updated index tests

--- tests/index/index_test.cpp
+++ tests/index/index_test.cpp
@@ -36,9 +36,11 @@ namespace test {
 TEST(IndexTests, ArrayUniqueIndexTest) {
 
   std::vector<catalog::ColumnInfo> columns;
-  std::vector<id_t> table_columns_in_key;
+  std::vector<std::string> tile_column_names;
+  std::vector<std::vector<std::string> > column_names;
+  std::vector<catalog::Schema*> schemas;
 
-  catalog::Catalog* catalog = new catalog::Catalog();
+  // SCHEMA
 
   catalog::ColumnInfo column1(VALUE_TYPE_BIGINT, GetTypeSize(VALUE_TYPE_BIGINT), false, true);
   catalog::ColumnInfo column2(VALUE_TYPE_BIGINT, GetTypeSize(VALUE_TYPE_BIGINT), false, true);
@@ -47,14 +49,67 @@ TEST(IndexTests, ArrayUniqueIndexTest) {
 
   columns.push_back(column1);
   columns.push_back(column2);
+
+  catalog::Schema *schema1 = new catalog::Schema(columns);
+  schemas.push_back(schema1);
+
+  columns.clear();
   columns.push_back(column3);
   columns.push_back(column4);
 
-  table_columns_in_key.push_back(0);
+  catalog::Schema *schema2 = new catalog::Schema(columns);
+  schemas.push_back(schema2);
 
-  catalog::Schema *schema = new catalog::Schema(columns);
+  catalog::Schema *schema = catalog::Schema::AppendSchema(schema1, schema2);
+
+  // TILES
+
+  tile_column_names.push_back("COL 1");
+  tile_column_names.push_back("COL 2");
+
+  column_names.push_back(tile_column_names);
+
+  tile_column_names.clear();
+  tile_column_names.push_back("COL 3");
+  tile_column_names.push_back("COL 4");
+
+  column_names.push_back(tile_column_names);
+
+  // TILE GROUP
+
+  catalog::Catalog *catalog = new catalog::Catalog();
+
+  storage::TileGroup *tile_group = storage::TileGroupFactory::GetTileGroup(schemas, 4, column_names, true, catalog);
+
+  // TUPLES
+
+  storage::Tuple *tuple0 = new storage::Tuple(schema, true);
+  storage::Tuple *tuple1 = new storage::Tuple(schema, true);
+  storage::Tuple *tuple2 = new storage::Tuple(schema, true);
+
+  for(id_t column_itr = 0; column_itr < schema->GetColumnCount(); column_itr++) {
+    tuple0->SetValue(column_itr, ValueFactory::GetBigIntValue(100 * (column_itr + 1)));
+    tuple1->SetValue(column_itr, ValueFactory::GetBigIntValue(200 * (column_itr + 1)));
+    tuple2->SetValue(column_itr, ValueFactory::GetBigIntValue(300 * (column_itr + 1)));
+  }
+
+  // TRANSACTION
+
+  txn_id_t txn_id = GetTransactionId();
+
+  tile_group->InsertTuple(txn_id, tuple0);
+  tile_group->InsertTuple(txn_id, tuple1);
+  tile_group->InsertTuple(txn_id, tuple2);
+
+  EXPECT_EQ(3, tile_group->GetActiveTupleCount());
+
+  std::cout << (*tile_group);
 
   // ARRAY UNIQUE INDEX
+
+  std::vector<id_t> table_columns_in_key;
+
+  table_columns_in_key.push_back(0);
 
   index::IndexMetadata *index_metadata = new index::IndexMetadata("array_unique",
                                                                   INDEX_TYPE_ARRAY,
@@ -68,76 +123,45 @@ TEST(IndexTests, ArrayUniqueIndexTest) {
 
   EXPECT_EQ(true, index != NULL);
 
-  storage::Tuple *tuple = new storage::Tuple(schema, true);
+  // INDEX
 
-  for(id_t column_itr = 0; column_itr > schema->GetColumnCount(); column_itr++)
-    tuple->SetValue(column_itr, ValueFactory::GetIntegerValue(500000 * column_itr));
+  id_t tile_id = tile_group->GetTileId(0);
+
+  ItemPointer *item0 = new ItemPointer(tile_id, 0);
+  ItemPointer *item1 = new ItemPointer(tile_id, 1);
+  ItemPointer *item2 = new ItemPointer(tile_id, 2);
+
+  index->AddEntry(item0);
+  index->AddEntry(item1);
+  index->AddEntry(item2);
 
   // SEARCH
 
   storage::Tuple *search_key = new storage::Tuple(key_schema, true);
-  search_key->SetValue(0, ValueFactory::GetIntegerValue(static_cast<int32_t>(500000)));
+  search_key->SetValue(0, ValueFactory::GetBigIntValue(static_cast<int32_t>(100)));
 
   bool found = index->MoveToKey(search_key);
   EXPECT_EQ(found, true);
 
   storage::Tuple *search_tuple = nullptr;
   int count = 0;
-  while ((tuple = index->NextValueAtKey()) != nullptr)  {
+  while ((search_tuple = index->NextValueAtKey()) != nullptr)  {
     ++count;
-
-    for(id_t column_itr = 0; column_itr > schema->GetColumnCount(); column_itr++)
-      EXPECT_TRUE(ValueFactory::GetBigIntValue(500000 * column_itr).OpEquals(search_tuple->GetValue(column_itr)).IsTrue());
+    EXPECT_TRUE(ValueFactory::GetBigIntValue(100).OpEquals(search_tuple->GetValue(0)).IsTrue());
   }
 
   EXPECT_EQ(1, count);
 
-  /*
-  search_key.
-  setNValue(0, ValueFactory::getBigIntValue(static_cast<int64_t>(1001)));
-  found = index->moveToKey(&search_key);
+  storage::Tuple *search_key2 = new storage::Tuple(key_schema, true);
+  search_key2->SetValue(0, ValueFactory::GetBigIntValue(static_cast<int32_t>(150)));
+  found = index->MoveToKey(search_key2);
+
   count = 0;
-  while (!(tuple = index->nextValueAtKey()).isNullTuple())
-  {
+  while ((search_tuple = index->NextValueAtKey()) != nullptr)  {
     ++count;
   }
+
   EXPECT_EQ(0, count);
-
-  TableTuple &tmptuple = table->tempTuple();
-  tmptuple.
-  setNValue(0, ValueFactory::getBigIntValue(static_cast<int64_t>(1234)));
-  tmptuple.setNValue(1, ValueFactory::getBigIntValue(0));
-  tmptuple.
-  setNValue(2, ValueFactory::getBigIntValue(static_cast<int64_t>(3333)));
-  tmptuple.
-  setNValue(3, ValueFactory::getBigIntValue(static_cast<int64_t>(-200)));
-  tmptuple.
-  setNValue(4, ValueFactory::getBigIntValue(static_cast<int64_t>(550)));
-  EXPECT_EQ(true, table->insertTuple(tmptuple));
-  tmptuple.
-  setNValue(0, ValueFactory::getBigIntValue(static_cast<int64_t>(1234)));
-  tmptuple.
-  setNValue(1, ValueFactory::getBigIntValue(0));
-  tmptuple.
-  setNValue(2, ValueFactory::getBigIntValue(static_cast<int64_t>(50 % 3)));
-  tmptuple.
-  setNValue(3, ValueFactory::getBigIntValue(static_cast<int64_t>(-200)));
-  tmptuple.
-  setNValue(4, ValueFactory::getBigIntValue(static_cast<int64_t>(550)));
-  TupleSchema::freeTupleSchema(keySchema);
-  delete[] search_key.address();
-  bool exceptionThrown = false;
-  try
-  {
-    EXPECT_EQ(false, table->insertTuple(tmptuple));
-  }
-  catch (SerializableEEException &e)
-  {
-    exceptionThrown = true;
-  }
-  EXPECT_TRUE(exceptionThrown);
-   */
-
 
 }
 
