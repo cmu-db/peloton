1fc52961e5a1e23d50405a75d51f8ed83ffe2da4
Eddy Yeo <eddy.yeo@gmail.com>
2015-03-27 00:48:59 -0400

    Fixed some style issues and made minor changes.

--- src/executor/abstract_executor.cpp
+++ src/executor/abstract_executor.cpp
@@ -1,26 +1,43 @@
-#include "abstract_executor.h"
+/**
+ * @brief Base class for all executors.
+ *
+ * Copyright(c) 2015, CMU
+ */
+
+#include "executor/abstract_executor.h"
+#include "executor/logical_tile.h"
 
 namespace nstore {
 namespace executor {
 
+//===--------------------------------------------------------------------===//
+// Abstract Executor
+//===--------------------------------------------------------------------===//
+
+AbstractExecutor::AbstractExecutor(planner::AbstractPlanNode *abstract_node)
+  : abstract_node_(abstract_node) {
+}
+
 bool AbstractExecutor::Init() {
 	//TODO Insert any code common to all executors here.
 
-	return Init();
+	return SubInit();
 }
 
-storage::Tile *AbstractExecutor::GetNextTile() {
+LogicalTile *AbstractExecutor::GetNextTile() {
 
 	//TODO Insert any code common to all executors here.
+  //TODO In the future, we might want to pass some kind of executor state to GetNextTile.
+  // e.g. params for prepared plans.
 
-	return GetNextTile();
+	return SubGetNextTile();
 }
 
 void AbstractExecutor::CleanUp() {
 
 	//TODO Insert any code common to all executors here.
 
-	CleanUp();
+	SubCleanUp();
 }
 
 } // namespace executor
--- src/executor/abstract_executor.h
+++ src/executor/abstract_executor.h
@@ -1,38 +1,45 @@
+/**
+ * @brief Header file for abstract executor.
+ *
+ * Copyright(c) 2015, CMU
+ */
 
 #pragma once
 
+#include "executor/logical_tile.h"
 #include "planner/abstract_plan_node.h"
-#include "storage/tile.h"
 
 namespace nstore {
 namespace executor {
 
-//===--------------------------------------------------------------------===//
-// Abstract Executor
-//===--------------------------------------------------------------------===//
-
 class AbstractExecutor {
-public:
+ public:
+ 
+ 	bool Init();
+ 
+ 	LogicalTile *GetNextTile();
+ 
+ 	void CleanUp();
+ 
+ 	virtual ~AbstractExecutor(){
+ 		delete abstract_node_;
+ 	}
+ 
+ protected:
+ 
+ 	AbstractExecutor(planner::AbstractPlanNode *abstract_node);
 
-	virtual bool Init() = 0;
+  /** @brief Init function to be overriden by subclass executors. */
+  virtual bool SubInit() = 0;
 
-	virtual storage::Tile *GetNextTile() = 0;
+  /** @brief GetNextTile function to be overriden by subclass executors. */
+  virtual LogicalTile *SubGetNextTile() = 0;
 
-	virtual void CleanUp() = 0;
+  /** @brief CleanUp function to be overriden by subclass executors. */
+  virtual void SubCleanUp() = 0;
 
-	virtual ~AbstractExecutor(){
-		delete plan_node;
-	}
-
-protected:
-
-	AbstractExecutor(planner::AbstractPlanNode *abstract_node) {
-		plan_node = abstract_node;
-	}
-
-private:
-
-	planner::AbstractPlanNode *plan_node;
+ 	planner::AbstractPlanNode *abstract_node_;
+ 
 };
 
 } // namespace executor
