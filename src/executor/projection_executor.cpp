079739ec3e121a2d0a122aa1a560212a8ced8968
Eddy Yeo <eddy.yeo@gmail.com>
2015-04-01 01:36:25 -0400

    Slight refactor of executors and improved style.

--- src/executor/projection_executor.cpp
+++ src/executor/projection_executor.cpp
@@ -9,43 +9,31 @@
  */
 
 #include "executor/projection_executor.h"
+
+#include <cassert>
+
+#include <unordered_set>
+
+#include "executor/logical_schema.h"
+#include "executor/logical_tile.h"
 #include "planner/projection_node.h"
 
 namespace nstore {
 namespace executor {
 
-//===--------------------------------------------------------------------===//
-// Projection Executor
-//===--------------------------------------------------------------------===//
-
-/** @brief Constructor for the projection executor. */
-ProjectionExecutor::ProjectionExecutor(
-    std::unique_ptr<planner::AbstractPlanNode> abstract_node,
-    std::vector<AbstractExecutor *>& children)
-  : AbstractExecutor(std::move(abstract_node), children) {
-  assert(children.size() == 1);
-}
+/**
+ * @brief Constructor for the projection executor.
+ * @param node Projection node corresponding to this executor.
+ */
+ProjectionExecutor::ProjectionExecutor(const planner::ProjectionNode *node)
+  : node_(node) {}
 
 /**
- * @brief Convert vector of output column ids provided by plan node into
- *        a set.
- *
- * TODO We might want the node to store the ids as a set instead of
- * having to convert from a vector here. Depends on how we decide to
- * deal with changing of ordering of columns (see header comment).
- * Or perhaps we can get planner to give us ids we *dont* want to output.
+ * @brief Nothing to init at the moment.
  *
  * @return True on success, false otherwise.
  */
 bool ProjectionExecutor::SubInit() {
-  planner::ProjectionNode *node =
-      dynamic_cast<planner::ProjectionNode *>(abstract_node_.get());
-  assert(node);
-
-  const std::vector<id_t>& ids = node->output_column_ids();
-  assert(ids.size() > 0);
-
-  output_column_ids_.insert(ids.begin(), ids.end());
   return true;
 }
 
@@ -60,19 +48,21 @@ LogicalTile *ProjectionExecutor::SubGetNextTile() {
   LogicalTile *tile = children_[0]->GetNextTile();
   LogicalSchema *schema = tile->schema();
 
+  const std::unordered_set<id_t> &output_column_ids =
+    node_->output_column_ids();
+
   // Loop through all columns and invalidate the ones that are not in the
   // output set.
   for (id_t id = 0; id < schema->NumCols(); id++) {
-    if (schema->IsValid(id) && output_column_ids_.count(id) == 0) {
+    if (schema->IsValid(id) && output_column_ids.count(id) == 0) {
       schema->InvalidateColumn(id);
     }
   }
   return tile;
 }
 
-/** @brief Nothing to clean up. */
-void ProjectionExecutor::SubCleanUp() {
-}
+/** @brief Nothing to clean up at the moment. */
+void ProjectionExecutor::SubCleanUp() {}
 
 } // namespace executor
 } // namespace nstore
