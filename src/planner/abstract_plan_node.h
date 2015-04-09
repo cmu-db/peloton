38ab03b965192bdfd36c8bdad6a3e9528b0ca6ef
Eddy Yeo <eddy.yeo@gmail.com>
2015-04-09 00:01:26 -0400

    Implemented constructor of materialization node.

--- src/planner/abstract_plan_node.h
+++ src/planner/abstract_plan_node.h
@@ -16,9 +16,10 @@ class AbstractPlanNode {
  public:
   virtual ~AbstractPlanNode();
 
-  void AddChild(AbstractPlanNode *child);
-
   const std::vector<AbstractPlanNode *>& children() const;
+
+ protected:
+  explicit AbstractPlanNode(std::vector<AbstractPlanNode *> &&children);
 
  private:
   /** @brief Children plan nodes in the plan tree. */
