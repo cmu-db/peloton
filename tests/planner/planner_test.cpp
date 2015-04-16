9f62a5bfc4e0a374ceca563894e1d8a7d1ca9718
jarulraj <jarulraj@cs.cmu.edu>
2015-04-16 12:56:16 -0400

    Added planner tests.

---
+++ tests/planner/planner_test.cpp
@@ -0,0 +1,32 @@
+/*-------------------------------------------------------------------------
+ *
+ * parser_test.cpp
+ * file description
+ *
+ * Copyright(c) 2015, CMU
+ *
+ * /n-store/tests/parser/parser_test.cpp
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "gtest/gtest.h"
+
+#include "harness.h"
+#include "planner/abstract_plan_node.h"
+
+namespace nstore {
+namespace test {
+
+//===--------------------------------------------------------------------===//
+// Planner Tests
+//===--------------------------------------------------------------------===//
+
+TEST(PlannerTests, BasicTest) {
+
+
+}
+
+} // End test namespace
+} // End nstore namespace
+
