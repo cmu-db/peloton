ed6b4a2e5bb45a3c4c5b8891e9a5be6fec4bf514
jarulraj <jarulraj@cs.cmu.edu>
2015-04-16 14:41:13 -0400

    Replacing parser

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -14,25 +14,11 @@
 #include <string>
 
 #include "parser/parser.h"
-#include "sql-parser/gda-sql-parser.h"
 
 namespace nstore {
 namespace parser {
 
 void Parser::ParseSQLString(std::string query) {
-
-  GdaSqlParser *parser = NULL;
-  GdaStatement *stmt = NULL;
-  const gchar *remain;
-  GError *error = NULL;
-
-  // create parser
-  parser = gda_sql_parser_new();
-
-  stmt = gda_sql_parser_parse_string(parser, query.c_str(), &remain, &error);
-
-  //Serialize to json
-  std::cout << gda_statement_serialize(stmt) << "\n";
 
 }
 
