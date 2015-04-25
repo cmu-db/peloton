73b17b9af1793472762ae46d203dd6c335f944ca
Eddy Yeo <eddy.yeo@gmail.com>
2015-04-25 17:33:35 -0400

    Fixed style and added concat_test boilerplate.

---
+++ tests/executor/concat_test.cpp
@@ -0,0 +1,16 @@
+/**
+ * @brief Test cases for concat node.
+ *
+ * Copyright(c) 2015, CMU
+ */
+
+#include "gtest/gtest.h"
+
+namespace nstore {
+namespace test {
+
+TEST(ConcatTests, TwoColsAddedTest) {
+}
+
+} // namespace test
+} // namespace nstore
