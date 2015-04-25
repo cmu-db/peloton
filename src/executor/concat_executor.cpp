d1999159e25d7347f1cc783b870c58d2d2fd1bef
Eddy Yeo <eddy.yeo@gmail.com>
2015-04-25 17:20:09 -0400

    Done implementing concat executor.

--- src/executor/concat_executor.cpp
+++ src/executor/concat_executor.cpp
@@ -5,27 +5,73 @@
  */
 
 #include "executor/concat_executor.h"
+
+#include <memory>
+
+#include "catalog/manager.h"
 #include "executor/logical_tile.h"
+#include "planner/concat_node.h"
 
 namespace nstore {
+
+namespace storage {
+class Tile;
+}
+
 namespace executor {
 
-/** @brief TODO */
+/**
+ * @brief Constructor for concat executor.
+ * @param node Concat node corresponding to this executor.
+ */
 ConcatExecutor::ConcatExecutor(const planner::AbstractPlanNode *node)
   : AbstractExecutor(node) {
-  (void) node;
 }
 
-/** @brief TODO */
+/**
+ * @brief Nothing to init at the moment.
+ *
+ * @return True on success, false otherwise.
+ */
 bool ConcatExecutor::SubInit() {
-  //TODO
-  return false;
+  assert(children_.size() == 1);
+  return true;
 }
 
-/** @brief TODO */
+/**
+ * @brief Adds a column to the logical tile, using the position lists.
+ *
+ * @return Logical tile with added columns.
+ */
 LogicalTile *ConcatExecutor::SubGetNextTile() {
-  //TODO
-  return nullptr;
+  assert(children_.size() == 1);
+
+  // Retrieve next tile.
+  std::unique_ptr<LogicalTile> source_tile(children_[0]->GetNextTile());
+  if (source_tile.get() == nullptr) {
+    return nullptr;
+  }
+
+  // Grab data from plan node.
+  const planner::ConcatNode &node = GetNode<planner::ConcatNode>();
+  const std::vector<planner::ConcatNode::ColumnPointer> &new_columns =
+    node.new_columns();
+
+  // Add new columns.
+  const bool own_base_tile = false;
+  for (unsigned int i = 0; i < new_columns.size(); i++) {
+    const planner::ConcatNode::ColumnPointer &col = new_columns[i];
+    auto &locator = catalog::Manager::GetInstance().locator;
+    auto it = locator.find(col.base_tile_oid);
+    assert(it != locator.end());
+    source_tile->AddColumn(
+        static_cast<storage::Tile *>(it->second),
+        own_base_tile,
+        col.origin_column_id,
+        col.position_list_idx);
+  }
+
+  return source_tile.release();
 }
 
 } // namespace executor
