bfb577a35fc6ed784c059a1675df2c36014e4b53
jarulraj <jarulraj@cs.cmu.edu>
2015-05-07 17:37:19 -0400

    Moved ColumnPointer to logical tile.

--- src/planner/concat_node.h
+++ src/planner/concat_node.h
@@ -11,34 +11,23 @@
 
 #include "planner/abstract_plan_node.h"
 #include "common/types.h"
+#include "executor/logical_tile.h"
 
 namespace nstore {
 namespace planner {
 
 class ConcatNode : public AbstractPlanNode {
  public:
-  /** @brief Stores the metadata for new column to be added. */
-  struct ColumnPointer {
-    /** @brief Position list that will correspond to this column. */
-    unsigned int position_list_idx;
-
-    /** @brief Id of base tile corresponding to this column. */
-    oid_t base_tile_oid;
-
-    /** @brief Original column id of this column in the base tile. */
-    id_t origin_column_id;
-  };
-
   ConcatNode(const ConcatNode &) = delete;
   ConcatNode& operator=(const ConcatNode &) = delete;
   ConcatNode(ConcatNode &&) = delete;
   ConcatNode& operator=(ConcatNode &&) = delete;
 
-  explicit ConcatNode(const std::vector<ColumnPointer> &new_columns)
+  explicit ConcatNode(const std::vector<executor::ColumnInfo> &new_columns)
     : new_columns_(new_columns) {
   }
 
-  inline const std::vector<ColumnPointer>& new_columns() const {
+  inline const std::vector<executor::ColumnInfo>& new_columns() const {
     return new_columns_;
   }
 
@@ -54,7 +43,7 @@ class ConcatNode : public AbstractPlanNode {
 
  private:
   /** @brief Vector of new columns to add to logical tile. */
-  std::vector<ColumnPointer> new_columns_;
+  std::vector<executor::ColumnInfo> new_columns_;
 };
 
 } // namespace planner
