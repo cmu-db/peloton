049a364d9d263cc008e6c45e20b3279cb4492a08
jarulraj <jarulraj@cs.cmu.edu>
2015-05-07 17:37:19 -0400

    Moved ColumnPointer to logical tile.

--- src/executor/materialization_executor.cpp
+++ src/executor/materialization_executor.cpp
@@ -42,6 +42,70 @@ bool MaterializationExecutor::DInit() {
 }
 
 /**
+ * @brief Generates map from each base tile to columns originally from that
+ *        base tile to be materialized.
+ * @param column_ids Ids of columns to be materialized.
+ * @param source_tile Logical tile that contains mapping from columns to
+ *        base tiles.
+ * @param tile_to_cols Map to be populated with mappings from tile to columns.
+ *
+ * We generate this mapping so that we can materialize columns tile by tile for
+ * efficiency reasons.
+ */
+void MaterializationExecutor::GenerateTileToColMap(
+    const std::unordered_map<id_t, id_t> &old_to_new_cols,
+    LogicalTile *source_tile,
+    std::unordered_map<storage::Tile *, std::vector<id_t> > &cols_in_physical_tile) {
+
+  for (const auto &kv : old_to_new_cols) {
+    id_t col = kv.first;
+
+    // figure out base physical tile for column in logical tile
+    storage::Tile *base_tile = source_tile->GetBaseTile(col);
+
+    std::vector<id_t> &cols_from_tile = cols_in_physical_tile[base_tile];
+    cols_from_tile.push_back(col);
+  }
+
+}
+
+/**
+ * @brief Does the actual copying of data into the new physical tile.
+ * @param source_tile Source tile to copy data from.
+ * @param tile_to_cols Map from base tile to columns in that tile
+ *        to be materialized.
+ * @param dest_tile New tile to copy data into.
+ */
+void MaterializationExecutor::MaterializeByTiles(
+    LogicalTile *source_tile,
+    const std::unordered_map<id_t, id_t> &old_to_new_cols,
+    const std::unordered_map<storage::Tile *, std::vector<id_t> > &tile_to_cols,
+    storage::Tile *dest_tile) {
+
+  // Copy over all data from each base tile.
+  for (const auto &kv : tile_to_cols) {
+
+    const std::vector<id_t> &old_column_ids = kv.second;
+
+    // Go over each column in given base physical tile
+    for (id_t old_col_id : old_column_ids) {
+      id_t new_tuple_id = 0;
+
+      auto it = old_to_new_cols.find(old_col_id);
+      assert(it != old_to_new_cols.end());
+      id_t new_col_id = it->second;
+
+      // Copy all tuples in the column to the physical tile
+      for (id_t old_tuple_id : *source_tile) {
+        Value value = source_tile->GetValue(old_tuple_id, old_col_id);
+        dest_tile->SetValue(value, new_tuple_id++, new_col_id);
+      }
+    }
+  }
+
+}
+
+/**
  * @brief Creates materialized physical tile from logical tile and wraps it
  *        in a new logical tile.
  *
@@ -50,16 +114,15 @@ bool MaterializationExecutor::DInit() {
 bool MaterializationExecutor::DExecute() {
   assert(children_.size() == 1);
 
-  // Retrieve next tile.
-  if (!children_[0]->Execute()) {
+  // Retrieve child tile.
+  const bool success = children_[0]->Execute();
+  if (!success) {
     return false;
   }
   std::unique_ptr<LogicalTile> source_tile(children_[0]->GetOutput());
 
-  const planner::MaterializationNode &node =
-      GetNode<planner::MaterializationNode>();
-  const std::unordered_map<id_t, id_t> &old_to_new_cols =
-      node.old_to_new_cols();
+  const planner::MaterializationNode &node = GetNode<planner::MaterializationNode>();
+  const std::unordered_map<id_t, id_t> &old_to_new_cols = node.old_to_new_cols();
 
   // Generate mappings.
   std::unordered_map<storage::Tile *, std::vector<id_t> > tile_to_cols;
@@ -75,7 +138,7 @@ bool MaterializationExecutor::DExecute() {
           node.schema(),
           num_tuples));
 
-  // Proceed to materialize by tile.
+  // Proceed to materialize logical tile by physical tile at a time.
   MaterializeByTiles(
       source_tile.get(),
       old_to_new_cols,
@@ -91,57 +154,6 @@ bool MaterializationExecutor::DExecute() {
   return true;
 }
 
-/**
- * @brief Generates map from each base tile to columns originally from that
- *        base tile to be materialized.
- * @param column_ids Ids of columns to be materialized.
- * @param source_tile Logical tile that contains mapping from columns to
- *        base tiles.
- * @param tile_to_cols Map to be populated with mappings from tile to columns.
- *
- * We generate this mapping so that we can materialize columns by tile for
- * efficiency reasons.
- */
-void MaterializationExecutor::GenerateTileToColMap(
-    const std::unordered_map<id_t, id_t> &old_to_new_cols,
-    LogicalTile *source_tile,
-    std::unordered_map<storage::Tile *, std::vector<id_t> > &tile_to_cols) {
-  for (const auto &kv : old_to_new_cols) {
-    id_t col = kv.first;
-    storage::Tile *base_tile = source_tile->GetBaseTile(col);
-    std::vector<id_t> &cols_from_tile = tile_to_cols[base_tile];
-    cols_from_tile.push_back(col);
-  }
-}
-
-/**
- * @brief Does the actual copying of data into the new tile.
- * @param source_tile Source tile to copy data from.
- * @param tile_to_cols Map from base tile to columns in that tile
- *        to be materialized.
- * @param dest_tile New tile to copy data into.
- */
-void MaterializationExecutor::MaterializeByTiles(
-    LogicalTile *source_tile,
-    const std::unordered_map<id_t, id_t> &old_to_new_cols,
-    const
-    std::unordered_map<storage::Tile *, std::vector<id_t> > &tile_to_cols,
-    storage::Tile *dest_tile) {
-  for (const auto &kv : tile_to_cols) {
-    // Copy over all data from each base tile.
-    const std::vector<id_t> &old_column_ids = kv.second;
-    for (id_t old_col_id : old_column_ids) {
-      int new_tuple_id = 0;
-      auto it = old_to_new_cols.find(old_col_id);
-      assert(it != old_to_new_cols.end());
-      id_t new_col_id = it->second;
-      for (id_t old_tuple_id : *source_tile) {
-        Value value = source_tile->GetValue(old_tuple_id, old_col_id);
-        dest_tile->SetValue(value, new_tuple_id++, new_col_id);
-      }
-    }
-  }
-}
 
 } // namespace executor
 } // namespace nstore
