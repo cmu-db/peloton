378ebdd4b298d8a79b5ea34c278304c79403dc7b
mindbergh <mindbergh@gmail.com>
2015-06-09 15:25:35 -0400

    outpit valgrind results as xml for Jenkins

--- tests/test-driver.sh
+++ tests/test-driver.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
+# Extract the file name of the tes executable
 name=${1##*/}
 echo $name
-valgrind --error-exitcode=1 --leak-check=full --quiet --track-origins=yes --log-file=valgrind_${name}.log -v ./${name} --gtest_output="xml:"$test".xml"
+valgrind --error-exitcode=1 --leak-check=full --quiet --track-origins=yes --xml=yes --xml-file=valgrind_${name}.xml --log-file=valgrind_${name}.log -v ./${name} --gtest_output=xml:"unit_"${name}".xml"
 #exec $1 --gtest_output="xml:"$1".xml"
