e2daae70deb1d3f44c40779aed418d1fc658130b
mindbergh <mindbergh@gmail.com>
2015-06-09 14:40:17 -0400

    make check invokes valgrind

--- tests/test-driver.sh
+++ tests/test-driver.sh
@@ -1,2 +1,5 @@
 #! /bin/sh
-exec $1 --gtest_output="xml:"$1".xml"
+name=${1##*/}
+echo $name
+valgrind --error-exitcode=1 --leak-check=full --quiet --track-origins=yes --log-file=valgrind_${name}.log -v ./${name} --gtest_output="xml:"$test".xml"
+#exec $1 --gtest_output="xml:"$1".xml"
