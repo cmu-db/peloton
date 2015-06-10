987181bc38431ef6fc2d5064a79cfa2a125532d1
jarulraj <jarulraj@cs.cmu.edu>
2015-06-10 15:38:58 -0400

    Integrating.

--- src/postgres/common/stub.c
+++ src/postgres/common/stub.c
@@ -12,25 +12,3 @@
 
 #include "postgres.h"
 
-#include "nodes/nodes.h"
-#include "mb/pg_wchar.h"
-#include "utils/fmgrtab.h"
-#include "utils/palloc.h"
-
-// src/include/bootstrap/bootstrap.h
-
-int  boot_yyparse(void) { }
-
-// src/include/replication/walsender_private.h
-
-Node *replication_parse_result;
-
-int replication_yyparse(void) { }
-
-void replication_scanner_init(const char *query_string) { }
-
-// include/utils/fmgrtab
-
-const FmgrBuiltin fmgr_builtins[];
-
-const int fmgr_nbuiltins;  /* number of entries in table */
