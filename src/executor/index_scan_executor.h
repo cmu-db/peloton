b222afbbc192c2041cd44d204a50e704a85e7698
mindbergh <mindbergh@gmail.com>
2015-06-12 11:33:19 -0400

    get rid of all warning and treat all warning as erorr

--- src/executor/index_scan_executor.h
+++ src/executor/index_scan_executor.h
@@ -29,7 +29,7 @@ class IndexScanExecutor : public AbstractExecutor {
  private:
 
   /** @brief Keeps track of current tile group id being scanned. */
-  oid_t current_tile_group_id_ = 0;
+  //oid_t current_tile_group_id_ = 0;
 
   /** @brief Result of index scan. */
   std::vector<LogicalTile *> result;
