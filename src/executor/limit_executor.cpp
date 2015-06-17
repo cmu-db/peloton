5c010d692d0d88c259cec637fed402174bd41cfd
jarulraj <jarulraj@cs.cmu.edu>
2015-06-17 18:16:30 -0400

    Minor fixes to limit executor.

--- src/executor/limit_executor.cpp
+++ src/executor/limit_executor.cpp
@@ -18,17 +18,29 @@ namespace executor {
  * @brief Constructor
  * @param node  LimitNode plan node corresponding to this executor
  */
-LimitExecutor::LimitExecutor(planner::AbstractPlanNode *node, Transaction *transaction)
+LimitExecutor::LimitExecutor(planner::AbstractPlanNode *node,
+                             Transaction *transaction)
   : AbstractExecutor(node, transaction){
 }
 
+/**
+ * @brief Do some basic checks and initialize executor state.
+ * @return true on success, false otherwise.
+ */
 bool LimitExecutor::DInit(){
   assert(children_.size() == 1);
+
+  num_skipped_ = START_OID;
+  num_returned_ = START_OID;
+
   return true;
 }
 
+/**
+ * @brief Creates logical tiles from the input logical tiles after applying limit.
+ * @return true on success, false otherwise.
+ */
 bool LimitExecutor::DExecute(){
-  assert(children_.size() == 1);
 
   // Grab data from plan node
   const planner::LimitNode &node = GetNode<planner::LimitNode>();
@@ -38,53 +50,36 @@ bool LimitExecutor::DExecute(){
   LOG_TRACE("Limit executor \n");
 
   while(num_returned_ < limit && children_[0]->Execute()){
+
     std::unique_ptr<LogicalTile> tile(children_[0]->GetOutput());
+
     for(oid_t tuple_id : *tile){
+
+      // "below" tuples
       if(num_skipped_ < offset){
-        tile->InvalidateTuple(tuple_id);  // "below" tuples
+        tile->InvalidateTuple(tuple_id);
         num_skipped_++;
       }
+      // good tuples
       else if(num_returned_ < limit){
-        num_returned_++;  // good tuples
+        num_returned_++;
       }
+      // "above" tuples
       else{
-        tile->InvalidateTuple(tuple_id);  // "above" tuples
+        tile->InvalidateTuple(tuple_id);
       }
+
     }
+
     // Avoid returning empty tiles
-    if(tile->GetTupleCount() > 0){
+    if(tile->GetTupleCount() > 0) {
       SetOutput(tile.release());
       return true;
     }
+
   }
 
   return false;
-
-//  if(!children_[0]->Execute() || num_returned_ == limit){
-//    return false;
-//  }
-//
-//  std::unique_ptr<LogicalTile> tile(children_[0]->GetOutput());
-//
-//  // TODO Naive implementation. Can be optimized.
-//  // TODO Now it's possible to return a tile with no valid tuples,
-//  // need to improve it.
-//  for(oid_t tuple_id : *tile){
-//    if(num_skipped_ < offset){
-//      tile->InvalidateTuple(tuple_id);  // "below" tuples
-//      num_skipped_++;
-//    }
-//    else if(num_returned_ < limit){
-//      num_returned_++;  // good tuples
-//    }
-//    else{
-//      tile->InvalidateTuple(tuple_id);  // "above" tuples
-//    }
-//  }
-//
-//  SetOutput(tile.release());
-//
-//  return true;
 }
 
 
