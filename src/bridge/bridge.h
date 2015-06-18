fe6ae6b4c948d10a6a76869ca3b8876c0aea7c25
JinwoongKim <jinwoongkim@cmu.edu>
2015-06-18 15:42:08 -0400

    Updating user table stats works.

--- src/bridge/bridge.h
+++ src/bridge/bridge.h
@@ -7,15 +7,16 @@
 
 #pragma once
 
-char* GetRelationName(unsigned int relation_id);
+char* GetRelationName(Oid relation_id);
 
-int GetNumberOfAttributes(unsigned int relation_id);
+int GetNumberOfAttributes(Oid  relation_id);
 
-float GetNumberOfTuples(unsigned int relation_id);
+float GetNumberOfTuples(Oid relation_id);
+
+void SetNumberOfTuples(Oid relation_id, float num_of_tuples);
 
 void GetDatabaseList(void);
 
 void GetTableList(void);
 
 void Test1(unsigned int);
-
