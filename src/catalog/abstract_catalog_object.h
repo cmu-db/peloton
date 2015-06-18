e1a6fe4eba85afcd63d08c4fdf77f84f26d836b5
Andy Pavlo <pavlo@cs.brown.edu>
2015-06-18 11:32:47 -0400

    Added AbstractCatalogObject. Working on refactoring the catalog object code to build on top of this. Currently sitting on a plane (seat 16E) headed to SFO. Good times...

---
+++ src/catalog/abstract_catalog_object.h
@@ -0,0 +1,91 @@
+/*-------------------------------------------------------------------------
+ *
+ * abstract_catalog_object.h
+ * file description
+ *
+ * Copyright(c) 2015, CMU
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include "common/types.h"
+
+#include <string>
+#include <mutex>
+
+namespace nstore {
+namespace catalog {
+
+//===--------------------------------------------------------------------===//
+// Abstract Catalog Object
+//===--------------------------------------------------------------------===//
+
+/**
+ * Base class for all catalog objecets
+ */
+class AbstractCatalogObject {
+    
+protected:
+
+    // Constructor
+    AbstractCatalogObject(oid_t id, std::string name) :
+        id_(id),
+        name_(name) {
+        
+        // Nothing else to do!
+    }
+    
+    // Deconstructor
+    ~AbstractCatalogObject() {
+        // I don't think there is anything that we need to do here
+    }
+
+public:
+    
+    //===--------------------------------------------------------------------===//
+    // ACCESSORS
+    //===--------------------------------------------------------------------===//
+    
+    inline oid_t GetId() const {
+        return id_;
+    }
+
+    inline std::string GetName() const {
+        return name_;
+    }
+    
+    inline void Lock() {
+        mutex_.lock();
+    }
+
+    inline void Unlock() {
+        mutex_.unlock();
+    }
+    
+private:
+
+    //===--------------------------------------------------------------------===//
+    // MEMBERS
+    //===--------------------------------------------------------------------===//
+    
+    // The unique identifier for this catalog object
+    oid_t id_;
+    
+    // The name of this catalog object
+    // All catalog objects need to have a name.
+    std::string name_;
+    
+    // Lock for this single
+    // TODO: Determine whether we really need this per catalog object. We probably don't
+    // and we're wasting memory by allocating it.
+    std::mutex mutex_;
+
+
+};
+
+} // End catalog namespace
+} // End nstore namespace
+
+
