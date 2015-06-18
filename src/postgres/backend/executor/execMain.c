2e568bf88c8ffd388b006463f3c7a45f4c2a14a4
Ming Fang <mindbergh@gmail.com>
2015-06-18 15:55:47 -0400

    prototype plan print routines

--- src/postgres/backend/executor/execMain.c
+++ src/postgres/backend/executor/execMain.c
@@ -60,6 +60,8 @@
 #include "utils/snapmgr.h"
 #include "utils/tqual.h"
 
+#include "nodes/pprint.h"
+
 
 /* Hooks for plugins to get control in ExecutorStart/Run/Finish/End */
 ExecutorStart_hook_type ExecutorStart_hook = NULL;
@@ -282,6 +284,12 @@ void
 ExecutorRun(QueryDesc *queryDesc,
 			ScanDirection direction, long count)
 {
+	//PlannedStmt *plan = queryDesc->plannedstmt;
+	//elog_node_display(LOG, "plan", plan, Debug_pretty_print);
+
+  print_queryDesc(queryDesc);
+
+
 	if (ExecutorRun_hook)
 		(*ExecutorRun_hook) (queryDesc, direction, count);
 	else
