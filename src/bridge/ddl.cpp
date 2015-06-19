3d56d3e458dfcbb2b3ab42d9b4cafe90aaa33c4b
JinwoongKim <jinwoongkim@cmu.edu>
2015-06-19 16:48:21 -0400

    Minor fix

---
+++ src/bridge/ddl.cpp
@@ -0,0 +1,20 @@
+/*-------------------------------------------------------------------------
+ *
+ * dll.cpp
+ * file description
+ *
+ * Copyright(c) 2015, CMU
+ *
+ * /n-store/src/bridge/ddl.cpp
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "bridge/ddl.h"
+
+namespace nstore {
+namespace executor {
+
+} // namespace executor
+} // namespace nstore
+
