2a9feb6ee5eae46c0a6e2d1b3d2a52bbba151f77
jarulraj <jarulraj@cs.cmu.edu>
2015-06-22 19:12:18 -0400

    Wrapping up basic template for aggregation.

--- src/backend/executor/aggregate_executor.cpp
+++ src/backend/executor/aggregate_executor.cpp
@@ -15,6 +15,7 @@
 #include "backend/executor/aggregate_executor.h"
 #include "backend/planner/aggregate_node.h"
 #include "backend/storage/table_factory.h"
+#include "backend/expression/container_tuple.h"
 
 #include <utility>
 #include <vector>
@@ -39,13 +40,12 @@ template<PlanNodeType aggregate_type>
 bool AggregateExecutor<aggregate_type>::DInit() {
   assert(children_.size() == 1);
 
-  LOG_TRACE("Aggregate Scan executor :: 1 child \n");
+  LOG_INFO("Aggregate Scan executor :: 1 child \n");
 
   // Grab info from plan node and check it
   const planner::AggregateNode &node = GetNode<planner::AggregateNode>();
 
   // Plan has info about :
-  //
   // (1) the aggregate columns, and
   // (2) the pass through columns
 
@@ -56,8 +56,7 @@ bool AggregateExecutor<aggregate_type>::DInit() {
 
   output_table = storage::TableFactory::GetDataTable(INVALID_OID,
                                                      output_table_schema,
-                                                     "temp_table",
-                                                     DEFAULT_TUPLES_PER_TILEGROUP);
+                                                     "temp_table");
 
   return true;
 }
@@ -69,7 +68,6 @@ bool AggregateExecutor<aggregate_type>::DInit() {
 template<PlanNodeType aggregate_type>
 bool AggregateExecutor<aggregate_type>::DExecute() {
 
-  /*
   // Grab info from plan node
   const planner::AggregateNode &node = GetNode<planner::AggregateNode>();
 
@@ -78,40 +76,30 @@ bool AggregateExecutor<aggregate_type>::DExecute() {
 
   // Get input tile and aggregate it
   std::unique_ptr<LogicalTile> tile(children_[0]->GetOutput());
-  */
-
-  /*
-  for (oid_t tuple_id : *tile) {
-    expression::ContainerTuple<LogicalTile> tuple(tile.get(), tuple_id);
-
-  }
-
-  std::vector<int> groupByColumns = node->getGroupByColumns();
-  storage::Tuple *prev(input_table->schema());
 
   Aggregator<aggregate_type> aggregator(group_by_key_schema,
-                                       node, &m_passThroughColumns,
-                                       input_table, output_table,
-                                       &agg_types,
-                                       &groupByColumns, &col_types);
+                                        node,
+                                        output_table);
 
-  LOG_TRACE("looping..");
-  for (TableTuple cur(input_table->schema()); it.next(cur);
-      prev.move(cur.address()))
-  {
-    if (!aggregator.nextTuple( cur, prev))
-    {
+  LOG_INFO("Looping..");
+
+  expression::ContainerTuple<LogicalTile> prev_tuple;
+
+  for (oid_t tuple_id : *tile) {
+    expression::ContainerTuple<LogicalTile> cur_tuple(tile.get(), tuple_id);
+
+    if (aggregator.NextTuple(cur_tuple, prev_tuple) == false) {
       return false;
     }
+    prev_tuple = cur_tuple;
   }
-  */
 
-  LOG_TRACE("finalizing..");
+  LOG_INFO("Finalizing..");
 
-  //if (!aggregator.Finalize(prev))
-  //  return false;
+  if (!aggregator.Finalize(prev_tuple))
+    return false;
 
-  LOG_TRACE("finished");
+  LOG_INFO("Finished");
 
   return true;
 }
