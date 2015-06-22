b39689b1e7cee6c216dda2bf4fc0bd30f4a97ccb
jarulraj <jarulraj@cs.cmu.edu>
2015-06-22 17:41:39 -0400

    Adding more stuff to support aggregate executor.

--- src/backend/planner/aggregate_node.h
+++ src/backend/planner/aggregate_node.h
@@ -15,6 +15,8 @@
 #include "backend/planner/abstract_plan_node.h"
 #include "backend/common/types.h"
 
+#include <map>
+
 namespace nstore {
 namespace planner {
 
@@ -30,36 +32,49 @@ class AggregateNode : public AbstractPlanNode {
     return PLAN_NODE_TYPE_AGGREGATE;
   }
 
+  const std::vector<oid_t>& GetAggregateColumnsOffsets() const {
+    return aggregate_columns_offsets;
+  }
+
+  const std::vector<oid_t>& GetGroupByColumnsOffsets() const {
+    return group_by_columns_offsets;
+  }
+
+  const std::map<oid_t, oid_t>& GetPassThroughColumnsMapping() const {
+    return pass_through_columns_mapping;
+  }
+
+  const std::vector<ExpressionType>& GetAggregateTypes() const {
+    return aggregate_types;
+  }
+
+  const catalog::Schema *GetOutputTableSchema() const {
+    return output_table_schema;
+  }
+
+  const catalog::Schema *GetGroupBySchema() const {
+    return group_by_key_schema;
+  }
+
  private:
 
-  /** @brief Columns in the output logical tile
-   *  These are not present in input logical tile
-   **/
-  std::vector<catalog::ColumnInfo> output_columns;
-
-  /**
-   *  Sample queries :
-   *
-   *  Consider this schema:
-   *  Products :: < ProductID | CategoryID | Units >
-   *
-   *  SELECT CategoryID, SUM(Units)
-   *  FROM Products
-   *  GROUP BY CategoryID;
-   *
-   *  SELECT DISTINCT ProductID, CategoryID
-   *  From Products
-   */
-
   /** @brief Aggregate columns */
-  std::vector<ExpressionType> aggregate_types;
-  std::vector<oid_t> aggregate_columns;
+  std::vector<oid_t> aggregate_columns_offsets;
 
   /** @brief Group by columns */
-  std::vector<oid_t> group_by_columns;
+  std::vector<oid_t> group_by_columns_offsets;
+
+  /** @brief Aggregate column schema */
+  catalog::Schema *group_by_key_schema;
 
   /** @brief Pass through columns */
-  std::vector<oid_t> pass_through_columns;
+  std::map<oid_t, oid_t> pass_through_columns_mapping;
+
+  /** @brief Aggregate types */
+  std::vector<ExpressionType> aggregate_types;
+
+  /** @brief Output columns */
+  catalog::Schema *output_table_schema;
 
 };
 
