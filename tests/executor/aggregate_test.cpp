8403986b30472cd9cfc093020520880a01b5cc9f
jarulraj <jarulraj@cs.cmu.edu>
2015-06-22 19:18:02 -0400

    Added stub for aggregate tests.

---
+++ tests/executor/aggregate_test.cpp
@@ -0,0 +1,39 @@
+/**
+ * @brief Test cases for aggregate node.
+ *
+ * Copyright(c) 2015, CMU
+ */
+
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "backend/common/types.h"
+#include "backend/common/value.h"
+#include "backend/executor/logical_tile.h"
+#include "backend/executor/aggregate_executor.h"
+#include "backend/executor/logical_tile_factory.h"
+#include "backend/planner/aggregate_node.h"
+#include "backend/storage/data_table.h"
+
+#include "executor/executor_tests_util.h"
+#include "executor/mock_executor.h"
+#include "harness.h"
+
+using ::testing::NotNull;
+using ::testing::Return;
+
+namespace nstore {
+namespace test {
+
+TEST(AggregateTests, DistinctTest){
+
+}
+
+
+} // namespace test
+} // namespace nstore
