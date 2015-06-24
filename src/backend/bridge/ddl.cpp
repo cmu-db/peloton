8b8be2c272b32b778bbc257a4ae6c461c6861428
JinwoongKim <jinwoongkim@cmu.edu>
2015-06-24 14:05:21 -0400

    Minor fix - Just enable CreateTable function to create the table without column information

--- src/backend/bridge/ddl.cpp
+++ src/backend/bridge/ddl.cpp
@@ -31,79 +31,93 @@ bool DDL::CreateTable(std::string table_name,
   // Construct schema with ddl_columnInfo
   if( schema == NULL ){
     std::vector<catalog::ColumnInfo> columnInfoVect;
+    ValueType currentValueType;
 
-    for( int column_itr = 0; column_itr < num_columns; column_itr++ ){
-
-      ValueType currentValueType;
-
-      switch(ddl_columnInfo[column_itr].type ){
-        // Could not find yet corresponding types in Postgres...
-        // Also - check below types again to make sure..
-        // TODO :: change the numbers to enum type
-
-        /* BOOLEAN */
-        case 16: // boolean, 'true'/'false'
-          currentValueType = VALUE_TYPE_BOOLEAN;
-          break;
-
-          /* INTEGER */
-        case 21: // -32 thousand to 32 thousand, 2-byte storage
-          currentValueType = VALUE_TYPE_SMALLINT;
-          ddl_columnInfo[column_itr].is_inlined = true;
-          break;
-        case 23: // -2 billion to 2 billion integer, 4-byte storage
-          currentValueType = VALUE_TYPE_INTEGER;
-          ddl_columnInfo[column_itr].is_inlined = true;
-          break;
-        case 20: // ~18 digit integer, 8-byte storage
-          currentValueType = VALUE_TYPE_BIGINT;
-          ddl_columnInfo[column_itr].is_inlined = true;
-          break;
-
-          /* DOUBLE */
-        case 701: // double-precision floating point number, 8-byte storage
-          currentValueType = VALUE_TYPE_DOUBLE;
-          ddl_columnInfo[column_itr].is_inlined = true;
-          break;
-
-          /* CHAR */
-        case 1042: // char(length), blank-padded string, fixed storage length
-          currentValueType = VALUE_TYPE_VARCHAR;
-          ddl_columnInfo[column_itr].is_inlined = true;
-          break;
-          // !!! NEED TO BE UPDATED ...
-        case 1043: // varchar(length), non-blank-padded string, variable storage length;
-          currentValueType = VALUE_TYPE_VARCHAR;
-          ddl_columnInfo[column_itr].is_inlined = true;
-          break;
-
-          /* TIMESTAMPS */
-        case 1114: // date and time
-        case 1184: // date and time with time zone
-          currentValueType = VALUE_TYPE_TIMESTAMP;
-          ddl_columnInfo[column_itr].is_inlined = true;
-          break;
-
-          /* DECIMAL */
-        case 1700: // numeric(precision, decimal), arbitrary precision number
-          currentValueType = VALUE_TYPE_DECIMAL;
-          break;
-
-          /* INVALID VALUE TYPE */
-        default:
-          currentValueType = VALUE_TYPE_INVALID;
-          printf("INVALID VALUE TYPE : %d \n", ddl_columnInfo[column_itr].type);
-          break;
+    if( num_columns == 0 )
+    {
+          currentValueType = VALUE_TYPE_NULL;
+          catalog::ColumnInfo *columnInfo = new catalog::ColumnInfo( currentValueType,
+                          0, 
+                          0,
+                          "",
+                          true,
+                          true);
+          // Add current columnInfo into the columnInfoVect
+          columnInfoVect.push_back(*columnInfo);
+    }else
+    {
+      for( int column_itr = 0; column_itr < num_columns; column_itr++ ){
+  
+  
+        switch(ddl_columnInfo[column_itr].type ){
+          // Could not find yet corresponding types in Postgres...
+          // Also - check below types again to make sure..
+          // TODO :: change the numbers to enum type
+  
+          /* BOOLEAN */
+          case 16: // boolean, 'true'/'false'
+            currentValueType = VALUE_TYPE_BOOLEAN;
+            break;
+  
+            /* INTEGER */
+          case 21: // -32 thousand to 32 thousand, 2-byte storage
+            currentValueType = VALUE_TYPE_SMALLINT;
+            ddl_columnInfo[column_itr].is_inlined = true;
+            break;
+          case 23: // -2 billion to 2 billion integer, 4-byte storage
+            currentValueType = VALUE_TYPE_INTEGER;
+            ddl_columnInfo[column_itr].is_inlined = true;
+            break;
+          case 20: // ~18 digit integer, 8-byte storage
+            currentValueType = VALUE_TYPE_BIGINT;
+            ddl_columnInfo[column_itr].is_inlined = true;
+            break;
+  
+            /* DOUBLE */
+          case 701: // double-precision floating point number, 8-byte storage
+            currentValueType = VALUE_TYPE_DOUBLE;
+            ddl_columnInfo[column_itr].is_inlined = true;
+            break;
+  
+            /* CHAR */
+          case 1042: // char(length), blank-padded string, fixed storage length
+            currentValueType = VALUE_TYPE_VARCHAR;
+            ddl_columnInfo[column_itr].is_inlined = true;
+            break;
+            // !!! NEED TO BE UPDATED ...
+          case 1043: // varchar(length), non-blank-padded string, variable storage length;
+            currentValueType = VALUE_TYPE_VARCHAR;
+            ddl_columnInfo[column_itr].is_inlined = true;
+            break;
+  
+            /* TIMESTAMPS */
+          case 1114: // date and time
+          case 1184: // date and time with time zone
+            currentValueType = VALUE_TYPE_TIMESTAMP;
+            ddl_columnInfo[column_itr].is_inlined = true;
+            break;
+  
+            /* DECIMAL */
+          case 1700: // numeric(precision, decimal), arbitrary precision number
+            currentValueType = VALUE_TYPE_DECIMAL;
+            break;
+  
+            /* INVALID VALUE TYPE */
+          default:
+            currentValueType = VALUE_TYPE_INVALID;
+            printf("INVALID VALUE TYPE : %d \n", ddl_columnInfo[column_itr].type);
+            break;
+        }
+  
+        catalog::ColumnInfo *columnInfo = new catalog::ColumnInfo( currentValueType,
+                                                                   ddl_columnInfo[column_itr].column_offset,
+                                                                   ddl_columnInfo[column_itr].column_length,
+                                                                   ddl_columnInfo[column_itr].name,
+                                                                   ddl_columnInfo[column_itr].allow_null,
+                                                                   ddl_columnInfo[column_itr].is_inlined );
+        // Add current columnInfo into the columnInfoVect
+        columnInfoVect.push_back(*columnInfo);
       }
-
-      catalog::ColumnInfo *columnInfo = new catalog::ColumnInfo( currentValueType,
-                                                                 ddl_columnInfo[column_itr].column_offset,
-                                                                 ddl_columnInfo[column_itr].column_length,
-                                                                 ddl_columnInfo[column_itr].name,
-                                                                 ddl_columnInfo[column_itr].allow_null,
-                                                                 ddl_columnInfo[column_itr].is_inlined );
-      // Add current columnInfo into the columnInfoVect
-      columnInfoVect.push_back(*columnInfo);
     }
 
     // TODO :: ColumnInfo may have nothing
