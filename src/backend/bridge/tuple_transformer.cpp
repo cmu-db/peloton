bca48d2c50b944680f4c7277451ca8020efbfc71
Ming Fang <mindbergh@gmail.com>
2015-06-25 20:57:52 -0400

    add tuple transformer; working on plan transformer
    Former-commit-id: 632470f5524850856cfac9bf79e4f394b8963dbf

--- src/backend/bridge/tuple_transformer.cpp
+++ src/backend/bridge/tuple_transformer.cpp
@@ -6,10 +6,18 @@
  */
 
 #include <iostream>
+//#include <cstring>
 
 #include "backend/bridge/tuple_transformer.h"
 #include "backend/common/value_peeker.h"
+#include "backend/storage/tuple.h"
+#include "backend/common/types.h"
+#include "backend/bridge/ddl.h"
 
+extern "C" {
+#include "fmgr.h"
+#include "utils/lsyscache.h"
+}
 void TestTupleTransformer(Datum datum, Oid atttypid) {
 
   nstore::Value p_value;
@@ -28,10 +36,7 @@ void TestTupleTransformer(Datum datum, Oid atttypid) {
   printf("\n");
 }
 
-
-
-nstore::Value
-DatumGetValue(Datum datum, Oid atttypid) {
+nstore::Value DatumGetValue(Datum datum, Oid atttypid) {
   nstore::Value value;
   nstore::Pool *data_pool = nullptr;
   int16_t smallint;
@@ -86,8 +91,7 @@ DatumGetValue(Datum datum, Oid atttypid) {
   return value;
 }
 
-Datum
-ValueGetDatum(nstore::Value value) {
+Datum ValueGetDatum(nstore::Value value) {
   nstore::ValueType value_type;
   nstore::ValuePeeker value_peeker;
   Datum datum;
@@ -103,33 +107,33 @@ ValueGetDatum(nstore::Value value) {
 
   switch (value_type) {
     case 4:
-		smallint = value_peeker.PeekSmallInt(value);
-		printf("%d\n", smallint);
-		datum = Int16GetDatum(smallint);
-		break;
+      smallint = value_peeker.PeekSmallInt(value);
+      printf("%d\n", smallint);
+      datum = Int16GetDatum(smallint);
+      break;
 
     case 5:
-		integer = value_peeker.PeekInteger(value);
-		printf("%d\n", integer);
-		datum = Int32GetDatum(integer);
-		break;
+      integer = value_peeker.PeekInteger(value);
+      printf("%d\n", integer);
+      datum = Int32GetDatum(integer);
+      break;
 
     case 6:
-		bigint = value_peeker.PeekBigInt(value);
-		printf("%ld\n", bigint);
-		datum = Int64GetDatum(bigint);
-		break;
+      bigint = value_peeker.PeekBigInt(value);
+      printf("%ld\n", bigint);
+      datum = Int64GetDatum(bigint);
+      break;
 
-	// TODO: Peloton changes
-	// varchar -- no function found yet -- vivek
+      // TODO: Peloton changes
+      // varchar -- no function found yet -- vivek
     case 9:
-    	break;
+      break;
 
     case 11:
-		timestamp = value_peeker.PeekTimestamp(value);
-		printf("%f\n", (float) timestamp);
-		datum = Float8GetDatum((float) timestamp);
-		break;
+      timestamp = value_peeker.PeekTimestamp(value);
+      printf("%f\n", (float) timestamp);
+      datum = Float8GetDatum((float) timestamp);
+      break;
 
     default:
       break;
@@ -138,3 +142,97 @@ ValueGetDatum(nstore::Value value) {
   }
 }
 
+namespace nstore {
+namespace bridge {
+storage::Tuple * TupleTransformer(TupleTableSlot * slot,
+                                  catalog::Schema *schema) {
+  TupleDesc typeinfo = slot->tts_tupleDescriptor;
+  int natts = typeinfo->natts;
+  int i;
+  Datum attr;
+  char *value;
+  bool isnull;
+  Oid typoutput;
+  bool typisvarlena;
+  unsigned attributeId;
+  Form_pg_attribute attributeP;
+  char *name;
+
+  Datum p_datum;
+  Oid p_oid;
+  std::vector<Value> vals;
+  std::vector<catalog::ColumnInfo> colums;
+  std::vector<oid_t> oid_t_vec;
+  oid_t num_columns = schema->GetColumnCount();
+
+  for (i = 0; i < natts; ++i) {
+    attr = slot_getattr(slot, i + 1, &isnull);
+    if (isnull)
+      continue;
+    Assert(typeinfo != NULL);
+    Assert(typeinfo->attrs[i] != NULL);
+    getTypeOutputInfo(typeinfo->attrs[i]->atttypid, &typoutput, &typisvarlena);
+
+    value = OidOutputFunctionCall(typoutput, attr);
+
+    // Print the attribute.
+    attributeId = (unsigned) i + 1;
+    attributeP = typeinfo->attrs[i];
+
+    name = NameStr(attributeP->attname);
+    for (oid_t column_itr = 0; column_itr < num_columns; column_itr++) {
+      // GetColumns and iterate it and compare with .. this.. one ..
+      if (!strcmp(name, (schema->GetColumnInfo(column_itr).name).c_str()))
+        oid_t_vec.push_back(column_itr);
+    }
+
+    p_oid = attributeP->atttypid;
+
+    switch (p_oid) {
+      // short int
+      case 21:
+        p_datum = Int16GetDatum((short )(atoi(value)));
+        break;
+        // integer
+      case 23:
+        p_datum = Int32GetDatum(atoi(value));
+        break;
+        // big int
+      case 20:
+        p_datum = Int64GetDatum(strtol(value,NULL,10));
+        break;
+        // real
+      case 700:
+        p_datum = Float4GetDatum(strtof(value, NULL));
+        break;
+        // double
+      case 701:
+        p_datum = Float8GetDatum(strtod(value, NULL));
+        break;
+        // char
+      case 1042:
+        p_datum = CStringGetDatum(value);
+        break;
+        // varchar
+      case 1043:
+        p_datum = CStringGetDatum(value);
+        break;
+      default:
+        p_datum = CStringGetDatum(value);
+        break;
+    }
+
+    vals.push_back(DatumGetValue(p_datum, p_oid));
+  }
+  assert(vals.size() == oid_t_vec.size()); /* the num of value should be the same as schema found */
+  catalog::Schema * tuple_schema = catalog::Schema::CopySchema(schema,
+                                                               oid_t_vec);
+	storage::Tuple *tuple(new storage::Tuple(tuple_schema, true));
+	i = 0;
+	for (auto val : vals) {
+	  tuple->SetValue(i++, val);
+	}
+	return tuple;
+}
+}
+}
