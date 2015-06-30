b09e262ff0741988ec582993806a8d569c4fce40
JinwoongKim <jinwoongkim@cmu.edu>
2015-06-30 17:31:48 -0400

    Commit for merging with Master
    Also - Make the interface of CreateIndex simply
           Add SetTable and SetIndex function in Constraint
    
    
    Former-commit-id: b262c8fb6702d4f43e40603375f06edee5230cce

--- src/backend/catalog/constraint.cpp
+++ src/backend/catalog/constraint.cpp
@@ -9,42 +9,17 @@
  */
 
 #include "backend/catalog/constraint.h"
-#include "backend/catalog/column.h"
 
 namespace peloton {
 namespace catalog {
 
 std::ostream& operator<<(std::ostream& os, const Constraint& constraint) {
 
-/*
-TODO :: Do it later...
     os << "\tCONSTRAINT ";
 
     os << constraint.GetName() << " " << ConstraintTypeToString(constraint.type) << "\n";
 
-    if(constraint.type == CONSTRAINT_TYPE_PRIMARY) {
-        os << "\t\tPrimary Key Columns : ";
-        for(auto col : constraint.columns) {
-            os << col->GetName() << " ";
-        }
-        os << "\n";
-    }
-    else if(constraint.type == CONSTRAINT_TYPE_FOREIGN) {
-        os << "\t\tLocal Columns : ";
-        for(auto col : constraint.columns) {
-            os << col->GetName() << " ";
-        }
-        os << "\n";
-
-        os << "\t\tForeign Columns : ";
-        for(auto col : constraint.foreign_columns) {
-            os << col->GetName() << " ";
-        }
-        os << "\n";
-    }
-
     os << "\n";
-*/
 
     return os;
 }
