78868438c9349fcbffc3bb53d699b7663b1e1861
Ming Fang <mindbergh@gmail.com>
2015-06-30 11:31:15 -0400

    Checkpoint.
    Former-commit-id: 7154d5b7f86131edfdb97e38734b1899899ae08f

--- src/postgres/backend/bootstrap/bootscanner.cpp
+++ src/postgres/backend/bootstrap/bootscanner.cpp
@@ -1,6 +1,6 @@
-#line 2 "bootscanner.c"
+#line 2 "bootscanner.cpp"
 
-#line 4 "bootscanner.c"
+#line 4 "bootscanner.cpp"
 
 #define  YY_INT_ALIGNED short int
 
@@ -792,7 +792,7 @@ fprintf_to_ereport(const char *fmt, const char *msg)
 static int	yyline = 1;			/* line number for error reporting */
 
 #define YY_NO_INPUT 1
-#line 796 "bootscanner.c"
+#line 796 "bootscanner.cpp"
 
 #define INITIAL 0
 
@@ -983,7 +983,7 @@ YY_DECL
 #line 74 "bootscanner.l"
 
 
-#line 987 "bootscanner.c"
+#line 987 "bootscanner.cpp"
 
 	if ( !(yy_init) )
 		{
@@ -1260,7 +1260,7 @@ YY_RULE_SETUP
 #line 143 "bootscanner.l"
 YY_FATAL_ERROR( "flex scanner jammed" );
 	YY_BREAK
-#line 1264 "bootscanner.c"
+#line 1264 "bootscanner.cpp"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
