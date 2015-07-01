d567525a684ba58dd34a1b58bef706f3cbd79159
jarulraj <jarulraj@cs.cmu.edu>
2015-07-01 16:59:40 -0400

    Enabled BootstrapPeloton. Disabled automatic restart to support debugging.
    Former-commit-id: 3a12efcde4389a251620c22ea06cb0142c773b6f

--- src/postgres/backend/executor/execMain.cpp
+++ src/postgres/backend/executor/execMain.cpp
@@ -1559,7 +1559,7 @@ ExecutePlan(EState *estate,
 		slot = ExecProcNode(planstate);
 
 		// TODO: Peloton Changes
-		peloton_send_node(planstate);
+		peloton_send_planstate(planstate);
 
 		/*
 		 * if the tuple is null, then we assume there is nothing more to
