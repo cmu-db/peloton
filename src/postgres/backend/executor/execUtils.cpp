d8ce4d8fa00077f3ce188233a43c956242b7b516
jarulraj <jarulraj@cs.cmu.edu>
2015-07-02 16:24:50 -0400

    Finished fixing dml messages. Now, we can access plans.

--- src/postgres/backend/executor/execUtils.cpp
+++ src/postgres/backend/executor/execUtils.cpp
@@ -78,11 +78,14 @@ CreateExecutorState(void)
 	/*
 	 * Create the per-query context for this Executor run.
 	 */
-	qcontext = AllocSetContextCreate(CurrentMemoryContext,
+	// TODO: Peloton Changes (Use TopSharedMemoryContext instead)
+	//qcontext = AllocSetContextCreate(CurrentMemoryContext,
+	qcontext = SHMAllocSetContextCreate(TopSharedMemoryContext,
 									 "ExecutorState",
 									 ALLOCSET_DEFAULT_MINSIZE,
 									 ALLOCSET_DEFAULT_INITSIZE,
-									 ALLOCSET_DEFAULT_MAXSIZE);
+									 ALLOCSET_DEFAULT_MAXSIZE,
+									 SHM_DEFAULT_SEGMENT);
 
 	/*
 	 * Make the EState node within the per-query context.  This way, we don't
