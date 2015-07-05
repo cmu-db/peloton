b240af5f85747726b01348ff9e75165a7eabf7a5
jarulraj <jarulraj@cs.cmu.edu>
2015-07-05 03:03:05 -0400

    Minor fix.
    Former-commit-id: c7506f5a22104dc96bb843cdd5dec535fd4d4fd5

--- src/postgres/backend/executor/execUtils.cpp
+++ src/postgres/backend/executor/execUtils.cpp
@@ -74,28 +74,17 @@ CreateExecutorState(void)
 	EState	   *estate;
 	MemoryContext qcontext;
 	MemoryContext oldcontext;
-	bool contains_context;
 
 	/*
 	 * Create the per-query context for this Executor run.
 	 */
 	// TODO: Peloton Changes
-	contains_context = SHMContextContains(TopSharedMemoryContext, CurrentMemoryContext);
-	if(contains_context == false) {
-	  qcontext = AllocSetContextCreate(CurrentMemoryContext,
-	                                   "ExecutorState",
-	                                   ALLOCSET_DEFAULT_MINSIZE,
-	                                   ALLOCSET_DEFAULT_INITSIZE,
-	                                   ALLOCSET_DEFAULT_MAXSIZE);
-	}
-	else {
-    qcontext = SHMAllocSetContextCreate(CurrentMemoryContext,
-                                     "ExecutorState",
-                                     ALLOCSET_DEFAULT_MINSIZE,
-                                     ALLOCSET_DEFAULT_INITSIZE,
-                                     ALLOCSET_DEFAULT_MAXSIZE,
-                                     SHM_DEFAULT_SEGMENT);
-	}
+  qcontext = SHMAllocSetContextCreate(CurrentMemoryContext,
+                                   "ExecutorState",
+                                   ALLOCSET_DEFAULT_MINSIZE,
+                                   ALLOCSET_DEFAULT_INITSIZE,
+                                   ALLOCSET_DEFAULT_MAXSIZE,
+                                   SHM_DEFAULT_SEGMENT);
 
 	/*
 	 * Make the EState node within the per-query context.  This way, we don't
