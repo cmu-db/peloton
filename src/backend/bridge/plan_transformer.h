a993595124d3d7151dbcd1c5089619d7d370bf92
Ming Fang <mindbergh@gmail.com>
2015-07-13 12:03:45 -0400

    working on index only scan transformer

--- src/backend/bridge/plan_transformer.h
+++ src/backend/bridge/plan_transformer.h
@@ -42,6 +42,7 @@ class PlanTransformer {
   static planner::AbstractPlanNode *TransformDelete(const ModifyTableState *plan_state);
 
   static planner::AbstractPlanNode *TransformSeqScan(const SeqScanState *plan_state);
+  static planner::AbstractPlanNode *TransformIndexOnlyScan(const IndexOnlyScanState *plan_state);
 
   static planner::AbstractPlanNode *TransformLimit(const LimitState *plan_state);
   static planner::AbstractPlanNode *TransformResult(const ResultState *plan_state);
