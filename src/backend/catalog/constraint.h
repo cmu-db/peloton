e031ce6fa7c7146b992718c774f7b151ec228521
jarulraj <jarulraj@cs.cmu.edu>
2015-07-14 12:22:12 -0400

    Pass over constraint.

--- src/backend/catalog/constraint.h
+++ src/backend/catalog/constraint.h
@@ -12,14 +12,14 @@
 
 #pragma once
 
-#include "backend/common/types.h"
-#include "backend/common/logger.h"
-
-#include "nodes/nodes.h" // TODO :: REMOVE, just for raw expr
-
 #include <string>
 #include <vector>
 #include <iostream>
+
+#include "backend/common/types.h"
+#include "backend/common/logger.h"
+
+#include "nodes/nodes.h"
 
 namespace peloton {
 
@@ -41,112 +41,116 @@ class ReferenceTableInfo;
 class Constraint
 {
 
-public:
-    // Configure ( type [, name] )
-    Constraint( ConstraintType type, std::string name = "", Node* raw_expr = nullptr )
-                : type(type), name(name) { raw_expr = (Node*) copyObject((void*) raw_expr ); }
+ public:
+  // Configure ( type [, name] )
+  Constraint(ConstraintType type, std::string constraint_name = "", Node* raw_expr = nullptr)
+ : constraint_type(type),
+   constraint_name(constraint_name) {
+    raw_expr = (Node*) copyObject((void*) raw_expr );
+  }
 
-    //===--------------------------------------------------------------------===//
-    // ACCESSORS
-    //===--------------------------------------------------------------------===//
+  //===--------------------------------------------------------------------===//
+  // ACCESSORS
+  //===--------------------------------------------------------------------===//
 
-    ConstraintType GetType() const {
-        return type;
-    }
+  ConstraintType GetType() const {
+    return constraint_type;
+  }
 
-    std::string GetName() const {
-        return name;
-    }
+  std::string GetName() const {
+    return constraint_name;
+  }
 
-    inline void SetReferenceTablePosition( int position ) {
-      if( position < 0 ){
-        LOG_ERROR(" ReferenceTable position can be negative !!");
-      }
-      reference_table_position = position;
-    }
+  // Offset into the list of "reference tables" in the Table.
+  inline void SetReferenceTablePosition(int position) {
+    AssertMsg(position < 0, "ReferenceTable position can't be negative");
+    reference_table_list_offset = position;
+  }
 
-    inline void SetUniqueIndexPosition( int position ) {
-      if( position < 0 ){
-        LOG_ERROR(" Unique Index position can be negative !!");
-      }
-      unique_index_position = position;
-    }
+  // Offset into the list of "unique indices" in the Table.
+  inline void SetUniqueIndexPosition( int position ) {
+    AssertMsg(position < 0, "Unique Index position can't be negative.");
+    unique_index_list_offset = position;
+  }
 
-    // Get a string representation of this constraint
-    // TODO Print out 
-    friend std::ostream& operator<<(std::ostream& os, const Constraint& constraint);
+  // Get a string representation of this constraint
+  friend std::ostream& operator<<(std::ostream& os, const Constraint& constraint);
 
-private:
-    
-    //===--------------------------------------------------------------------===//
-    // MEMBERS
-    //===--------------------------------------------------------------------===//
-    
-    // The type of constraint
-    ConstraintType type = CONSTRAINT_TYPE_INVALID;
+ private:
 
-    // Constraint name ( if needed )
-    std::string name = "";
+  //===--------------------------------------------------------------------===//
+  // MEMBERS
+  //===--------------------------------------------------------------------===//
 
-    // FIXME :: Cooked expr
-    // Raw_default_expr ( if needed )
-    Node* raw_expr = nullptr;
+  // The type of constraint
+  ConstraintType constraint_type = CONSTRAINT_TYPE_INVALID;
 
-    // Unique index and reference table position in Table
-    int reference_table_position = -1;
-    int unique_index_position = -1;
+  // Constraint name
+  std::string constraint_name;
+
+  // Raw_default_expr
+  // FIXME :: Cooked expr
+  Node* raw_expr = nullptr;
+
+  // Offsets into the Unique index and reference table lists in Table
+  int reference_table_list_offset = -1;
+
+  int unique_index_list_offset = -1;
 
 };
 
 
 //===--------------------------------------------------------------------===//
-// ReferenceTable Class
+// ReferenceTableInfo Class
 //===--------------------------------------------------------------------===//
 
 class ReferenceTableInfo {
 
-  public:
-  ReferenceTableInfo( oid_t PrimaryKeyTableId,
-                      std::vector<std::string> pk_column_names,
-                      std::vector<std::string> fk_column_names,
-                      char fk_update_action,
-                      char fk_delete_action,
-                      std::string name ) 
+ public:
+  ReferenceTableInfo(oid_t reference_table_id,
+                     std::vector<std::string> pk_column_names,
+                     std::vector<std::string> fk_column_names,
+                     char fk_update_action,
+                     char fk_delete_action,
+                     std::string constraint_name)
 
-                      : PrimaryKeyTableId(PrimaryKeyTableId),
-                      pk_column_names(pk_column_names),
-                      fk_column_names(fk_column_names),
-                      fk_update_action(fk_update_action),
-                      fk_delete_action(fk_delete_action),
-                      name(name)
-                      { }
+ : reference_table_id(reference_table_id),
+   pk_column_names(pk_column_names),
+   fk_column_names(fk_column_names),
+   fk_update_action(fk_update_action),
+   fk_delete_action(fk_delete_action),
+   constraint_name(constraint_name) {
+  }
 
-  std::vector<std::string> GetFKColumnNames()
-  {
+  std::vector<std::string> GetFKColumnNames() {
     return fk_column_names;
   }
 
-  oid_t GetPrimaryKeyTableId()
-  {
-    return PrimaryKeyTableId;
+  oid_t GetReferenceTableId() {
+    return reference_table_id;
   }
 
-  std::string GetName()
-  {
-    return name;
+  std::string GetName() {
+    return constraint_name;
   }
 
-  private:
+ private:
 
-  oid_t PrimaryKeyTableId = INVALID_OID;
+  oid_t reference_table_id = INVALID_OID;
 
+  // Columns in the reference table (sink)
   std::vector<std::string> pk_column_names;
+
+  // Columns in the current table (source)
+  // Can be a single column or multiple columns depending on the constraint
   std::vector<std::string> fk_column_names;
 
+  // What to do when foreign key is updated or deleted ?
+  // FIXME: Not used in our executors currently
   char fk_update_action;
   char fk_delete_action;
 
-  std::string name;
+  std::string constraint_name;
 
 };
 
