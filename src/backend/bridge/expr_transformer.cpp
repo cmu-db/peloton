b7ee799b15ecdf07f42869ae56c83bda883f6878
Ming Fang <mindbergh@gmail.com>
2015-07-14 11:05:15 -0400

    add expr transformer for list, add index scan plan transformer

--- src/backend/bridge/expr_transformer.cpp
+++ src/backend/bridge/expr_transformer.cpp
@@ -17,7 +17,6 @@
 
 #include "expr_transformer.h"
 
-
 #include "backend/bridge/tuple_transformer.h"
 #include "backend/common/logger.h"
 #include "backend/common/value.h"
@@ -36,7 +35,8 @@ ReMapPgFunc(Oid func_id,
                 expression::AbstractExpression* lc,
                 expression::AbstractExpression* rc);
 
-void ExprTransformer::PrintPostgressExprTree(const ExprState* expr_state, std::string prefix) {
+void ExprTransformer::PrintPostgressExprTree(const ExprState* expr_state,
+                                             std::string prefix) {
   auto tag = nodeTag(expr_state->expr);
 
   // TODO Not complete.
@@ -49,17 +49,29 @@ void ExprTransformer::PrintPostgressExprTree(const ExprState* expr_state, std::s
 expression::AbstractExpression* ExprTransformer::TransformExpr(
     const ExprState* expr_state) {
 
-  if(!expr_state)
+  if (!expr_state)
     return nullptr;
 
-  expression::AbstractExpression* peloton_expr;
+  expression::AbstractExpression* peloton_expr = nullptr;
+
+  /* If it is a List, do not get type from expr */
+  switch (expr_state->type) {
+     case T_List:
+      peloton_expr = TransformList(
+          reinterpret_cast<const List*>(expr_state));
+      break;
+    default:
+      break;
+  }
+
+  if (peloton_expr != nullptr) return peloton_expr;
 
   /*
    * NOTICE:
    * In Postgres, Expr and ExprState is NOT one-to-one.
    * Be careful of the Expr tag and ExprState type.
    */
-  switch(nodeTag(expr_state->expr)){
+  switch (nodeTag(expr_state->expr)) {
     case T_Const:
       peloton_expr = TransformConstant(expr_state);
       break;
@@ -70,20 +82,17 @@ expression::AbstractExpression* ExprTransformer::TransformExpr(
 
     case T_Var:
       peloton_expr = TransformVar(expr_state);
-      break;
-
+     break;
     default:
-      LOG_ERROR("Unsupported Postgres Expr type: %u\n", nodeTag(expr_state->expr));
+      LOG_ERROR("Unsupported Postgres Expr type: %u\n",
+                nodeTag(expr_state->expr))
+      ;
   }
-
-
-
 
   return peloton_expr;
 }
 
-bool ExprTransformer::CleanExprTree(
-    expression::AbstractExpression* root) {
+bool ExprTransformer::CleanExprTree(expression::AbstractExpression* root) {
 
   // AbstractExpression's destructor already handles deleting children
   delete root;
@@ -96,17 +105,20 @@ expression::AbstractExpression* ExprTransformer::TransformConstant(
 
   auto const_expr = reinterpret_cast<const Const*>(es->expr);
 
-  if(!(const_expr->constbyval)) {
-    LOG_ERROR("Sorry, we don't handle by-reference constant values currently.\n");
+  LOG_INFO("Handle Const");
+
+  if (!(const_expr->constbyval)) {
+    LOG_ERROR(
+        "Sorry, we don't handle by-reference constant values currently.\n");
   }
 
   Value value;
 
-  if(const_expr->constisnull){ // Constant is null
+  if (const_expr->constisnull) {  // Constant is null
     value = ValueFactory::GetNullValue();
-  }
-  else {  // non null
-    value = TupleTransformer::GetValue(const_expr->constvalue, const_expr->consttype);
+  } else {  // non null
+    value = TupleTransformer::GetValue(const_expr->constvalue,
+                                       const_expr->consttype);
   }
 
   LOG_INFO("Const : ");
@@ -193,6 +205,25 @@ ReMapPgFunc(Oid func_id,
 }
 
 
+
+expression::AbstractExpression* ExprTransformer::TransformList(
+    const List* list) {
+  ListCell *l;
+  int length = list_length(list);
+  assert(length > 0);
+  LOG_INFO("Handle List of length %d", length);
+
+  expression::AbstractExpression *peloton_expr;
+
+  foreach(l, list)
+  {
+    const ExprState *expr_state = reinterpret_cast<const ExprState*>(lfirst(l));
+    peloton_expr = ExprTransformer::TransformExpr(expr_state);
+  }
+
+  return peloton_expr;
+
+}
 
 } /* namespace bridge */
 } /* namespace peloton */
