2f0ab5c68b8e521557d471e0bc4d5c5e82b7d34e
JinwoongKim <jinwoongkim@cmu.edu>
2015-07-15 23:46:00 -0400

    Bridge_test - added comments and try to make it readable

--- src/backend/bridge/bridge_test.cpp
+++ src/backend/bridge/bridge_test.cpp
@@ -1,12 +1,11 @@
 /*-------------------------------------------------------------------------
  *
- *
- * plan_transformer_test.cpp
+ * bridge_test.cpp
  * file description
  *
  * Copyright(c) 2015, CMU
- *
- * Author: Ming Fang
+
+ * /peloton/tests/bridge/bridge_test.cpp
  *
  *-------------------------------------------------------------------------
  */
@@ -17,8 +16,6 @@
 #include "backend/bridge/bridge.h"
 #include "backend/bridge/ddl.h"
 #include "backend/storage/database.h"
-
-#include <vector>
 
 namespace peloton {
 namespace test {
@@ -31,10 +28,10 @@ namespace test {
 #define COLOR_CYAN    "\x1b[36m"
 #define COLOR_RESET   "\x1b[0m"
 
-//===--------------------------------------------------------------------===//
-// CreateTable TEST 
-//===--------------------------------------------------------------------===//
-
+/**
+ * @brief Test "DDL::CreateTable" function
+ *        with various test cases
+ */
 void BridgeTest::DDL_CreateTable_TEST() {
 
   DDL_CreateTable_TEST_INVALID_OID();
@@ -43,25 +40,34 @@ void BridgeTest::DDL_CreateTable_TEST() {
 
 }
 
+/**
+ * @brief CreateTable with INVALID OID
+ *        It MUST return false 
+ */
 void BridgeTest::DDL_CreateTable_TEST_INVALID_OID() {
 
-  // ColumnInfo
+  // Empty ColumnInfo
   std::vector<catalog::ColumnInfo> column_infos;
 
+  // Table name and oid
   std::string table_name = "test_table_invalid_oid";
   oid_t table_oid = INVALID_OID;
 
   // Create a table
   bool status = bridge::DDL::CreateTable( table_oid, table_name, column_infos );
-  assert( status == false );
-  printf( COLOR_GREEN ":::::: %s DONE" COLOR_RESET "\n", __func__ );
 
+  // CHECK :: status must be false
+  assert( status == false );
+
+  printf( COLOR_GREEN ":::::: %s DONE" COLOR_RESET "\n", __func__ );
 }
 
-void BridgeTest::DDL_CreateTable_TEST_COLUMNS() {
- 
-  storage::Database* db = storage::Database::GetDatabaseById( GetCurrentDatabaseOid() );
-
+/**
+ * @brief Create a simple ColumnInfo just for convenience 
+ * @return the vector of ColumnInfo
+ */
+std::vector<catalog::ColumnInfo> BridgeTest::CreateSimpleColumns()
+{
   // ColumnInfo
   std::vector<catalog::ColumnInfo> column_infos;
 
@@ -75,6 +81,20 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMNS() {
   column_infos.push_back(*column_info3);
   column_infos.push_back(*column_info4);
 
+  return column_infos;
+}
+
+/**
+ * @brief Create a table with simple Columns
+ */
+void BridgeTest::DDL_CreateTable_TEST_COLUMNS() {
+ 
+  storage::Database* db = storage::Database::GetDatabaseById( GetCurrentDatabaseOid() );
+
+  // Get the simple columns
+  std::vector<catalog::ColumnInfo> column_infos = CreateSimpleColumns();
+
+  // Table name and oid
   std::string table_name = "test_table_basic_columns";
   oid_t table_oid = 20001;
 
@@ -82,19 +102,20 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMNS() {
   bool status = bridge::DDL::CreateTable( table_oid, table_name, column_infos );
   assert ( status );
 
+  // Get the table pointer
   storage::DataTable* table = db->GetTableById(table_oid);
 
-  // Table name and oid
+  // TODO::CHECK :: Table name and oid
   assert( strcmp( (table->GetName()).c_str(), table_name.c_str()) == 0 );
   assert( table->GetId() == table_oid );
 
-  // Schema
+  // Get the table's schema to get a column 
   catalog::Schema* schema = table->GetSchema();
 
   // The first column
   catalog::ColumnInfo column = schema->GetColumnInfo(0);
 
-  // Column name, offset, length, type
+  // TODO::CHECK ::  Column name, length, type
   assert( strcmp( column.GetName().c_str(), "id" ) == 0 );
   assert( column.GetLength() == 4 );
   assert( column.GetType() == VALUE_TYPE_INTEGER );
@@ -102,7 +123,7 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMNS() {
   // The second column
   column = schema->GetColumnInfo(1);
 
-  // Column name, offset, length, type
+  // TODO::CHECK ::  Column name, length, type
   assert( strcmp( column.GetName().c_str(), "name" ) == 0 );
   assert( column.GetLength() == 68 );
   assert( column.GetType() == VALUE_TYPE_VARCHAR );
@@ -110,7 +131,7 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMNS() {
   // The third column
   column = schema->GetColumnInfo(2);
 
-  // Column name, offset, length, type
+  // TODO::CHECK ::  Column name, length, type
   assert( strcmp( column.GetName().c_str(), "time" ) == 0 );
   assert( column.GetLength() == 8 );
   assert( column.GetType() == VALUE_TYPE_TIMESTAMP );
@@ -118,7 +139,7 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMNS() {
   // The fourth column
   column = schema->GetColumnInfo(3);
 
-  // Column name, offset, length, type
+  // TODO::CHECK ::  Column name, length, type
   assert( strcmp( column.GetName().c_str(), "salary" ) == 0 );
   assert( column.GetLength() == 8 );
   assert( column.GetType() == VALUE_TYPE_DOUBLE );
@@ -126,40 +147,19 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMNS() {
   printf( COLOR_GREEN ":::::: %s DONE" COLOR_RESET "\n", __func__ );
 }
 
-// Create a table and Column-level constraints
-// such as primary key, unique, and reference table 
+/**
+ * @brief Create a table with simple columns that contain
+ *        column-level constraints such as single column 
+ *        primary key, unique, and reference table 
+ */
 void BridgeTest::DDL_CreateTable_TEST_COLUMN_CONSTRAINTS() {
 
   storage::Database* db = storage::Database::GetDatabaseById( GetCurrentDatabaseOid() );
 
-  std::vector<catalog::Constraint> constraint_infos;
-  catalog::Constraint *notnull_constraint = new catalog::Constraint( CONSTRAINT_TYPE_NOTNULL );
-  catalog::Constraint *primary_key_constraint = new catalog::Constraint( CONSTRAINT_TYPE_PRIMARY, "THIS_IS_PRIMARY_KEY_CONSTRAINT" );
-  catalog::Constraint *unique_constraint = new catalog::Constraint( CONSTRAINT_TYPE_UNIQUE, "THIS_IS_UNIQUE_CONSTRAINT" );
+  // Get the simple columns
+  std::vector<catalog::ColumnInfo> column_infos = CreateSimpleColumns();
 
-
-  // ColumnInfo
-  std::vector<catalog::ColumnInfo> column_infos;
-
-  constraint_infos.push_back( *notnull_constraint );
-  catalog::ColumnInfo *column_info1 = new catalog::ColumnInfo( VALUE_TYPE_INTEGER, 4, "id", constraint_infos); 
-  constraint_infos.clear();
-
-  constraint_infos.push_back( *primary_key_constraint );
-  catalog::ColumnInfo *column_info2 = new catalog::ColumnInfo( VALUE_TYPE_VARCHAR, 68, "name", constraint_infos); 
-  constraint_infos.clear();
-
-  constraint_infos.push_back( *unique_constraint );
-  catalog::ColumnInfo *column_info3 = new catalog::ColumnInfo( VALUE_TYPE_TIMESTAMP, 8, "time", constraint_infos); 
-  constraint_infos.clear();
-
-  catalog::ColumnInfo *column_info4 = new catalog::ColumnInfo( VALUE_TYPE_DOUBLE, 8, "salary" );
-
-  column_infos.push_back(*column_info1);
-  column_infos.push_back(*column_info2);
-  column_infos.push_back(*column_info3);
-  column_infos.push_back(*column_info4);
-
+  // Table name and oid
   std::string table_name = "test_table_column_constraint";
   oid_t table_oid = 20002;
 
@@ -167,13 +167,22 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMN_CONSTRAINTS() {
   bool status = bridge::DDL::CreateTable( table_oid, table_name, column_infos );
   assert ( status );
 
+  // Get the table pointer and schema
   storage::DataTable* table = db->GetTableById(table_oid);
+  catalog::Schema *schema = table->GetSchema();
 
-  // Table name and oid
-  assert( strcmp( (table->GetName()).c_str(), table_name.c_str()) == 0 );
-  assert( table->GetId() == table_oid );
+  // Create the constrains
+  catalog::Constraint *notnull_constraint = new catalog::Constraint( CONSTRAINT_TYPE_NOTNULL );
+  catalog::Constraint *primary_key_constraint = new catalog::Constraint( CONSTRAINT_TYPE_PRIMARY, "THIS_IS_PRIMARY_KEY_CONSTRAINT" );
+  catalog::Constraint *unique_constraint = new catalog::Constraint( CONSTRAINT_TYPE_UNIQUE, "THIS_IS_UNIQUE_CONSTRAINT" );
 
-  // Create an index for Primary key constraint
+  // Add one constraint to the one column
+  schema->AddConstraintByColumnName( "id",    notnull_constraint );
+  schema->AddConstraintByColumnName( "name",  primary_key_constraint );
+  schema->AddConstraintByColumnName( "time",  unique_constraint );
+
+  // Create a primary key index
+  // NOTE:: It must be called automatically in active or bootstrap mode
   std::vector<std::string> key_column_names;
   key_column_names.push_back("name");
   bridge::IndexInfo* index_info = new bridge::IndexInfo( "THIS_IS_PRIMARY_KEY_CONSTRAINT",
@@ -183,11 +192,10 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMN_CONSTRAINTS() {
                                                           INDEX_TYPE_PRIMARY_KEY,
                                                           true,
                                                           key_column_names);
-
   status = bridge::DDL::CreateIndex( *index_info );
   assert( status );
 
-  // Create an index for unique constraint
+  // Create a unique index
   key_column_names.clear();
   key_column_names.push_back("time");
   index_info = new bridge::IndexInfo( "THIS_IS_UNIQUE_CONSTRAINT",
@@ -201,14 +209,13 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMN_CONSTRAINTS() {
   status = bridge::DDL::CreateIndex( *index_info );
   assert( status );
 
-  // Create a reference table
-  table_name = "pk_table";
+  // Create a reference table that will be referenced by another table
+  std::string pktable_name = "pktable";
   oid_t pktable_oid = 20003;
-
-  // Create a table
-  status = bridge::DDL::CreateTable( pktable_oid, table_name, column_infos );
+  status = bridge::DDL::CreateTable( pktable_oid, pktable_name, column_infos );
   assert ( status );
 
+  // Construct ReferenceTableInfo
   std::vector<std::string> pk_column_names;
   std::vector<std::string> fk_column_names;
   pk_column_names.push_back("name");
@@ -221,93 +228,28 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMN_CONSTRAINTS() {
                                                                                        'c',
                                                                                        "THIS_IS_FOREIGN_CONSTRAINT");
   reference_table_infos.push_back( *reference_table_info );
+
+  // Current table ----> reference table
   status = peloton::bridge::DDL::SetReferenceTables( reference_table_infos, table_oid );
   assert ( status );
 
-  // Schema
-  catalog::Schema* schema = table->GetSchema();
+
+  // TODO::CHECK :: check only regarding constraint. Skip others, such as, column name, length, etc.
 
   // The first column
+  // TODO::CHECK :: Not null constraint type 
   catalog::ColumnInfo column = schema->GetColumnInfo(0);
-
-  // Column name, offset, length, type, constraint not null
-  assert( strcmp( column.GetName().c_str(), "id" ) == 0 );
-  assert( column.GetLength() == 4 );
-  assert( column.GetType() == VALUE_TYPE_INTEGER );
-
-  constraint_infos = column.GetConstraints();
-  assert( constraint_infos.size() == 1 );
-
-  ConstraintType type = constraint_infos[0].GetType();
-  assert( type == CONSTRAINT_TYPE_NOTNULL );
+  std::vector<catalog::Constraint> constraint_infos = column.GetConstraints();
+  assert( constraint_infos[0].GetType() == CONSTRAINT_TYPE_NOTNULL );
 
   // The second column
+  // TODO::CHECK :: Primary key constraint type, name
   column = schema->GetColumnInfo(1);
   constraint_infos = column.GetConstraints();
-  assert( constraint_infos.size() == 1 );
+  assert( constraint_infos[0].GetType() == CONSTRAINT_TYPE_PRIMARY );
+  assert( strcmp( constraint_infos[0].GetName().c_str(), "THIS_IS_PRIMARY_KEY_CONSTRAINT") == 0 );
 
-  // Column name, offset, length, type, constraint not null
-  assert( strcmp( column.GetName().c_str(), "name" ) == 0 );
-  assert( column.GetLength() == 68 );
-  assert( column.GetType() == VALUE_TYPE_VARCHAR );
-
-  type = constraint_infos[0].GetType();
-  assert( type == CONSTRAINT_TYPE_PRIMARY );
-  std::string name = constraint_infos[0].GetName();
-  assert( strcmp( name.c_str(), "THIS_IS_PRIMARY_KEY_CONSTRAINT") == 0 );
-
-  // The third column
-  column = schema->GetColumnInfo(2);
-  constraint_infos = column.GetConstraints();
-  assert( constraint_infos.size() == 1 );
-
-  // Column name, offset, length, type, constraint not null
-  assert( strcmp( column.GetName().c_str(), "time" ) == 0 );
-  assert( column.GetLength() == 8 );
-  assert( column.GetType() == VALUE_TYPE_TIMESTAMP );
-
-  type = constraint_infos[0].GetType();
-  assert( type == CONSTRAINT_TYPE_UNIQUE );
-  name = constraint_infos[0].GetName();
-  assert( strcmp( name.c_str(), "THIS_IS_UNIQUE_CONSTRAINT") == 0 );
-
-  // The fourth column
-  column = schema->GetColumnInfo(3);
-  constraint_infos = column.GetConstraints();
-  assert( constraint_infos.size() == 1 );
-
-  // Column name, offset, length, type, constraint not null
-  assert( strcmp( column.GetName().c_str(), "salary" ) == 0 );
-  assert( column.GetLength() == 8 );
-  assert( column.GetType() == VALUE_TYPE_DOUBLE );
-
-  // Check  reference table
-  type = constraint_infos[0].GetType();
-  assert( type == CONSTRAINT_TYPE_FOREIGN );
-  name = constraint_infos[0].GetName();
-  assert( strcmp( name.c_str(), "THIS_IS_FOREIGN_CONSTRAINT") == 0 );
-  assert( constraint_infos[0].GetReferenceTableOffset() == 1 );
-
-  //Get the pk table and test simple case, name and oid
-  storage::DataTable* pktable = table->GetReferenceTable(0);
-
-  // Table name and oid
-    assert( strcmp( (pktable->GetName()).c_str(), "pk_table" ) == 0 );
-    assert( pktable->GetId() == 20003 );
-
-    pk_column_names.clear();
-    fk_column_names.clear();
-   pk_column_names  = table->GetReferenceTableInfo(0)->GetPKColumnNames();
-   fk_column_names  = table->GetReferenceTableInfo(0)->GetFKColumnNames();
-   assert( pk_column_names.size() == 1 );
-   assert( fk_column_names.size() == 1 );
-   assert(  strcmp( pk_column_names[0].c_str(), "name") == 0);
-   assert(  strcmp( fk_column_names[0].c_str(), "salary") == 0);
-
-  assert(  table->GetReferenceTableInfo(0)->GetUpdateAction() == 'r' );
-  assert(  table->GetReferenceTableInfo(0)->GetDeleteAction() == 'c' );
-
-  // Check  index for primary key constraint
+  // TODO::CHECK :: the primary key index name, type, etc.
   index::Index *index = table->GetIndexByOid( 30001 );
   assert( strcmp(index->GetName().c_str(), "THIS_IS_PRIMARY_KEY_CONSTRAINT") == 0 );
   assert(  index->GetColumnCount() == 1 );
@@ -315,13 +257,49 @@ void BridgeTest::DDL_CreateTable_TEST_COLUMN_CONSTRAINTS() {
   assert ( index->GetIndexType() == INDEX_TYPE_PRIMARY_KEY);
   assert ( index->HasUniqueKeys()  == true);
 
-  // Check  index for unique constraint
+
+  // The third column
+  // TODO::CHECK :: Unique constraint name, type, etc.
+  column = schema->GetColumnInfo(2);
+  constraint_infos = column.GetConstraints();
+  assert( constraint_infos[0].GetType() == CONSTRAINT_TYPE_UNIQUE );
+  assert( strcmp( constraint_infos[0].GetName().c_str(), "THIS_IS_UNIQUE_CONSTRAINT") == 0 );
+
+  // TODO::Check  index for unique constraint
   index = table->GetIndexByOid( 30002 );
   assert( strcmp(index->GetName().c_str(), "THIS_IS_UNIQUE_CONSTRAINT") == 0 );
   assert(  index->GetColumnCount() == 1 );
   assert ( index->GetIndexMethodType()   == INDEX_METHOD_TYPE_BTREE_MULTIMAP);
   assert ( index->GetIndexType() == INDEX_TYPE_UNIQUE);
   assert ( index->HasUniqueKeys()  == true);
+
+  // The fourth column
+  column = schema->GetColumnInfo(3);
+  constraint_infos = column.GetConstraints();
+  assert( constraint_infos.size() == 1 );
+
+  // TODO::CHECK::  reference table
+  assert( constraint_infos[0].GetType() == CONSTRAINT_TYPE_FOREIGN );
+  assert( strcmp( constraint_infos[0].GetName().c_str(), "THIS_IS_FOREIGN_CONSTRAINT") == 0 );
+  assert( constraint_infos[0].GetReferenceTableOffset() == 1 );
+
+  // TODO::CHECK:: pktable name and oid
+  storage::DataTable* pktable = table->GetReferenceTable(0);
+  assert( strcmp( (pktable->GetName()).c_str(), "pktable" ) == 0 );
+  assert( table->GetId() == table_oid );
+
+  // TODO::CHECK:: pktable size name, update/delete action
+  pk_column_names.clear();
+  fk_column_names.clear();
+  pk_column_names  = table->GetReferenceTableInfo(0)->GetPKColumnNames();
+  fk_column_names  = table->GetReferenceTableInfo(0)->GetFKColumnNames();
+  assert( pk_column_names.size() == 1 );
+  assert( fk_column_names.size() == 1 );
+  assert(  strcmp( pk_column_names[0].c_str(), "name") == 0);
+  assert(  strcmp( fk_column_names[0].c_str(), "salary") == 0);
+
+  assert(  table->GetReferenceTableInfo(0)->GetUpdateAction() == 'r' );
+  assert(  table->GetReferenceTableInfo(0)->GetDeleteAction() == 'c' );
 
   printf( COLOR_GREEN ":::::: %s DONE" COLOR_RESET "\n", __func__ );
 }
