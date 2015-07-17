f315fd976dcfa447658c8df7598aaedb44179126
JinwoongKim <jinwoongkim@cmu.edu>
2015-07-17 18:37:15 -0400

    Refactoring bridge_test

--- src/backend/bridge/bridge_test.h
+++ src/backend/bridge/bridge_test.h
@@ -21,6 +21,11 @@ namespace peloton {
 
 namespace catalog {
 class Column;
+class ForeignKey;
+}
+
+namespace index {
+class Index;
 }
 
 namespace test {
@@ -63,6 +68,39 @@ class BridgeTest {
                           std::string column_name, 
                           int length, 
                           ValueType type);
+
+  static bool CheckColumnWithConstraint(catalog::Column& column,
+                                        ConstraintType constraint_type,
+                                        std::string constraint_name,
+                                        int constraint_count, 
+                                        int foreign_key_offset = -1);
+
+  static bool CheckIndex(index::Index* index,
+                         std::string index_name,
+                         oid_t column_count,
+                         IndexType method_type,
+                         IndexConstraintType constraint_type,
+                         bool unique);
+
+  static bool CheckForeignKey(catalog::ForeignKey* foreign_key,
+                         oid_t pktable_oid,
+                         std::string constraint_name,
+                         int pk_column_names_count,
+                         int fk_column_names_count,
+                         char fk_update_action,
+                         char fk_delete_action);
+
+  static void CreateSamplePrimaryKeyIndex(std::string table_name,
+                                          oid_t index_oid);
+
+  static void CreateSampleUniqueIndex(std::string table_name,
+                                      oid_t index_oid);
+
+  static void CreateSampleForeignKey(oid_t pktable_oid,
+                                     std::string pktable_name,
+                                     std::vector<catalog::Column>& columns,
+                                     oid_t table_oid);
+
 
 
 
