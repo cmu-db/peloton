d33ee944cd81860fe4d22e663163f2eb71c5ea12
jarulraj <jarulraj@cs.cmu.edu>
2015-07-22 19:24:29 -0400

    Added better backtrace.
    Former-commit-id: 96f139cd976338dcb89e0b3d11f46097b2e7741f

--- src/backend/common/stack_trace.h
+++ src/backend/common/stack_trace.h
@@ -12,11 +12,31 @@
 
 #pragma once
 
+#define BACKWARD_HAS_DW 1
+
+#include "backend/common/backward.h"
+
 namespace peloton {
 
 //===--------------------------------------------------------------------===//
 //  Stack Trace
 //===--------------------------------------------------------------------===//
+
+class StackTracer {
+ public:
+  static std::vector<int> make_default_signals();
+
+  StackTracer(const std::vector<int>& signals = make_default_signals());
+
+  bool loaded() const { return _loaded; }
+
+ private:
+  backward::details::handle<char*> _stack_content;
+  bool                   _loaded;
+
+  static void sig_handler(int, siginfo_t* info, void* _ctx);
+
+};
 
 void GetStackTrace(int signum);
 
