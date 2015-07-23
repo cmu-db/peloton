f48b17221f0779c3d929cb586cf04306bea08077
Ming Fang <mindbergh@gmail.com>
2015-07-23 11:23:24 -0400

    add jdbc test for read modify write

--- scripts/jdbc/src/PelotonTest.java
+++ scripts/jdbc/src/PelotonTest.java
@@ -13,12 +13,13 @@ public class PelotonTest {
   private final String DROP = "DROP TABLE IF EXISTS peloton_test";
   private final String DDL = "CREATE TABLE peloton_test (id INT PRIMARY KEY, data TEXT)";
   private final String INSERT = "INSERT INTO peloton_test VALUES (?,?)";
-  private final String SEQSCAN = "SELECT * FROM peloton_test WHERE id != ?";
+  private final String SEQSCAN = "SELECT * FROM peloton_test";
   private final String INDEXSCAN = "SELECT * FROM peloton_test WHERE id = ?";
   private final String BITMAPSCAN = "SELECT * FROM peloton_test WHERE id > ? and id < ?";
   private final String UPDATE_BY_INDEXSCAN = "UPDATE peloton_test SET data=? WHERE id=?";
   private final String UPDATE_BY_SCANSCAN = "UPDATE peloton_test SET data=?";
   private final String DELETE_BY_INDEXSCAN = "DELETE FROM peloton_test WHERE id = ?";
+  private final String SELECT_FOR_UPDATE = "SELECT * FROM peloton_test WHERE id = ? FOR UPDATE";
 
   private final Connection conn;
 
@@ -50,7 +51,6 @@ public class PelotonTest {
     Statement stmt = conn.createStatement();
     stmt.execute(DROP);
     stmt.execute(DDL);
-    conn.setAutoCommit(true);
     System.out.println("Test db created.");
   }
 
@@ -71,14 +71,14 @@ public class PelotonTest {
   }
 
   public void SeqScan() throws SQLException {
-    System.out.println("SeqScan Test:");
+    System.out.println("\nSeqScan Test:");
     System.out.println("Query: " + SEQSCAN);
     PreparedStatement stmt = conn.prepareStatement(SEQSCAN);
-    stmt.setInt(1, 3);
     ResultSet r = stmt.executeQuery();
     while (r.next()) {
-      System.out.println("SeqScan: " + r.getString(2));
+      System.out.println("SeqScan: id = " + r.getString(1) + ", " + r.getString(2));
     }
+    r.close();
   }
 
   /**
@@ -87,7 +87,7 @@ public class PelotonTest {
    * @throws SQLException
    */
   public void IndexScan(int i) throws SQLException {
-    System.out.println("IndexScan Test: ? = " + i);
+    System.out.println("\nIndexScan Test: ? = " + i);
     System.out.println("Query: " + INDEXSCAN);
     PreparedStatement stmt = conn.prepareStatement(INDEXSCAN);
     stmt.setInt(1, i);
@@ -95,6 +95,7 @@ public class PelotonTest {
     while (r.next()) {
       System.out.println("IndexScanTest got tuple: id: " + r.getString(1) + ", data: " + r.getString(2));
     }
+    r.close();
   }
 
   /**
@@ -103,7 +104,7 @@ public class PelotonTest {
    * @throws SQLException
    */
   public void BitmapScan(int i, int j) throws SQLException {
-    System.out.println("BitmapScan Test: ? = " + i + ", " + j);
+    System.out.println("\nBitmapScan Test: ? = " + i + ", " + j);
     System.out.println("Query: " + BITMAPSCAN);
     PreparedStatement stmt = conn.prepareStatement(BITMAPSCAN);
     stmt.setInt(1, i);
@@ -112,50 +113,75 @@ public class PelotonTest {
     while (r.next()) {
       System.out.println("BitmapScanTest got tuple: id: " + r.getString(1) + ", data: " + r.getString(2));
     }
+    r.close();
   }
 
   public void UpdateByIndex(int i) throws SQLException {
-    System.out.println("Update Test: ? = " + i);
+    System.out.println("\nUpdate Test: ? = " + i);
     System.out.println("Query: " + UPDATE_BY_INDEXSCAN);
     PreparedStatement stmt = conn.prepareStatement(UPDATE_BY_INDEXSCAN);
     stmt.setInt(2, i);
     stmt.setString(1, "Updated");
     stmt.executeUpdate();
-    System.out.println("Inserted: id: " + i + ", data: Updated");
+    System.out.println("Updated: id: " + i + ", data: Updated");
 
   }
 
 
   public void UpdateBySeqScan() throws SQLException {
-    System.out.println("Update Test: ");
+    System.out.println("\nUpdate Test: ");
     System.out.println("Query: " + UPDATE_BY_SCANSCAN);
     PreparedStatement stmt = conn.prepareStatement(UPDATE_BY_SCANSCAN);
     stmt.setString(1, "Updated");
     stmt.executeUpdate();
-    System.out.println("Inserted: data: Updated");
+    System.out.println("Updated: data: Updated");
 
   }
 
 
   public void DeleteByIndexScan(int i) throws SQLException {
-    System.out.println("Delete Test: ");
+    System.out.println("\nDelete Test: ");
     System.out.println("Query: " + DELETE_BY_INDEXSCAN);
     PreparedStatement stmt = conn.prepareStatement(DELETE_BY_INDEXSCAN);
     stmt.setInt(1, i);
     stmt.executeUpdate();
     System.out.println("Deleted: id = " + i);
-
   }
+
+  public void ReadModifyWrite(int i) throws SQLException {
+    System.out.println("\nReadModifyWrite Test: ");
+    System.out.println("Query: " + SELECT_FOR_UPDATE);
+    PreparedStatement stmt = conn.prepareStatement(SELECT_FOR_UPDATE);
+
+    stmt.setInt(1, i);
+    ResultSet r = stmt.executeQuery();
+    while (r.next()) {
+      System.out.println("ReadModifyWriteTest got tuple: id: " + r.getString(1) + ", data: " + r.getString(2));
+    }
+
+    r.close();
+
+    stmt = conn.prepareStatement(UPDATE_BY_INDEXSCAN);
+    stmt.setInt(2, i);
+    stmt.setString(1, "Updated");
+    stmt.executeUpdate();
+
+    System.out.println("Updated: id: " + i + ", data: Updated");
+  }
+
+
   static public void main(String[] args) throws Exception {
     PelotonTest pt = new PelotonTest();
     pt.Init();
     for (int i = 0; i < 10; i++) {
       pt.Insert(i);
     }
+
+    pt.ReadModifyWrite(3);
     //pt.BitmapScan(2, 5);
     //pt.SeqScan();
     //pt.DeleteByIndexScan(3);
-    //pt.SeqScan();
+    pt.SeqScan();
     //pt.UpdateBySeqScan();
     //pt.IndexScan(3);
     pt.Close();
