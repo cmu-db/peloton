0be9e138ba066cebce44df956c62f9758883ae82
jarulraj <jarulraj@cs.cmu.edu>
2015-07-25 19:44:38 -0400

    Updated the sample.

--- tests/brain/clusterer_test.cpp
+++ tests/brain/clusterer_test.cpp
@@ -11,6 +11,8 @@
  */
 
 #include <cstdio>
+#include <random>
+#include <chrono>
 
 #include "gtest/gtest.h"
 #include "harness.h"
@@ -34,12 +36,19 @@ TEST(ClustererTests, BasicTest) {
   std::vector<double> columns_accessed(column_count, 0);
 
   double sample_weight = 1;
-  for(int sample_itr = 0 ; sample_itr < 10; sample_itr ++) {
 
-    for(column_itr = 0 ; column_itr < column_count ; column_itr++)
-      columns_accessed[column_itr] = rand() % 1;
+  // initialize a uniform distribution between 0 and 1
+  std::mt19937_64 rng;
+  std::uniform_real_distribution<double> uniform(0, 1);
+
+  for(int sample_itr = 0 ; sample_itr < 10; sample_itr ++) {
+    for(column_itr = 0 ; column_itr < column_count ; column_itr++) {
+      columns_accessed[column_itr] = uniform(rng);
+    }
 
     brain::Sample sample(columns_accessed, sample_weight);
+    std::cout << sample;
+
     clusterer.ProcessSample(sample);
   }
 
