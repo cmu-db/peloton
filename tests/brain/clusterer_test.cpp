840bd300c8d9bb16d1ab584ee21b8541e6cb2f5e
jarulraj <jarulraj@cs.cmu.edu>
2015-07-25 15:56:15 -0400

    Added template for clusterer test.
    Former-commit-id: c4aa05371a4b2df61cb99b674460d415d3f1eaec

---
+++ tests/brain/clusterer_test.cpp
@@ -0,0 +1,34 @@
+/*-------------------------------------------------------------------------
+ *
+ * clusterer_test.cpp
+ * file description
+ *
+ * Copyright(c) 2015, CMU
+ *
+ * /peloton/tests/brain/clusterer_test.cpp
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include <cstdio>
+
+#include "gtest/gtest.h"
+#include "harness.h"
+
+#include "backend/brain/clusterer.h"
+
+namespace peloton {
+namespace test {
+
+//===--------------------------------------------------------------------===//
+// Clusterer Tests
+//===--------------------------------------------------------------------===//
+
+TEST(ClustererTests, BasicTest) {
+
+  EXPECT_EQ(1, 1);
+
+}
+
+}  // End test namespace
+}  // End peloton namespace
