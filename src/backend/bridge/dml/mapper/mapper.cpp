f5b665cc5bde14150a6af211d82664e0176c3db2
Ming Fang <mindbergh@gmail.com>
2015-07-28 01:02:40 -0400

    add new swich branch for nested loop join
    Former-commit-id: 43d5a7c61c2f628589809b6798f4e50bcf432d70

--- src/backend/bridge/dml/mapper/mapper.cpp
+++ src/backend/bridge/dml/mapper/mapper.cpp
@@ -77,6 +77,10 @@ planner::AbstractPlanNode *PlanTransformer::TransformPlan(
       plan_node = PlanTransformer::TransformLimit(
           reinterpret_cast<const LimitState *>(plan_state));
       break;
+    case T_NestLoop:
+      plan_node = PlanTransformer::TransformNestLoop(
+          reinterpret_cast<const NestLoopState*>(plan_state));
+      break;
     default: {
       LOG_ERROR("Unsupported Postgres Plan State Tag: %u Plan Tag: %u ",
                 nodeTag(plan_state), nodeTag(plan));
