76a04fe02cc5b39f6a44130ff794825823b96572
Ziqiang Feng <csfzq2009@gmail.com>
2015-07-29 12:34:03 -0400

    Add plan node and executor for projection. Compilable.

---
+++ src/backend/executor/projection_executor.h
@@ -0,0 +1,43 @@
+/**
+ * @brief Header file for Project executor.
+ *
+ * Copyright(c) 2015, CMU
+ */
+
+#pragma once
+
+#include "backend/executor/abstract_executor.h"
+#include "backend/planner/project_info.h"
+
+namespace peloton {
+namespace executor {
+
+class ProjectionExecutor : public AbstractExecutor {
+ public:
+  ProjectionExecutor(const ProjectionExecutor &) = delete;
+  ProjectionExecutor &operator=(const ProjectionExecutor &) = delete;
+  ProjectionExecutor(const ProjectionExecutor &&) = delete;
+  ProjectionExecutor &operator=(const ProjectionExecutor &&) = delete;
+
+  explicit ProjectionExecutor(planner::AbstractPlanNode *node,
+                              ExecutorContext *executor_context);
+
+ protected:
+  bool DInit();
+
+  bool DExecute();
+
+ private:
+  //===--------------------------------------------------------------------===//
+  // Executor State
+  //===--------------------------------------------------------------------===//
+
+  /** @brief Projection Info.            */
+  const planner::ProjectInfo* project_info_ = nullptr;
+
+  /** @brief Schema of projected tuples. */
+  const catalog::Schema* schema_ = nullptr;
+};
+
+} /* namespace executor */
+} /* namespace peloton */
