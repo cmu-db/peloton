3be98d1e55124577da0304fc86deb332d7d197d2
Ziqiang Feng <csfzq2009@gmail.com>
2015-07-29 12:34:03 -0400

    Add plan node and executor for projection. Compilable.
    Former-commit-id: d505d7cfc0be6ea165ea8df11814aaf9a248a080

--- src/backend/bridge/dml/executor/plan_executor.cpp
+++ src/backend/bridge/dml/executor/plan_executor.cpp
@@ -155,7 +155,7 @@ void CleanExecutorTree(executor::AbstractExecutor *root) {
 }
 
 /**
- * @brief Add a Materialization node if the root of the exector tree is seqscan
+ * @brief Add a Materialization node if the root of the executor tree is seqscan
  * or limit
  * @param the current executor tree
  * @return new root of the executor tree
@@ -242,8 +242,7 @@ void PlanExecutor::ExecutePlan(planner::AbstractPlanNode *plan,
 
     std::unique_ptr<executor::LogicalTile> tile(executor_tree->GetOutput());
 
-    // FIXME Some executor just doesn't return tiles (e.g., Update).
-    // Should we continue instead of break here?
+    // Some executor just doesn't return tiles (e.g., Update).
     if (tile.get() == nullptr) {
       continue;
     }
