37fe9d9ad403e75f0e0a45ad35405931ba5412b2
JinwoongKim <jinwoongkim@cmu.edu>
2015-07-31 11:05:35 -0400

    Added CreateForeignkeys

--- src/backend/bridge/ddl/bootstrap.h
+++ src/backend/bridge/ddl/bootstrap.h
@@ -68,6 +68,9 @@ class Bootstrap {
   static void CreateIndexes(raw_index_info** raw_indexes, 
                             oid_t index_count);
 
+  static void CreateForeignkeys(raw_foreignkey_info** raw_foreignkeys, 
+                                oid_t foreignkey_count);
+
   static std::vector<catalog::Column>
          CreateColumns(raw_column_info** raw_columns, 
                        oid_t column_count);
