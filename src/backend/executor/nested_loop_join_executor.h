a699840ed77c8086926222ac575d7fc8ac5c9f82
Ming Fang <mindbergh@gmail.com>
2015-08-04 01:12:03 -0400

    add support for direct map projection for nest loop join
    Former-commit-id: 3438d640c1284709e6614416230b99a0368c3a3b

--- src/backend/executor/nested_loop_join_executor.h
+++ src/backend/executor/nested_loop_join_executor.h
@@ -35,6 +35,13 @@ class NestedLoopJoinExecutor : public AbstractExecutor {
   bool DExecute();
 
  private:
+
+  //===--------------------------------------------------------------------===//
+  // Helper
+  //===--------------------------------------------------------------------===//
+  std::vector<LogicalTile::ColumnInfo> BuildSchema(std::vector<LogicalTile::ColumnInfo> left,
+                                                   std::vector<LogicalTile::ColumnInfo> right);
+
   //===--------------------------------------------------------------------===//
   // Executor State
   //===--------------------------------------------------------------------===//
