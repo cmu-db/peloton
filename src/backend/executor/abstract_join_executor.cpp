558e828f276de7718c846c097e9a17fb453bb462
Ming Fang <mindbergh@gmail.com>
2015-08-04 16:48:17 -0400

    change comment

--- src/backend/executor/abstract_join_executor.cpp
+++ src/backend/executor/abstract_join_executor.cpp
@@ -1,11 +1,11 @@
 /*-------------------------------------------------------------------------
  *
- * nested_loop_join.cpp
+ * merge_join.cpp
  * file description
  *
  * Copyright(c) 2015, CMU
  *
- * /peloton/src/executor/nested_loop_join_executor.cpp
+ * /peloton/src/executor/merge_join_executor.cpp
  *
  *-------------------------------------------------------------------------
  */
