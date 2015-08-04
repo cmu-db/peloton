ec2834fe2e1fad42b8f3102b08ff38b8151e1df7
jarulraj <jarulraj@cs.cmu.edu>
2015-08-04 16:48:21 -0400

    Cleaned up tile group header and a lot of couts

--- src/backend/storage/tile_group_header.cpp
+++ src/backend/storage/tile_group_header.cpp
@@ -14,6 +14,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <sstream>
 
 namespace peloton {
 namespace storage {
@@ -56,7 +57,7 @@ std::ostream& operator<<(std::ostream& os,
 
     peloton::ItemPointer location = tile_group_header.GetPrevItemPointer(header_itr);
     os << " prev : "
-       << "[ " << location.block << " , " << location.offset << " ]\n";
+        << "[ " << location.block << " , " << location.offset << " ]\n";
   }
 
   os << "\t-----------------------------------------------------------\n";
@@ -66,9 +67,9 @@ std::ostream& operator<<(std::ostream& os,
 
 void TileGroupHeader::PrintVisibility(txn_id_t txn_id, cid_t at_cid) {
   oid_t active_tuple_slots = GetNextTupleSlot();
+  std::stringstream os;
 
-  std::cout
-      << "\t-----------------------------------------------------------\n";
+  os << "\t-----------------------------------------------------------\n";
 
   for (oid_t header_itr = 0; header_itr < active_tuple_slots; header_itr++) {
     bool own = (txn_id == GetTransactionId(header_itr));
@@ -81,45 +82,46 @@ void TileGroupHeader::PrintVisibility(txn_id_t txn_id, cid_t at_cid) {
 
     int width = 10;
 
-    std::cout << "\tslot :: " << std::setw(width) << header_itr;
+    os << "\tslot :: " << std::setw(width) << header_itr;
 
-    std::cout << " txn id : ";
+    os << " txn id : ";
     if (txn_id == MAX_TXN_ID)
-      std::cout << std::setw(width) << "MAX_TXN_ID";
+      os << std::setw(width) << "MAX_TXN_ID";
     else
-      std::cout << std::setw(width) << txn_id;
+      os << std::setw(width) << txn_id;
 
-    std::cout << " beg cid : ";
+    os << " beg cid : ";
     if (beg_commit_id == MAX_CID)
-      std::cout << std::setw(width) << "MAX_CID";
+      os << std::setw(width) << "MAX_CID";
     else
-      std::cout << std::setw(width) << beg_commit_id;
+      os << std::setw(width) << beg_commit_id;
 
-    std::cout << " end cid : ";
+    os << " end cid : ";
     if (end_commit_id == MAX_CID)
-      std::cout << std::setw(width) << "MAX_CID";
+      os << std::setw(width) << "MAX_CID";
     else
-      std::cout << std::setw(width) << end_commit_id;
+      os << std::setw(width) << end_commit_id;
 
     peloton::ItemPointer location = GetPrevItemPointer(header_itr);
-    std::cout << " prev : "
-              << "[ " << location.block << " , " << location.offset
-              << " ]";  //<<
+    os << " prev : "
+        << "[ " << location.block << " , " << location.offset
+        << " ]";  //<<
 
-    std::cout << " own : " << own;
-    std::cout << " activated : " << activated;
-    std::cout << " invalidated : " << invalidated << " ";
+    os << " own : " << own;
+    os << " activated : " << activated;
+    os << " invalidated : " << invalidated << " ";
 
     // Visible iff past Insert || Own Insert
     if ((!own && activated && !invalidated) ||
         (own && !activated && !invalidated))
-      std::cout << "\t\t[ true  ]\n";
+      os << "\t\t[ true  ]\n";
     else
-      std::cout << "\t\t[ false ]\n";
+      os << "\t\t[ false ]\n";
   }
 
-  std::cout
-      << "\t-----------------------------------------------------------\n";
+  os << "\t-----------------------------------------------------------\n";
+
+  LOG_INFO("%s", os.str().c_str());
 }
 
 }  // End storage namespace
