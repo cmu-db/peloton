37e42b193b585b321ee8ab8b71b686d2d7f5f22b
jarulraj <jarulraj@cs.cmu.edu>
2015-08-03 22:19:08 -0400

    Checkpoint
    Former-commit-id: 90ddf2d707fa18d6623e92646b1b8eae77510b97

--- tests/executor/index_scan_test.cpp
+++ tests/executor/index_scan_test.cpp
@@ -35,26 +35,17 @@ TEST(IndexScanTests, IndexPredicateTest) {
   std::vector<oid_t> column_ids({0, 1, 3});
 
   //===--------------------------------------------------------------------===//
-  // Start <= Tuple
+  // ATTR 0 <= 110
   //===--------------------------------------------------------------------===//
 
-  // Set start key
   auto index = data_table->GetIndex(0);
-  const catalog::Schema* index_key_schema = index->GetKeySchema();
-  std::unique_ptr<storage::Tuple> start_key(
-      new storage::Tuple(index_key_schema, true));
-
-  start_key->SetValue(0, ValueFactory::GetIntegerValue(0));
-
-  std::unique_ptr<storage::Tuple> end_key(nullptr);
-
   std::vector<oid_t> key_column_ids;
   std::vector<ExpressionType> expr_types;
   std::vector<Value> values;
 
   key_column_ids.push_back(0);
   expr_types.push_back(ExpressionType::EXPRESSION_TYPE_COMPARE_LTE);
-  values.push_back(start_key->GetValue(0));
+  values.push_back(ValueFactory::GetIntegerValue(110));
 
   // Create index scan desc
   planner::IndexScanNode::IndexScanDesc index_scan_desc(
@@ -94,11 +85,141 @@ TEST(IndexScanTests, IndexPredicateTest) {
   EXPECT_FALSE(executor.Execute());
   EXPECT_EQ(result_tiles.size(), expected_num_tiles);
   EXPECT_EQ(result_tiles[0].get()->GetTupleCount(), 5);
-
-  std::cout << *(result_tiles[0].get());
+  EXPECT_EQ(result_tiles[1].get()->GetTupleCount(), 5);
+  EXPECT_EQ(result_tiles[2].get()->GetTupleCount(), 2);
 
   txn_manager.CommitTransaction(txn);
+}
 
+TEST(IndexScanTests, MultiColumnPredicateTest) {
+  // First, generate the table with index
+  std::unique_ptr<storage::DataTable> data_table(
+      ExecutorTestsUtil::CreateAndPopulateTable());
+
+  // Column ids to be added to logical tile after scan.
+  std::vector<oid_t> column_ids({0, 1, 3});
+
+  //===--------------------------------------------------------------------===//
+  // ATTR 1 > 50 & ATTR 0 <= 30
+  //===--------------------------------------------------------------------===//
+
+  auto index = data_table->GetIndex(1);
+  std::vector<oid_t> key_column_ids;
+  std::vector<ExpressionType> expr_types;
+  std::vector<Value> values;
+
+  key_column_ids.push_back(1);
+  key_column_ids.push_back(0);
+  expr_types.push_back(ExpressionType::EXPRESSION_TYPE_COMPARE_GT);
+  expr_types.push_back(ExpressionType::EXPRESSION_TYPE_COMPARE_LTE);
+  values.push_back(ValueFactory::GetIntegerValue(50));
+  values.push_back(ValueFactory::GetIntegerValue(30));
+
+  // Create index scan desc
+  planner::IndexScanNode::IndexScanDesc index_scan_desc(
+      index,
+      key_column_ids,
+      expr_types,
+      values);
+
+  expression::AbstractExpression *predicate = nullptr;
+
+  // Create plan node.
+  planner::IndexScanNode node(predicate,
+                              column_ids,
+                              data_table.get(),
+                              index_scan_desc);
+
+  auto& txn_manager = concurrency::TransactionManager::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+  std::unique_ptr<executor::ExecutorContext> context(
+      new executor::ExecutorContext(txn));
+
+  // Run the executor
+  executor::IndexScanExecutor executor(&node, context.get());
+  int expected_num_tiles = 2;
+
+  EXPECT_TRUE(executor.Init());
+
+  std::vector<std::unique_ptr<executor::LogicalTile> > result_tiles;
+
+  for (int i = 0; i < expected_num_tiles; i++) {
+    EXPECT_TRUE(executor.Execute());
+    std::unique_ptr<executor::LogicalTile> result_tile(executor.GetOutput());
+    EXPECT_THAT(result_tile, NotNull());
+    result_tiles.emplace_back(result_tile.release());
+  }
+
+  EXPECT_FALSE(executor.Execute());
+  EXPECT_EQ(result_tiles.size(), expected_num_tiles);
+  EXPECT_EQ(result_tiles[0].get()->GetTupleCount(), 4);
+  EXPECT_EQ(result_tiles[1].get()->GetTupleCount(), 5);
+
+  txn_manager.CommitTransaction(txn);
+}
+
+TEST(IndexScanTests, MultiColumnPredicateTest2) {
+  // First, generate the table with index
+  std::unique_ptr<storage::DataTable> data_table(
+      ExecutorTestsUtil::CreateAndPopulateTable());
+
+  // Column ids to be added to logical tile after scan.
+  std::vector<oid_t> column_ids({0, 1, 3});
+
+  //===--------------------------------------------------------------------===//
+  // ATTR 1 > 50
+  //===--------------------------------------------------------------------===//
+
+  auto index = data_table->GetIndex(1);
+  std::vector<oid_t> key_column_ids;
+  std::vector<ExpressionType> expr_types;
+  std::vector<Value> values;
+
+  key_column_ids.push_back(1);
+  expr_types.push_back(ExpressionType::EXPRESSION_TYPE_COMPARE_GT);
+  values.push_back(ValueFactory::GetIntegerValue(50));
+
+  // Create index scan desc
+  planner::IndexScanNode::IndexScanDesc index_scan_desc(
+      index,
+      key_column_ids,
+      expr_types,
+      values);
+
+  expression::AbstractExpression *predicate = nullptr;
+
+  // Create plan node.
+  planner::IndexScanNode node(predicate,
+                              column_ids,
+                              data_table.get(),
+                              index_scan_desc);
+
+  auto& txn_manager = concurrency::TransactionManager::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+  std::unique_ptr<executor::ExecutorContext> context(
+      new executor::ExecutorContext(txn));
+
+  // Run the executor
+  executor::IndexScanExecutor executor(&node, context.get());
+  int expected_num_tiles = 2;
+
+  EXPECT_TRUE(executor.Init());
+
+  std::vector<std::unique_ptr<executor::LogicalTile> > result_tiles;
+
+  for (int i = 0; i < expected_num_tiles; i++) {
+    EXPECT_TRUE(executor.Execute());
+    std::unique_ptr<executor::LogicalTile> result_tile(executor.GetOutput());
+    EXPECT_THAT(result_tile, NotNull());
+    result_tiles.emplace_back(result_tile.release());
+  }
+
+  EXPECT_FALSE(executor.Execute());
+  EXPECT_EQ(result_tiles.size(), expected_num_tiles);
+  EXPECT_EQ(result_tiles[0].get()->GetTupleCount(), 5);
+  EXPECT_EQ(result_tiles[1].get()->GetTupleCount(), 5);
+
+  txn_manager.CommitTransaction(txn);
 }
 
 }  // namespace test
