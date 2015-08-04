cbe196bb9ca61e6e9a6efe982d01ad796f6d6630
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-04 00:06:06 -0400

    Implemented simple logger

--- src/backend/logging/logger.cpp
+++ src/backend/logging/logger.cpp
@@ -18,11 +18,32 @@
 namespace peloton {
 namespace logging {
 
-void Logger::logging_MainLoop(void){
-  for(;;){
-      sleep(3);
-      std::cout << "logging main loop \n";
+Logger& Logger::GetInstance()
+{
+  static Logger logger;
+  return logger;
+}
+
+void Logger::logging_MainLoop(){
+
+  // TODO :: performance optimization
+  for(int i=0; ;i+=2){
+    sleep(3);
+    AddQueue(i);
+    if( i == 20 ) Flush();
   }
+}
+
+void Logger::AddQueue(int element){
+  std::lock_guard<std::mutex> lock(queue_mutex);
+  queue.push_back(element);
+}
+
+void Logger::Flush(){
+  std::lock_guard<std::mutex> lock(queue_mutex);
+  for( auto element : queue )
+    std::cout << "element : " << element << std::endl;
+  queue.clear();
 }
 
 }  // namespace logging
