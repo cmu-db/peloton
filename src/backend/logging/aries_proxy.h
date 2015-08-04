8255a1ea0b72241b22d92dd14d4d527c199cb4d7
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-04 19:51:59 -0400

    Checkpoint -
    Redeisgn the logging design structures
    
    
    Former-commit-id: f610127bcf65d6097d74c1033deab4fcb40436c3

--- src/backend/logging/aries_proxy.h
+++ src/backend/logging/aries_proxy.h
@@ -1,45 +1,51 @@
 /*-------------------------------------------------------------------------
  *
- * logger.h
+ * aries_proxy.h
  * file description
  *
  * Copyright(c) 2015, CMU
  *
- * /peloton/src/backend/logging/logger.h
+ * /peloton/src/backend/logging/aries_proxy.h
  *
  *-------------------------------------------------------------------------
  */
 
 #pragma once
 
-#include "backend/common/types.h"
-#include "backend/logging/logdefs.h"
 #include "backend/logging/logproxy.h"
 #include "backend/logging/logrecord.h"
+
+#include <mutex>
+#include <vector>
 
 namespace peloton {
 namespace logging {
 
 //===--------------------------------------------------------------------===//
-// Logger 
+// Aries Proxy
 //===--------------------------------------------------------------------===//
 
-class Logger{
+// log queue
+static std::vector<LogRecord> buffer;
+
+static std::mutex buffer_mutex;
+
+class AriesProxy : public LogProxy{
 
   public:
-    Logger() = delete;
 
-    Logger(LoggerId logger_id, LogProxy *proxy) 
-    : logger_id(logger_id), proxy(proxy) {};
-    
-    void logging_MainLoop(void);
+    AriesProxy() {}
 
-    void log(LogRecord record);
+    void logging_MainLoop(void) const;
+
+    void log(LogRecord records) const;
 
   private:
-    LoggerId logger_id = INVALID_LOGGER_ID;
 
-    LogProxy *proxy;
+    size_t GetBufferSize() const;
+
+    void Flush() const;
+ 
 };
 
 }  // namespace logging
---
+++ src/backend/logging/logmanager.cpp
@@ -0,0 +1,63 @@
+/*-------------------------------------------------------------------------
+ *
+ * logmanager.cpp
+ * file description
+ *
+ * Copyright(c) 2015, CMU
+ *
+ * /peloton/src/backend/logging/logmanager.cpp
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "backend/logging/logdefs.h"
+#include "backend/logging/logmanager.h"
+#include "backend/logging/logproxy.h"
+#include "backend/logging/aries_proxy.h"
+//#include "backend/logging/peloton_proxy.h"
+
+namespace peloton {
+namespace logging {
+
+LogManager& LogManager::GetInstance(void)
+{
+  static LogManager logManager;
+  return logManager;
+}
+
+void LogManager::StartAriesLogging(void){
+  auto& logManager = GetInstance();
+  auto logger = logManager.GetAriesLogger();
+  logger->logging_MainLoop();
+}
+
+void LogManager::StartPelotonLogging(void){
+  auto& logManager = GetInstance();
+  auto logger = logManager.GetPelotonLogger();
+  logger->logging_MainLoop();
+}
+
+void LogManager::InitAriesLogger(void) {
+  AriesProxy* ariesProxy = new AriesProxy();
+  aries_logger = new Logger(ARIES_LOGGER_ID, ariesProxy);
+}
+
+void LogManager::InitPelotonLogger(void) {
+  // create a proxy here 
+  peloton_logger = new Logger(PELOTON_LOGGER_ID, nullptr);
+}
+
+Logger* LogManager::GetAriesLogger(void) {
+  if(aries_logger==nullptr) InitAriesLogger();
+  return aries_logger;
+}
+
+Logger* LogManager::GetPelotonLogger(void) {
+  if( peloton_logger==nullptr) InitPelotonLogger();
+  return peloton_logger;
+}
+
+}  // namespace logging
+}  // namespace peloton
+
+
