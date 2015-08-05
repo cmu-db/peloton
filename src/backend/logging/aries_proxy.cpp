891b4d96094a5ab3927e3ac5ed79f17b4bb36bd8
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-05 18:35:22 -0400

    Pass the buffer size as a parameter to determine flushing timing
    Added comment for logging
    
    
    Former-commit-id: 01c7f1abc36c3c5cb7bbaf47147bddf8440aa202

--- src/backend/logging/aries_proxy.cpp
+++ src/backend/logging/aries_proxy.cpp
@@ -23,20 +23,32 @@ void AriesProxy::logging_MainLoop() const{
   // TODO :: performance optimization
   for(int i=0;i<50;i+=2){
     sleep(2);
-    if( GetBufferSize() > 2 ) Flush();
+    if( GetBufferSize() > buffer_size ) Flush();
   }
 }
 
+/**
+ * @brief Recording log record
+ * @param log record 
+ */
 void AriesProxy::log(LogRecord record) const{
   std::lock_guard<std::mutex> lock(aries_buffer_mutex);
   aries_buffer.push_back(record);
 }
 
+/**
+ * @brief Get buffer size
+ * @return return the size of buffer
+ */
 size_t AriesProxy::GetBufferSize() const{
   std::lock_guard<std::mutex> lock(aries_buffer_mutex);
   return aries_buffer.size();
 }
 
+/**
+ * TODO ::
+ * @brief flush all record, for now it's just printing out
+ */
 void AriesProxy::Flush() const{
   std::lock_guard<std::mutex> lock(aries_buffer_mutex);
   for( auto record : aries_buffer )
--- src/backend/logging/logmanager.cpp
+++ src/backend/logging/logmanager.cpp
@@ -19,39 +19,75 @@
 namespace peloton {
 namespace logging {
 
+/**
+ * @brief Getting the singleton for log manager
+ * @return thread local log manager
+ */
 LogManager& LogManager::GetInstance(void)
 {
   static LogManager logManager;
   return logManager;
 }
 
-void LogManager::StartAriesLogging(void){
+/**
+ * @brief Start Aries logging
+ * @param buffer size size of the buffer to flush
+ */
+void LogManager::StartAriesLogging(oid_t buffer_size){
   auto& logManager = GetInstance();
+  logManager.InitAriesLogger(buffer_size);
   auto logger = logManager.GetAriesLogger();
   logger->logging_MainLoop();
 }
 
-void LogManager::StartPelotonLogging(void){
+/**
+ * @brief Start Peloton logging
+ * @param buffer size size of the buffer to flush
+ */
+void LogManager::StartPelotonLogging(oid_t buffer_size){
   auto& logManager = GetInstance();
+  logManager.InitPelotonLogger(buffer_size);
   auto logger = logManager.GetPelotonLogger();
   logger->logging_MainLoop();
 }
 
-void LogManager::InitAriesLogger(void) {
-  AriesProxy* aries_proxy = new AriesProxy();
+/**
+ * @brief Initialize Aries logger
+ * @param buffer size size of the buffer to flush
+ */
+void LogManager::InitAriesLogger(oid_t buffer_size) {
+  AriesProxy* aries_proxy = new AriesProxy(buffer_size);
   aries_logger = new Logger(ARIES_LOGGER_ID, aries_proxy);
 }
 
-void LogManager::InitPelotonLogger(void) {
-  PelotonProxy* peloton_proxy = new PelotonProxy();
+/**
+ * @brief Initialize Peloton logger
+ * @param buffer size size of the buffer to flush
+ */
+void LogManager::InitPelotonLogger(oid_t buffer_size) {
+  PelotonProxy* peloton_proxy = new PelotonProxy(buffer_size);
   peloton_logger = new Logger(PELOTON_LOGGER_ID, peloton_proxy);
 }
 
+/**
+ * @brief Get the Aries logger
+ *  NOTE :: buffer size is only needed for logger that runs logging_MainLoop
+ *  otherwise, logger will be initialized without buffer size and it will
+ *  be used for log record
+ * @return aries logger
+ */
 Logger* LogManager::GetAriesLogger(void) {
   if(aries_logger==nullptr) InitAriesLogger();
   return aries_logger;
 }
 
+/**
+ * @brief Get the Peloton logger
+ *  NOTE :: buffer size is only needed for logger that runs logging_MainLoop
+ *  otherwise, logger will be initialized without buffer size and it will
+ *  be used for log record
+ * @return peloton logger
+ */
 Logger* LogManager::GetPelotonLogger(void) {
   if( peloton_logger==nullptr) InitPelotonLogger();
   return peloton_logger;
