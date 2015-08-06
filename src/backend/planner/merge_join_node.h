3efd5f23159a7f441fc38122ef0c33e4b2c3038b
Ming Fang <mindbergh@gmail.com>
2015-08-06 15:18:53 -0400

    add JoinClause struct for merger join plan node

--- src/backend/planner/merge_join_node.h
+++ src/backend/planner/merge_join_node.h
@@ -26,6 +26,24 @@ namespace planner {
 
 class MergeJoinNode : public AbstractJoinPlanNode {
  public:
+  struct JoinClause {
+    JoinClause(const expression::AbstractExpression *left,
+               const expression::AbstractExpression *right,
+               bool reversed)
+    : left_(left), right_(right), reversed_(reversed) {}
+
+    JoinClause(const JoinClause &other) = delete;
+
+    JoinClause(JoinClause &&other)
+    :left_(std::move(other.left_)),
+     right_(std::move(other.right_)),
+     reversed_(other.reversed_) {}
+
+    std::unique_ptr<const expression::AbstractExpression> left_;
+    std::unique_ptr<const expression::AbstractExpression> right_;
+    bool reversed_;
+  };
+
   MergeJoinNode(const MergeJoinNode &) = delete;
   MergeJoinNode &operator=(const MergeJoinNode &) = delete;
   MergeJoinNode(MergeJoinNode &&) = delete;
@@ -33,9 +51,9 @@ class MergeJoinNode : public AbstractJoinPlanNode {
 
   MergeJoinNode(const expression::AbstractExpression *predicate,
                 const ProjectInfo *proj_info,
-                const expression::AbstractExpression *join_clause)
+                std::vector<JoinClause> &join_clauses)
       : AbstractJoinPlanNode(JOIN_TYPE_INVALID, predicate, proj_info),
-        join_clause_(join_clause) {  // FIXME
+        join_clauses_(std::move(join_clauses)) {  // FIXME
    // Nothing to see here...
   }
 
@@ -43,14 +61,14 @@ class MergeJoinNode : public AbstractJoinPlanNode {
     return PLAN_NODE_TYPE_MERGEJOIN;
   }
 
-  const inline expression::AbstractExpression *GetJoinClause() const {
-    return join_clause_.get();
+  const inline std::vector<JoinClause> &GetJoinClauses() const {
+    return join_clauses_;
   }
 
   inline std::string GetInfo() const { return "MergeJoin"; }
 
  private:
-  std::unique_ptr<const expression::AbstractExpression> join_clause_;
+  std::vector<JoinClause> join_clauses_;
 };
 
 }  // namespace planner
