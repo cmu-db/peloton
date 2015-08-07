cd306d5feafc351c001b6cd15527e4e3dc7974c5
Ziqiang Feng <csfzq2009@gmail.com>
2015-08-07 16:34:55 -0400

    Gain some more knowledge about PlanState.targetlist and PlanState.ps_ProjInfo.
    Former-commit-id: e2964b4b6921bf4fbfc922db553d1cced5233dac

--- src/backend/bridge/dml/mapper/mapper_agg.cpp
+++ src/backend/bridge/dml/mapper/mapper_agg.cpp
@@ -1,4 +1,3 @@
-
 #include "backend/bridge/dml/mapper/mapper.h"
 #include "backend/bridge/ddl/schema_transformer.h"
 #include "backend/planner/aggregate_node.h"
@@ -8,35 +7,44 @@
 #include "executor/nodeAgg.h"
 #include "utils/syscache.h"
 
-
 namespace peloton {
 namespace bridge {
 
-
 planner::AbstractPlanNode*
-PlanTransformer::TransformAgg(const AggState *plan_state){
+PlanTransformer::TransformAgg(const AggState *plan_state) {
   const AggState* agg_state = plan_state;
 
-  auto num_aggs = agg_state->numaggs; // number of unique aggregates
+  auto num_aggs = agg_state->numaggs;  // number of unique aggregates
   auto num_phases = agg_state->numphases;
 
   LOG_INFO("Number of (unique) Agg nodes: %d \n", num_aggs);
   LOG_INFO("Number of Agg phases: %d \n", num_phases);
 
-  for(int aggno = 0; aggno < num_aggs; aggno++){
+  for (int aggno = 0; aggno < num_aggs; aggno++) {
     auto transfn_oid = agg_state->peragg[aggno].transfn_oid;
     LOG_INFO("Unique Agg # : %d , transfn_oid : %u \n", aggno, transfn_oid);
   }
 
   auto output_schema = SchemaTransformer::GetSchemaFromTupleDesc(
-          agg_state->ss.ps.ps_ResultTupleSlot->tts_tupleDescriptor);
+      agg_state->ss.ps.ps_ResultTupleSlot->tts_tupleDescriptor);
 
-  auto target_list = BuildTargetList(agg_state->ss.ps.targetlist, output_schema->GetColumnCount());
+  LOG_INFO("ps.targetlist : ");
+  auto target_list = (
+      BuildTargetList(agg_state->ss.ps.targetlist,
+                      output_schema->GetColumnCount()));
 
+  for(auto target : target_list){
+    delete target.second;
+    target.second = nullptr;
+  }
+
+  LOG_INFO("ps.ps_ProjInfo : ");
+  std::unique_ptr<const planner::ProjectInfo> proj_info(
+      BuildProjectInfo(agg_state->ss.ps.ps_ProjInfo,
+                       output_schema->GetColumnCount()));
 
   return nullptr;
 }
-
 
 }
 }
