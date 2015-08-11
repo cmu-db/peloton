ce4b87fca00fa916e5133914ea95a526eb0cf6d8
jarulraj <jarulraj@cs.cmu.edu>
2015-08-11 11:09:14 -0400

    Major refactoring

---
+++ src/backend/bridge/dml/mapper/dml_utils.h
@@ -0,0 +1,47 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// ddl_utils.h
+//
+// Identification: src/backend/bridge/ddl/ddl_utils.h
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "backend/planner/abstract_plan.h"
+#include "backend/bridge/dml/mapper/dml_raw_structures.h"
+
+#include "postgres.h"
+#include "c.h"
+#include "nodes/execnodes.h"
+
+namespace peloton {
+namespace bridge {
+
+//===--------------------------------------------------------------------===//
+// DML UTILS
+//===--------------------------------------------------------------------===//
+
+class DMLUtils {
+ public:
+  DMLUtils(const DMLUtils &) = delete;
+  DMLUtils &operator=(const DMLUtils &) = delete;
+  DMLUtils(DMLUtils &&) = delete;
+  DMLUtils &operator=(DMLUtils &&) = delete;
+
+  static AbstractPlanState *BuildPlanState(
+      AbstractPlanState *root,
+      PlanState *planstate,
+      bool left_child);
+
+  static AbstractPlanState *peloton_prepare_data(
+      PlanState *planstate);
+
+};
+
+}  // namespace bridge
+}  // namespace peloton
