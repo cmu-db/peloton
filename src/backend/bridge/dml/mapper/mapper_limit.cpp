702c93e485c9d26c7f7cba1837004c098ec68f40
jarulraj <jarulraj@cs.cmu.edu>
2015-08-11 18:26:20 -0400

    Added support for limit and lock rows
    Former-commit-id: 0c59a25498b3db52eef79a8deaa46849d418654b

--- src/backend/bridge/dml/mapper/mapper_limit.cpp
+++ src/backend/bridge/dml/mapper/mapper_limit.cpp
@@ -13,10 +13,6 @@
 #include "backend/bridge/dml/mapper/mapper.h"
 #include "backend/planner/limit_plan.h"
 
-extern Datum ExecEvalExprSwitchContext(ExprState *expression,
-                                       ExprContext *econtext, bool *isNull,
-                                       ExprDoneCond *isDone);
-
 namespace peloton {
 namespace bridge {
 
@@ -31,65 +27,19 @@ namespace bridge {
  * @return Pointer to the constructed AbstractPlan
  */
 planner::AbstractPlan *PlanTransformer::TransformLimit(
-    const LimitState *node) {
-  ExprContext *econtext = node->ps.ps_ExprContext;
-  Datum val;
-  bool isNull;
-  int64 limit;
-  int64 offset;
-  bool noLimit;
-  bool noOffset;
+    const LimitPlanState *limit_state) {
 
-  /* Resolve limit and offset */
-  if (node->limitOffset) {
-    val = ExecEvalExprSwitchContext(node->limitOffset, econtext, &isNull, NULL);
-    /* Interpret NULL offset as no offset */
-    if (isNull)
-      offset = 0;
-    else {
-      offset = DatumGetInt64(val);
-      if (offset < 0) {
-        LOG_ERROR("OFFSET must not be negative, offset = %ld", offset);
-      }
-      noOffset = false;
-    }
-  } else {
-    /* No OFFSET supplied */
-    offset = 0;
-  }
+  // TODO: does not do pass down bound to child node
+  // TODO: handle no limit and no offset cases
+  LOG_INFO("Flags :: Limit: %d, Offset: %d", limit_state->noLimit, limit_state->noOffset);
+  LOG_INFO("Limit: %ld, Offset: %ld", limit_state->limit, limit_state->offset);
+  auto plan_node = new planner::LimitPlan(limit_state->limit, limit_state->offset);
 
-  if (node->limitCount) {
-    val = ExecEvalExprSwitchContext(node->limitCount, econtext, &isNull, NULL);
-    /* Interpret NULL count as no limit (LIMIT ALL) */
-    if (isNull) {
-      limit = 0;
-      noLimit = true;
-    } else {
-      limit = DatumGetInt64(val);
-      if (limit < 0) {
-        LOG_ERROR("LIMIT must not be negative, limit = %ld", limit);
-      }
-      noLimit = false;
-    }
-  } else {
-    /* No LIMIT supplied */
-    limit = 0;
-    noLimit = true;
-  }
-
-  /* TODO: does not do pass down bound to child node
-   * In Peloton, they are both unsigned. But both of them cannot be negative,
-   * The is safe */
-  /* TODO: handle no limit and no offset cases, in which the corresponding value
-   * is 0 */
-  LOG_INFO("Flags :: Limit: %d, Offset: %d", noLimit, noOffset);
-  LOG_INFO("Limit: %ld, Offset: %ld", limit, offset);
-  auto plan_node = new planner::LimitPlan(limit, offset);
-
-  /* Resolve child plan */
-  PlanState *subplan_state = outerPlanState(node);
+  // Resolve child plan
+  AbstractPlanState *subplan_state = outerAbstractPlanState(limit_state);
   assert(subplan_state != nullptr);
-  //plan_node->AddChild(PlanTransformer::TransformPlan(subplan_state));
+  plan_node->AddChild(TransformPlan(subplan_state));
+
   return plan_node;
 }
 
