a47757955f7d2d5d8fae131749397f1ded76a24d
Ziqiang Feng <csfzq2009@gmail.com>
2015-08-11 13:20:40 -0400

    Now we can extract all info for typyical aggregate queries.
    Former-commit-id: 9faccac3c2acd26dac7e03b6594d74558542dfd3

--- src/backend/bridge/dml/mapper/mapper_agg.cpp
+++ src/backend/bridge/dml/mapper/mapper_agg.cpp
@@ -1,3 +1,4 @@
+#include "backend/bridge/dml/expr/pg_func_map.h"
 #include "backend/bridge/dml/mapper/mapper.h"
 #include "backend/bridge/ddl/schema_transformer.h"
 #include "backend/planner/aggregateV2_node.h"
@@ -15,18 +16,10 @@ PlanTransformer::TransformAgg(const AggState *plan_state) {
   const AggState* agg_state = plan_state;
   auto agg = reinterpret_cast<const Agg*>(agg_state->ss.ps.plan);
 
-  auto num_aggs = agg_state->numaggs;  // number of unique aggregates
   auto num_phases = agg_state->numphases;
-
-  LOG_INFO("Number of (unique) Agg nodes: %d \n", num_aggs);
   LOG_INFO("Number of Agg phases: %d \n", num_phases);
 
   assert(1 == num_phases);  // When we'll have >1 phases?
-
-  for (int aggno = 0; aggno < num_aggs; aggno++) {
-    auto transfn_oid = agg_state->peragg[aggno].transfn_oid;
-    LOG_INFO("Unique Agg # : %d , transfn_oid : %u \n", aggno, transfn_oid);
-  }
 
   std::unique_ptr<catalog::Schema> output_schema(
       SchemaTransformer::GetSchemaFromTupleDesc(
@@ -44,6 +37,32 @@ PlanTransformer::TransformAgg(const AggState *plan_state) {
   /* Get Aggregate terms */
   std::vector<planner::AggregateV2Node::AggTerm> unique_agg_terms;
 
+  auto num_aggs = agg_state->numaggs;  // number of unique aggregates
+  LOG_INFO("Number of (unique) Agg nodes: %d \n", num_aggs);
+  for (int aggno = 0; aggno < num_aggs; aggno++) {
+    auto transfn_oid = agg_state->peragg[aggno].transfn_oid;
+
+    auto itr = peloton::bridge::kPgFuncMap.find(transfn_oid);
+    if (kPgFuncMap.end() == itr) {
+      LOG_ERROR("Unmapped Transit function Id : %u\n", transfn_oid);
+      return nullptr;
+    }
+
+    // We don't check whether the mapped exprtype is a valid aggregate type here.
+    PltFuncMetaInfo fn_meta = itr->second;
+    // We only take the first argument as input to aggregator
+    GenericExprState *gstate = (GenericExprState *) lfirst(
+        list_head(agg_state->peragg[aggno].aggrefstate->args));
+    auto agg_expr = ExprTransformer::TransformExpr(gstate->arg);
+
+    unique_agg_terms.emplace_back(fn_meta.exprtype, agg_expr);
+
+    LOG_INFO(
+        "Unique Agg # : %d , transfn_oid : %u\n , aggtype = %s \n expr = %s",
+        aggno, transfn_oid, ExpressionTypeToString(fn_meta.exprtype).c_str(),
+        agg_expr->DebugInfo().c_str());
+  }
+
   /* Get Group by columns */
   std::vector<oid_t> groupby_col_ids;
   LOG_INFO("agg.numCols = %d", agg->numCols);
@@ -59,7 +78,8 @@ PlanTransformer::TransformAgg(const AggState *plan_state) {
 
   auto retval = new planner::AggregateV2Node(proj_info.release(),
                                              predicate.release(),
-                                             unique_agg_terms, groupby_col_ids);
+                                             std::move(unique_agg_terms),
+                                             std::move(groupby_col_ids));
 
   // Find children
   auto lchild = TransformPlan(outerPlanState(agg_state));
