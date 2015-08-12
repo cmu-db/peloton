bf75b7fb4ae5fed7053aed459b5ed25d9883c2ec
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-12 08:56:08 -0400

    Added Class LogRecordHeader
    Former-commit-id: 5e4407261897fcf662eb61b1d24f5b4dc30c3e50

--- src/backend/logging/logrecord.cpp
+++ src/backend/logging/logrecord.cpp
@@ -29,9 +29,9 @@ bool LogRecord::SerializeLogRecord(){
   bool status = true;
 
   // Serialize the header of LogRecord
-  SerializeLogRecordHeader(output);
+  log_record_header.SerializeLogRecordHeader(output);
 
-  switch(log_record_type){
+  switch(log_record_header.GetType()){
 
     case LOGRECORD_TYPE_INSERT_TUPLE:
     case LOGRECORD_TYPE_DELETE_TUPLE:
@@ -55,31 +55,8 @@ bool LogRecord::SerializeLogRecord(){
   return status;
 }
 
-/**
- * @brief Serialize LogRecordHeader
- * @param output  
- */
-void LogRecord::SerializeLogRecordHeader(CopySerializeOutput& output){
-  output.WriteEnumInSingleByte(log_record_type);
-  output.WriteShort(db_oid);
-  output.WriteShort(table_oid);
-  output.WriteLong(txn_id);
-}
-
-/**
- * @brief Deserialize LogRecordHeader
- * @param input  
- */
-void LogRecord::DeserializeLogRecordHeader(CopySerializeInput& input){
-  log_record_type = (LogRecordType)(input.ReadEnumInSingleByte());
-  db_oid = (oid_t)(input.ReadShort());
-  table_oid = (oid_t)(input.ReadShort());
-  txn_id = (txn_id_t)(input.ReadLong());
-}
-
-size_t LogRecord::GetLogRecordHeaderSize() {
-  // enum + oid_t + oid_t + long
-  return sizeof(char)+sizeof(int16_t)+sizeof(int16_t)+sizeof(int64_t);
+LogRecordHeader LogRecord::GetHeader() const{
+  return log_record_header;
 }
 
 char* LogRecord::GetSerializedLogRecord() const{
@@ -90,35 +67,11 @@ size_t LogRecord::GetSerializedLogRecordSize() const{
   return serialized_log_record_size;
 }
 
-LogRecordType LogRecord::GetType() const{
-  return log_record_type;
-}
-
-oid_t LogRecord::GetDbId() const{
-  return db_oid;
-}
-
-oid_t LogRecord::GetTableId() const{
-  return table_oid;
-}
-
-txn_id_t LogRecord::GetTxnId() const{
-  return txn_id;
-}
-
-ItemPointer LogRecord::GetItemPointer() const{
-  return itemPointer;
-}
-
 std::ostream& operator<<(std::ostream& os, const LogRecord& record) {
-  os << "#LOG TYPE:" << LogRecordTypeToString(record.GetType());
-  os << " #Db  ID:" << record.GetDbId();
-  os << " #Tb  ID:" << record.GetTableId();
-  os << " #Txn ID:" << record.GetTxnId();
-  os << " #Location :" << record.GetItemPointer().block;
-  os << " " << record.GetItemPointer().offset;
-  os << "\n";
+
+  os << record.GetHeader();
   return os;
+
 }
 
 }  // namespace logging
