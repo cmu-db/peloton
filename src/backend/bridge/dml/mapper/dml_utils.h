008814610f889c60e83fff1962a7086430e8fbd0
Ziqiang Feng <csfzq2009@gmail.com>
2015-08-14 16:14:43 -0400

    Fix SeqScan under new SHM.
    Former-commit-id: e20ff1b37a04cee98feacb47cef6fd4b9f39166d

--- src/backend/bridge/dml/mapper/dml_utils.h
+++ src/backend/bridge/dml/mapper/dml_utils.h
@@ -33,13 +33,11 @@ class DMLUtils {
   DMLUtils(DMLUtils &&) = delete;
   DMLUtils &operator=(DMLUtils &&) = delete;
 
-  static AbstractPlanState *PreparePlanState(
-      AbstractPlanState *root,
-      PlanState *planstate,
-      bool left_child);
+  static AbstractPlanState *PreparePlanState(AbstractPlanState *root,
+                                             PlanState *planstate,
+                                             bool left_child);
 
-  static AbstractPlanState *peloton_prepare_data(
-      PlanState *planstate);
+  static AbstractPlanState *peloton_prepare_data(PlanState *planstate);
 
  private:
 
@@ -55,15 +53,12 @@ class DMLUtils {
   static void PrepareDeleteState(ModifyTablePlanState *info,
                                  ModifyTableState *mt_plan_state);
 
-  static ResultPlanState *PrepareResultState(
-      ResultState *result_plan_state);
+  static ResultPlanState *PrepareResultState(ResultState *result_plan_state);
 
-  static void PrepareAbstractScanState(
-      AbstractScanPlanState* ss_plan_state,
-      const ScanState& ss_state);
+  static void PrepareAbstractScanState(AbstractScanPlanState* ss_plan_state,
+                                       const ScanState& ss_state);
 
-  static SeqScanPlanState *PrepareSeqScanState(
-      SeqScanState *ss_plan_state);
+  static SeqScanPlanState *PrepareSeqScanState(SeqScanState *ss_plan_state);
 
   static IndexScanPlanState *PrepareIndexScanState(
       IndexScanState *iss_plan_state);
@@ -74,21 +69,19 @@ class DMLUtils {
   static BitmapHeapScanPlanState *PrepareBitmapHeapScanState(
       BitmapHeapScanState *bhss_plan_state);
 
-  static LockRowsPlanState *PrepareLockRowsState(
-      LockRowsState *lr_plans_tate);
+  static LockRowsPlanState *PrepareLockRowsState(LockRowsState *lr_plans_tate);
 
-  static LimitPlanState *PrepareLimitState(
-      LimitState *limit_plan_state);
+  static LimitPlanState *PrepareLimitState(LimitState *limit_plan_state);
 
   static MaterialPlanState *PrepareMaterialState(
       MaterialState *material_plan_state);
 
-  static NestLoopPlanState *PrepareNestLoopState(
-      NestLoopState *nl_plan_state);
+  static NestLoopPlanState *PrepareNestLoopState(NestLoopState *nl_plan_state);
 
-  static PelotonProjectionInfo *BuildProjectInfo(
-      ProjectionInfo *proj_info,
-      int column_count);
+  static AggPlanState *PrepareAggPlanState(AggState *agg_plan_state);
+
+  static PelotonProjectionInfo *BuildProjectInfo(ProjectionInfo *proj_info,
+                                                 int column_count);
 
 };
 
