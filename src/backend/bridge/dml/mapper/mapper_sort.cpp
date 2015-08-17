1cab5d957052fbe7446ec33898f355afaa0d5402
Ziqiang Feng <csfzq2009@gmail.com>
2015-08-17 18:09:19 -0400

    Seems we can extract sort key column idx and DESC flags now.
    Former-commit-id: 55ad15a49455523a91014069d0c4e7432aa32732

--- src/backend/bridge/dml/mapper/mapper_sort.cpp
+++ src/backend/bridge/dml/mapper/mapper_sort.cpp
@@ -2,29 +2,28 @@
 #include "backend/bridge/ddl/schema_transformer.h"
 #include "backend/planner/order_by_plan.h"
 
-#include "utils/tuplesort.h"
+
 
 namespace peloton {
 namespace bridge {
 
 planner::AbstractPlan*
-PlanTransformer::TransformSort(const SortPlanState *plan_state){
+PlanTransformer::TransformSort(const SortPlanState *plan_state) {
 
   auto sort = plan_state->sort;
 
   int numCols = sort->numCols;
   AttrNumber* sortColIdx = sort->sortColIdx;
+  bool *reverse_flags = plan_state->reverse_flags;
 
-  for (int i = 0; i < numCols; i++) {
-    LOG_INFO("Sort Col Idx : %d, Sort OperatorOid : %u", sortColIdx[i],
-             sort->sortOperators[i]);
+  for(int i=0; i < numCols; i++){
+    LOG_INFO("Sort Col Idx : %u , reverse : %u", sortColIdx[i], reverse_flags[i]);
   }
+
 
   auto lchild = TransformPlan(outerAbstractPlanState(plan_state));
   return lchild;
 //  retval->AddChild(lchild);
-
-
 
 }
 
