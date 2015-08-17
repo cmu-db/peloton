69194693ea1bce9800342a808a32956ed4341b53
Ziqiang Feng <csfzq2009@gmail.com>
2015-08-17 15:29:15 -0400

    Getting information from SortState.

--- src/backend/bridge/dml/mapper/mapper_sort.cpp
+++ src/backend/bridge/dml/mapper/mapper_sort.cpp
@@ -2,16 +2,32 @@
 #include "backend/bridge/ddl/schema_transformer.h"
 #include "backend/planner/order_by_node.h"
 
+#include "utils/tuplesort.h"
 
 namespace peloton {
-namespace bridge{
+namespace bridge {
 
 planner::AbstractPlanNode*
-PlanTransformer::TransformSort(const SortState *plan_state){
+PlanTransformer::TransformSort(const SortState *plan_state) {
+  auto sort_state = plan_state;
+  auto sort = reinterpret_cast<const Sort*>(plan_state->ss.ps.plan);
 
-  return nullptr;
+  int numCols = sort->numCols;
+  AttrNumber* sortColIdx = sort->sortColIdx;
+
+  for (int i = 0; i < numCols; i++) {
+    LOG_INFO("Sort Col Idx : %d, Sort OperatorOid : %u", sortColIdx[i],
+             sort->sortOperators[i]);
+  }
+
+  (void) sort_state;
+
+  auto lchild = TransformPlan(outerPlanState(sort_state));
+
+  // return the child temporarily
+  return lchild;
+
 }
-
 
 }
 }
