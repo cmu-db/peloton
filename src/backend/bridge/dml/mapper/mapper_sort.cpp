75e5bcd2bc632c14e946ce3bfeca2f945650a320
Ziqiang Feng <csfzq2009@gmail.com>
2015-08-17 18:33:34 -0400

    Order By seems to be working for simple cases: No expression in sort keys; No projection / reorder of columns.
    Former-commit-id: 74c809809c7f694c6df8b9e131dbbe739aa8ee35

--- src/backend/bridge/dml/mapper/mapper_sort.cpp
+++ src/backend/bridge/dml/mapper/mapper_sort.cpp
@@ -2,8 +2,6 @@
 #include "backend/bridge/ddl/schema_transformer.h"
 #include "backend/planner/order_by_plan.h"
 
-
-
 namespace peloton {
 namespace bridge {
 
@@ -16,14 +14,26 @@ PlanTransformer::TransformSort(const SortPlanState *plan_state) {
   AttrNumber* sortColIdx = sort->sortColIdx;
   bool *reverse_flags = plan_state->reverse_flags;
 
-  for(int i=0; i < numCols; i++){
-    LOG_INFO("Sort Col Idx : %u , reverse : %u", sortColIdx[i], reverse_flags[i]);
+  std::vector<oid_t> sort_keys;
+  std::vector<bool> descend_flags;
+  std::vector<oid_t> output_col_ids;
+  storage::AbstractBackend *backend = new storage::VMBackend();
+
+  for (int i = 0; i < numCols; i++) {
+    LOG_INFO("Sort Col Idx : %u , reverse : %u", sortColIdx[i],
+             reverse_flags[i]);
+    sort_keys.push_back(
+        static_cast<oid_t>(AttrNumberGetAttrOffset(sortColIdx[i])));
+    descend_flags.push_back(reverse_flags[i]);
   }
 
+  auto retval = new planner::OrderByPlan(sort_keys, descend_flags,
+                                         output_col_ids, backend);
 
   auto lchild = TransformPlan(outerAbstractPlanState(plan_state));
-  return lchild;
-//  retval->AddChild(lchild);
+  retval->AddChild(lchild);
+
+  return retval;
 
 }
 
