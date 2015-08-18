8970d72e33af1fb85f5aa2703e18591824fcfc51
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-18 18:28:49 -0400

    Checkpoint

--- tests/logging/logging_tests_util.h
+++ tests/logging/logging_tests_util.h
@@ -8,24 +8,40 @@ namespace peloton {
 
 namespace catalog {
 class Column;
+class Schema;
+}
+namespace storage {
+class Tuple;
+class DataTable;
 }
 
 namespace test {
 
 class LoggingTestsUtil{
 public:
+  static void WritingSimpleLog(oid_t db_oid, oid_t table_oid);
+
+  static void CreateDatabaseAndTable(oid_t db_oid, oid_t table_oid);
+
+  static void DropDatabaseAndTable(oid_t db_oid, oid_t table_oid);
+
+  static void CheckTuples(oid_t db_oid, oid_t table_oid);
+
+private:
+
   static void CreateDatabase(oid_t db_oid);
 
   static void DropDatabase(oid_t db_oid);
 
-  static void CreateTable(oid_t db_oid,
-                          oid_t table_oid,
-                          std::string table_name);
-
-  static void DropTable(oid_t db_oid,
-                        oid_t table_oid);
+  static storage::DataTable* CreateSimpleTable(oid_t db_oid, oid_t table_oid);
 
   static std::vector<catalog::Column> CreateSimpleColumns(void);
+
+  static std::vector<storage::Tuple*> CreateSimpleTuples(catalog::Schema* schema);
+
+  static void InsertTuples(storage::DataTable* table);
+
 };
+
 }  // End test namespace
 }  // End peloton namespace
