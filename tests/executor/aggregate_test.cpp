2a6bd9b51fae5e2226dd1adf8d18c95ec3293ca0
Ziqiang Feng <csfzq2009@gmail.com>
2015-08-18 19:47:31 -0400

    count distinct seems to pass simple test cases.
    Former-commit-id: 97b989baf422ee3c625ae91fa43bb3e502a9332c

--- tests/executor/aggregate_test.cpp
+++ tests/executor/aggregate_test.cpp
@@ -41,7 +41,7 @@ using ::testing::Return;
 namespace peloton {
 namespace test {
 
-TEST(AggregateTests, SortDistinctTest) {
+TEST(AggregateTests, SortedDistinctTest) {
 
   /*
    * SELECT d, a, b, c FROM table GROUP BY a, b, c, d;
@@ -91,8 +91,8 @@ TEST(AggregateTests, SortDistinctTest) {
 
   // OK) Create the plan node
   planner::AggregatePlan node(proj_info, predicate, std::move(agg_terms),
-                                std::move(group_by_columns),
-                                output_table_schema, AGGREGATE_TYPE_SORT);
+                              std::move(group_by_columns), output_table_schema,
+                              AGGREGATE_TYPE_SORT);
 
   // Create and set up executor
   auto &txn_manager = concurrency::TransactionManager::GetInstance();
@@ -137,7 +137,7 @@ TEST(AggregateTests, SortDistinctTest) {
 
 }
 
-TEST(AggregateTests, SortSumGroupByTest) {
+TEST(AggregateTests, SortedSumGroupByTest) {
   /*
    * SELECT a, SUM(b) from table GROUP BY a;
    */
@@ -170,7 +170,8 @@ TEST(AggregateTests, SortSumGroupByTest) {
 
   // 3) Set up unique aggregates
   std::vector<planner::AggregatePlan::AggTerm> agg_terms;
-  planner::AggregatePlan::AggTerm sumb(EXPRESSION_TYPE_AGGREGATE_SUM, expression::TupleValueFactory(0, 1));
+  planner::AggregatePlan::AggTerm sumb(EXPRESSION_TYPE_AGGREGATE_SUM,
+                                       expression::TupleValueFactory(0, 1));
   agg_terms.push_back(sumb);
 
   // 4) Set up predicate (empty)
@@ -187,8 +188,8 @@ TEST(AggregateTests, SortSumGroupByTest) {
 
   // OK) Create the plan node
   planner::AggregatePlan node(proj_info, predicate, std::move(agg_terms),
-                                std::move(group_by_columns),
-                                output_table_schema, AGGREGATE_TYPE_SORT);
+                              std::move(group_by_columns), output_table_schema,
+                              AGGREGATE_TYPE_SORT);
 
   // Create and set up executor
   auto &txn_manager = concurrency::TransactionManager::GetInstance();
@@ -259,8 +260,10 @@ TEST(AggregateTests, SortSumMaxGroupByTest) {
 
   // 3) Set up unique aggregates
   std::vector<planner::AggregatePlan::AggTerm> agg_terms;
-  planner::AggregatePlan::AggTerm sumb (EXPRESSION_TYPE_AGGREGATE_SUM, expression::TupleValueFactory(0, 1));
-  planner::AggregatePlan::AggTerm maxc (EXPRESSION_TYPE_AGGREGATE_MAX, expression::TupleValueFactory(0, 2));
+  planner::AggregatePlan::AggTerm sumb(EXPRESSION_TYPE_AGGREGATE_SUM,
+                                       expression::TupleValueFactory(0, 1));
+  planner::AggregatePlan::AggTerm maxc(EXPRESSION_TYPE_AGGREGATE_MAX,
+                                       expression::TupleValueFactory(0, 2));
   agg_terms.push_back(sumb);
   agg_terms.push_back(maxc);
 
@@ -278,8 +281,8 @@ TEST(AggregateTests, SortSumMaxGroupByTest) {
 
   // OK) Create the plan node
   planner::AggregatePlan node(proj_info, predicate, std::move(agg_terms),
-                                std::move(group_by_columns),
-                                output_table_schema, AGGREGATE_TYPE_SORT);
+                              std::move(group_by_columns), output_table_schema,
+                              AGGREGATE_TYPE_SORT);
 
   // Create and set up executor
   auto &txn_manager = concurrency::TransactionManager::GetInstance();
@@ -370,8 +373,8 @@ TEST(AggregateTests, HashDistinctTest) {
 
   // OK) Create the plan node
   planner::AggregatePlan node(proj_info, predicate, std::move(agg_terms),
-                                std::move(group_by_columns),
-                                output_table_schema, AGGREGATE_TYPE_HASH);
+                              std::move(group_by_columns), output_table_schema,
+                              AGGREGATE_TYPE_HASH);
 
   // Create and set up executor
   auto &txn_manager = concurrency::TransactionManager::GetInstance();
@@ -441,7 +444,8 @@ TEST(AggregateTests, HashSumGroupByTest) {
 
   // 3) Set up unique aggregates
   std::vector<planner::AggregatePlan::AggTerm> agg_terms;
-  planner::AggregatePlan::AggTerm sumC (EXPRESSION_TYPE_AGGREGATE_SUM, expression::TupleValueFactory(0, 2));
+  planner::AggregatePlan::AggTerm sumC(EXPRESSION_TYPE_AGGREGATE_SUM,
+                                       expression::TupleValueFactory(0, 2));
   agg_terms.push_back(sumC);
 
   // 4) Set up predicate (empty)
@@ -458,8 +462,8 @@ TEST(AggregateTests, HashSumGroupByTest) {
 
   // OK) Create the plan node
   planner::AggregatePlan node(proj_info, predicate, std::move(agg_terms),
-                                std::move(group_by_columns),
-                                output_table_schema, AGGREGATE_TYPE_HASH);
+                              std::move(group_by_columns), output_table_schema,
+                              AGGREGATE_TYPE_HASH);
 
   // Create and set up executor
   auto &txn_manager = concurrency::TransactionManager::GetInstance();
@@ -490,7 +494,107 @@ TEST(AggregateTests, HashSumGroupByTest) {
   std::unique_ptr<executor::LogicalTile> result_tile(executor.GetOutput());
   EXPECT_GE(3, result_tile->GetTupleCount());
 
-  std::cout << *result_tile;
+//  std::cout << *result_tile;
+
+}
+
+TEST(AggregateTests, HashCountDistinctGroupByTest) {
+  /*
+   * SELECT a, COUNT(b), COUNT(DISTINCT b) from table group by a
+   */
+  const int tuple_count = TESTS_TUPLES_PER_TILEGROUP;
+
+  // Create a table and wrap it in logical tiles
+  std::unique_ptr<storage::DataTable> data_table(
+      ExecutorTestsUtil::CreateTable(tuple_count, false));
+  ExecutorTestsUtil::PopulateTable(data_table.get(), 2 * tuple_count, false,
+  true,
+                                   true);
+
+  std::unique_ptr<executor::LogicalTile> source_logical_tile1(
+      executor::LogicalTileFactory::WrapTileGroup(data_table->GetTileGroup(0)));
+
+  std::unique_ptr<executor::LogicalTile> source_logical_tile2(
+      executor::LogicalTileFactory::WrapTileGroup(data_table->GetTileGroup(1)));
+
+  // (1-5) Setup plan node
+
+  // 1) Set up group-by columns
+  std::vector<oid_t> group_by_columns = { 0 };
+
+  // 2) Set up project info
+  planner::ProjectInfo::DirectMapList direct_map_list = { { 0, { 0, 0 } }, { 1,
+      { 1, 0 } }, { 2, { 1, 1 } } };
+
+  auto proj_info = new planner::ProjectInfo(planner::ProjectInfo::TargetList(),
+                                            std::move(direct_map_list));
+
+  // 3) Set up unique aggregates
+  std::vector<planner::AggregatePlan::AggTerm> agg_terms;
+  planner::AggregatePlan::AggTerm countB(EXPRESSION_TYPE_AGGREGATE_COUNT,
+                                         expression::TupleValueFactory(0, 1),
+                                         false);  // Flag distinct
+  planner::AggregatePlan::AggTerm countDistinctB(
+      EXPRESSION_TYPE_AGGREGATE_COUNT, expression::TupleValueFactory(0, 1),
+      true);  // Flag distinct
+  agg_terms.push_back(countB);
+  agg_terms.push_back(countDistinctB);
+
+  // 4) Set up predicate (empty)
+  expression::AbstractExpression* predicate = nullptr;
+
+  // 5) Create output table schema
+  auto data_table_schema = data_table.get()->GetSchema();
+  std::vector<oid_t> set = { 0, 1, 1};
+  std::vector<catalog::Column> columns;
+  for (auto column_index : set) {
+    columns.push_back(data_table_schema->GetColumn(column_index));
+  }
+  auto output_table_schema = new catalog::Schema(columns);
+
+  // OK) Create the plan node
+  planner::AggregatePlan node(proj_info, predicate, std::move(agg_terms),
+                              std::move(group_by_columns), output_table_schema,
+                              AGGREGATE_TYPE_HASH);
+
+  // Create and set up executor
+  auto &txn_manager = concurrency::TransactionManager::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+  std::unique_ptr<executor::ExecutorContext> context(
+      new executor::ExecutorContext(txn));
+
+  executor::AggregateExecutor executor(&node, context.get());
+  MockExecutor child_executor;
+  executor.AddChild(&child_executor);
+
+  EXPECT_CALL(child_executor, DInit()).WillOnce(Return(true));
+
+  EXPECT_CALL(child_executor, DExecute()).WillOnce(Return(true)).WillOnce(
+      Return(true)).WillOnce(Return(false));
+
+  EXPECT_CALL(child_executor, GetOutput()).WillOnce(
+      Return(source_logical_tile1.release())).WillOnce(
+      Return(source_logical_tile2.release()));
+
+  EXPECT_TRUE(executor.Init());
+
+  EXPECT_TRUE(executor.Execute());
+
+  txn_manager.CommitTransaction(txn);
+
+  /* Verify result */
+  std::unique_ptr<executor::LogicalTile> result_tile(executor.GetOutput());
+  EXPECT_TRUE(result_tile.get() != nullptr);
+  EXPECT_TRUE(
+      result_tile->GetValue(0, 0).OpEquals(ValueFactory::GetIntegerValue(0))
+          .IsTrue());
+  EXPECT_TRUE(
+      result_tile->GetValue(0, 1).OpEquals(ValueFactory::GetIntegerValue(10))
+          .IsTrue());
+
+  EXPECT_TRUE(
+      result_tile->GetValue(0, 2).OpEquals(ValueFactory::GetIntegerValue(3))
+          .IsTrue());
 
 }
 
