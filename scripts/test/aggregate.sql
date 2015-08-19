50574ad575ca4bf814f9095f6f23ec5eca93e494
Ziqiang Feng <csfzq2009@gmail.com>
2015-08-18 20:04:38 -0400

    Simple agg(distinct x) seems to be working in Psql frontend.
    Former-commit-id: 513f82b6637b37793378e5722fd11bdc8ec55d51

--- scripts/test/aggregate.sql
+++ scripts/test/aggregate.sql
@@ -22,3 +22,13 @@ select b, max(a) from fooagg group by b having max(a) > 4;
 
 select b, max(c) from fooagg group by b;
 select b, max(c) from fooagg group by b having max(c) > 2;
+
+
+-- simple distinct
+insert into fooagg values (6, 11, 1.1, '#6');
+insert into fooagg values (7, 11, 1.2, '#7');
+
+select * from fooagg;
+
+select b, count(c), count(distinct c) from fooagg group by b;
+select b, sum(c), sum(distinct c) from fooagg group by b;
