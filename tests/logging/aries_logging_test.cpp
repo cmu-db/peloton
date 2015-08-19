524a6d66a147baa288d52ed2025aab01d79debe7
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-19 09:16:56 -0400

    Checkpoint - CheckTuples is kind of working
    Also - Refactoring & clean up
    
    
    Former-commit-id: 74218dca599b3bc207046491e708c58990ad91a1

--- tests/logging/aries_logging_test.cpp
+++ tests/logging/aries_logging_test.cpp
@@ -1,9 +1,7 @@
 #include "gtest/gtest.h"
 
-#include "backend/logging/logmanager.h"
 #include "logging/logging_tests_util.h"
-
-#include <thread>
+#include "backend/common/logger.h"
 
 namespace peloton {
 namespace test {
@@ -14,85 +12,20 @@ namespace test {
 
 TEST(AriesLoggingTest, logging_start_end) {
 
- //FIXME
+ //FIXME :: Hard coded file path
  std::string filename = "/home/parallels/git/peloton/build/aries.log";
+
  if( std::remove(filename.c_str()) < 0 ){
    LOG_ERROR("Failed to remove aries logfile"); 
  }
-   
- //TODO :: Make it function?
- {
-   // start a thread for logging
-   auto& logManager = logging::LogManager::GetInstance();
-   logManager.SetMainLoggingType(LOGGING_TYPE_ARIES);
-   std::thread thread(&logging::LogManager::StandbyLogging, 
-       &logManager, 
-       logManager.GetMainLoggingType());
 
-   // When the frontend logger gets ready to logging,
-   // start logging
-   while(1){
-     if( logManager.GetLoggingStatus() == LOGGING_STATUS_TYPE_STANDBY){
-       logManager.StartLogging();
-       break;
-     }
-   }
+ // writing simple log file
+ LoggingTestsUtil::PrepareLogFile();
 
-   LoggingTestsUtil::WritingSimpleLog(20000, 10000);
+ sleep(1);
 
-   sleep(1);
-
-   if( logManager.EndLogging() ){
-     thread.join();
-   }else{
-     LOG_ERROR("Failed to terminate logging thread"); 
-   }
- }
-
-  sleep(1);
-
-  //TODO :: Make it function?
-  {
-    LoggingTestsUtil::CreateDatabaseAndTable(20000, 10000);
-
-    // start a thread for logging
-    auto& logManager = logging::LogManager::GetInstance();
-    logManager.SetMainLoggingType(LOGGING_TYPE_ARIES);
-    std::thread thread(&logging::LogManager::StandbyLogging, 
-                       &logManager, 
-                       logManager.GetMainLoggingType());
-
-    // When the frontend logger gets ready to logging,
-    // start logging
-    while(1){
-      if( logManager.GetLoggingStatus() == LOGGING_STATUS_TYPE_STANDBY){
-        // Create corresponding database and table
-        logManager.StartLogging(); // Change status to recovery
-        break;
-      }
-    }
-
-    //wait recovery
-    while(1){
-      // escape when recovery is done
-      if( logManager.GetLoggingStatus() == LOGGING_STATUS_TYPE_ONGOING){
-        break;
-      }
-    }
-
-    // Check the tuples
-    LoggingTestsUtil::CheckTuples(20000,10000);
-
-    // Check the next oid
-    LoggingTestsUtil::CheckNextOid();
-
-    if( logManager.EndLogging() ){
-      thread.join();
-    }else{
-      LOG_ERROR("Failed to terminate logging thread"); 
-    }
-    LoggingTestsUtil::DropDatabaseAndTable(20000, 10000);
-  }
+ // recover database and check the tuples
+ LoggingTestsUtil::CheckTupleAfterRecovery();
 
 }
 
