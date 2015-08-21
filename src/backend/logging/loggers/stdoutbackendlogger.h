e83cf7c82d625094faf018b16033a4dafc945d82
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-21 09:50:31 -0400

    Checkpoint - Remove useless sleep commands, improve logging structure

--- src/backend/logging/loggers/stdoutbackendlogger.h
+++ src/backend/logging/loggers/stdoutbackendlogger.h
@@ -33,19 +33,9 @@ class StdoutBackendLogger : public BackendLogger{
 
     void Update(LogRecord* record);
 
-    void Commit(void);
-
-    void Truncate(oid_t offset);
-
-    LogRecord* GetLogRecord(oid_t offset);
-
   private:
 
     StdoutBackendLogger(){ logging_type = LOGGING_TYPE_STDOUT;}
-
-    std::vector<LogRecord*> stdout_local_queue;
-
-    std::mutex stdout_local_queue_mutex;
 
 };
 
