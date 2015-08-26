6798b45982d936dccc0b1c6c1b95bf4f9f18acde
Ming Fang <mindbergh@gmail.com>
2015-08-26 10:01:43 -0400

    fix logging for isdelete is incorrect problem
    Former-commit-id: eb892db47f7f0577a9ec2171b4216bd13041c4dc

--- src/backend/storage/tile_group_header.h
+++ src/backend/storage/tile_group_header.h
@@ -243,9 +243,18 @@ class TileGroupHeader {
   bool IsDeletable(const oid_t tuple_slot_id,
                    __attribute__((unused))         txn_id_t txn_id,
                    __attribute__((unused))         cid_t at_lcid) {
+    txn_id_t tuple_txn_id = GetTransactionId(tuple_slot_id);
+    cid_t tuple_begin_cid = GetBeginCommitId(tuple_slot_id);
     cid_t tuple_end_cid = GetEndCommitId(tuple_slot_id);
 
-    return tuple_end_cid == MAX_CID;
+    bool deletable = tuple_end_cid == MAX_CID;
+
+    LOG_INFO(
+        "<%p, %u> :(vtid, vbeg, vend) = (%lu, %lu, %lu), (tid, lcid) = (%lu, %lu), deletable = %d",
+        this, tuple_slot_id, tuple_txn_id, tuple_begin_cid, tuple_end_cid,
+        txn_id, at_lcid, deletable);
+
+    return deletable;
   }
 
   void PrintVisibility(txn_id_t txn_id, cid_t at_cid);
