aab0a18cbc16e6cc439849fa6bb81e2193e44008
jarulraj <jarulraj@cs.cmu.edu>
2015-08-27 11:52:08 -0400

    Went over the utility functions

--- src/backend/logging/loggers/aries_frontend_logger.h
+++ src/backend/logging/loggers/aries_frontend_logger.h
@@ -20,6 +20,7 @@
 namespace peloton {
 namespace logging {
 
+// TODO: Why is this global ?
 static std::vector<LogRecord*> aries_global_queue;
 
 //===--------------------------------------------------------------------===//
@@ -44,62 +45,75 @@ class AriesFrontendLogger : public FrontendLogger{
   // Recovery
   //===--------------------------------------------------------------------===//
 
-  void Recovery(void);
+  void DoRecovery(void);
 
-  LogRecordType GetNextLogRecordType(void);
+  void InsertTuple(concurrency::Transaction* recovery_txn);
 
-  size_t LogFileSize();
+  void DeleteTuple(concurrency::Transaction* recovery_txn);
 
-  bool IsFileBroken(size_t size_to_read);
+  void UpdateTuple(concurrency::Transaction* recovery_txn);
+
+  void AddTransactionToRecoveryTable(void);
+
+  void RemoveTransactionFromRecoveryTable(void);
+
+  void MoveCommittedTuplesToRecoveryTxn(concurrency::Transaction* recovery_txn);
+
+  void MoveTuples(concurrency::Transaction* destination,
+                  concurrency::Transaction* source);
+
+  void AbortTuplesFromRecoveryTable(void);
+
+  void AbortTransactionInRecoveryTable();
+
+  void AbortTuples(concurrency::Transaction* txn);
+
+  //===--------------------------------------------------------------------===//
+  // Utility functions
+  //===--------------------------------------------------------------------===//
+
+  size_t GetLogFileSize();
+
+  bool IsFileTruncated(size_t size_to_read);
 
   size_t GetNextFrameSize(void);
 
   size_t GetLogRecordCount() const;
 
-  bool ReadTxnRecord(TransactionRecord &txnRecord);
+  LogRecordType GetNextLogRecordType(void);
 
-  bool ReadTupleRecordHeader(TupleRecord& tupleRecord);
+  bool ReadTransactionRecordHeader(TransactionRecord &txn_record);
+
+  bool ReadTupleRecordHeader(TupleRecord& tuple_record);
 
   storage::Tuple* ReadTupleRecordBody(catalog::Schema* schema,
                                       Pool *pool);
 
+  // Wrappers
   storage::DataTable* GetTable(TupleRecord tupleRecord);
 
   storage::TileGroup* GetTileGroup(oid_t tile_group_id);
 
-  void MoveTuples(concurrency::Transaction* destination,
-                  concurrency::Transaction* source);
-
-  void AbortTuples(concurrency::Transaction* txn);
-
-  void AbortTxnInRecoveryTable();
-
-  void InsertTuple(concurrency::Transaction* recovery_txn);
-  void DeleteTuple(concurrency::Transaction* recovery_txn);
-  void UpdateTuple(concurrency::Transaction* recovery_txn);
-
-  void AddTxnToRecoveryTable(void);
-  void RemoveTxnFromRecoveryTable(void);
-  void MoveCommittedTuplesToRecoveryTxn(concurrency::Transaction* recovery_txn);
-  void AbortTuplesFromRecoveryTable(void);
+ private:
 
   //===--------------------------------------------------------------------===//
   // Member Variables
   //===--------------------------------------------------------------------===//
 
   // FIXME :: Hard coded file name
-  std::string filename = "aries.log";
+  std::string file_name = "aries.log";
 
   // File pointer and descriptor
   FILE* log_file;
-
   int log_file_fd;
 
-  // Txn table
+  // Txn table during recovery
   std::map<txn_id_t, concurrency::Transaction *> recovery_txn_table;
 
   // Keep tracking max oid for setting next_oid in manager
+  // For active processing after recovery
   oid_t max_oid = INVALID_OID;
+
 };
 
 }  // namespace logging
