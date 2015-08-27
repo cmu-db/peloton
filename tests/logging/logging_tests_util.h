c4eb47d3ad2d3cbcdb7ce704af1e0af913588f60
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-27 16:39:01 -0400

    Test code refactoring

--- tests/logging/logging_tests_util.h
+++ tests/logging/logging_tests_util.h
@@ -21,27 +21,31 @@ class LoggingTestsUtil{
 
 public:
 
+  /////////////////////////////////////////////////////////////////////
+  // PREPARE LOG FILE
+  /////////////////////////////////////////////////////////////////////
+
   static bool PrepareLogFile(LoggingType logging_type);
+
+  static size_t GetLogFileSize(std::string file_name);
+
+  /////////////////////////////////////////////////////////////////////
+  // CHECK RECOVERY
+  /////////////////////////////////////////////////////////////////////
 
   static void CheckAriesRecovery(void);
 
   static void CheckPelotonRecovery(void);
 
-private:
-
-  static void WritingSimpleLog(oid_t db_oid, oid_t table_oid, LoggingType logging_type);
-
   static void CheckTupleCount(oid_t db_oid, oid_t table_oid);
 
-  static void CreateDatabaseAndTable(oid_t db_oid, oid_t table_oid);
+private:
 
-  static void CreateDatabase(oid_t db_oid);
+  /////////////////////////////////////////////////////////////////////
+  // WRITING LOG RECORD
+  /////////////////////////////////////////////////////////////////////
 
-  static storage::DataTable* CreateSimpleTable(oid_t db_oid, oid_t table_oid);
-
-  static std::vector<catalog::Column> CreateSimpleColumns(void);
-
-  static std::vector<storage::Tuple*> CreateSimpleTuple(catalog::Schema* schema, oid_t num_of_tuples);
+  static void WritingSimpleLog(oid_t db_oid, oid_t table_oid, LoggingType logging_type);
 
   static void ParallelWriting(storage::DataTable* table);
 
@@ -50,6 +54,24 @@ private:
   static void DeleteTuples(storage::DataTable* table, ItemPointer location, bool committed);
 
   static void UpdateTuples(storage::DataTable* table, ItemPointer location, bool committed);
+  
+  /////////////////////////////////////////////////////////////////////
+  // CREATE PELOTON OBJECT 
+  /////////////////////////////////////////////////////////////////////
+
+  static void CreateDatabase(oid_t db_oid);
+
+  static void CreateDatabaseAndTable(oid_t db_oid, oid_t table_oid);
+
+  static storage::DataTable* CreateSimpleTable(oid_t db_oid, oid_t table_oid);
+
+  static std::vector<catalog::Column> CreateSimpleColumns(void);
+
+  static std::vector<storage::Tuple*> CreateSimpleTuple(catalog::Schema* schema, oid_t num_of_tuples);
+
+  /////////////////////////////////////////////////////////////////////
+  // DROP PELOTON OBJECT
+  /////////////////////////////////////////////////////////////////////
 
   static void DropDatabaseAndTable(oid_t db_oid, oid_t table_oid);
 
