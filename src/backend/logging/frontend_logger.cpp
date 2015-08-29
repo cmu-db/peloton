979aaa569bd9b9f30ee19c87ff28e207d6f1eac3
JinwoongKim <jinwoongkim@cmu.edu>
2015-08-29 04:32:31 -0400

    Peloton logging check point & refactoring

--- src/backend/logging/frontend_logger.cpp
+++ src/backend/logging/frontend_logger.cpp
@@ -11,6 +11,7 @@
  */
 
 #include "backend/common/logger.h"
+#include "backend/logging/log_manager.h"
 #include "backend/logging/frontend_logger.h"
 #include "backend/logging/loggers/aries_frontend_logger.h"
 #include "backend/logging/loggers/peloton_frontend_logger.h"
@@ -18,8 +19,7 @@
 namespace peloton {
 namespace logging {
 
-/**
- * @brief Return the frontend logger based on logging type
+/** * @brief Return the frontend logger based on logging type
  * @param logging type can be stdout(debug), aries, peloton
  */
 FrontendLogger* FrontendLogger::GetFrontendLogger(LoggingType logging_type){
@@ -40,6 +40,115 @@ FrontendLogger* FrontendLogger::GetFrontendLogger(LoggingType logging_type){
   }
 
   return frontendLogger;
+}
+
+
+/**
+ * @brief MainLoop
+ */
+void FrontendLogger::MainLoop(void) {
+
+  auto& logManager = LogManager::GetInstance();
+
+  /////////////////////////////////////////////////////////////////////
+  // STANDBY MODE
+  /////////////////////////////////////////////////////////////////////
+
+  LOG_TRACE("Frontendlogger] Standby Mode");
+  // Standby before we are ready to recovery
+  while(logManager.GetStatus(LOGGING_TYPE_PELOTON) == LOGGING_STATUS_TYPE_STANDBY ){
+    sleep(1);
+  }
+
+  // Do recovery if we can, otherwise terminate
+  switch(logManager.GetStatus(LOGGING_TYPE_PELOTON)){
+    case LOGGING_STATUS_TYPE_RECOVERY:{
+      LOG_TRACE("Frontendlogger] Recovery Mode");
+
+      /////////////////////////////////////////////////////////////////////
+      // RECOVERY MODE
+      /////////////////////////////////////////////////////////////////////
+
+      // First, do recovery if needed
+      DoRecovery();
+
+      // Now, enable active logging
+      logManager.SetLoggingStatus(LOGGING_TYPE_PELOTON, LOGGING_STATUS_TYPE_LOGGING);
+
+      break;
+    }
+
+    case LOGGING_STATUS_TYPE_LOGGING:{
+      LOG_TRACE("Frontendlogger] Ongoing Mode");
+    }
+    break;
+
+    default:
+    break;
+  }
+
+  /////////////////////////////////////////////////////////////////////
+  // LOGGING MODE
+  /////////////////////////////////////////////////////////////////////
+
+  // Periodically, wake up and do logging
+  while(logManager.GetStatus(LOGGING_TYPE_PELOTON) == LOGGING_STATUS_TYPE_LOGGING){
+    sleep(1);
+
+    // Collect LogRecords from all backend loggers
+    CollectLogRecord();
+
+    // Flush the data to the file
+    Flush();
+  }
+
+  /////////////////////////////////////////////////////////////////////
+  // TERMINATE MODE
+  /////////////////////////////////////////////////////////////////////
+
+  // flush any remaining log records
+  CollectLogRecord();
+  Flush();
+
+
+  /////////////////////////////////////////////////////////////////////
+  // SLEEP MODE
+  /////////////////////////////////////////////////////////////////////
+
+  LOG_TRACE("Frontendlogger] Sleep Mode");
+
+  //Setting frontend logger status to sleep
+  logManager.SetLoggingStatus(LOGGING_TYPE_PELOTON, 
+                              LOGGING_STATUS_TYPE_SLEEP);
+}
+
+/**
+ * @brief Collect the LogRecord from BackendLoggers
+ */
+void FrontendLogger::CollectLogRecord() {
+
+  backend_loggers = GetBackendLoggers();
+
+  {
+    std::lock_guard<std::mutex> lock(backend_logger_mutex);
+
+    // Look at the commit mark of the backend loggers of the current frontend logger
+    for( auto backend_logger : backend_loggers){
+      auto local_queue_size = backend_logger->GetLocalQueueSize();
+
+      // Skip current backend_logger, nothing to do
+      if(local_queue_size == 0 ) continue; 
+
+      for(oid_t log_record_itr=0; log_record_itr<local_queue_size; log_record_itr++){
+        // Shallow copy the log record from backend_logger to here
+        global_queue.push_back(backend_logger->GetLogRecord(log_record_itr));
+      }
+
+      // truncate the local queue 
+      backend_logger->TruncateLocalQueue(local_queue_size);
+    }
+  }
+
 }
 
 /**
