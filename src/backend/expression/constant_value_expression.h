d0baf3e9fbbdc500c993d3d49bb8c49d3fac3653
jarulraj <jarulraj@cs.cmu.edu>
2015-09-02 00:35:55 -0400

    All tests pass

--- src/backend/expression/constant_value_expression.h
+++ src/backend/expression/constant_value_expression.h
@@ -21,33 +21,38 @@ namespace peloton {
 namespace expression {
 
 class ConstantValueExpression : public AbstractExpression {
-    public:
-    ConstantValueExpression(const Value &value)
-        : AbstractExpression(EXPRESSION_TYPE_VALUE_CONSTANT) {
-        this->value = value;
-    }
+ public:
+  ConstantValueExpression(const Value &value)
+ : AbstractExpression(EXPRESSION_TYPE_VALUE_CONSTANT) {
+    /**
+     * A deep copy is desired here because we don't know
+     * if the expression will live longer than the passed value
+     * or if uninlined value will be freed somewhere else
+     */
+    this->value = ValueFactory::Clone(value);
+  }
 
-    virtual ~ConstantValueExpression() {
-        value.Free();
-    }
+  virtual ~ConstantValueExpression() {
+    value.Free();
+  }
 
-    Value
-    Evaluate(__attribute__((unused)) const AbstractTuple *tuple1,
-             __attribute__((unused)) const AbstractTuple *tuple2,
-             __attribute__((unused)) executor::ExecutorContext *context) const
-    {
-        LOG_TRACE ("returning constant value as Value:%s type:%d",
-                     value.Debug().c_str(), (int) this->m_type);
-        return this->value;
-    }
+  Value
+  Evaluate(__attribute__((unused)) const AbstractTuple *tuple1,
+           __attribute__((unused)) const AbstractTuple *tuple2,
+           __attribute__((unused)) executor::ExecutorContext *context) const
+  {
+    LOG_TRACE ("returning constant value as Value:%s type:%d",
+               value.Debug().c_str(), (int) this->m_type);
+    return this->value;
+  }
 
-    std::string DebugInfo(const std::string &spacer) const {
-        return spacer + "OptimizedConstantValueExpression:" +
-          value.Debug() + "\n";
-    }
+  std::string DebugInfo(const std::string &spacer) const {
+    return spacer + "OptimizedConstantValueExpression:" +
+        value.Debug() + "\n";
+  }
 
-  protected:
-    Value value;
+ protected:
+  Value value;
 };
 
 }  // End expression namespace
