36a0d6c3ffda0e013c8e716321c45d6f47d970c6
jarulraj <jarulraj@cs.cmu.edu>
2015-09-17 04:28:51 -0400

    Added arithmetic

--- src/backend/benchmark/hyadapt/workload.cpp
+++ src/backend/benchmark/hyadapt/workload.cpp
@@ -31,12 +31,15 @@
 #include "backend/executor/logical_tile.h"
 #include "backend/executor/logical_tile_factory.h"
 #include "backend/executor/materialization_executor.h"
+#include "backend/executor/projection_executor.h"
 #include "backend/expression/abstract_expression.h"
 #include "backend/expression/expression_util.h"
+#include "backend/expression/constant_value_expression.h"
 #include "backend/index/index_factory.h"
 #include "backend/planner/abstract_plan.h"
 #include "backend/planner/materialization_plan.h"
 #include "backend/planner/seq_scan_plan.h"
+#include "backend/planner/projection_plan.h"
 #include "backend/storage/backend_vm.h"
 #include "backend/storage/tile.h"
 #include "backend/storage/tile_group.h"
@@ -94,7 +97,7 @@ static storage::DataTable* CreateTable() {
   // Create schema first
   std::vector<catalog::Column> columns;
 
-  for(oid_t col_itr = 0 ; col_itr <= col_count; col_itr++) {
+  for(oid_t col_itr = 0 ; col_itr < col_count; col_itr++) {
     auto column =
         catalog::Column(VALUE_TYPE_INTEGER, GetTypeSize(VALUE_TYPE_INTEGER),
                         "" + std::to_string(col_itr), is_inlined);
@@ -163,13 +166,15 @@ static void LoadTable(storage::DataTable *table) {
   const bool allocate = true;
   auto txn = txn_manager.BeginTransaction();
 
+  std::cout << "TUPLE COUNT :: " << tuple_count << "\n";
+
   for (int rowid = 0; rowid < tuple_count; rowid++) {
     int populate_value = rowid;
 
     storage::Tuple tuple(table_schema, allocate);
 
-    for(oid_t col_itr = 0 ; col_itr <= col_count; col_itr++) {
-      auto value = ValueFactory::GetIntegerValue(populate_value + col_itr);
+    for(oid_t col_itr = 0 ; col_itr < col_count; col_itr++) {
+      auto value = ValueFactory::GetIntegerValue(populate_value);
       tuple.SetValue(col_itr, value);
     }
 
@@ -217,11 +222,12 @@ void RunDirectTest() {
 
   start = std::chrono::system_clock::now();
 
+  auto txn = txn_manager.BeginTransaction();
+
   /////////////////////////////////////////////////////////
   // SEQ SCAN + PREDICATE
   /////////////////////////////////////////////////////////
 
-  auto txn = txn_manager.BeginTransaction();
   std::unique_ptr<executor::ExecutorContext> context(
       new executor::ExecutorContext(txn));
 
@@ -229,8 +235,8 @@ void RunDirectTest() {
   std::vector<oid_t> column_ids;
   oid_t column_count = state.projectivity * state.column_count;
 
-  for(oid_t col_itr = 0 ; col_itr <= column_count; col_itr++) {
-    column_ids.push_back(col_itr);
+  for(oid_t col_itr = 0 ; col_itr < column_count; col_itr++) {
+    column_ids.push_back(hyadapt_column_ids[col_itr] - 1);
   }
 
   // Create and set up seq scan executor
@@ -300,7 +306,137 @@ void RunAggregateTest() {
 }
 
 void RunArithmeticTest() {
+  std::chrono::time_point<std::chrono::system_clock> start, end;
 
+  std::cout << "LAYOUT :: " << peloton_layout << "\n";
+
+  std::unique_ptr<storage::DataTable> table(CreateAndLoadTable());
+
+  const int lower_bound = GetLowerBound();
+  const bool is_inlined = true;
+  const int tile_group_count = state.scale_factor;
+  auto &txn_manager = concurrency::TransactionManager::GetInstance();
+
+  start = std::chrono::system_clock::now();
+
+  auto txn = txn_manager.BeginTransaction();
+
+  /////////////////////////////////////////////////////////
+  // SEQ SCAN + PREDICATE
+  /////////////////////////////////////////////////////////
+
+  std::unique_ptr<executor::ExecutorContext> context(
+      new executor::ExecutorContext(txn));
+
+  // Column ids to be added to logical tile after scan.
+  // We need all columns because projection can require any column
+  std::vector<oid_t> column_ids;
+  oid_t column_count = state.column_count;
+
+  for(oid_t col_itr = 0 ; col_itr < column_count; col_itr++) {
+    column_ids.push_back(hyadapt_column_ids[col_itr] - 1);
+  }
+
+  // Create and set up seq scan executor
+  auto predicate = CreatePredicate(lower_bound);
+  planner::SeqScanPlan seq_scan_node(table.get(), predicate, column_ids);
+  int expected_num_tiles = tile_group_count;
+
+  executor::SeqScanExecutor seq_scan_executor(&seq_scan_node, context.get());
+
+  /////////////////////////////////////////////////////////
+  // PROJECTION
+  /////////////////////////////////////////////////////////
+
+  planner::ProjectInfo::TargetList target_list;
+  planner::ProjectInfo::DirectMapList direct_map_list;
+
+  // Construct schema of projection
+  std::vector<catalog::Column> columns;
+  auto orig_schema = table.get()->GetSchema();
+  columns.push_back(orig_schema->GetColumn(0));
+  auto projection_schema = new catalog::Schema(columns);
+
+  // target list
+  expression::AbstractExpression *sum_expr = nullptr;
+  oid_t projection_column_count = state.projectivity * state.column_count;
+
+  for(oid_t col_itr = 0 ; col_itr < projection_column_count; col_itr++) {
+    auto hyadapt_colum_id = hyadapt_column_ids[col_itr] - 1;
+    auto column_expr = expression::TupleValueFactory(0, hyadapt_colum_id);
+    if(sum_expr == nullptr)
+      sum_expr = column_expr;
+    else {
+      sum_expr = expression::OperatorFactory(
+          EXPRESSION_TYPE_OPERATOR_PLUS, sum_expr, column_expr);
+    }
+  }
+
+  planner::ProjectInfo::Target target = std::make_pair(0, sum_expr);
+  target_list.push_back(target);
+
+  auto project_info = new planner::ProjectInfo(std::move(target_list), std::move(direct_map_list));
+
+  planner::ProjectionPlan node(project_info, projection_schema);
+
+  // Create and set up executor
+  executor::ProjectionExecutor projection_executor(&node, nullptr);
+  projection_executor.AddChild(&seq_scan_executor);
+
+  /////////////////////////////////////////////////////////
+  // MATERIALIZE
+  /////////////////////////////////////////////////////////
+
+  // Create and set up materialization executor
+  std::vector<catalog::Column> output_columns;
+  std::unordered_map<oid_t, oid_t> old_to_new_cols;
+  oid_t col_itr = 0;
+  column_ids = { 0 };
+  for(auto column_id : column_ids) {
+    auto column =
+        catalog::Column(VALUE_TYPE_INTEGER, GetTypeSize(VALUE_TYPE_INTEGER),
+                        "SUM", is_inlined);
+    output_columns.push_back(column);
+
+    old_to_new_cols[col_itr] = col_itr;
+    col_itr++;
+  }
+
+  std::unique_ptr<catalog::Schema> output_schema(
+      new catalog::Schema(output_columns));
+  bool physify_flag = true;  // is going to create a physical tile
+  planner::MaterializationPlan mat_node(old_to_new_cols, output_schema.release(),
+                                        physify_flag);
+
+  executor::MaterializationExecutor mat_executor(&mat_node, nullptr);
+  mat_executor.AddChild(&projection_executor);
+  bool status = false;
+
+  /////////////////////////////////////////////////////////
+  // EXECUTE
+  /////////////////////////////////////////////////////////
+
+  status = mat_executor.Init();
+  assert(status == true);
+
+  std::vector<std::unique_ptr<executor::LogicalTile>> result_tiles;
+
+  while(mat_executor.Execute() == true) {
+    std::unique_ptr<executor::LogicalTile> result_tile(mat_executor.GetOutput());
+    assert(result_tile != nullptr);
+    result_tiles.emplace_back(result_tile.release());
+  }
+
+  status = mat_executor.Execute();
+  assert(status == false);
+
+  txn_manager.CommitTransaction(txn);
+
+  end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
+
+  std::cout << "logical tile count :: " << result_tiles.size() << "\n";
+  std::cout << "duration :: " << elapsed_seconds.count() << "s\n";
 }
 
 }  // namespace hyadapt
