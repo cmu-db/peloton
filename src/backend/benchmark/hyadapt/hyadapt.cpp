3dcfe7809b7edf5b23b8a3da0799b33c1ef487d9
jarulraj <jarulraj@cs.cmu.edu>
2015-09-18 00:39:48 -0400

    Added experimental mode

--- src/backend/benchmark/hyadapt/hyadapt.cpp
+++ src/backend/benchmark/hyadapt/hyadapt.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <iostream>
+#include <fstream>
 
 #include "backend/benchmark/hyadapt/hyadapt.h"
 #include "backend/benchmark/hyadapt/configuration.h"
@@ -25,23 +26,45 @@ configuration state;
 // Main Entry Point
 void RunBenchmark(){
 
-  switch(state.operator_type) {
-    case OPERATOR_TYPE_DIRECT:
-      RunDirectTest();
-      break;
+  // Single run
+  if(state.experiment_type == EXPERIMENT_TYPE_INVALID) {
+    std::unique_ptr<storage::DataTable>table(CreateAndLoadTable());
 
-    case OPERATOR_TYPE_AGGREGATE:
-      RunAggregateTest();
-      break;
+    switch(state.operator_type) {
+      case OPERATOR_TYPE_DIRECT:
+        RunDirectTest(table.get());
+        break;
 
-    case OPERATOR_TYPE_ARITHMETIC:
-      RunArithmeticTest();
-      break;
+      case OPERATOR_TYPE_AGGREGATE:
+        RunAggregateTest(table.get());
+        break;
 
-    default:
-      std::cout << "Unsupported test type : " << state.operator_type << "\n";
-      break;
+      case OPERATOR_TYPE_ARITHMETIC:
+        RunArithmeticTest(table.get());
+        break;
+
+      default:
+        std::cout << "Unsupported test type : " << state.operator_type << "\n";
+        break;
+    }
+
   }
+  // Experiment
+  else {
+
+    switch(state.experiment_type) {
+      case EXPERIMENT_TYPE_PROJECTIVITY:
+        RunProjectivityExperiment();
+        break;
+
+      default:
+        std::cout << "Unsupported experiment type : " << state.experiment_type << "\n";
+        break;
+    }
+
+  }
+
+
 
 }
 
@@ -52,7 +75,7 @@ void RunBenchmark(){
 int main(int argc, char **argv) {
 
   peloton::benchmark::hyadapt::ParseArguments(argc, argv,
-                                               peloton::benchmark::hyadapt::state);
+                                              peloton::benchmark::hyadapt::state);
 
   peloton::benchmark::hyadapt::RunBenchmark();
 
