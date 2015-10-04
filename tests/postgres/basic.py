79ac1d3fac62bf74280ed2f7ae63644050d55654
Ming Fang <mindbergh@gmail.com>
2015-10-04 13:55:46 -0400

    fix peloton is not found in python test scripts

--- tests/postgres/basic.py
+++ tests/postgres/basic.py
@@ -52,7 +52,7 @@ else:
     TOOLS_DIR = reduce(os.path.join, [BUILD_DIR, "tools"])
     SRC_DIR = reduce(os.path.join, [BUILD_DIR, "src"])
 
-LIB_DIR = reduce(os.path.join, [ROOT_DIR, ".libs"])
+LIB_DIR = reduce(os.path.join, [SRC_DIR, ".libs"])
 my_env = os.environ.copy()
 my_env['LD_LIBRARY_PATH'] = LIB_DIR
 
@@ -67,6 +67,10 @@ class BasicTest(unittest.TestCase):
     def setUp(self):
         LOG.info("Kill previous Peloton")
         cmd = 'pkill -9 "peloton"'
+        self.exec_cmd(cmd, False)
+
+        LOG.info("Creating symbolic link for peloton")
+        cmd = 'ln -s ' + LIB_DIR + "/peloton " + TOOLS_DIR + "/.libs/"
         self.exec_cmd(cmd, False)
 
         LOG.info("Setting up temp data dir")
@@ -109,6 +113,8 @@ class BasicTest(unittest.TestCase):
     def tearDown(self):
         LOG.info("Cleaning up the data dir")
         shutil.rmtree(self.temp_dir_path)
+
+        os.remove(TOOLS_DIR + "/.libs/peloton")
 
 
 ## ==============================================
