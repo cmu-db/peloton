baf815a7f4908aa00b5611c617b3cb27df53ac75
jarulraj <jarulraj@cs.cmu.edu>
2015-10-04 16:01:14 -0400

    Dynamic hybrid and # of cols

--- src/postgres/backend/utils/misc/guc.cpp
+++ src/postgres/backend/utils/misc/guc.cpp
@@ -488,6 +488,7 @@ int			row_security;
 // TODO: Peloton Changes
 int			peloton_mode;
 int     peloton_layout;
+int     peloton_projectivity;
 
 /*
  * This really belongs in pg_shmem.c, but is defined here so that it doesn't
