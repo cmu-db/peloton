fb0576bd4866289a3d5d73c4de752851935a2be3
Ming Fang <mindbergh@gmail.com>
2015-10-05 18:23:55 -0400

    add mock plan
    Former-commit-id: 24877fb6b609047cb907bee6e2625658ddf2b215

---
+++ src/backend/planner/mock_plan.h
@@ -0,0 +1,50 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// mock_plan.h
+//
+// Identification: src/backend/planner/mock_plan.h
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+
+#include "backend/common/types.h"
+#include "backend/planner/abstract_plan.h"
+#include "gmock/gmock.h"
+
+namespace peloton {
+
+namespace test {
+//===--------------------------------------------------------------------===//
+// Abstract Plan
+//===--------------------------------------------------------------------===//
+
+class MockPlan : public planner::AbstractPlan {
+ public:
+  MockPlan() : planner::AbstractPlan() {}
+
+  //===--------------------------------------------------------------------===//
+  // Accessors
+  //===--------------------------------------------------------------------===//
+
+  inline PlanNodeType GetPlanNodeType() const {
+    return PLAN_NODE_TYPE_MOCK;
+  }
+
+  //===--------------------------------------------------------------------===//
+  // Utilities
+  //===--------------------------------------------------------------------===//
+
+  inline std::string GetInfo() const {
+    return "Mock";
+  }
+
+};
+
+}  // namespace test
+}  // namespace peloton
