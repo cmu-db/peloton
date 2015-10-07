e677af64870672ee57d375972901ab64ca35ed71
Jiajun Wang <ericwang1985@gmail.com>
2015-10-07 19:16:27 -0400

    Initial peloton logger implementation based on persistent log objects in NVM.

--- src/backend/logging/loggers/peloton_backend_logger.cpp
+++ src/backend/logging/loggers/peloton_backend_logger.cpp
@@ -31,37 +31,6 @@ void PelotonBackendLogger::Log(LogRecord* record){
   std::lock_guard<std::mutex> lock(local_queue_mutex);
   record->Serialize();
   local_queue.push_back(record);
-
-  if(record->GetType() == LOGRECORD_TYPE_TRANSACTION_END)  {
-
-    // Get the previous record
-    assert(local_queue.size() >= 2);
-    auto previous_record = local_queue[local_queue.size() - 2];
-    auto previous_record_type = previous_record->GetType();
-
-    // TODO: We update local queue size only after we are sure about
-    // whether the transaction is committed or aborted
-
-    // Handle commit
-    if(previous_record_type == LOGRECORD_TYPE_TRANSACTION_COMMIT)  {
-      // Update the log queue offset of last committed transaction
-      last_committed_txn_queue_offset = local_queue.size();
-    }
-    // Handle abort
-    else if(previous_record_type == LOGRECORD_TYPE_TRANSACTION_ABORT)  {
-
-      // Remove aborted log record
-      for(oid_t log_record_itr=last_committed_txn_queue_offset; log_record_itr<local_queue.size();
-          log_record_itr++){
-        delete local_queue[log_record_itr];;
-      }
-
-      // Clean up the local queue of the backend logger
-      local_queue.erase(local_queue.begin()+last_committed_txn_queue_offset, local_queue.end());
-    }
-
-  }
-
 }
 
 /**
@@ -69,7 +38,7 @@ void PelotonBackendLogger::Log(LogRecord* record){
  * @return local queue size
  */
 size_t PelotonBackendLogger::GetLocalQueueSize(void) const{
-  return last_committed_txn_queue_offset;
+  return local_queue.size();
 }
 
 /**
@@ -80,16 +49,13 @@ void PelotonBackendLogger::TruncateLocalQueue(oid_t offset){
   {
     std::lock_guard<std::mutex> lock(local_queue_mutex);
 
-    // let's wait for the frontend logger to flush !
-    // the frontend logger will call our Commit to reset it.
-    wait_for_flushing = true;
-
     // cleanup the queue
     local_queue.erase(local_queue.begin(),
                       local_queue.begin()+offset);
 
-    // last committed log record position
-    last_committed_txn_queue_offset -= offset;
+    // let's wait for the frontend logger to flush !
+    // the frontend logger will call our Commit to reset it.
+    wait_for_flushing = true;
   }
 }
 
