60eab8970d5dfc25761c4b689fcae72c77664306
jarulraj <jarulraj@cs.cmu.edu>
2015-10-15 00:27:23 -0400

    Adding ycsb
    Former-commit-id: 7e37eea21623aadcf91dd324cfd11b29a389643d

---
+++ src/backend/benchmark/ycsb/configuration.h
@@ -0,0 +1,62 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// configuration.h
+//
+// Identification: benchmark/ycsb/configuration.h
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <string>
+#include <getopt.h>
+#include <vector>
+#include <sys/time.h>
+#include <iostream>
+
+#include "backend/storage/data_table.h"
+
+namespace peloton {
+namespace benchmark {
+namespace ycsb{
+
+enum OperatorType{
+  OPERATOR_TYPE_INVALID = 0,         /* invalid */
+
+  OPERATOR_TYPE_READ = 1,
+  OPERATOR_TYPE_UPDATE = 2
+};
+
+class configuration {
+ public:
+
+  OperatorType operator_type;
+
+  LayoutType layout;
+
+  // size of the table
+  int scale_factor;
+
+  // column count
+  int column_count;
+
+  int tuples_per_tilegroup;
+
+  // # of times to run operator
+  unsigned long transactions;
+
+ };
+
+void Usage(FILE *out);
+
+void ParseArguments(int argc, char* argv[], configuration& state);
+
+void GenerateSequence(oid_t column_count);
+
+}  // namespace ycsb
+}  // namespace benchmark
+}  // namespace peloton
