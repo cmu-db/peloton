9c339ad40295f42bb133954649d0d50eed4ec8dc
jarulraj <jarulraj@cs.cmu.edu>
2015-10-15 03:36:03 -0400

    Refactoring

--- src/backend/benchmark/hyadapt/workload.h
+++ src/backend/benchmark/hyadapt/workload.h
@@ -20,13 +20,15 @@ namespace hyadapt{
 
 extern configuration state;
 
-storage::DataTable *CreateAndLoadTable(LayoutType layout_type);
+extern storage::DataTable *hyadapt_table;
 
-void RunDirectTest(storage::DataTable *table);
+void CreateAndLoadTable(LayoutType layout_type);
 
-void RunAggregateTest(storage::DataTable *table);
+void RunDirectTest();
 
-void RunArithmeticTest(storage::DataTable *table);
+void RunAggregateTest();
+
+void RunArithmeticTest();
 
 void RunProjectivityExperiment();
 
