541875f1b994899d5b79975a73b040291315c2a1
Jiajun Wang <ericwang1985@gmail.com>
2015-11-08 09:12:28 -0500

    Make file based back up testable. But still no free.
    Former-commit-id: 8799abf70243b5e65c3a87649751372d96a729ac

--- src/backend/storage/backend_file.cpp
+++ src/backend/storage/backend_file.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "backend/storage/backend_file.h"
+#include "backend/logging/log_manager.h"
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <fstream>
@@ -20,6 +21,10 @@ namespace peloton {
 namespace storage {
 
 BackendFile::BackendFile() {
+  // XXX only for supporting log test. If use this class for general purpose, remove following setting
+  auto& log_manager = logging::LogManager::GetInstance();
+  file_name = log_manager.GetLogFile();
+
   // create a big file
   std::ofstream backend_file(file_name, std::ios::binary | std::ios::out);
   backend_file.seekp(file_size - 1);
@@ -28,26 +33,38 @@ BackendFile::BackendFile() {
   // do mmap
   fd = open(file_name.c_str(), O_RDWR, 0);
   backend_space = (char *)mmap(nullptr, file_size, PROT_READ|PROT_WRITE, MAP_FILE|MAP_SHARED, fd, 0);
+  close(fd);
 }
 
 BackendFile::~BackendFile() {
   munmap(backend_space, file_size);
-  close(fd);
+  //remove(file_name.c_str());
 }
 
 void *BackendFile::Allocate(size_t size) {
-  std::lock_guard<std::mutex> lock(backend_mutex);
-  void * ret = (void *) (backend_space + current_pos);
-  current_pos += size;
+  void *ret = nullptr;
+  {
+    std::lock_guard<std::mutex> lock(backend_mutex);
+    ret = (void *) (backend_space + current_pos);
+    current_pos += size;
+  }
+  if (ret) {
+    chunk_size_recs.insert(std::make_pair(ret, size));
+  }
   return ret;
 }
 
 void BackendFile::Free(void *ptr) {
-  // do nothing
+  // do nothing about the space
+  if (chunk_size_recs.find(ptr) != chunk_size_recs.end()) {
+    chunk_size_recs.erase(ptr);
+  }
 }
 
 void BackendFile::Sync(void *ptr) {
-  msync(backend_space, file_size, MS_SYNC);
+  if (chunk_size_recs.find(ptr) != chunk_size_recs.end()) {
+    msync(ptr, chunk_size_recs.at(ptr), MS_SYNC);
+  }
 }
 
 }  // End storage namespace
