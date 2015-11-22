8ddcb2ce2c5156665c892cb412ec5beb7464ecae
jarulraj <jarulraj@cs.cmu.edu>
2015-11-22 02:10:21 -0500

    Added autoconf to requirement list

--- scripts/vagrant/bootstrap.sh
+++ scripts/vagrant/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Set up environment
-apt-get -y install git g++ pkg-config libtool libjson-spirit-dev libreadline-dev libmm-dev libdw-dev libssl-dev
+apt-get -y install git g++ autoconf pkg-config libtool libjson-spirit-dev libreadline-dev libmm-dev libdw-dev libssl-dev
