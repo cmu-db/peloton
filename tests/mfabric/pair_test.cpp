2635feb75c5b25970c0578f7a2be653b586d984a
jarulraj <jarulraj@cs.cmu.edu>
2015-11-24 16:41:11 -0500

    Fixing pair test
    Former-commit-id: ecdaf8322190b49fce7d00b7cc982dc6226a2d6a

--- tests/mfabric/pair_test.cpp
+++ tests/mfabric/pair_test.cpp
@@ -33,57 +33,66 @@ namespace test {
 
 int send_name(int sock, const char *name)
 {
-	printf ("%s: SENDING \"%s\"\n", name, name);
-	int sz_n = strlen (name) + 1; // '\0' too
-	return nn_send (sock, name, sz_n, 0);
+  printf("Send name \n");
+
+  printf ("%s: SENDING \"%s\"\n", name, name);
+  int sz_n = strlen (name) + 1; // '\0' too
+  return nn_send (sock, name, sz_n, 0);
 }
 
 int recv_name(int sock, const char *name)
 {
-	char *buf = NULL;
-	int result = nn_recv (sock, &buf, NN_MSG, 0);
-	if (result > 0)
-	{
-		printf ("%s: RECEIVED \"%s\"\n", name, buf);
-		nn_freemsg (buf);
-	}
-	return result;
+  printf("Recv name \n");
+
+  char *buf = NULL;
+  int result = nn_recv (sock, &buf, NN_MSG, 0);
+  if (result > 0)
+  {
+    printf ("%s: RECEIVED \"%s\"\n", name, buf);
+    nn_freemsg (buf);
+  }
+  return result;
 }
 
 int send_recv(int sock, const char *name)
 {
-	int to = 100;
-	assert (nn_setsockopt (sock, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof (to)) >= 0);
-	while(1)
-	{
-		recv_name(sock, name);
-		sleep(1);
-		send_name(sock, name);
-	}
+  int to = 100;
+  assert (nn_setsockopt (sock, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof (to)) >= 0);
+  while(1)
+  {
+    printf("Send Recv \n");
+    recv_name(sock, name);
+    //sleep(1);
+    send_name(sock, name);
+  }
 }
 
 int node0 (const char *url)
 {
-	int sock = nn_socket (AF_SP, NN_PAIR);
-	assert (sock >= 0);
-	assert (nn_bind (sock, url) >= 0);
-	send_recv(sock, NODE0);
-	return nn_shutdown (sock, 0);
+  int sock = nn_socket (AF_SP, NN_PAIR);
+  assert (sock >= 0);
+  assert (nn_bind (sock, url) >= 0);
+  send_recv(sock, NODE0);
+  return nn_shutdown (sock, 0);
 }
 
 int node1 (const char *url)
 {
-	int sock = nn_socket (AF_SP, NN_PAIR);
-	assert (sock >= 0);
-	assert (nn_connect (sock, url) >= 0);
-	send_recv(sock, NODE1);
-	return nn_shutdown (sock, 0);
+  int sock = nn_socket (AF_SP, NN_PAIR);
+  assert (sock >= 0);
+  assert (nn_connect (sock, url) >= 0);
+  send_recv(sock, NODE1);
+  return nn_shutdown (sock, 0);
 }
 
 TEST(PairTest, BasicTest) {
 
- node0(NODE0);
- node1(NODE1);
+  std::thread send(node0,"ipc:///tmp/pair.ipc");
+  send.detach();
+  std::thread recv(node1,"ipc:///tmp/pair.ipc");
+  recv.detach();
+
+  sleep(3);
 
 }
 
