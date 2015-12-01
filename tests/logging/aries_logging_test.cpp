f4a54c06a975bf8704a13dfcd0d97afcade831c3
Jiajun Wang <ericwang1985@gmail.com>
2015-12-01 08:18:23 -0500

    Make file path configurable.

--- tests/logging/aries_logging_test.cpp
+++ tests/logging/aries_logging_test.cpp
@@ -12,20 +12,12 @@ namespace test {
 // Logging Test 
 //===--------------------------------------------------------------------===//
 
-std::string aries_log_file_name = "/tmp/aries.log";
+std::string aries_log_file_name = "aries.log";
 
 /**
  * @brief writing a simple log with multiple threads
  */
 TEST(AriesLoggingTest, writing_logfile) {
-
-  std::ifstream log_file(aries_log_file_name.c_str());
-
-  // Reset the log file if exists
-  if( log_file.good() ){
-    EXPECT_TRUE(std::remove(aries_log_file_name.c_str()) == 0 );
-  }
-  log_file.close();
 
   // Prepare a simple log file
   EXPECT_TRUE(LoggingTestsUtil::PrepareLogFile(LOGGING_TYPE_ARIES, aries_log_file_name));
@@ -38,12 +30,6 @@ TEST(AriesLoggingTest, writing_logfile) {
  * @brief recovery test
  */
 TEST(AriesLoggingTest, recovery) {
-
-  std::ifstream log_file(aries_log_file_name.c_str());
-
-  // Reset the log file if exists
-  EXPECT_TRUE(log_file.good());
-  log_file.close();
 
   // Do recovery
   LoggingTestsUtil::CheckRecovery(LOGGING_TYPE_ARIES, aries_log_file_name);
