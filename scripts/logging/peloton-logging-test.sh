dc41ceb012f8fa927df160da18dc41bbe56c7a4e
Jiajun Wang <ericwang1985@gmail.com>
2015-12-02 13:47:01 -0500

    Fix file path params in test script.

--- scripts/logging/peloton-logging-test.sh
+++ scripts/logging/peloton-logging-test.sh
@@ -13,9 +13,9 @@ do
   tests/peloton_logging_test -t $tuples -b $thread_number -c $do_check
   tests/peloton_logging_test -t $tuples -b $thread_number -c $do_check -r 1
   echo "-----------test with $tuples, thread $thread_number, ON $file_dir------------------"
-  tests/aries_logging_test -t $tuples -b $thread_number -c $do_check -d file_dir
-  tests/peloton_logging_test -t $tuples -b $thread_number -c $do_check -d file_dir
-  tests/peloton_logging_test -t $tuples -b $thread_number -c $do_check -r 1 -d file_dir
+  tests/aries_logging_test -t $tuples -b $thread_number -c $do_check -d $file_dir
+  tests/peloton_logging_test -t $tuples -b $thread_number -c $do_check -d $file_dir
+  tests/peloton_logging_test -t $tuples -b $thread_number -c $do_check -r 1 -d $file_dir
   tuple_count=$(($tuple_count * 4))
 done
 
