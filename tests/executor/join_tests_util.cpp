87cec30facb93ba4e7934e246d77f39c19b5d190
jarulraj <jarulraj@cs.cmu.edu>
2015-12-16 10:18:37 +0530

    Refactoring join tests

--- tests/executor/join_tests_util.cpp
+++ tests/executor/join_tests_util.cpp
@@ -19,23 +19,63 @@
 namespace peloton {
 namespace test {
 
-bool JoinTestsUtil::ExecuteJoinTest(storage::DataTable *leftTable,
-                                    storage::DataTable *rightTable,
-                                    storage::DataTable *expected,
-                                    PelotonJoinType joinType,
-                                    expression::AbstractExpression *predicate) {
-  storage::DataTable *result;  // FIXME
+// Create join predicate
+expression::AbstractExpression *JoinTestsUtil::CreateJoinPredicate() {
+  expression::AbstractExpression *predicate = nullptr;
 
-  // Do a tuple-by-tuple comparison
-  //     TupleIterator leftItr(&tile);
-  //     Tuple leftTuple(&tile.schema);
-  //     TupleIterator rightItr(&tile);
-  //     Tuple rightTuple(&tile.schema);
-  //     while (tile_itr.Next(tuple)) {
-  //
-  //     }
+  // LEFT.1 == RIGHT.1
 
-  return (true);
+  expression::TupleValueExpression *left_table_attr_1 =
+      new expression::TupleValueExpression(0, 1);
+  expression::TupleValueExpression *right_table_attr_1 =
+      new expression::TupleValueExpression(1, 1);
+
+  expression::ComparisonExpression<expression::CmpEq> *comp_a =
+      new expression::ComparisonExpression<expression::CmpEq>(
+          EXPRESSION_TYPE_COMPARE_EQUAL, left_table_attr_1, right_table_attr_1);
+
+  // LEFT.3 > 50.0
+
+  expression::TupleValueExpression *left_table_attr_3 =
+      new expression::TupleValueExpression(0, 1);
+  expression::ConstantValueExpression *const_val_1 =
+      new expression::ConstantValueExpression(
+          ValueFactory::GetDoubleValue(50.0));
+  expression::ComparisonExpression<expression::CmpGt> *comp_b =
+      new expression::ComparisonExpression<expression::CmpGt>(
+          EXPRESSION_TYPE_COMPARE_GREATERTHAN, left_table_attr_3, const_val_1);
+
+  predicate = new expression::ConjunctionExpression<expression::ConjunctionAnd>(
+      EXPRESSION_TYPE_CONJUNCTION_AND, comp_a, comp_b);
+
+  return predicate;
+}
+
+planner::ProjectInfo *JoinTestsUtil::CreateProjection() {
+  // Create the plan node
+  planner::ProjectInfo::TargetList target_list;
+  planner::ProjectInfo::DirectMapList direct_map_list;
+
+  /////////////////////////////////////////////////////////
+  // PROJECTION 0
+  /////////////////////////////////////////////////////////
+
+  // direct map
+  planner::ProjectInfo::DirectMap direct_map1 = std::make_pair(
+      0, std::make_pair(0, 1));
+  planner::ProjectInfo::DirectMap direct_map2 = std::make_pair(
+      1, std::make_pair(1, 1));
+  planner::ProjectInfo::DirectMap direct_map3 = std::make_pair(
+      2, std::make_pair(1, 0));
+  planner::ProjectInfo::DirectMap direct_map4 = std::make_pair(
+      3, std::make_pair(0, 0));
+  direct_map_list.push_back(direct_map1);
+  direct_map_list.push_back(direct_map2);
+  direct_map_list.push_back(direct_map3);
+  direct_map_list.push_back(direct_map4);
+
+  return new planner::ProjectInfo(std::move(target_list),
+                                  std::move(direct_map_list));
 }
 
 }  // namespace test
