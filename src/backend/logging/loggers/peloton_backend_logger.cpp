c0021874b43b438ca58d67e56f312cb57307f8fc
jarulraj <jarulraj@cs.cmu.edu>
2015-12-23 09:02:51 -0500

    More refactoring

--- src/backend/logging/loggers/peloton_backend_logger.cpp
+++ src/backend/logging/loggers/peloton_backend_logger.cpp
@@ -21,8 +21,8 @@ namespace peloton {
 namespace logging {
 
 PelotonBackendLogger* PelotonBackendLogger::GetInstance(){
-  thread_local static PelotonBackendLogger pInstance;
-  return &pInstance;
+  thread_local static PelotonBackendLogger instance;
+  return &instance;
 }
 
 /**
@@ -36,6 +36,8 @@ void PelotonBackendLogger::Log(LogRecord* record){
     std::lock_guard<std::mutex> lock(local_queue_mutex);
     local_queue.push_back(record);
   }
+
+  // Notify the frontend in case the transaction has ended
   if(record->GetType() == LOGRECORD_TYPE_TRANSACTION_END)  {
     auto& log_manager = logging::LogManager::GetInstance();
     log_manager.NotifyFrontendLogger(true);
@@ -43,16 +45,13 @@ void PelotonBackendLogger::Log(LogRecord* record){
 }
 
 LogRecord* PelotonBackendLogger::GetTupleRecord(LogRecordType log_record_type, 
-                                              txn_id_t txn_id, 
-                                              oid_t table_oid, 
-                                              ItemPointer insert_location, 
-                                              ItemPointer delete_location, 
-                                              void* data,
-                                              oid_t db_oid){
-  // Just, ignore the data
-  data = nullptr;
-
-  // Build the log record
+                                                txn_id_t txn_id,
+                                                oid_t table_oid,
+                                                ItemPointer insert_location,
+                                                ItemPointer delete_location,
+                                                void* data,
+                                                oid_t db_oid){
+  // Figure the log record type
   switch(log_record_type){
     case LOGRECORD_TYPE_TUPLE_INSERT:  {
       log_record_type = LOGRECORD_TYPE_PELOTON_TUPLE_INSERT; 
@@ -75,14 +74,19 @@ LogRecord* PelotonBackendLogger::GetTupleRecord(LogRecordType log_record_type,
     }
   }
 
-  LogRecord* record = new TupleRecord(log_record_type, 
-                                      txn_id,
-                                      table_oid,
-                                      insert_location,
-                                      delete_location,
-                                      data,
-                                      db_oid);
-  return record;
+  // Don't make use of "data" in case of peloton log records
+  data = nullptr;
+
+  // Build the tuple log record
+  LogRecord* tuple_record = new TupleRecord(log_record_type, 
+                                            txn_id,
+                                            table_oid,
+                                            insert_location,
+                                            delete_location,
+                                            data,
+                                            db_oid);
+
+  return tuple_record;
 }
 
 }  // namespace logging
