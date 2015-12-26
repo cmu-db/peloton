fd06397ce0b0412596c566273a074b563f4bcdb4
jarulraj <jarulraj@cs.cmu.edu>
2015-12-26 02:42:11 -0500

    Checkpoint
    Former-commit-id: e5d67aba414fd67c460435e2b72a96510950f599

--- src/backend/logging/loggers/aries_backend_logger.cpp
+++ src/backend/logging/loggers/aries_backend_logger.cpp
@@ -32,10 +32,12 @@ AriesBackendLogger* AriesBackendLogger::GetInstance(){
 void AriesBackendLogger::Log(LogRecord* record){
   // Enqueue the serialized log record into the queue
   record->Serialize(output_buffer);
+
   {
     std::lock_guard<std::mutex> lock(local_queue_mutex);
     local_queue.push_back(record);
   }
+
   if(record->GetType() == LOGRECORD_TYPE_TRANSACTION_END)  {
     auto& log_manager = logging::LogManager::GetInstance();
     log_manager.NotifyFrontendLogger(true);
