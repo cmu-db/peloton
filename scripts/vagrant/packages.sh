f6779e2b58ca7cd8b57efb90c6b9cfed35d63d15
Joy Arulraj <jarulraj@cs.cmu.edu>
2015-12-31 11:31:28 -0500

    Update packages.sh

--- scripts/vagrant/packages.sh
+++ scripts/vagrant/packages.sh
@@ -3,5 +3,11 @@
 # Set up environment
 apt-get -y install git g++ autoconf pkg-config libtool libjson-spirit-dev libreadline-dev libmm-dev libdw-dev libssl-dev
 
+# Get dependencies script
+wget https://raw.githubusercontent.com/cmu-db/peloton/master/scripts/installation/dependencies.py
+
 # Install dependencies
-python https://raw.githubusercontent.com/cmu-db/peloton/master/scripts/installation/dependencies.py
+python dependencies.py
+
+# Cleanup
+rm dependencies.py
