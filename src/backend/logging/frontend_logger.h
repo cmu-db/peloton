4a07d8253d50808a261301da1ea1425256e3e320
jarulraj <jarulraj@cs.cmu.edu>
2016-01-02 03:11:33 -0500

    Checkpoint
    Former-commit-id: 03617097919c52c7074d1cefe2182a3264ed780b

--- src/backend/logging/frontend_logger.h
+++ src/backend/logging/frontend_logger.h
@@ -34,13 +34,9 @@ class FrontendLogger : public Logger{
 
   public:
 
-    FrontendLogger(){ logger_type = LOGGER_TYPE_FRONTEND; }
+    FrontendLogger();
 
-    ~FrontendLogger(){
-      for(auto backend_logger : backend_loggers){
-        delete backend_logger;
-      }
-    }
+    ~FrontendLogger();
 
     static FrontendLogger* GetFrontendLogger(LoggingType logging_type);
 
@@ -75,8 +71,8 @@ class FrontendLogger : public Logger{
     std::vector<LogRecord*> global_queue;
 
     // period with which it collects log records from backend loggers
-    // (in milliseconds)
-    uint32 wait_timeout = 5;
+    // (in microseconds)
+    int64_t wait_timeout = 5;
 
     // used to indicate if backend has new logs
     bool need_to_collect_new_log_records = false;
