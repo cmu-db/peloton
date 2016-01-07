f8f35a18750f36b031555251c211b54bd52dca78
jarulraj <jarulraj@cs.cmu.edu>
2016-01-07 15:58:32 +0530

    Updated tests

--- scripts/testing/join/nested_loop_join.sql
+++ scripts/testing/join/nested_loop_join.sql
@@ -15,18 +15,9 @@ SELECT * FROM A LEFT OUTER JOIN B ON A.value = B.value ORDER BY A.id;;
 SELECT * FROM A RIGHT OUTER JOIN B ON A.value = B.value ORDER BY A.id;;
 SELECT * FROM A FULL OUTER JOIN B ON A.value = B.value ORDER BY A.id;;
 
--- load in some data
+-- load in the data
 
 INSERT INTO A VALUES(0, 1);
-
--- nested loop join with one empty table
-SELECT * FROM A INNER JOIN B ON A.value = B.value ORDER BY A.id;
-SELECT * FROM A LEFT OUTER JOIN B ON A.value = B.value ORDER BY A.id;;
-SELECT * FROM A RIGHT OUTER JOIN B ON A.value = B.value ORDER BY A.id;;
-SELECT * FROM A FULL OUTER JOIN B ON A.value = B.value ORDER BY A.id;;
-
--- load in more data
-
 INSERT INTO A VALUES(1, 11);
 INSERT INTO A VALUES(2, 12);
 INSERT INTO A VALUES(3, 13);
