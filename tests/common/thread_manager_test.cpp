471adc9e06dcd35465e438aa8307245d96e24ed3
jarulraj <jarulraj@cs.cmu.edu>
2016-01-10 13:27:54 +0530

    Added a basic thread manager

---
+++ tests/common/thread_manager_test.cpp
@@ -0,0 +1,43 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// cache_test.cpp
+//
+// Identification: tests/common/cache_test.cpp
+//
+// Copyright (c) 201CACHE_SIZE, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "gtest/gtest.h"
+#include "backend/common/thread_manager.h"
+
+namespace peloton {
+namespace test {
+
+//===--------------------------------------------------------------------===//
+// Thread Manager Test
+//===--------------------------------------------------------------------===//
+
+TEST(ThreadManagerTests, BasicTest) {
+
+  auto& thread_manager = ThreadManager::GetInstance();
+  std::shared_ptr<std::thread> t1(new std::thread);
+
+  bool status = thread_manager.AttachThread(t1);
+  EXPECT_EQ(status, true);
+
+  status = thread_manager.AttachThread(t1);
+  EXPECT_EQ(status, false);
+
+  status = thread_manager.DetachThread(t1);
+  EXPECT_EQ(status, true);
+
+  status = thread_manager.DetachThread(t1);
+  EXPECT_EQ(status, false);
+
+}
+
+}  // End test namespace
+}  // End peloton namespace
