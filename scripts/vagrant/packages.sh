263a993013214dadb7a6ec27ff5ae310526a5577
Joy Arulraj <jarulraj@cs.cmu.edu>
2016-01-14 15:04:16 +0530

    Update packages.sh
    Former-commit-id: e123155e06be6dcefff0ae6cb51cf16fe061ee1b

--- scripts/vagrant/packages.sh
+++ scripts/vagrant/packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set up environment
-apt-get -y install git g++ autoconf pkg-config libtool libjson-spirit-dev libreadline-dev libmm-dev libdw-dev libssl-dev python-numpy uuid-dev
+apt-get -y install git g++ autoconf pkg-config libtool libjson-spirit-dev libreadline-dev libmm-dev libdw-dev libssl-dev python-numpy uuid-dev clang-format-3.6
 
 # Get dependencies script
 wget https://raw.githubusercontent.com/cmu-db/peloton/master/scripts/installation/dependencies.py
