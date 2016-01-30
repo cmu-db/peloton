50f7bb71a8d4f76f974cedb36ce146ff38c10f2a
jarulraj <jarulraj@cs.cmu.edu>
2016-01-30 11:54:26 -0500

    Added btree index cpp
    Former-commit-id: d4c3055710897d86c2ce0ecd733de7fea3ae288c

--- src/backend/index/btree_index.h
+++ src/backend/index/btree_index.h
@@ -2,9 +2,9 @@
 //
 //                         PelotonDB
 //
-// btree_multi_index.h
+// btree_index.h
 //
-// Identification: src/backend/index/btree_multi_index.h
+// Identification: src/backend/index/btree_index.h
 //
 // Copyright (c) 2015, Carnegie Mellon University Database Group
 //
@@ -26,7 +26,7 @@ namespace peloton {
 namespace index {
 
 /**
- * B+tree-based index implementation.
+ * STX B+tree-based index implementation.
  *
  * @see Index
  */
@@ -40,188 +40,25 @@ class BtreeIndex : public Index {
   // typedef std::multimap<KeyType, ValueType, KeyComparator> MapType;
 
  public:
-  BtreeIndex(IndexMetadata *metadata)
- : Index(metadata),
-   container(KeyComparator(metadata)),
-   equals(metadata),
-   comparator(metadata) {}
+  BtreeIndex(IndexMetadata *metadata);
 
-  ~BtreeIndex() {}
+  ~BtreeIndex();
 
-  bool InsertEntry(const storage::Tuple *key, const ItemPointer location) {
-    KeyType index_key;
-    index_key.SetFromKey(key);
+  bool InsertEntry(const storage::Tuple *key, const ItemPointer location);
 
-    {
-      index_lock.WriteLock();
+  bool DeleteEntry(const storage::Tuple *key, const ItemPointer location);
 
-      // Insert the key, val pair
-      container.insert(std::pair<KeyType, ValueType>(index_key, location));
-
-      index_lock.Unlock();
-    }
-
-    return true;
-  }
-
-  bool DeleteEntry(const storage::Tuple *key, const ItemPointer location) {
-    KeyType index_key;
-    index_key.SetFromKey(key);
-
-    {
-      index_lock.WriteLock();
-
-      // Delete the < key, location > pair
-      bool try_again = true;
-      while(try_again == true) {
-        // Unset try again
-        try_again = false;
-
-        // Lookup matching entries
-        auto entries = container.equal_range(index_key);
-        for (auto iterator = entries.first; iterator != entries.second; iterator++) {
-          ItemPointer value = iterator->second;
-
-          if ((value.block == location.block) && (value.offset == location.offset)) {
-            container.erase(iterator);
-            // Set try again
-            try_again = true;
-            break;
-          }
-        }
-
-      }
-
-      index_lock.Unlock();
-    }
-
-    return true;
-  }
-
-  bool UpdateEntry(const storage::Tuple *key, const ItemPointer location) {
-    KeyType index_key;
-    index_key.SetFromKey(key);
-
-    {
-      index_lock.WriteLock();
-
-      // insert the key, val pair
-      container.insert(std::pair<KeyType, ValueType>(index_key, location));
-
-      index_lock.Unlock();
-    }
-
-    return true;
-  }
+  bool UpdateEntry(const storage::Tuple *key, const ItemPointer location);
 
   std::vector<ItemPointer> Scan(const std::vector<Value> &values,
                                 const std::vector<oid_t> &key_column_ids,
-                                const std::vector<ExpressionType> &expr_types) {
-    std::vector<ItemPointer> result;
-    KeyType index_key;
+                                const std::vector<ExpressionType> &expr_types);
 
-    {
-      index_lock.ReadLock();
+  std::vector<ItemPointer> ScanAllKeys();
 
-      // check if we have leading column equality
-      oid_t leading_column_id = 0;
-      auto key_column_ids_itr = std::find(
-          key_column_ids.begin(), key_column_ids.end(), leading_column_id);
-      bool special_case = false;
-      if (key_column_ids_itr != key_column_ids.end()) {
-        auto offset = std::distance(key_column_ids.begin(), key_column_ids_itr);
-        if (expr_types[offset] == EXPRESSION_TYPE_COMPARE_EQUAL) {
-          special_case = true;
-          LOG_INFO("Special case");
-        }
-      }
+  std::vector<ItemPointer> ScanKey(const storage::Tuple *key);
 
-      auto itr_begin = container.begin();
-      auto itr_end = container.end();
-      storage::Tuple *start_key = nullptr;
-      // start scanning from upper bound if possible
-      if (special_case == true) {
-        start_key = new storage::Tuple(metadata->GetKeySchema(), true);
-        // set the lower bound tuple
-        auto all_equal =
-            SetLowerBoundTuple(start_key, values, key_column_ids, expr_types);
-        index_key.SetFromKey(start_key);
-
-        // all equal case
-        if (all_equal) {
-          auto ret = container.equal_range(index_key);
-          itr_begin = ret.first;
-          itr_end = ret.second;
-          LOG_INFO("equal range return %ld", std::distance(itr_begin, itr_end));
-        } else {
-          itr_begin = container.upper_bound(index_key);
-        }
-      }
-
-      // scan all entries comparing against arbitrary key
-      for (auto itr = itr_begin; itr != itr_end; itr++) {
-        auto index_key = itr->first;
-        auto tuple = index_key.GetTupleForComparison(metadata->GetKeySchema());
-
-        if (Compare(tuple, key_column_ids, expr_types, values) == true) {
-          ItemPointer location = itr->second;
-          result.push_back(location);
-        }
-      }
-
-      delete start_key;
-
-      index_lock.Unlock();
-    }
-
-    return result;
-  }
-
-  std::vector<ItemPointer> ScanAllKeys() {
-    std::vector<ItemPointer> result;
-
-    {
-      index_lock.ReadLock();
-
-      auto itr = container.begin();
-
-      // scan all entries
-      while (itr != container.end()) {
-        ItemPointer location = itr->second;
-        result.push_back(location);
-        itr++;
-      }
-
-      index_lock.Unlock();
-    }
-
-    return result;
-  }
-
-  /**
-   * @brief Return all locations related to this key.
-   */
-  std::vector<ItemPointer> ScanKey(const storage::Tuple *key) {
-    std::vector<ItemPointer> retval;
-    KeyType index_key;
-    index_key.SetFromKey(key);
-
-    {
-      index_lock.ReadLock();
-
-      // find the <key, location> pair
-      auto entries = container.equal_range(index_key);
-      for (auto entry = entries.first; entry != entries.second; ++entry) {
-        retval.push_back(entry->second);
-      }
-
-      index_lock.Unlock();
-    }
-
-    return std::move(retval);
-  }
-
-  std::string GetTypeName() const { return "Btree"; }
+  std::string GetTypeName() const;
 
  protected:
   MapType container;
