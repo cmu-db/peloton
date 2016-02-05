d9368f5200f3e4c7e08fca478e92a771f6953bb5
jarulraj <jarulraj@cs.cmu.edu>
2016-02-05 15:29:16 -0500

    Added bwtree template

--- src/backend/index/btree_index.cpp
+++ src/backend/index/btree_index.cpp
@@ -19,7 +19,7 @@ namespace peloton {
 namespace index {
 
 template <typename KeyType, class KeyComparator, class KeyEqualityChecker>
-BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::BtreeIndex(
+BTreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::BTreeIndex(
     IndexMetadata *metadata)
     : Index(metadata),
       container(KeyComparator(metadata)),
@@ -27,10 +27,10 @@ BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::BtreeIndex(
       comparator(metadata) {}
 
 template <typename KeyType, class KeyComparator, class KeyEqualityChecker>
-BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::~BtreeIndex() {}
+BTreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::~BTreeIndex() {}
 
 template <typename KeyType, class KeyComparator, class KeyEqualityChecker>
-bool BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::InsertEntry(
+bool BTreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::InsertEntry(
     const storage::Tuple *key, const ItemPointer location) {
   KeyType index_key;
   index_key.SetFromKey(key);
@@ -48,7 +48,7 @@ bool BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::InsertEntry(
 }
 
 template <typename KeyType, class KeyComparator, class KeyEqualityChecker>
-bool BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::DeleteEntry(
+bool BTreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::DeleteEntry(
     const storage::Tuple *key, const ItemPointer location) {
   KeyType index_key;
   index_key.SetFromKey(key);
@@ -86,7 +86,7 @@ bool BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::DeleteEntry(
 
 template <typename KeyType, class KeyComparator, class KeyEqualityChecker>
 std::vector<ItemPointer>
-BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::Scan(
+BTreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::Scan(
     const std::vector<Value> &values,
     const std::vector<oid_t> &key_column_ids,
     const std::vector<ExpressionType> &expr_types,
@@ -174,7 +174,7 @@ BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::Scan(
 
 template <typename KeyType, class KeyComparator, class KeyEqualityChecker>
 std::vector<ItemPointer>
-BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::ScanAllKeys() {
+BTreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::ScanAllKeys() {
   std::vector<ItemPointer> result;
 
   {
@@ -200,7 +200,7 @@ BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::ScanAllKeys() {
  */
 template <typename KeyType, class KeyComparator, class KeyEqualityChecker>
 std::vector<ItemPointer>
-BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::ScanKey(
+BTreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::ScanKey(
     const storage::Tuple *key) {
   std::vector<ItemPointer> result;
   KeyType index_key;
@@ -223,37 +223,37 @@ BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::ScanKey(
 
 template <typename KeyType, class KeyComparator, class KeyEqualityChecker>
 std::string
-BtreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::GetTypeName() const {
+BTreeIndex<KeyType, KeyComparator, KeyEqualityChecker>::GetTypeName() const {
   return "Btree";
 }
 
 // Explicit template instantiation
-template class BtreeIndex<GenericKey<4>, GenericComparator<4>,
+template class BTreeIndex<GenericKey<4>, GenericComparator<4>,
 GenericEqualityChecker<4>>;
-template class BtreeIndex<GenericKey<8>, GenericComparator<8>,
+template class BTreeIndex<GenericKey<8>, GenericComparator<8>,
 GenericEqualityChecker<8>>;
-template class BtreeIndex<GenericKey<12>, GenericComparator<12>,
+template class BTreeIndex<GenericKey<12>, GenericComparator<12>,
 GenericEqualityChecker<12>>;
-template class BtreeIndex<GenericKey<16>, GenericComparator<16>,
+template class BTreeIndex<GenericKey<16>, GenericComparator<16>,
 GenericEqualityChecker<16>>;
-template class BtreeIndex<GenericKey<24>, GenericComparator<24>,
+template class BTreeIndex<GenericKey<24>, GenericComparator<24>,
 GenericEqualityChecker<24>>;
-template class BtreeIndex<GenericKey<32>, GenericComparator<32>,
+template class BTreeIndex<GenericKey<32>, GenericComparator<32>,
 GenericEqualityChecker<32>>;
-template class BtreeIndex<GenericKey<48>, GenericComparator<48>,
+template class BTreeIndex<GenericKey<48>, GenericComparator<48>,
 GenericEqualityChecker<48>>;
-template class BtreeIndex<GenericKey<64>, GenericComparator<64>,
+template class BTreeIndex<GenericKey<64>, GenericComparator<64>,
 GenericEqualityChecker<64>>;
-template class BtreeIndex<GenericKey<96>, GenericComparator<96>,
+template class BTreeIndex<GenericKey<96>, GenericComparator<96>,
 GenericEqualityChecker<96>>;
-template class BtreeIndex<GenericKey<128>, GenericComparator<128>,
+template class BTreeIndex<GenericKey<128>, GenericComparator<128>,
 GenericEqualityChecker<128>>;
-template class BtreeIndex<GenericKey<256>, GenericComparator<256>,
+template class BTreeIndex<GenericKey<256>, GenericComparator<256>,
 GenericEqualityChecker<256>>;
-template class BtreeIndex<GenericKey<512>, GenericComparator<512>,
+template class BTreeIndex<GenericKey<512>, GenericComparator<512>,
 GenericEqualityChecker<512>>;
 
-template class BtreeIndex<TupleKey, TupleKeyComparator,
+template class BTreeIndex<TupleKey, TupleKeyComparator,
 TupleKeyEqualityChecker>;
 
 }  // End index namespace
--- src/backend/index/index_factory.cpp
+++ src/backend/index/index_factory.cpp
@@ -19,6 +19,7 @@
 #include "backend/index/index_key.h"
 
 #include "backend/index/btree_index.h"
+#include "backend/index/bwtree_index.h"
 
 namespace peloton {
 namespace index {
@@ -39,16 +40,16 @@ Index *IndexFactory::GetInstance(IndexMetadata *metadata) {
 
   if (ints_only && (index_type == INDEX_TYPE_BTREE)) {
     if (key_size <= sizeof(uint64_t)) {
-      return new BtreeIndex<IntsKey<1>, IntsComparator<1>,
+      return new BTreeIndex<IntsKey<1>, IntsComparator<1>,
                             IntsEqualityChecker<1>>(metadata);
     } else if (key_size <= sizeof(int64_t) * 2) {
-      return new BtreeIndex<IntsKey<2>, IntsComparator<2>,
+      return new BTreeIndex<IntsKey<2>, IntsComparator<2>,
                             IntsEqualityChecker<2>>(metadata);
     } else if (key_size <= sizeof(int64_t) * 3) {
-      return new BtreeIndex<IntsKey<3>, IntsComparator<3>,
+      return new BTreeIndex<IntsKey<3>, IntsComparator<3>,
                             IntsEqualityChecker<3>>(metadata);
     } else if (key_size <= sizeof(int64_t) * 4) {
-      return new BtreeIndex<IntsKey<4>, IntsComparator<4>,
+      return new BTreeIndex<IntsKey<4>, IntsComparator<4>,
                             IntsEqualityChecker<4>>(metadata);
     } else {
       throw IndexException(
@@ -59,43 +60,106 @@ Index *IndexFactory::GetInstance(IndexMetadata *metadata) {
 
   if (index_type == INDEX_TYPE_BTREE) {
     if (key_size <= 4) {
-      return new BtreeIndex<GenericKey<4>, GenericComparator<4>,
+      return new BTreeIndex<GenericKey<4>, GenericComparator<4>,
                             GenericEqualityChecker<4>>(metadata);
     } else if (key_size <= 8) {
-      return new BtreeIndex<GenericKey<8>, GenericComparator<8>,
+      return new BTreeIndex<GenericKey<8>, GenericComparator<8>,
                             GenericEqualityChecker<8>>(metadata);
     } else if (key_size <= 12) {
-      return new BtreeIndex<GenericKey<12>, GenericComparator<12>,
+      return new BTreeIndex<GenericKey<12>, GenericComparator<12>,
                             GenericEqualityChecker<12>>(metadata);
     } else if (key_size <= 16) {
-      return new BtreeIndex<GenericKey<16>, GenericComparator<16>,
+      return new BTreeIndex<GenericKey<16>, GenericComparator<16>,
                             GenericEqualityChecker<16>>(metadata);
     } else if (key_size <= 24) {
-      return new BtreeIndex<GenericKey<24>, GenericComparator<24>,
+      return new BTreeIndex<GenericKey<24>, GenericComparator<24>,
                             GenericEqualityChecker<24>>(metadata);
     } else if (key_size <= 32) {
-      return new BtreeIndex<GenericKey<32>, GenericComparator<32>,
+      return new BTreeIndex<GenericKey<32>, GenericComparator<32>,
                             GenericEqualityChecker<32>>(metadata);
     } else if (key_size <= 48) {
-      return new BtreeIndex<GenericKey<48>, GenericComparator<48>,
+      return new BTreeIndex<GenericKey<48>, GenericComparator<48>,
                             GenericEqualityChecker<48>>(metadata);
     } else if (key_size <= 64) {
-      return new BtreeIndex<GenericKey<64>, GenericComparator<64>,
+      return new BTreeIndex<GenericKey<64>, GenericComparator<64>,
                             GenericEqualityChecker<64>>(metadata);
     } else if (key_size <= 96) {
-      return new BtreeIndex<GenericKey<96>, GenericComparator<96>,
+      return new BTreeIndex<GenericKey<96>, GenericComparator<96>,
                             GenericEqualityChecker<96>>(metadata);
     } else if (key_size <= 128) {
-      return new BtreeIndex<GenericKey<128>, GenericComparator<128>,
+      return new BTreeIndex<GenericKey<128>, GenericComparator<128>,
                             GenericEqualityChecker<128>>(metadata);
     } else if (key_size <= 256) {
-      return new BtreeIndex<GenericKey<256>, GenericComparator<256>,
+      return new BTreeIndex<GenericKey<256>, GenericComparator<256>,
                             GenericEqualityChecker<256>>(metadata);
     } else if (key_size <= 512) {
-      return new BtreeIndex<GenericKey<512>, GenericComparator<512>,
+      return new BTreeIndex<GenericKey<512>, GenericComparator<512>,
                             GenericEqualityChecker<512>>(metadata);
     } else {
-      return new BtreeIndex<TupleKey, TupleKeyComparator,
+      return new BTreeIndex<TupleKey, TupleKeyComparator,
+                            TupleKeyEqualityChecker>(metadata);
+    }
+  }
+
+  if (ints_only && (index_type == INDEX_TYPE_BWTREE)) {
+    if (key_size <= sizeof(uint64_t)) {
+      return new BWTreeIndex<IntsKey<1>, IntsComparator<1>,
+                            IntsEqualityChecker<1>>(metadata);
+    } else if (key_size <= sizeof(int64_t) * 2) {
+      return new BWTreeIndex<IntsKey<2>, IntsComparator<2>,
+                            IntsEqualityChecker<2>>(metadata);
+    } else if (key_size <= sizeof(int64_t) * 3) {
+      return new BWTreeIndex<IntsKey<3>, IntsComparator<3>,
+                            IntsEqualityChecker<3>>(metadata);
+    } else if (key_size <= sizeof(int64_t) * 4) {
+      return new BWTreeIndex<IntsKey<4>, IntsComparator<4>,
+                            IntsEqualityChecker<4>>(metadata);
+    } else {
+      throw IndexException(
+          "We currently only support tree index on non-unique "
+          "integer keys of size 32 bytes or smaller...");
+    }
+  }
+
+  if (index_type == INDEX_TYPE_BWTREE) {
+    if (key_size <= 4) {
+      return new BWTreeIndex<GenericKey<4>, GenericComparator<4>,
+                            GenericEqualityChecker<4>>(metadata);
+    } else if (key_size <= 8) {
+      return new BWTreeIndex<GenericKey<8>, GenericComparator<8>,
+                            GenericEqualityChecker<8>>(metadata);
+    } else if (key_size <= 12) {
+      return new BWTreeIndex<GenericKey<12>, GenericComparator<12>,
+                            GenericEqualityChecker<12>>(metadata);
+    } else if (key_size <= 16) {
+      return new BWTreeIndex<GenericKey<16>, GenericComparator<16>,
+                            GenericEqualityChecker<16>>(metadata);
+    } else if (key_size <= 24) {
+      return new BWTreeIndex<GenericKey<24>, GenericComparator<24>,
+                            GenericEqualityChecker<24>>(metadata);
+    } else if (key_size <= 32) {
+      return new BWTreeIndex<GenericKey<32>, GenericComparator<32>,
+                            GenericEqualityChecker<32>>(metadata);
+    } else if (key_size <= 48) {
+      return new BWTreeIndex<GenericKey<48>, GenericComparator<48>,
+                            GenericEqualityChecker<48>>(metadata);
+    } else if (key_size <= 64) {
+      return new BWTreeIndex<GenericKey<64>, GenericComparator<64>,
+                            GenericEqualityChecker<64>>(metadata);
+    } else if (key_size <= 96) {
+      return new BWTreeIndex<GenericKey<96>, GenericComparator<96>,
+                            GenericEqualityChecker<96>>(metadata);
+    } else if (key_size <= 128) {
+      return new BWTreeIndex<GenericKey<128>, GenericComparator<128>,
+                            GenericEqualityChecker<128>>(metadata);
+    } else if (key_size <= 256) {
+      return new BWTreeIndex<GenericKey<256>, GenericComparator<256>,
+                            GenericEqualityChecker<256>>(metadata);
+    } else if (key_size <= 512) {
+      return new BWTreeIndex<GenericKey<512>, GenericComparator<512>,
+                            GenericEqualityChecker<512>>(metadata);
+    } else {
+      return new BTreeIndex<TupleKey, TupleKeyComparator,
                             TupleKeyEqualityChecker>(metadata);
     }
   }
