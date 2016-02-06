6f4c69bac6c81175607fae65c5f912876465ca56
Ghatage <ghatageanup@gmail.com>
2016-02-06 16:28:58 -0500

    Add serialization abstract classes
    Former-commit-id: a2b953ea1331303ca2946af72b9b728024b83b0f

---
+++ src/backend/mfabric/boost_serializer.cpp
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// nanomsg.cpp
+//
+// Identification: src/backend/mfabric/nanomsg.cpp
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "nanomsg.h"
+
+namespace peloton {
+namespace mfabric {
+
+
+
+}
+}
