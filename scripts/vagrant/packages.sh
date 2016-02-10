c1c625653e09a916e4e408f488332a946c64d828
Joy Arulraj <jarulraj@cs.cmu.edu>
2016-02-10 15:46:59 -0500

    Update packages.sh
    Former-commit-id: 6e594143ee82e44ebd03f3032483026d4e167d34

--- scripts/vagrant/packages.sh
+++ scripts/vagrant/packages.sh
@@ -3,7 +3,7 @@
 # Set up environment
 apt-get -y install git g++ autoconf pkg-config libtool libjson-spirit-dev libreadline-dev \
     libmm-dev libdw-dev libssl-dev python-numpy uuid-dev clang-format-3.3 valgrind \
-    python-pip python-xmlrunner default-jdk default-jre ant kcachegrind
+    python-pip python-xmlrunner default-jdk default-jre ant kcachegrind libboost-all-dev
 
 # Pip
 pip install --upgrade unittest-xml-reporting
