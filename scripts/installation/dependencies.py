8737792c6cb5282a16949ca647feeefe1b12bef0
Ghatage <ghatageanup@gmail.com>
2016-02-14 13:13:56 -0800

    Build libraft.a

--- scripts/installation/dependencies.py
+++ scripts/installation/dependencies.py
@@ -48,6 +48,81 @@ NVML_DIR = os.path.join(THIRD_PARTY_DIR, "nvml")
 NANOMSG_DIR = os.path.join(THIRD_PARTY_DIR, "nanomsg")
 LOGCABIN_DIR = os.path.join(THIRD_PARTY_DIR, "logcabin-1.1.0")
 
+LOGCABIN_OBJ = 'build/Server/Main.o  \
+build/Server/ClientService.o  \
+build/Server/ControlService.o  \
+build/Server/Globals.o  \
+build/Server/RaftConsensus.o  \
+build/Server/RaftConsensusInvariants.o  \
+build/Server/RaftService.o  \
+build/Server/ServerStats.o  \
+build/Server/StateMachine.o  \
+build/Server/SnapshotMetadata.pb.o  \
+build/Server/SnapshotStateMachine.pb.o  \
+build/Server/SnapshotStats.pb.o  \
+build/Storage/FilesystemUtil.o  \
+build/Storage/Layout.o  \
+build/Storage/Log.o  \
+build/Storage/LogFactory.o  \
+build/Storage/MemoryLog.o  \
+build/Storage/SegmentedLog.o  \
+build/Storage/SimpleFileLog.o  \
+build/Storage/SnapshotFile.o  \
+build/Storage/SegmentedLog.pb.o  \
+build/Storage/SimpleFileLog.pb.o  \
+build/Tree/ProtoBuf.o  \
+build/Tree/Tree.o  \
+build/Tree/Snapshot.pb.o  \
+build/Client/Backoff.o  \
+build/Client/Client.o  \
+build/Client/ClientImpl.o  \
+build/Client/LeaderRPC.o  \
+build/Client/MockClientImpl.o  \
+build/Client/SessionManager.o  \
+build/Client/Util.o  \
+build/Protocol/Client.pb.o  \
+build/Protocol/Raft.pb.o  \
+build/Protocol/RaftLogMetadata.pb.o  \
+build/Protocol/ServerControl.pb.o  \
+build/Protocol/ServerStats.pb.o  \
+build/RPC/Address.o  \
+build/RPC/ClientRPC.o  \
+build/RPC/ClientSession.o  \
+build/RPC/MessageSocket.o  \
+build/RPC/OpaqueClientRPC.o  \
+build/RPC/OpaqueServer.o  \
+build/RPC/OpaqueServerRPC.o  \
+build/RPC/Protocol.o  \
+build/RPC/Server.o  \
+build/RPC/ServerRPC.o  \
+build/RPC/ThreadDispatchService.o  \
+build/Event/File.o  \
+build/Event/Loop.o  \
+build/Event/Signal.o  \
+build/Event/Timer.o  \
+build/Core/Buffer.o  \
+build/Core/Checksum.o  \
+build/Core/ConditionVariable.o  \
+build/Core/Config.o  \
+build/Core/Debug.o  \
+build/Core/ProtoBuf.o  \
+build/Core/Random.o  \
+build/Core/RollingStat.o  \
+build/Core/ThreadId.o  \
+build/Core/Time.o  \
+build/Core/StringUtil.o  \
+build/Core/Util.o  \
+build/Core/ProtoBufTest.pb.o'
+
+LOGCABIN_MAIN_OBJ = 'g++ -o build/Server/Main.o -c \
+-std=c++11 -fno-strict-overflow \
+-fPIC -Wall -Wextra -Wcast-align \
+-Wcast-qual -Wconversion -Weffc++ \
+-Wformat=2 -Wmissing-format-attribute \
+-Wno-non-template-friend -Wno-unused-parameter \
+-Woverloaded-virtual -Wwrite-strings -DSWIG -g \
+-DDEBUG -I. -Iinclude build/Server/Main.cc'
+
 ## ==============================================
 ## Utilities
 ## ==============================================
@@ -119,6 +194,24 @@ def install_dependencies():
     os.chdir(LOGCABIN_DIR)
     cmd = 'scons'
     exec_cmd(cmd)
+    LOG.info("LogCabin build finished")
+
+    # Change Main.cc main function
+    cmd = 'sed -i s/main\(/logcabin_main\(/ build/Server/Main.cc'
+    exec_cmd(cmd)
+    LOG.info("Replaced main with logcabin_main")
+
+    # Re-compile Main.cc
+    cmd = LOGCABIN_MAIN_OBJ
+    exec_cmd(cmd)
+    LOG.info("Recompiled Main.cc")
+
+    # Make archive here
+    cmd = 'ar rcs libraft.a ' + LOGCABIN_OBJ
+    exec_cmd(cmd)
+    LOG.info("Created Archive")
+    # Just need to link it with these flags while compiling and using it: -lpthread -lprotobuf -lrt -lcryptopp
+
     os.chdir('..')
 
     LOG.info("Finished installing logcabin library")
