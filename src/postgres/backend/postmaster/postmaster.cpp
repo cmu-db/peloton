038b3d26503a51efb55de8491e2297aff11d839e
Michael-Tieying-Zhang <tieyingz@cs.cmu.edu>
2016-02-15 17:44:13 -0500

    fix the compile problem of protobuf

--- src/postgres/backend/postmaster/postmaster.cpp
+++ src/postgres/backend/postmaster/postmaster.cpp
@@ -63,6 +63,17 @@
  *-------------------------------------------------------------------------
  */
 
+//TODO: protobuf test by Michael
+#include <nanomsg/nn.h>
+#include <nanomsg/reqrep.h>
+#include <nanomsg/pair.h>
+#include <nanomsg/bus.h>
+#include "backend/message/message.pelotonmsg.pb.h"
+#include "backend/message/message.query.pb.h"
+//#include "backend/message/nanomsg.h"
+//#include "backend/message/sender.h"
+// end test
+
 #include "postgres.h"
 
 #include <unistd.h>
@@ -91,18 +102,6 @@
 #ifdef HAVE_PTHREAD_IS_THREADED_NP
 #include <pthread.h>
 #endif
-
-// Test nanomsg by Michael
-#include <nanomsg/nn.h>
-#include <nanomsg/reqrep.h>
-#include <nanomsg/pair.h>
-#include <nanomsg/bus.h>
-//#include "/home/michael/peloton/src/backend/message/message.pelotonmsg.pb.h"
-//#include "backend/message/message.query.pb.h"
-//#include "backend/message/nanomsg.h"
-//#include "backend/message/pelotomsg.pb.h"
-//#include "backend/message/sender.h"
-// end test
 
 #include "access/transam.h"
 #include "access/xlog.h"
