f5eed4ff5c797f04a4fdf92ea0765e9798148fed
jarulraj <jarulraj@cs.cmu.edu>
2016-02-17 16:31:37 -0500

    Refactored logcabin and added raft library
    Former-commit-id: 8d4b47d1f28cdc63844ee04f1d13a5c722831ae9

--- scripts/installation/dependencies.py
+++ scripts/installation/dependencies.py
@@ -46,82 +46,86 @@ THIRD_PARTY_DIR = os.path.join(ROOT_DIR, "third_party")
 
 NVML_DIR = os.path.join(THIRD_PARTY_DIR, "nvml")
 NANOMSG_DIR = os.path.join(THIRD_PARTY_DIR, "nanomsg")
-LOGCABIN_DIR = os.path.join(THIRD_PARTY_DIR, "logcabin-1.1.0")
+LOGCABIN_DIR = os.path.join(THIRD_PARTY_DIR, "logcabin")
 
-LOGCABIN_OBJ = 'build/Server/Main.o  \
-build/Server/ClientService.o  \
-build/Server/ControlService.o  \
-build/Server/Globals.o  \
-build/Server/RaftConsensus.o  \
-build/Server/RaftConsensusInvariants.o  \
-build/Server/RaftService.o  \
-build/Server/ServerStats.o  \
-build/Server/StateMachine.o  \
-build/Server/SnapshotMetadata.pb.o  \
-build/Server/SnapshotStateMachine.pb.o  \
-build/Server/SnapshotStats.pb.o  \
-build/Storage/FilesystemUtil.o  \
-build/Storage/Layout.o  \
-build/Storage/Log.o  \
-build/Storage/LogFactory.o  \
-build/Storage/MemoryLog.o  \
-build/Storage/SegmentedLog.o  \
-build/Storage/SimpleFileLog.o  \
-build/Storage/SnapshotFile.o  \
-build/Storage/SegmentedLog.pb.o  \
-build/Storage/SimpleFileLog.pb.o  \
-build/Tree/ProtoBuf.o  \
-build/Tree/Tree.o  \
-build/Tree/Snapshot.pb.o  \
-build/Client/Backoff.o  \
-build/Client/Client.o  \
-build/Client/ClientImpl.o  \
-build/Client/LeaderRPC.o  \
-build/Client/MockClientImpl.o  \
-build/Client/SessionManager.o  \
-build/Client/Util.o  \
-build/Protocol/Client.pb.o  \
-build/Protocol/Raft.pb.o  \
-build/Protocol/RaftLogMetadata.pb.o  \
-build/Protocol/ServerControl.pb.o  \
-build/Protocol/ServerStats.pb.o  \
-build/RPC/Address.o  \
-build/RPC/ClientRPC.o  \
-build/RPC/ClientSession.o  \
-build/RPC/MessageSocket.o  \
-build/RPC/OpaqueClientRPC.o  \
-build/RPC/OpaqueServer.o  \
-build/RPC/OpaqueServerRPC.o  \
-build/RPC/Protocol.o  \
-build/RPC/Server.o  \
-build/RPC/ServerRPC.o  \
-build/RPC/ThreadDispatchService.o  \
-build/Event/File.o  \
-build/Event/Loop.o  \
-build/Event/Signal.o  \
-build/Event/Timer.o  \
-build/Core/Buffer.o  \
-build/Core/Checksum.o  \
-build/Core/ConditionVariable.o  \
-build/Core/Config.o  \
-build/Core/Debug.o  \
-build/Core/ProtoBuf.o  \
-build/Core/Random.o  \
-build/Core/RollingStat.o  \
-build/Core/ThreadId.o  \
-build/Core/Time.o  \
-build/Core/StringUtil.o  \
-build/Core/Util.o  \
-build/Core/ProtoBufTest.pb.o'
+LOGCABIN_OBJ = '\
+    build/Server/PelotonLogCabin.o \
+    build/Server/ClientService.o  \
+    build/Server/ControlService.o  \
+    build/Server/Globals.o  \
+    build/Server/RaftConsensus.o  \
+    build/Server/RaftConsensusInvariants.o  \
+    build/Server/RaftService.o  \
+    build/Server/ServerStats.o  \
+    build/Server/StateMachine.o  \
+    build/Server/SnapshotMetadata.pb.o  \
+    build/Server/SnapshotStateMachine.pb.o  \
+    build/Server/SnapshotStats.pb.o  \
+    build/Storage/FilesystemUtil.o  \
+    build/Storage/Layout.o  \
+    build/Storage/Log.o  \
+    build/Storage/LogFactory.o  \
+    build/Storage/MemoryLog.o  \
+    build/Storage/SegmentedLog.o  \
+    build/Storage/SimpleFileLog.o  \
+    build/Storage/SnapshotFile.o  \
+    build/Storage/SegmentedLog.pb.o  \
+    build/Storage/SimpleFileLog.pb.o  \
+    build/Tree/ProtoBuf.o  \
+    build/Tree/Tree.o  \
+    build/Tree/Snapshot.pb.o  \
+    build/Client/Backoff.o  \
+    build/Client/Client.o  \
+    build/Client/ClientImpl.o  \
+    build/Client/LeaderRPC.o  \
+    build/Client/MockClientImpl.o  \
+    build/Client/SessionManager.o  \
+    build/Client/Util.o  \
+    build/Protocol/Client.pb.o  \
+    build/Protocol/Raft.pb.o  \
+    build/Protocol/RaftLogMetadata.pb.o  \
+    build/Protocol/ServerControl.pb.o  \
+    build/Protocol/ServerStats.pb.o  \
+    build/RPC/Address.o  \
+    build/RPC/ClientRPC.o  \
+    build/RPC/ClientSession.o  \
+    build/RPC/MessageSocket.o  \
+    build/RPC/OpaqueClientRPC.o  \
+    build/RPC/OpaqueServer.o  \
+    build/RPC/OpaqueServerRPC.o  \
+    build/RPC/Protocol.o  \
+    build/RPC/Server.o  \
+    build/RPC/ServerRPC.o  \
+    build/RPC/ThreadDispatchService.o  \
+    build/Event/File.o  \
+    build/Event/Loop.o  \
+    build/Event/Signal.o  \
+    build/Event/Timer.o  \
+    build/Core/Buffer.o  \
+    build/Core/Checksum.o  \
+    build/Core/ConditionVariable.o  \
+    build/Core/Config.o  \
+    build/Core/Debug.o  \
+    build/Core/ProtoBuf.o  \
+    build/Core/Random.o  \
+    build/Core/RollingStat.o  \
+    build/Core/ThreadId.o  \
+    build/Core/Time.o  \
+    build/Core/StringUtil.o  \
+    build/Core/Util.o  \
+    build/Core/ProtoBufTest.pb.o \
+    '
 
-LOGCABIN_MAIN_OBJ = 'g++ -o build/Server/Main.o -c \
--std=c++11 -fno-strict-overflow \
--fPIC -Wall -Wextra -Wcast-align \
--Wcast-qual -Wconversion -Weffc++ \
--Wformat=2 -Wmissing-format-attribute \
--Wno-non-template-friend -Wno-unused-parameter \
--Woverloaded-virtual -Wwrite-strings -DSWIG -g \
--DDEBUG -I. -Iinclude build/Server/Main.cc'
+LOGCABIN_MAIN_OBJ = '\
+    g++ -o build/Server/Main.o -c \
+    -std=c++11 -fno-strict-overflow \
+    -fPIC -Wall -Wextra -Wcast-align \
+    -Wcast-qual -Wconversion -Weffc++ \
+    -Wformat=2 -Wmissing-format-attribute \
+    -Wno-non-template-friend -Wno-unused-parameter \
+    -Woverloaded-virtual -Wwrite-strings -DSWIG -g \
+    -DDEBUG -I. -Iinclude build/Server/PelotonLogCabin.cc\
+    '
 
 ## ==============================================
 ## Utilities
@@ -137,10 +141,10 @@ def exec_cmd(cmd):
     # TRY
     FNULL = open(os.devnull, 'w')
     try:
-        if verbose == True:
-            subprocess.check_call(args, env=my_env)
-        else:
-            subprocess.check_call(args, stdout=FNULL, stderr=subprocess.STDOUT, env=my_env)
+      if verbose == True:
+        subprocess.check_call(args, env=my_env)
+      else:
+        subprocess.check_call(args, stdout=FNULL, stderr=subprocess.STDOUT, env=my_env)
     # Exception
     except subprocess.CalledProcessError as e:
         print "Command     :: ", e.cmd
@@ -148,7 +152,7 @@ def exec_cmd(cmd):
         print "Output      :: ", e.output
     # Finally
     finally:
-        FNULL.close()
+      FNULL.close()
 
 def install_dependencies():
 
@@ -190,24 +194,16 @@ def install_dependencies():
     ## LogCabin Library
     ## ==============================================
     LOG.info(LOGCABIN_DIR)
-    LOG.info("Installing LogCabin library")
+    LOG.info("Installing raft library")
     os.chdir(LOGCABIN_DIR)
     cmd = 'scons'
     exec_cmd(cmd)
-    LOG.info("LogCabin build finished")
-
-    # Change Main.cc main function
-    #cmd = 'sed -i s/main\(/logcabin_main\(/ build/Server/Main.cc'
-    cmd = 'cp ./build/Server/Main.cc ./build/Server/Main.cc.bak'
-    exec_cmd(cmd)
-    cmd = 'cp ./Server/Main.logcabin ./build/Server/Main.cc'
-    exec_cmd(cmd)
-    LOG.info("Replaced main with logcabin_main")
+    LOG.info("Finished building raft library")
 
     # Re-compile Main.cc
     cmd = LOGCABIN_MAIN_OBJ
     exec_cmd(cmd)
-    LOG.info("Recompiled Main.cc")
+    LOG.info("Compiling PelotonLogCabin.cc")
 
     # Make archive here
     cmd = 'ar rcs libraft.a ' + LOGCABIN_OBJ
@@ -222,8 +218,6 @@ def install_dependencies():
 ## ==============================================
 ## MAIN
 ## ==============================================
-## MAIN
-## ==============================================
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Install Dependencies')
@@ -232,7 +226,6 @@ if __name__ == '__main__':
 
     try:
         prev_dir = os.getcwd()
-
         install_dependencies()
 
     finally:
