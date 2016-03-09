31d066ed0291c5f0c411d46672d0f3c114fa4df1
Joy Arulraj <jarulraj@cs.cmu.edu>
2016-03-09 17:39:15 -0500

    Update packages.sh

--- scripts/installation/packages.sh
+++ scripts/installation/packages.sh
@@ -2,9 +2,9 @@
 
 # Set up environment
 apt-get -y install git g++ autoconf pkg-config libtool libjson-spirit-dev libreadline-dev \
-    libmm-dev libdw-dev libssl-dev python-numpy uuid-dev clang-format-3.3 valgrind \
-    python-pip python-xmlrunner default-jdk default-jre ant kcachegrind libboost-all-dev \
-    libcrypto++-dev scons protobuf-compiler libprotobuf-dev protobuf-compiler
+    libdw-dev libssl-dev valgrind \
+    python-pip python-xmlrunner libboost-all-dev \
+    libcrypto++-dev libprotobuf-dev protobuf-compiler
 
 # Pip
 pip install unittest-xml-reporting
