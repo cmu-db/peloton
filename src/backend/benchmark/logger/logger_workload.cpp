150c57d07e4ae879c04a5c2ddd72f123172586c0
jarulraj <jarulraj@cs.cmu.edu>
2016-03-10 17:27:24 -0500

    More refactoring

--- src/backend/benchmark/logger/logger_workload.cpp
+++ src/backend/benchmark/logger/logger_workload.cpp
@@ -38,77 +38,9 @@
 // GUC Variables
 //===--------------------------------------------------------------------===//
 
-// Logging mode
-extern LoggingType peloton_logging_mode;
-
-extern size_t peloton_data_file_size;
-
-extern int64_t peloton_wait_timeout;
-
 namespace peloton {
 namespace benchmark {
 namespace logger {
-
-std::string wal_log_file_name = "wal.log";
-
-std::string wbl_log_file_name = "wbl.log";
-
-//===--------------------------------------------------------------------===//
-// PREPARE LOG FILE
-//===--------------------------------------------------------------------===//
-
-bool PrepareLogFile(std::string file_name);
-
-//===--------------------------------------------------------------------===//
-// CHECK RECOVERY
-//===--------------------------------------------------------------------===//
-
-void ResetSystem(void);
-
-void DoRecovery(std::string file_name);
-
-//===--------------------------------------------------------------------===//
-// WRITING LOG RECORD
-//===--------------------------------------------------------------------===//
-
-void BuildLog(oid_t db_oid, oid_t table_oid);
-
-void RunBackends(storage::DataTable* table,
-                 const std::vector<storage::Tuple*>& tuples);
-
-std::vector<ItemPointer> InsertTuples(
-    storage::DataTable* table, const std::vector<storage::Tuple*>& tuples,
-    bool committed);
-
-void DeleteTuples(storage::DataTable* table,
-                  const std::vector<ItemPointer>& locations,
-                  bool committed);
-
-std::vector<ItemPointer> UpdateTuples(
-    storage::DataTable* table, const std::vector<ItemPointer>& locations,
-    const std::vector<storage::Tuple*>& tuples, bool committed);
-
-//===--------------------------------------------------------------------===//
-// Utility functions
-//===--------------------------------------------------------------------===//
-
-void CreateDatabase(oid_t db_oid);
-
-void CreateDatabaseAndTable(oid_t db_oid, oid_t table_oid);
-
-storage::DataTable* CreateUserTable(oid_t db_oid, oid_t table_oid);
-
-std::vector<catalog::Column> CreateSchema(void);
-
-std::vector<storage::Tuple*> CreateTuples(catalog::Schema* schema,
-                                          oid_t num_of_tuples,
-                                          VarlenPool* pool);
-
-void DropDatabaseAndTable(oid_t db_oid, oid_t table_oid);
-
-void DropDatabase(oid_t db_oid);
-
-void CheckTupleCount(oid_t db_oid, oid_t table_oid, oid_t expected);
 
 //===--------------------------------------------------------------------===//
 // PREPARE LOG FILE
@@ -705,50 +637,6 @@ size_t GetLogFileSize() {
   auto log_file_size = log_stats.st_size;
 
   return log_file_size;
-}
-
-void RunActiveExperiment() {
-
-  // First, set the global peloton logging mode and pmem file size
-  peloton_logging_mode = state.logging_type;
-  peloton_data_file_size = state.data_file_size;
-  peloton_wait_timeout = state.wait_timeout;
-
-  //===--------------------------------------------------------------------===//
-  // WAL
-  //===--------------------------------------------------------------------===//
-  if (IsBasedOnWriteAheadLogging(peloton_logging_mode)) {
-    // Prepare a simple log file
-    PrepareLogFile(wal_log_file_name);
-
-    // Reset data
-    ResetSystem();
-
-    // Do recovery
-    DoRecovery(wal_log_file_name);
-
-  }
-  //===--------------------------------------------------------------------===//
-  // WBL
-  //===--------------------------------------------------------------------===//
-  else if (IsBasedOnWriteBehindLogging(peloton_logging_mode)) {
-    // Test a simple log process
-    PrepareLogFile(wbl_log_file_name);
-
-    // Do recovery
-    DoRecovery(wbl_log_file_name);
-
-  }
-  //===--------------------------------------------------------------------===//
-  // None
-  //===--------------------------------------------------------------------===//
-  else if (state.logging_type == LOGGING_TYPE_INVALID) {
-    // Test a simple log process
-    PrepareLogFile(wbl_log_file_name);
-
-    // No recovery
-  }
-
 }
 
 }  // namespace logger
