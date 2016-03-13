b289c874785cb3240065cf806b2edb44ac2cbff1
Michael-Tieying-Zhang <tieyingz@cs.cmu.edu>
2016-03-13 13:32:35 -0400

    add rpc server test

---
+++ tests/networking/rpc_server_test.cpp
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// rpc_server_test.cpp
+//
+// Identification: /peloton/tests/networking/rpc_server_test.cpp
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+
+#include "harness.h"
+#include "backend/networking/rpc_server.h"
+#include "backend/networking/peloton_service.h"
+
+namespace peloton {
+namespace test {
+
+class RpcServerTests : public PelotonTest {};
+
+TEST_F(RpcServerTests, BasicTest) {
+
+  networking::RpcServer rpc_server(9001);
+  networking::PelotonService service;
+
+  bool status = rpc_server.RegisterService(&service);
+  EXPECT_EQ(status, true);
+
+  auto ptr = rpc_server.FindMethod(1);
+  EXPECT_EQ(ptr, NULL);
+}
+
+}
+}
