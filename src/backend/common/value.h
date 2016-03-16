2b35d3fdeffb55b365251ed8eeea3989ffbe03b6
Heqing Ya <normanyahq@gmail.com>
2016-03-16 02:51:39 -0400

    add isNan support for Decimal

--- src/backend/common/value.h
+++ src/backend/common/value.h
@@ -2416,8 +2416,6 @@ class Value {
   }
 
  private:
-
-
   static Value GetDecimalValueFromString(const std::string &value) {
     Value retval(VALUE_TYPE_DECIMAL);
     retval.CreateDecimalFromString(value);
@@ -3170,7 +3168,10 @@ inline bool Value::IsNull() const {
 inline bool Value::IsNan() const {
   if (GetValueType() == VALUE_TYPE_DOUBLE) {
     return std::isnan(GetDouble());
+  } else if ((GetValueType() == VALUE_TYPE_DECIMAL)) {
+    return std::isnan(GetDecimal());
   }
+
   return false;
 }
 
