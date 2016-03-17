607d55f825914193e0a501926bbd1e1f34165c23
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-03-17 18:55:07 -0400

    Memcached query parser source files

---
+++ src/postgres/backend/postmaster/memcached_query_parser.cpp
@@ -0,0 +1,11 @@
+//
+// Created by siddharth on 17/3/16.
+//
+#include "postgres/backend/postmaster/memcached_query_parser.h"
+
+namespace peloton {
+namespace memcached {
+
+
+} // end memcached namespace
+} // end peloton namespace
---
+++ src/postgres/backend/postmaster/memcached_query_parser.h
@@ -0,0 +1,14 @@
+//
+// Created by siddharth on 17/3/16.
+//
+
+namespace peloton {
+namespace memcached {
+
+class QueryParser {
+
+};
+
+} // end memcached namespace
+} // end peloton namespace
+
