3282fe2f6bc60a0a0f4aa652a188c4a72c89ad3b
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-03-20 10:36:03 -0400

    add class file for checkpoint

---
+++ src/backend/logging/checkpoint/simple_checkpoint.h
@@ -0,0 +1,36 @@
+/*-------------------------------------------------------------------------
+ *
+ * simple_checkpoint.h
+ * file description
+ *
+ * Copyright(c) 2015, CMU
+ *
+ * /peloton/src/backend/logging/checkpoint/simple_checkpoint.h
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include "backend/logging/checkpoint.h"
+
+namespace peloton {
+namespace logging {
+
+//===--------------------------------------------------------------------===//
+// Simple Checkpoint
+//===--------------------------------------------------------------------===//
+
+class SimpleCheckpoint : public Checkpoint {
+ public:
+  SimpleCheckpoint(const SimpleCheckpoint &) = delete;
+  SimpleCheckpoint &operator=(const SimpleCheckpoint &) = delete;
+  SimpleCheckpoint(SimpleCheckpoint &&) = delete;
+  SimpleCheckpoint &operator=(SimpleCheckpoint &&) = delete;
+  SimpleCheckpoint() {}
+
+ private:
+};
+
+}  // namespace logging
+}  // namespace peloton
