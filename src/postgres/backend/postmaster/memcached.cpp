9502d742905a7cbdf3be035db98c88099d819c42
Di Xiao <dxiao1@andrew.cmu.edu>
2016-03-20 21:01:05 -0400

    sql statement for SET / GET

--- src/postgres/backend/postmaster/memcached.cpp
+++ src/postgres/backend/postmaster/memcached.cpp
@@ -2,3 +2,9 @@
 // GET, SET, ADD, REPLACE, DELETE, CAS, FLUSH_ALL, INCR, DECR, APPEND, PREPEND, STATS
 
 // TODO find way to interact with postgres / peloton?
+
+SET
+insert into test (k, d) values ('$key', '$value') on conflict (k) do update set d = excluded.d;
+
+GET
+select d from test where k = '$key';
