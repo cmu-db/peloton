25d497cc63a298c28878b6cdcd2fab56925e6ea8
studentX <nitin.badam@gmail.com>
2016-03-25 02:41:11 -0400

    query parser started for ycsb

--- src/postgres/backend/postmaster/memcached_query_parser.cpp
+++ src/postgres/backend/postmaster/memcached_query_parser.cpp
@@ -6,6 +6,33 @@
 namespace peloton {
 namespace memcached {
 
+  void QueryParser::parseQuery(){
+    //set <key> <flags> <exptime> <bytes> [noreply]\r\n<value>
+    //add <key> <flags> <exptime> <bytes> [noreply]\r\n<value>
+    //replace <key> <flags> <exptime> <bytes> [noreply]\r\n<value>
+    //delete <key> [<time>] [noreply]
+    //get <key>
+    //multiget
+
+    std::string new_line_del (" ");
+    std::size_t found = memcached_query.find(new_line_del);
+    if (found!=std::string::npos)
+    {
+      std::string command = memcached_query.substr(0, found);
+      if(command == "get"){
+        
+      }
+      else if(command == "delete"){
+
+      }
+      else if(command == "set" || command == "add" || command == "replace" ){
+
+      }
+    }else {
+      //INVALID CASE
+    }
+  }
+
 
 } // end memcached namespace
 } // end peloton namespace
