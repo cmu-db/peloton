333cacafdd1b4cae3c0b3874cff46d3e2ca66536
studentX <nitin.badam@gmail.com>
2016-03-25 02:41:11 -0400

    query parser started for ycsb

--- src/postgres/backend/postmaster/memcached_query_parser.h
+++ src/postgres/backend/postmaster/memcached_query_parser.h
@@ -1,14 +1,27 @@
 //
 // Created by siddharth on 17/3/16.
 //
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
 
 namespace peloton {
 namespace memcached {
 
 class QueryParser {
-    QueryParser();
-    ~QueryParser() {};
-};
+private:
+  std::string memcached_query;
+  void parseQuery();
+
+public:
+  QueryParser(std::string query) {
+    memcached_query = query;
+    parseQuery();
+  };
+
+  ~QueryParser() {};
+}; // end Query parser class
 
 } // end memcached namespace
 } // end peloton namespace
