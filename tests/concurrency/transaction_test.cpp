6b86d9f6b65e9c21f0903d8be89d880dc07676a5
Xian Ran <xranthoar@gmail.com>
2016-03-24 21:51:11 -0400

    add repeatable read test

--- tests/concurrency/transaction_test.cpp
+++ tests/concurrency/transaction_test.cpp
@@ -30,13 +30,13 @@ void ExecuteSchedule(concurrency::TransactionManager *txn_manager,
   // Sleep for the first operation, this gives the opportunity to schedule
   // the start time of a txn
   int last_time = schedule->times[0];
-  std::this_thread::sleep_for(std::chrono::microseconds(last_time));
+  std::this_thread::sleep_for(std::chrono::milliseconds(last_time));
 
   auto transaction = txn_manager->BeginTransaction();
   for (int i = 0; i < (int)schedule->operations.size(); i++) {
     // Sleep milliseconds
     std::this_thread::sleep_for(
-        std::chrono::microseconds(schedule->times[i] - last_time));
+        std::chrono::milliseconds(schedule->times[i] - last_time));
     last_time = schedule->times[i];
 
     // Prepare data for operation
@@ -52,24 +52,29 @@ void ExecuteSchedule(concurrency::TransactionManager *txn_manager,
         break;
       }
       case TXN_OP_READ: {
+        LOG_TRACE("Execute Read");
         int result = TransactionTestsUtil::ExecuteRead(transaction, table, id);
         schedule->results.push_back(result);
         break;
       }
       case TXN_OP_DELETE: {
+        LOG_TRACE("Execute Delete");
         TransactionTestsUtil::ExecuteDelete(transaction, table, id);
         break;
       }
       case TXN_OP_UPDATE: {
+        LOG_TRACE("Execute Update");
         TransactionTestsUtil::ExecuteUpdate(transaction, table, id, value);
         break;
       }
       case TXN_OP_ABORT: {
+        LOG_TRACE("Abort");
         // Assert last operation
         assert(i == (int)schedule->operations.size() - 1);
         break;
       }
       case TXN_OP_NOTHING: {
+        LOG_TRACE("Do nothing");
         // Do nothing
         break;
       }
@@ -79,6 +84,7 @@ void ExecuteSchedule(concurrency::TransactionManager *txn_manager,
     txn_manager->AbortTransaction();
   else
     txn_manager->CommitTransaction();
+  LOG_TRACE("Txn finished");
 }
 
 void ThreadExecuteSchedule(concurrency::TransactionManager *txn_manager,
@@ -123,9 +129,9 @@ TEST_F(TransactionTests, AbortTest) {
   TransactionSchedule schedule1;
   TransactionSchedule schedule2;
   // Schedule one thread to update and abort, another thread to read afterwards
-  schedule1.AddUpdate(0, 1000, 0);
-  schedule1.AddAbort(500);
-  schedule2.AddRead(0, 1000);
+  schedule1.AddUpdate(0, 100, 0);
+  schedule1.AddAbort(50);
+  schedule2.AddRead(0, 100);
   schedules.AddSchedule(&schedule1);
   schedules.AddSchedule(&schedule2);
   LaunchParallelTest(2, ThreadExecuteSchedule, &txn_manager, table.get(),
@@ -134,18 +140,81 @@ TEST_F(TransactionTests, AbortTest) {
   EXPECT_EQ(schedule2.results[0], 0);
 }
 
-TEST_F(TransactionTests, SnapshotIsolationTest) {
+TEST_F(TransactionTests, ReadCommittedTest) {
   auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
   std::unique_ptr<storage::DataTable> table(
       TransactionTestsUtil::CreateTable());
-  TransactionSchedule schedule1;
-  //  TransactionSchedule schedule2;
-  //  TransactionSchedule schedule3;
 
-  schedule1.AddInsert(1, 1, 0);
-  schedule1.AddRead(1, 0);
-  ExecuteSchedule(&txn_manager, table.get(), &schedule1);
-  EXPECT_EQ(1, schedule1.results[0]);
+  TransactionSchedules schedules;
+
+  // Insert (1, 1) at 500 ms, commit at 1500 ms
+  TransactionSchedule schedule1;
+  schedule1.AddInsert(1, 1, 500);
+  schedule1.AddDoNothing(1500);
+  // Start transaction at 0 ms, but read at 2000 ms, should read nothing
+  TransactionSchedule schedule2;
+  schedule2.AddDoNothing(0);
+  schedule2.AddRead(1, 2000);
+  // Read 1 at 1000 ms, should not read the uncommitted version
+  TransactionSchedule schedule3;
+  schedule3.AddRead(1, 1000);
+  // Read 1 at 2000 ms, should read the committed version
+  TransactionSchedule schedule4;
+  schedule4.AddRead(1, 2000);
+
+  schedules.AddSchedule(&schedule1);
+  schedules.AddSchedule(&schedule2);
+  schedules.AddSchedule(&schedule3);
+  schedules.AddSchedule(&schedule4);
+
+  LaunchParallelTest(4, ThreadExecuteSchedule, &txn_manager, table.get(),
+                     &schedules);
+
+  EXPECT_EQ(-1, schedule2.results[0]);
+  EXPECT_EQ(-1, schedule3.results[0]);
+  EXPECT_EQ(1, schedule4.results[0]);
+}
+
+TEST_F(TransactionTests, RepeatableReadTest) {
+  auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+  std::unique_ptr<storage::DataTable> table(
+      TransactionTestsUtil::CreateTable());
+
+  TransactionSchedules schedules;
+
+  // Update (0, 0) to (0, 1) at 500 ms
+  TransactionSchedule schedule1;
+  schedule1.AddUpdate(0, 1, 500);
+  // Update (0, 1) to (0, 2) at 1500 ms
+  TransactionSchedule schedule2;
+  schedule2.AddUpdate(0, 2, 1500);
+  // Delete 0 at 2500 ms
+  TransactionSchedule schedule3;
+  schedule3.AddDelete(0, 2500);
+  // Read at 0 ms, 1000 ms, 2000 ms, 3000 ms, should always read the value 0
+  TransactionSchedule schedule4;
+  schedule4.AddRead(0, 0);
+  schedule4.AddRead(0, 1000);
+  schedule4.AddRead(0, 2000);
+  schedule4.AddRead(0, 3000);
+  // Another thread reads at 3000 ms, should read nothing
+  TransactionSchedule schedule5;
+  schedule5.AddRead(0, 3000);
+
+  schedules.AddSchedule(&schedule1);
+  schedules.AddSchedule(&schedule2);
+  schedules.AddSchedule(&schedule3);
+  schedules.AddSchedule(&schedule4);
+  schedules.AddSchedule(&schedule5);
+
+  LaunchParallelTest(schedules.size(), ThreadExecuteSchedule, &txn_manager,
+                     table.get(), &schedules);
+
+  EXPECT_EQ(0, schedule4.results[0]);
+  EXPECT_EQ(0, schedule4.results[1]);
+  EXPECT_EQ(0, schedule4.results[2]);
+  EXPECT_EQ(0, schedule4.results[3]);
+  EXPECT_EQ(-1, schedule5.results[0]);
 }
 
 }  // End test namespace
