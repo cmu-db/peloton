789d20fb482dd225cee01b62520050da27fe4b34
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-03-25 19:34:25 -0400

    Bug: fixing memcached

---
+++ src/postgres/include/postmaster/memcached_socket.h
@@ -0,0 +1,39 @@
+//
+// Created by siddharth on 25/3/16.
+//
+
+#include "../libpq/libpq-be.h"
+
+/* wrapper over pgsocket used for memcached */
+struct MemcachedSocket {
+private:
+  Port *port;
+  size_t buf_ptr;
+  size_t buf_size;
+  std::string buffer;
+public:
+  inline MemcachedSocket(Port *port) :
+      port(port), buf_ptr(0), buf_size(0),
+      buffer(MC_SOCK_BUFFER_SIZE_BYTES, 0)
+  {}
+
+  inline void close_socket() {
+    for (;;) {
+      int status = (port->sock);
+      if (status < 0) {
+        // failed close
+        if (errno == EINTR) {
+          // interrupted, try closing again
+          continue;
+        }
+      }
+      return;
+    }
+  }
+
+  bool refill_buffer();
+
+  // store a line in buffer into new_line, return false
+  // if read failed
+  bool read_line(std::string &new_line);
+};
