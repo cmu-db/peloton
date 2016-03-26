dabaa2df8a7cdea756d6c2b268279fdb20947b3f
Yingjun Wu <wu.yj0616@gmail.com>
2016-03-26 01:24:04 -0400

    refactor rw-set in a transaction

--- src/backend/concurrency/transaction.cpp
+++ src/backend/concurrency/transaction.cpp
@@ -23,55 +23,113 @@ namespace peloton {
 namespace concurrency {
 
 void Transaction::RecordRead(const oid_t &tile_group_id, const oid_t &tuple_id) {
-  read_tuples[tile_group_id].push_back(tuple_id);
+  if (rw_set.find(tile_group_id) != rw_set.end() && 
+    rw_set.at(tile_group_id).find(tuple_id) == rw_set.at(tile_group_id).end()) {
+    assert(type != RW_TYPE_DELETE && type != RW_TYPE_INS_DEL);
+    return;
+  } else {
+    rw_set[tile_group_id][tuple_id] = RW_TYPE_READ;
+  }
 }
 
 void Transaction::RecordWrite(const oid_t &tile_group_id, const oid_t &tuple_id) {
-  written_tuples[tile_group_id].push_back(tuple_id);
+  if (rw_set.find(tile_group_id) != rw_set.end() && 
+    rw_set.at(tile_group_id).find(tuple_id) == rw_set.at(tile_group_id).end()) {
+    RWType &type = rw_set.at(tile_group_id).at(tuple_id);
+    if (type == RW_TYPE_READ) {
+      type = RW_TYPE_UPDATE;
+      return;
+    }
+    if (type == RW_TYPE_UPDATE) {
+      return;
+    }
+    if (type == RW_TYPE_INSERT) {
+      return;
+    }
+    if (type == RW_TYPE_DELETE) {
+      assert(false);
+      return;
+    }
+    assert(false);
+  } else {
+    rw_set[tile_group_id][tuple_id] = RW_TYPE_UPDATE;
+  }
 }
 
 void Transaction::RecordInsert(const oid_t &tile_group_id, const oid_t &tuple_id) {
-  inserted_tuples[tile_group_id].push_back(tuple_id);
+  if (rw_set.find(tile_group_id) != rw_set.end() && 
+    rw_set.at(tile_group_id).find(tuple_id) == rw_set.at(tile_group_id).end()) {
+    RWType &type = rw_set.at(tile_group_id).at(tuple_id);
+    assert(false);
+  } else {
+    rw_set[tile_group_id][tuple_id] = RW_TYPE_INSERT;
+  }
 }
 
 void Transaction::RecordDelete(const oid_t &tile_group_id, const oid_t &tuple_id) {
-  deleted_tuples[tile_group_id].push_back(tuple_id);
+  if (rw_set.find(tile_group_id) != rw_set.end() && 
+    rw_set.at(tile_group_id).find(tuple_id) == rw_set.at(tile_group_id).end()) {
+    RWType &type = rw_set.at(tile_group_id).at(tuple_id);
+    if (type == RW_TYPE_READ) {
+      type = RW_TYPE_DELETE;
+      return;
+    }
+    if (type == RW_TYPE_UPDATE) {
+      type = RW_TYPE_DELETE;
+      return;
+    }
+    if (type == RW_TYPE_INSERT) {
+      type = RW_TYPE_INS_DEL;
+      return;
+    }
+    if (type == RW_TYPE_DELETE) {
+      assert(false);
+      return;
+    }
+    assert(false);
+  } else {
+    rw_set[tile_group_id][tuple_id] = RW_TYPE_DELETE;
+  }
 }
 
 void Transaction::RecordRead(const ItemPointer &location) {
-  read_tuples[location.block].push_back(location.offset);
+  RecordRead(location.block, location.offset);
 }
 
 void Transaction::RecordWrite(const ItemPointer &location) {
-  written_tuples[location.block].push_back(location.offset);
+  RecordWrite(location.block, location.offset);
 }
 
 void Transaction::RecordInsert(const ItemPointer &location) {
-  inserted_tuples[location.block].push_back(location.offset);
+  RecordInsert(location.block, location.offset);
 }
 
 void Transaction::RecordDelete(const ItemPointer &location) {
-  deleted_tuples[location.block].push_back(location.offset);
+  RecordDelete(location.block, location.offset);
 }
 
-const std::map<oid_t, std::vector<oid_t>> &Transaction::GetReadTuples() {
-  return read_tuples;
+const std::map<oid_t, std::map<oid_t, RWType>> &Transaction::GetRWSet() {
+  return rw_set;
 }
 
-const std::map<oid_t, std::vector<oid_t>> &Transaction::GetWrittenTuples() {
-  return written_tuples;
-}
+// const std::map<oid_t, std::vector<oid_t>> &Transaction::GetReadTuples() {
+//   return rw_set;
+// }
 
-const std::map<oid_t, std::vector<oid_t>> &Transaction::GetInsertedTuples() {
-  return inserted_tuples;
-}
+// const std::map<oid_t, std::vector<oid_t>> &Transaction::GetWrittenTuples() {
+//   return written_tuples;
+// }
 
-const std::map<oid_t, std::vector<oid_t>> &Transaction::GetDeletedTuples() {
-  return deleted_tuples;
-}
+// const std::map<oid_t, std::vector<oid_t>> &Transaction::GetInsertedTuples() {
+//   return inserted_tuples;
+// }
+
+// const std::map<oid_t, std::vector<oid_t>> &Transaction::GetDeletedTuples() {
+//   return deleted_tuples;
+// }
 
 void Transaction::ResetState(void) {
-  read_tuples.clear();
+  rw_set.clear();
   written_tuples.clear();
   inserted_tuples.clear();
   deleted_tuples.clear();
