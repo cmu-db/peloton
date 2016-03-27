c289f7a5404e4055fcc590c66cf664faa849f7fc
Matt Perron <Perron.Matt@gmail.com>
2016-03-26 22:33:24 -0400

    recovery working again!

--- src/backend/storage/tile_group.cpp
+++ src/backend/storage/tile_group.cpp
@@ -205,13 +205,15 @@ oid_t TileGroup::InsertTuple(const Tuple *tuple) {
  */
 oid_t TileGroup::InsertTupleFromRecovery(cid_t commit_id, oid_t tuple_slot_id,
                              const Tuple *tuple) {
-  if (tile_group_header->GetBeginCommitId(tuple_slot_id) > commit_id){
-      return tuple_slot_id;
-  }
   auto status = tile_group_header->GetEmptyTupleSlot(tuple_slot_id);
 
-  // No more slots
+    // No more slots
   if (status == false) return INVALID_OID;
+  cid_t current_begin_cid = tile_group_header->GetBeginCommitId(tuple_slot_id);
+  if (current_begin_cid != MAX_CID && current_begin_cid > commit_id){
+      return tuple_slot_id;
+  }
+
 
   LOG_TRACE("Tile Group Id :: %lu status :: %lu out of %lu slots ",
             tile_group_id, tuple_slot_id, num_tuple_slots);
@@ -251,6 +253,10 @@ oid_t TileGroup::InsertTupleFromRecovery(cid_t commit_id, oid_t tuple_slot_id,
 }
 
 oid_t TileGroup::DeleteTupleFromRecovery(cid_t commit_id, oid_t tuple_slot_id){
+  auto status = tile_group_header->GetEmptyTupleSlot(tuple_slot_id);
+
+      // No more slots
+    if (status == false) return INVALID_OID;
   // Set MVCC info
   tile_group_header->SetTransactionId(tuple_slot_id, INVALID_TXN_ID);
   tile_group_header->SetBeginCommitId(tuple_slot_id, commit_id);
@@ -262,6 +268,10 @@ oid_t TileGroup::DeleteTupleFromRecovery(cid_t commit_id, oid_t tuple_slot_id){
 }
 
 oid_t TileGroup::UpdateTupleFromRecovery(cid_t commit_id, oid_t tuple_slot_id, ItemPointer new_location){
+  auto status = tile_group_header->GetEmptyTupleSlot(tuple_slot_id);
+
+  // No more slots
+  if (status == false) return INVALID_OID;
   // Set MVCC info
   tile_group_header->SetTransactionId(tuple_slot_id, INVALID_TXN_ID);
   tile_group_header->SetBeginCommitId(tuple_slot_id, commit_id);
