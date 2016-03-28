09462948a049d516cc3413182da6b8dc7c3e6ab9
Xian Ran <xranthoar@gmail.com>
2016-03-28 02:50:23 -0400

    more single txn test

--- tests/concurrency/transaction_test.cpp
+++ tests/concurrency/transaction_test.cpp
@@ -61,28 +61,35 @@ TEST_F(TransactionTests, SingleTransactionTest) {
     auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
     std::unique_ptr<storage::DataTable> table(
         TransactionTestsUtil::CreateTable());
-    // read, read, read, read, update, read
+    // read, read, read, read, update, read, read not exist
+    // another txn read
     {
-      TransactionScheduler scheduler(1, table.get(), &txn_manager);
+      TransactionScheduler scheduler(2, table.get(), &txn_manager);
       scheduler.Txn(0).Read(0);
       scheduler.Txn(0).Read(0);
       scheduler.Txn(0).Read(0);
       scheduler.Txn(0).Read(0);
       scheduler.Txn(0).Update(0, 1);
       scheduler.Txn(0).Read(0);
+      scheduler.Txn(0).Read(100);
       scheduler.Txn(0).Commit();
+      scheduler.Txn(1).Read(0);
+      scheduler.Txn(1).Commit();
 
       scheduler.Run();
 
       EXPECT_EQ(RESULT_SUCCESS, scheduler.schedules[0].txn_result);
+      EXPECT_EQ(RESULT_SUCCESS, scheduler.schedules[1].txn_result);
       EXPECT_EQ(0, scheduler.schedules[0].results[0]);
       EXPECT_EQ(0, scheduler.schedules[0].results[1]);
       EXPECT_EQ(0, scheduler.schedules[0].results[2]);
       EXPECT_EQ(0, scheduler.schedules[0].results[3]);
       EXPECT_EQ(1, scheduler.schedules[0].results[4]);
+      EXPECT_EQ(-1, scheduler.schedules[0].results[5]);
+      EXPECT_EQ(1, scheduler.schedules[1].results[0]);
     }
 
-    // update, update, update, update
+    // update, update, update, update, read
     {
       TransactionScheduler scheduler(1, table.get(), &txn_manager);
       scheduler.Txn(0).Update(0, 1);
@@ -96,6 +103,55 @@ TEST_F(TransactionTests, SingleTransactionTest) {
 
       EXPECT_EQ(RESULT_SUCCESS, scheduler.schedules[0].txn_result);
       EXPECT_EQ(4, scheduler.schedules[0].results[0]);
+    }
+
+    // delete not exist, delete exist, read deleted, update deleted,
+    // read deleted, insert back, update inserted, read newly updated
+    {
+      TransactionScheduler scheduler(1, table.get(), &txn_manager);
+
+      scheduler.Txn(0).Delete(100);
+      scheduler.Txn(0).Delete(0);
+      scheduler.Txn(0).Read(0);
+      scheduler.Txn(0).Update(0, 1);
+      scheduler.Txn(0).Read(0);
+      scheduler.Txn(0).Insert(0, 2);
+      scheduler.Txn(0).Update(0, 3);
+      scheduler.Txn(0).Read(0);
+      scheduler.Txn(0).Commit();
+
+      scheduler.Run();
+
+      EXPECT_EQ(RESULT_SUCCESS, scheduler.schedules[0].txn_result);
+      EXPECT_EQ(-1, scheduler.schedules[0].results[0]);
+      EXPECT_EQ(-1, scheduler.schedules[0].results[1]);
+      EXPECT_EQ(3, scheduler.schedules[0].results[2]);
+    }
+
+    // insert, delete inserted, read deleted, insert again, delete again
+    // read deleted, insert again, read inserted, update inserted, read updated
+    {
+      TransactionScheduler scheduler(1, table.get(), &txn_manager);
+
+      scheduler.Txn(0).Insert(1000, 0);
+      scheduler.Txn(0).Delete(1000);
+      scheduler.Txn(0).Read(1000);
+      scheduler.Txn(0).Insert(1000, 1);
+      scheduler.Txn(0).Delete(1000);
+      scheduler.Txn(0).Read(1000);
+      scheduler.Txn(0).Insert(1000, 2);
+      scheduler.Txn(0).Read(1000);
+      scheduler.Txn(0).Update(1000, 3);
+      scheduler.Txn(0).Read(1000);
+      scheduler.Txn(0).Commit();
+
+      scheduler.Run();
+
+      EXPECT_EQ(RESULT_SUCCESS, scheduler.schedules[0].txn_result);
+      EXPECT_EQ(-1, scheduler.schedules[0].results[0]);
+      EXPECT_EQ(-1, scheduler.schedules[0].results[1]);
+      EXPECT_EQ(2, scheduler.schedules[0].results[2]);
+      EXPECT_EQ(3, scheduler.schedules[0].results[3]);
     }
   }
 }
