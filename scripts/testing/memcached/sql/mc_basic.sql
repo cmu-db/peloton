c7e900194b096c56426fe6b7b11a703cc9e0449b
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-03-29 18:21:55 -0400

    Update: Full DML support for Memcached added

--- scripts/testing/memcached/sql/mc_basic.sql
+++ scripts/testing/memcached/sql/mc_basic.sql
@@ -9,24 +9,13 @@ DROP TABLE IF EXISTS Persons;
 
 -- SELECT * FROM Persons;
 
-CREATE TABLE Persons
-(
-PersonID int,
-LastName varchar(255),
-FirstName varchar(255),
-Address varchar(255),
-City varchar(255)
-); 
+CREATE TABLE Persons (PersonID int, LastName varchar(255), FirstName varchar(255), Address varchar(255), City varchar(255))
 
-INSERT INTO Persons
-VALUES ('1', 'san', 'sid', 'centre', 'pitt');
+INSERT INTO Persons VALUES ('1', 'san', 'sid', 'centre', 'pitt')
 
-INSERT INTO Persons
-VALUES ('2', 'san2', 'sid2', 'centr2e', 'pefeitt');
+INSERT INTO Persons VALUES ('2', 'san2', 'sid2', 'centr2e', 'pefeitt')
 
 INSERT INTO Persons
 VALUES ('3', 'san3', 'sid3', 'centr3e', 'pefeitrwt');
 
-SELECT * FROM Persons;
-
-SELECT * FROM Persons WHERE personid=1;
+SELECT * FROM Persons
