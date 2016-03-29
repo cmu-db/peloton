823fe352277dce9a61190ffd0e1ad7c91051af1e
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-03-29 18:33:37 -0400

    Update: Untested binary string support added

--- scripts/testing/memcached/sql/mc_basic.sql
+++ scripts/testing/memcached/sql/mc_basic.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS Persons;
+
 
 -- CREATE TABLE  IF NOT EXISTS Persons
 -- (
@@ -9,13 +9,14 @@ DROP TABLE IF EXISTS Persons;
 
 -- SELECT * FROM Persons;
 
+DROP TABLE IF EXISTS Persons
+
 CREATE TABLE Persons (PersonID int, LastName varchar(255), FirstName varchar(255), Address varchar(255), City varchar(255))
 
 INSERT INTO Persons VALUES ('1', 'san', 'sid', 'centre', 'pitt')
 
 INSERT INTO Persons VALUES ('2', 'san2', 'sid2', 'centr2e', 'pefeitt')
 
-INSERT INTO Persons
-VALUES ('3', 'san3', 'sid3', 'centr3e', 'pefeitrwt');
+INSERT INTO Persons VALUES ('3', 'san3', 'sid3', 'centr3e', 'pefeitrwt')
 
-SELECT * FROM Persons
+SELECT * FROM Persons WHERE personid=2
