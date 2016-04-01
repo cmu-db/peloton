b3304ef36bb7a9568d24626634730cfc84f3cde5
Xian Ran <xranthoar@gmail.com>
2016-03-31 23:44:19 -0400

    pass all test

--- tests/concurrency/transaction_test.cpp
+++ tests/concurrency/transaction_test.cpp
@@ -23,8 +23,11 @@ namespace test {
 
 class TransactionTests : public PelotonTest {};
 
-static std::vector<ConcurrencyType> TEST_TYPES = {CONCURRENCY_TYPE_SSI};
-                                                  // CONCURRENCY_TYPE_2PL};
+static std::vector<ConcurrencyType> TEST_TYPES = {
+  CONCURRENCY_TYPE_SSI,
+  CONCURRENCY_TYPE_2PL,
+  CONCURRENCY_TYPE_SSI
+};
 
 void TransactionTest(concurrency::TransactionManager *txn_manager) {
   uint64_t thread_id = TestingHarness::GetInstance().GetThreadId();
