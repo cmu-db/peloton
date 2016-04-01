ffc1dbaf2364d8a9fb8406346e8d774e498e9c7c
Xian Ran <xranthoar@gmail.com>
2016-03-31 22:25:58 -0400

    rafactor ssi, made code cleaner

--- src/backend/concurrency/ssi_txn_manager.h
+++ src/backend/concurrency/ssi_txn_manager.h
@@ -14,6 +14,7 @@
 
 #include "backend/concurrency/transaction_manager.h"
 #include "backend/storage/tile_group.h"
+#include "backend/catalog/manager.h"
 
 namespace peloton {
 namespace concurrency {
@@ -27,10 +28,10 @@ struct SsiTxnContext {
 };
 
 struct ReadList {
-  txn_id_t txnId;
+  txn_id_t txn_id;
   ReadList *next;
-  ReadList() : txnId(INVALID_TXN_ID), next(nullptr) {}
-  ReadList(txn_id_t t) : txnId(t), next(nullptr) {}
+  ReadList() : txn_id(INVALID_TXN_ID), next(nullptr) {}
+  ReadList(txn_id_t t) : txn_id(t), next(nullptr) {}
 };
 
 class SsiTxnManager : public TransactionManager {
@@ -75,11 +76,10 @@ class SsiTxnManager : public TransactionManager {
   virtual Transaction *BeginTransaction() {
     Transaction *txn = TransactionManager::BeginTransaction();
     {
-      std::lock_guard<std::mutex> lock(running_txns_mutex_);
-      assert(running_txns_.find(txn->GetTransactionId()) ==
-             running_txns_.end());
-      // running_txns_[txn->GetTransactionId()] = SsiTxnContext(txn);
-      running_txns_.insert(
+      std::lock_guard<std::mutex> lock(txn_table_mutex_);
+      assert(txn_table_.find(txn->GetTransactionId()) ==
+             txn_table_.end());
+      txn_table_.insert(
           std::make_pair(txn->GetTransactionId(), SsiTxnContext(txn)));
     }
     return txn;
@@ -92,31 +92,130 @@ class SsiTxnManager : public TransactionManager {
   virtual Result AbortTransaction();
 
  private:
-  std::mutex running_txns_mutex_;
-  std::map<txn_id_t, SsiTxnContext> running_txns_;
+  std::mutex txn_table_mutex_;
+  std::map<txn_id_t, SsiTxnContext> txn_table_;
 
   // init reserved area of a tuple
   // creator txnid | lock (for read list) | read list head
   // The txn_id could only be the cur_txn's txn id.
   void InitTupleReserved(const txn_id_t txn_id, const oid_t tile_group_id,
-                         const oid_t tuple_id);
-  inline txn_id_t *GetCreatorAddr(const char *reserved_area) {
-    return ((txn_id_t *)(reserved_area + CREATOR_OFFSET));
-  }
-  inline txn_id_t *GetLockAddr(const char *reserved_area) {
-    return ((txn_id_t *)(reserved_area + LOCK_OFFSET));
-  }
-  inline ReadList **GetListAddr(const char *reserved_area) {
-    return ((ReadList **)(reserved_area + LIST_OFFSET));
-  }
-  char *GetReservedAreaAddr(const oid_t tile_group_id, const oid_t tuple_id);
+                         const oid_t tuple_id) {
+    LOG_INFO("init reserved txn %ld, group %ld tid %ld", txn_id, tile_group_id,
+             tuple_id);
 
-  bool GetReadLock(txn_id_t *lock_addr, txn_id_t who);
-  bool ReleaseReadLock(txn_id_t *lock_addr, txn_id_t holder);
+    auto tile_group_header =
+        catalog::Manager::GetInstance().GetTileGroup(tile_group_id)->GetHeader();
+
+    assert(tile_group_header->GetTransactionId(tuple_id) == txn_id);
+    assert(current_txn->GetTransactionId() == txn_id);
+
+    auto reserved_area = tile_group_header->GetReservedFieldRef(tuple_id);
+
+    *(txn_id_t *)(reserved_area + CREATOR_OFFSET) = txn_id;
+    *(txn_id_t *)(reserved_area + LOCK_OFFSET) = INITIAL_TXN_ID;
+    *(ReadList **)(reserved_area + LIST_OFFSET) = nullptr;
+  }
+
+  // Get creator of a tuple
+  inline txn_id_t GetCreatorTxnId(storage::TileGroup *tile_group, const oid_t &tuple_id) {
+    return *(txn_id_t *)(tile_group->GetHeader()->GetReservedFieldRef(tuple_id) + CREATOR_OFFSET);
+  }
+
+  void GetReadLock(storage::TileGroup *tile_group, const oid_t tuple_id, txn_id_t txn_id) {
+    txn_id_t *lock_addr = (txn_id_t *)(tile_group->GetHeader()->GetReservedFieldRef(tuple_id) 
+                                + LOCK_OFFSET);
+
+    while (true) {
+      while (*lock_addr != INITIAL_TXN_ID)
+        ;
+      if (atomic_cas(lock_addr, INITIAL_TXN_ID, txn_id)) {
+        return true;
+      }
+    }
+  }
+
+  void ReleaseReadLock(storage::TileGroup *tile_group, const oid_t tuple_id, txn_id_t txn_id) {
+    txn_id_t *lock_addr = (txn_id_t *)(tile_group->GetHeader()->GetReservedFieldRef(tuple_id) 
+                                + LOCK_OFFSET);
+    
+    auto res = atomic_cas(lock_addr, txn_id, INITIAL_TXN_ID);
+    assert(res);
+  }
+
+
+  // Add the current txn into the reader list of a tuple
+  void AddSIReader(storage::TileGroup *tile_group, const oid_t &tuple_id) {
+    auto txn_id = current_txn->GetTransactionId();
+    ReadList *reader = new ReadList(txn_id);
+    reader->txn_id = txn_id;
+    ReadList **headp = (ReadList **)(tile_group->GetHeader()->GetReservedFieldRef(tuple_id)
+                               + LIST_OFFSET);
+    GetReadLock(tile_group, tuple_id, txn_id);
+    
+    reader->next = *headp;
+    *headp = reader;
+
+    ReleaseReadLock(tile_group, tuple_id, txn_id);
+  }
+
+  // Remove reader from the reader list of a tuple
+  void RemoveSIReader(storage::TileGroup *tile_group, const oid_t tuple_id, txn_id_t txn_id) {
+    ReadList **headp = (ReadList **)(tile_group->GetHeader()->GetReservedFieldRef(tuple_id)
+                               + LIST_OFFSET);
+
+    GetReadLock(tile_group, tuple_id, txn_id);
+
+    ReadList fake_header;
+    fake_header.next = *headp;
+    auto prev = &fake_header;
+    auto next = prev->next;
+    bool find = false;
+
+    while (next != nullptr) {
+      if (next->txn_id == txn_id) {
+        find = true;
+        prev->next = next->next;
+        LOG_INFO("find in %ld group %ld tuple %ld", next->txn_id,
+                 tile_group->GetTileGroupId(), tuple_id);
+        delete next;
+        break;
+      }
+      prev = next;
+      next = next->next;
+    }
+
+    *headp = fake_header.next;
+
+    ReleaseReadLock(tile_group, tuple_id, txn_id);
+    assert(find);
+  }
+
+  ReadList *GetReaderList(storage::TileGroup *tile_group, const oid_t &tuple_id) {
+    return *(ReadList **)(tile_group->GetHeader()->GetReservedFieldRef(tuple_id) + LIST_OFFSET);
+  }
+
+  bool GetInConflict(txn_id_t txn_id) {
+    return txn_table_.at(txn_id).in_conflict_;
+  }
+
+  bool GetOutConflict(txn_id_t txn_id) {
+    return txn_table_.at(txn_id).out_conflict_;
+  }
+
+  void SetInConflict(txn_id_t txn_id) {
+    txn_table_.at(txn_id).in_conflict_ = true;
+  }
+
+  void SetOutConflict(txn_id_t txn_id) {
+    txn_table_.at(txn_id).out_conflict_ = true;
+  }
+
+
   void RemoveReader(txn_id_t txn_id);
 
   // Free contexts for SSI manager
   void CleanUp();
+
   static const int CREATOR_OFFSET = 0;
   static const int LOCK_OFFSET = (CREATOR_OFFSET + sizeof(txn_id_t));
   static const int LIST_OFFSET = (LOCK_OFFSET + sizeof(txn_id_t));
