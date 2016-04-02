cd8512ae9e83d8b4a89796a870ec50b739ea0d7f
Matt Perron <Perron.Matt@gmail.com>
2016-04-01 23:38:26 -0400

    fixed update and delete bugs, added unit tests for log recovery

--- tests/logging/logging_test.cpp
+++ tests/logging/logging_test.cpp
@@ -94,7 +94,6 @@ TEST(LoggingTests, BasicInsertTest) {
   db.AddTable(recovery_table);
 
   auto tuples = BuildLoggingTuples(recovery_table, 1, false, false);
-  printf("%lu\n", recovery_table->GetDatabaseOid());
   EXPECT_EQ(recovery_table->GetNumberOfTuples(), 0);
   EXPECT_EQ(recovery_table->GetTileGroupCount(), 1);
   EXPECT_EQ(tuples.size(),1 );
@@ -106,40 +105,134 @@ TEST(LoggingTests, BasicInsertTest) {
   Value val1 = tuples[0]->GetValue(1);
   Value val2 = tuples[0]->GetValue(2);
   Value val3 = tuples[0]->GetValue(3);
-  for(auto tuple : tuples){
-      logging::TupleRecord curr_rec(LOGRECORD_TYPE_TUPLE_INSERT, test_commit_id,
-		              recovery_table->GetOid(), ItemPointer(10,5),
-			      INVALID_ITEMPOINTER, tuple, DEFAULT_DB_ID);
-      curr_rec.SetTuple(tuple);
-      fel.InsertTuple(&curr_rec);
-      curr_rec.SetTuple(nullptr);
-  }
+  auto curr_rec = new logging::TupleRecord (LOGRECORD_TYPE_TUPLE_INSERT, test_commit_id,
+			  recovery_table->GetOid(), ItemPointer(100,5),
+			  INVALID_ITEMPOINTER, tuples[0], DEFAULT_DB_ID);
+  curr_rec->SetTuple(tuples[0]);
+  fel.InsertTuple(curr_rec);
+  delete curr_rec;
 
-  auto tg_header = recovery_table->GetTileGroupById(10)->GetHeader();
+  auto tg_header = recovery_table->GetTileGroupById(100)->GetHeader();
   EXPECT_TRUE(tg_header->GetBeginCommitId(5) <= test_commit_id);
   EXPECT_EQ(tg_header->GetEndCommitId(5), MAX_CID);
 
-  EXPECT_TRUE(val0.Compare(recovery_table->GetTileGroupById(10)->GetValue(5, 0)) == 0);
-  EXPECT_TRUE(val1.Compare(recovery_table->GetTileGroupById(10)->GetValue(5, 1)) == 0);
-  EXPECT_TRUE(val2.Compare(recovery_table->GetTileGroupById(10)->GetValue(5, 2)) == 0);
-  EXPECT_TRUE(val3.Compare(recovery_table->GetTileGroupById(10)->GetValue(5, 3)) == 0);
+  EXPECT_TRUE(val0.Compare(recovery_table->GetTileGroupById(100)->GetValue(5, 0)) == 0);
+  EXPECT_TRUE(val1.Compare(recovery_table->GetTileGroupById(100)->GetValue(5, 1)) == 0);
+  EXPECT_TRUE(val2.Compare(recovery_table->GetTileGroupById(100)->GetValue(5, 2)) == 0);
+  EXPECT_TRUE(val3.Compare(recovery_table->GetTileGroupById(100)->GetValue(5, 3)) == 0);
 
   EXPECT_EQ(recovery_table->GetNumberOfTuples(), 1);
   EXPECT_EQ(recovery_table->GetTileGroupCount(), 2);
-//  delete recovery_table;
 
 }
 
 TEST(LoggingTests, BasicUpdateTest) {
+  auto recovery_table = ExecutorTestsUtil::CreateTable(1024);
+  auto &manager =  catalog::Manager::GetInstance();
+  storage::Database db(DEFAULT_DB_ID);
+  manager.AddDatabase(&db);
+  db.AddTable(recovery_table);
 
+  auto tuples = BuildLoggingTuples(recovery_table, 1, false, false);
+  EXPECT_EQ(recovery_table->GetNumberOfTuples(), 0);
+  EXPECT_EQ(recovery_table->GetTileGroupCount(), 1);
+  EXPECT_EQ(tuples.size(),1 );
+  logging::WriteAheadFrontendLogger fel(true);
+//  auto bel = logging::WriteAheadBackendLogger::GetInstance();
+  cid_t test_commit_id = 10;
+
+  Value val0 = tuples[0]->GetValue(0);
+  Value val1 = tuples[0]->GetValue(1);
+  Value val2 = tuples[0]->GetValue(2);
+  Value val3 = tuples[0]->GetValue(3);
+
+  auto curr_rec = new logging::TupleRecord (LOGRECORD_TYPE_TUPLE_UPDATE, test_commit_id,
+			recovery_table->GetOid(), ItemPointer(100,5),
+			ItemPointer(100,4), tuples[0], DEFAULT_DB_ID);
+  curr_rec->SetTuple(tuples[0]);
+  fel.UpdateTuple(curr_rec);
+  delete curr_rec;
+
+
+  auto tg_header = recovery_table->GetTileGroupById(100)->GetHeader();
+  EXPECT_TRUE(tg_header->GetBeginCommitId(5) <= test_commit_id);
+  EXPECT_EQ(tg_header->GetEndCommitId(5), MAX_CID);
+  EXPECT_EQ(tg_header->GetEndCommitId(4), test_commit_id);
+
+  EXPECT_TRUE(val0.Compare(recovery_table->GetTileGroupById(100)->GetValue(5, 0)) == 0);
+  EXPECT_TRUE(val1.Compare(recovery_table->GetTileGroupById(100)->GetValue(5, 1)) == 0);
+  EXPECT_TRUE(val2.Compare(recovery_table->GetTileGroupById(100)->GetValue(5, 2)) == 0);
+  EXPECT_TRUE(val3.Compare(recovery_table->GetTileGroupById(100)->GetValue(5, 3)) == 0);
+
+  EXPECT_EQ(recovery_table->GetNumberOfTuples(), 1);
+  EXPECT_EQ(recovery_table->GetTileGroupCount(), 2);
 }
 
 TEST(LoggingTests, BasicDeleteTest) {
+  auto recovery_table = ExecutorTestsUtil::CreateTable(1024);
+  auto &manager =  catalog::Manager::GetInstance();
+  storage::Database db(DEFAULT_DB_ID);
+  manager.AddDatabase(&db);
+  db.AddTable(recovery_table);
 
+  EXPECT_EQ(recovery_table->GetNumberOfTuples(), 0);
+  EXPECT_EQ(recovery_table->GetTileGroupCount(), 1);
+  logging::WriteAheadFrontendLogger fel(true);
+
+  cid_t test_commit_id = 10;
+
+  auto curr_rec = new logging::TupleRecord(LOGRECORD_TYPE_TUPLE_UPDATE, test_commit_id,
+		      recovery_table->GetOid(), INVALID_ITEMPOINTER,
+		      ItemPointer(100,4), nullptr, DEFAULT_DB_ID);
+  fel.DeleteTuple(curr_rec);
+
+  delete curr_rec;
+
+  auto tg_header = recovery_table->GetTileGroupById(100)->GetHeader();
+  EXPECT_EQ(tg_header->GetEndCommitId(4), test_commit_id);
+
+
+//  EXPECT_EQ(recovery_table->GetNumberOfTuples(), 1);
+  EXPECT_EQ(recovery_table->GetTileGroupCount(), 2);
 }
 
 TEST(LoggingTests, OutOfOrderCommitTest){
+  auto recovery_table = ExecutorTestsUtil::CreateTable(1024);
+  auto &manager =  catalog::Manager::GetInstance();
+  storage::Database db(DEFAULT_DB_ID);
+  manager.AddDatabase(&db);
+  db.AddTable(recovery_table);
 
+  auto tuples = BuildLoggingTuples(recovery_table, 1, false, false);
+  EXPECT_EQ(recovery_table->GetNumberOfTuples(), 0);
+  EXPECT_EQ(recovery_table->GetTileGroupCount(), 1);
+  EXPECT_EQ(tuples.size(),1 );
+  logging::WriteAheadFrontendLogger fel(true);
+//  auto bel = logging::WriteAheadBackendLogger::GetInstance();
+  cid_t test_commit_id = 10;
+
+  auto curr_rec = new logging::TupleRecord(LOGRECORD_TYPE_TUPLE_UPDATE, test_commit_id+1,
+  		      recovery_table->GetOid(), INVALID_ITEMPOINTER,
+  		      ItemPointer(100,5), nullptr, DEFAULT_DB_ID);
+  fel.DeleteTuple(curr_rec);
+  delete curr_rec;
+
+  EXPECT_EQ(recovery_table->GetTileGroupCount(), 2);
+
+  curr_rec = new logging::TupleRecord(LOGRECORD_TYPE_TUPLE_INSERT, test_commit_id,
+			recovery_table->GetOid(), ItemPointer(100,5),
+			INVALID_ITEMPOINTER, tuples[0], DEFAULT_DB_ID);
+
+  curr_rec->SetTuple(tuples[0]);
+  fel.InsertTuple(curr_rec);
+
+  delete curr_rec;
+
+  auto tg_header = recovery_table->GetTileGroupById(100)->GetHeader();
+  EXPECT_EQ(tg_header->GetEndCommitId(5), test_commit_id+1);
+
+  EXPECT_EQ(recovery_table->GetNumberOfTuples(), 0);
+  EXPECT_EQ(recovery_table->GetTileGroupCount(), 2);
 }
 
 }  // End test namespace
