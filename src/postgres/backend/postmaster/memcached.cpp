9aacb623db93e0b2db9d4aaa5684e62ad3a132ea
Di Xiao <dxiao1@andrew.cmu.edu>
2016-04-05 19:28:52 -0400

    update schema and prepared statement for Memcached

--- src/postgres/backend/postmaster/memcached.cpp
+++ src/postgres/backend/postmaster/memcached.cpp
@@ -6,49 +6,59 @@
 // TODO CAS semantic: gets -> unique_cas_token
 int setup() {
 
+// "
+// CREATE FUNCTION test() RETURNS varchar AS $$
+//     select * from test
+//     RETURNING 'ab';
+// $$ LANGUAGE SQL;
+// "
+
 std::string setup = "
 DROP TABLE test;
-CREATE TABLE test ( k VARCHAR(40) PRIMARY KEY, v VARCHAR(400) );
+CREATE TABLE test ( key VARCHAR(200) PRIMARY KEY, value VARCHAR(2048), flag smallint, size smallint );
 
 DEALLOCATE GET;
 PREPARE GET (text) AS
-  SELECT d FROM TEST WHERE k = $1;
+  SELECT key, value, flag, size FROM TEST WHERE key = $1;
 
 DEALLOCATE SET;
-PREPARE SET (text, text) AS
-  INSERT INTO test (k, v) VALUES ($1, $2) ON CONFLICT (k) DO UPDATE SET v = excluded.v;
+PREPARE SET (text, text, smallint, smallint) AS
+  INSERT INTO test (key, value, flag, size) VALUES ($1, $2, $3, $4) ON CONFLICT (key) DO UPDATE SET value = excluded.value, flag = excluded.flag, size = excluded.size;
 
 DEALLOCATE ADD;
-PREPARE ADD (text, text) AS
-  INSERT INTO test (k, v) VALUES ($1, $2) ON CONFLICT (k) DO UPDATE SET v = excluded.v;
+PREPARE ADD (text, text, smallint, smallint) AS
+  INSERT INTO test (key, value, flag, size) VALUES ($1, $2, $3, $4) ON CONFLICT (key) DO UPDATE SET value = excluded.value, flag = excluded.flag, size = excluded.size;
 
 DEALLOCATE REPLACE;
-PREPARE REPLACE (text, text) AS
-  UPDATE test SET v = $2 WHERE k=$1;
+PREPARE REPLACE (text, text, smallint, smallint) AS
+  UPDATE test SET value = $2, flag = $3, size = $4 WHERE key=$1;
+"
 
+std::string not_needed = 
+"
 DEALLOCATE APPEND;
 PREPARE APPEND (text, text) AS
-  UPDATE test SET v=CONCAT(v,$2) WHERE k=$1;
+  UPDATE test SET value=CONCAT(value,$2) WHERE key=$1;
 
 DEALLOCATE PREPEND;
 PREPARE PREPEND (text, text) AS
-  UPDATE test SET v=CONCAT($2,v) WHERE k=$1;
+  UPDATE test SET value=CONCAT($2,value) WHERE key=$1;
 
 DEALLOCATE INCR;
 PREPARE INCR (text) AS
-  UPDATE test SET v=CAST(v as int)+1 WHERE k=$1;
+  UPDATE test SET value=CAST(value as int)+1 WHERE key=$1;
 
 DEALLOCATE DECR;
 PREPARE DECR (text) AS
-  UPDATE test SET v=CAST(v as int)+1 WHERE k=$1;
+  UPDATE test SET value=CAST(value as int)+1 WHERE key=$1;
 
 DEALLOCATE DELETE;
 PREPARE DELETE (text) AS
-  DELETE FROM test WHERE k=$1;
+  DELETE FROM test WHERE key=$1;
 
 DEALLOCATE CAS;
 PREPARE CAS (text, text, text) AS
-  UPDATE test SET v = case when v = '' then 'Y' else 'N' end;
+  UPDATE test SET value = case when v = '' then 'Y' else 'N' end;
 "
 }
 
