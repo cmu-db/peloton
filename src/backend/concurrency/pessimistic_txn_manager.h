ddfe5b958421cb06c84e2f3ea629dd0b504e05cd
Yingjun Wu <wu.yj0616@gmail.com>
2016-04-06 00:26:58 -0400

    refactor txn managers

--- src/backend/concurrency/pessimistic_txn_manager.h
+++ src/backend/concurrency/pessimistic_txn_manager.h
@@ -64,6 +64,43 @@ class PessimisticTxnManager : public TransactionManager {
 
   virtual Result AbortTransaction();
 
+  virtual Transaction *BeginTransaction() {
+    txn_id_t txn_id = GetNextTransactionId();
+    cid_t begin_cid = GetNextCommitId();
+    Transaction *txn = new Transaction(txn_id, begin_cid);
+    current_txn = txn;
+    
+    running_txn_buckets_[txn_id % RUNNING_TXN_BUCKET_NUM][txn_id] = begin_cid;
+
+    return txn;
+  }
+
+  virtual void EndTransaction() {
+    txn_id_t txn_id = current_txn->GetTransactionId();
+
+    running_txn_buckets_[txn_id % RUNNING_TXN_BUCKET_NUM].erase(txn_id);
+    
+    delete current_txn;
+    current_txn = nullptr;
+  }
+
+  virtual cid_t GetMaxCommittedCid() {
+    cid_t min_running_cid = 0;
+    for (size_t i = 0; i < RUNNING_TXN_BUCKET_NUM; ++i) {
+      {
+        auto iter = running_txn_buckets_[i].lock_table();
+        for (auto &it : iter) {
+          if (min_running_cid == 0 || it.second < min_running_cid) {
+            min_running_cid = it.second;
+          }
+        }
+      }
+    }
+    assert(min_running_cid > 0);
+    return min_running_cid - 1;
+  }
+
+
  private:
 #define READ_COUNT_MASK 0xFF
 #define TXNID_MASK 0x00FFFFFFFFFFFFFF
@@ -77,6 +114,9 @@ class PessimisticTxnManager : public TransactionManager {
 
   void ReleaseReadLock(const storage::TileGroupHeader *const tile_group_header,
                        const oid_t &tuple_id);
+
+
+  cuckoohash_map<txn_id_t, cid_t> running_txn_buckets_[RUNNING_TXN_BUCKET_NUM];
 };
 }
 }
