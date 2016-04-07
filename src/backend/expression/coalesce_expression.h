79c3e4915dffecd997f5fc0add23144c781be72a
heqing <normanyahq@gmail.com>
2016-04-07 05:02:09 -0400

    use unique_ptr in coalesce_expression.h

--- src/backend/expression/coalesce_expression.h
+++ src/backend/expression/coalesce_expression.h
@@ -12,7 +12,8 @@
 
 #include "backend/expression/abstract_expression.h"
 #include "backend/expression/expression_util.h"
-
+#include <memory>
+#include <vector>
 namespace peloton {
 namespace expression {
 
@@ -20,23 +21,18 @@ namespace expression {
 // expression that initially does not evaluate to NULL.
 class CoalesceExpression : public AbstractExpression {
  public:
-  CoalesceExpression(ValueType vt,
-                     const std::vector<AbstractExpression *> &expressions)
-      : AbstractExpression(EXPRESSION_TYPE_OPERATOR_COALESCE),
-        expressions(expressions),
-        value_type(vt) {}
-
-  virtual ~CoalesceExpression() {
-    for (auto value : expressions) {
-      delete value;
-      value = nullptr;
+  typedef std::unique_ptr<AbstractExpression> AbstractExprPtr;
+  CoalesceExpression(ValueType vt, std::vector<AbstractExprPtr> &t_expressions)
+      : AbstractExpression(EXPRESSION_TYPE_OPERATOR_COALESCE), value_type(vt) {
+    for (auto &expression : t_expressions) {
+      expressions.push_back(std::move(expression));
     }
   }
 
   Value Evaluate(const AbstractTuple *tuple1, const AbstractTuple *tuple2,
                  executor::ExecutorContext *context) const {
-    for (auto value : expressions) {
-      auto result = value->Evaluate(tuple1, tuple2, context);
+    for (auto &expression : expressions) {
+      auto result = expression->Evaluate(tuple1, tuple2, context);
       if (result.IsNull()) continue;
       return result;
     }
@@ -49,12 +45,12 @@ class CoalesceExpression : public AbstractExpression {
   }
 
   AbstractExpression *Copy() const {
-    std::vector<AbstractExpression *> copied_expression;
-    for (AbstractExpression *expression : expressions) {
+    std::vector<AbstractExprPtr> copied_expression;
+    for (auto &expression : expressions) {
       if (expression == nullptr) {
         continue;
       }
-      copied_expression.push_back(expression->Copy());
+      copied_expression.push_back(AbstractExprPtr(expression->Copy()));
     }
 
     return new CoalesceExpression(value_type, copied_expression);
@@ -62,7 +58,7 @@ class CoalesceExpression : public AbstractExpression {
 
  private:
   // Expression arguments
-  std::vector<AbstractExpression *> expressions;
+  std::vector<AbstractExprPtr> expressions;
 
   ValueType value_type;
 };
