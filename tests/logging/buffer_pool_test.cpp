883ae3d54f8533fb1bed3684cb4924538bd17bd2
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-04-12 09:54:12 -0400

    more unit tests for buffer pool

--- tests/logging/buffer_pool_test.cpp
+++ tests/logging/buffer_pool_test.cpp
@@ -12,6 +12,8 @@
 
 #include "harness.h"
 #include "backend/logging/circular_buffer_pool.h"
+#include "logging/logging_tests_util.h"
+#include "executor/executor_tests_util.h"
 
 namespace peloton {
 namespace test {
@@ -22,16 +24,16 @@ namespace test {
 
 class BufferPoolTests : public PelotonTest {};
 
-void EnqueueTest(logging::CircularBufferPool *buffer_pool) {
-  for (unsigned int i = 0; i < BUFFER_POOL_SIZE; i++) {
-    std::unique_ptr<logging::LogBuffer> buf(new logging::LogBuffer());
+void EnqueueTest(logging::CircularBufferPool *buffer_pool, unsigned int count) {
+  for (unsigned int i = 0; i < count; i++) {
+    std::unique_ptr<logging::LogBuffer> buf(new logging::LogBuffer(0));
     buf->SetSize(i);
     buffer_pool->Put(std::move(buf));
   }
 }
 
-void DequeueTest(logging::CircularBufferPool *buffer_pool) {
-  for (unsigned int i = 0; i < BUFFER_POOL_SIZE; i++) {
+void DequeueTest(logging::CircularBufferPool *buffer_pool, unsigned int count) {
+  for (unsigned int i = 0; i < count; i++) {
     auto buf = std::move(buffer_pool->Get());
     assert(buf);
     EXPECT_EQ(buf->GetSize(), i);
@@ -39,13 +41,53 @@ void DequeueTest(logging::CircularBufferPool *buffer_pool) {
 }
 
 TEST_F(BufferPoolTests, BufferPoolBasicTest) {
+  logging::CircularBufferPool buffer_pool0;
+  EnqueueTest(&buffer_pool0, 5);
+  EXPECT_EQ(buffer_pool0.GetSize(), 5);
+
+  DequeueTest(&buffer_pool0, 5);
+  EXPECT_EQ(buffer_pool0.GetSize(), 0);
+
+  EnqueueTest(&buffer_pool0, BUFFER_POOL_SIZE);
+  EXPECT_EQ(buffer_pool0.GetSize(), BUFFER_POOL_SIZE);
+
   for (int i = 0; i < 10; i++) {
-    logging::CircularBufferPool buffer_pool;
-    std::thread enqueue_thread(EnqueueTest, &buffer_pool);
-    std::thread dequeue_thread(DequeueTest, &buffer_pool);
+    logging::CircularBufferPool buffer_pool1;
+    std::thread enqueue_thread(EnqueueTest, &buffer_pool1, BUFFER_POOL_SIZE);
+    std::thread dequeue_thread(DequeueTest, &buffer_pool1, BUFFER_POOL_SIZE);
     enqueue_thread.join();
     dequeue_thread.join();
   }
+}
+
+TEST_F(BufferPoolTests, LogBufferBasicTest) {
+  size_t tile_group_size = TESTS_TUPLES_PER_TILEGROUP;
+  size_t table_tile_group_count = 3;
+
+  std::unique_ptr<storage::DataTable> recovery_table(
+      ExecutorTestsUtil::CreateTable(tile_group_size));
+
+  // prepare tuples
+  auto mutate = true;
+  auto random = false;
+  int num_rows = tile_group_size * table_tile_group_count;
+  std::vector<std::shared_ptr<storage::Tuple>> tuples =
+      LoggingTestsUtil::BuildTuples(recovery_table.get(), num_rows, mutate,
+                                    random);
+  std::vector<logging::TupleRecord> records =
+      LoggingTestsUtil::BuildTupleRecords(tuples, tile_group_size,
+                                          table_tile_group_count);
+  logging::LogBuffer log_buffer(0);
+  size_t total_length = 0;
+  for (auto record : records) {
+    assert(record.GetTuple()->GetSchema());
+    CopySerializeOutput output_buffer;
+    record.Serialize(output_buffer);
+    size_t len = record.GetMessageLength();
+    total_length += len;
+    log_buffer.WriteRecord(&record);
+  }
+  EXPECT_EQ(log_buffer.GetSize(), total_length);
 }
 
 }  // End test namespace
---
+++ tests/logging/logging_tests_util.cpp
@@ -0,0 +1,94 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// checkpoint_test.cpp
+//
+// Identification: tests/logging/checkpoint_test.cpp
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "logging/logging_tests_util.h"
+
+#define DEFAULT_RECOVERY_CID 15
+
+namespace peloton {
+namespace test {
+
+//===--------------------------------------------------------------------===//
+// LoggingTests Util
+//===--------------------------------------------------------------------===//
+
+std::vector<logging::TupleRecord> LoggingTestsUtil::BuildTupleRecords(
+    std::vector<std::shared_ptr<storage::Tuple>> &tuples,
+    size_t tile_group_size, size_t table_tile_group_count) {
+  std::vector<logging::TupleRecord> records;
+  for (size_t block = 1; block <= table_tile_group_count; ++block) {
+    for (size_t offset = 0; offset < tile_group_size; ++offset) {
+      ItemPointer location(block, offset);
+      auto &tuple = tuples[(block - 1) * tile_group_size + offset];
+      assert(tuple->GetSchema());
+      logging::TupleRecord record(LOGRECORD_TYPE_WAL_TUPLE_INSERT,
+                                  INITIAL_TXN_ID, INVALID_OID, location,
+                                  INVALID_ITEMPOINTER, tuple.get(), DEFAULT_DB_ID);
+      record.SetTuple(tuple.get());
+      records.push_back(record);
+    }
+  }
+  LOG_INFO("Built a vector of %lu tuple WAL insert records", records.size());
+  return records;
+}
+
+std::vector<std::shared_ptr<storage::Tuple>> LoggingTestsUtil::BuildTuples(
+    storage::DataTable *table, int num_rows, bool mutate, bool random) {
+  std::vector<std::shared_ptr<storage::Tuple>> tuples;
+  LOG_INFO("build a vector of %d tuples", num_rows);
+
+  // Random values
+  std::srand(std::time(nullptr));
+  const catalog::Schema *schema = table->GetSchema();
+  // Ensure that the tile group is as expected.
+  assert(schema->GetColumnCount() == 4);
+
+  // Insert tuples into tile_group.
+  const bool allocate = true;
+  auto testing_pool = TestingHarness::GetInstance().GetTestingPool();
+
+  for (int rowid = 0; rowid < num_rows; rowid++) {
+    int populate_value = rowid;
+    if (mutate) populate_value *= 3;
+
+    std::shared_ptr<storage::Tuple> tuple(new storage::Tuple(schema, allocate));
+
+    // First column is unique in this case
+    tuple->SetValue(0,
+                    ValueFactory::GetIntegerValue(
+                        ExecutorTestsUtil::PopulatedValue(populate_value, 0)),
+                    testing_pool);
+
+    // In case of random, make sure this column has duplicated values
+    tuple->SetValue(
+        1, ValueFactory::GetIntegerValue(ExecutorTestsUtil::PopulatedValue(
+               random ? std::rand() % (num_rows / 3) : populate_value, 1)),
+        testing_pool);
+
+    tuple->SetValue(
+        2, ValueFactory::GetDoubleValue(ExecutorTestsUtil::PopulatedValue(
+               random ? std::rand() : populate_value, 2)),
+        testing_pool);
+
+    // In case of random, make sure this column has duplicated values
+    Value string_value = ValueFactory::GetStringValue(
+        std::to_string(ExecutorTestsUtil::PopulatedValue(
+            random ? std::rand() % (num_rows / 3) : populate_value, 3)));
+    tuple->SetValue(3, string_value, testing_pool);
+    assert(tuple->GetSchema());
+    tuples.push_back(std::move(tuple));
+  }
+  return tuples;
+}
+
+}  // End test namespace
+}  // End peloton namespace
