ef72db9b75f1f5b1adfa3d240fee6d69a37b90ac
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-04-21 19:14:17 -0400

    Add sync_commit as argument to ycsb

--- src/backend/benchmark/ycsb/ycsb.cpp
+++ src/backend/benchmark/ycsb/ycsb.cpp
@@ -35,45 +35,45 @@ std::ofstream out("outputfile.summary");
 
 static void WriteOutput(double stat) {
   LOG_INFO("----------------------------------------------------------");
-  LOG_INFO("%lf %d %d :: %lf tps", state.update_ratio, state.scale_factor,
-           state.column_count, stat);
+  LOG_INFO("%lf %d %d %d %d:: %lf tps", state.update_ratio, state.scale_factor,
+           state.column_count, state.logging_enabled, state.sync_commit,  stat);
 
   out << state.update_ratio << " ";
   out << state.scale_factor << " ";
   out << state.column_count << " ";
+  out << state.logging_enabled << " ";
+  out << state.sync_commit << " ";
   out << stat << "\n";
   out.flush();
 }
 
 inline void YCSBBootstrapLogger() {
-  if (!state.logging_enabled) return;
+  if (state.logging_enabled <= 0) return;
   peloton_logging_mode = LOGGING_TYPE_DRAM_NVM;
   auto& log_manager = peloton::logging::LogManager::GetInstance();
   if (peloton_logging_mode != LOGGING_TYPE_INVALID) {
     // Launching a thread for logging
     if (!log_manager.IsInLoggingMode()) {
-      // Set default logging mode
-      log_manager.SetSyncCommit(true);
-      //		  elog(DEBUG2, "Wait for standby mode");
+      // Set sync commit mode
+      if (state.sync_commit == 1) {
+        log_manager.SetSyncCommit(true);
+      }
 
       // Wait for standby mode
       std::thread(&peloton::logging::LogManager::StartStandbyMode, &log_manager)
           .detach();
       log_manager.WaitForModeTransition(peloton::LOGGING_STATUS_TYPE_STANDBY,
                                         true);
-      //		  elog(DEBUG2, "Standby mode");
 
       // Clean up database tile state before recovery from checkpoint
       log_manager.PrepareRecovery();
 
       // Do any recovery
       log_manager.StartRecoveryMode();
-      //		  elog(DEBUG2, "Wait for logging mode");
 
       // Wait for logging mode
       log_manager.WaitForModeTransition(peloton::LOGGING_STATUS_TYPE_LOGGING,
                                         true);
-      //		  elog(DEBUG2, "Logging mode");
 
       // Done recovery
       log_manager.DoneRecovery();
