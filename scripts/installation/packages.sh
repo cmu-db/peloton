0905b57ed5e1fd371d5930894efaafb15912434b
jarulraj <jarulraj@cs.cmu.edu>
2016-04-23 09:54:56 -0400

    Removed json spirit stuff

--- scripts/installation/packages.sh
+++ scripts/installation/packages.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
 # Set up environment
-apt-get -y install git g++ autoconf pkg-config libtool libjson-spirit-dev libreadline-dev \
-    libssl-dev valgrind \
+apt-get -y install git g++ autoconf pkg-config libtool libreadline-dev \
+    libssl-dev valgrind libboost1.55-dev libboost1.55-all-dev \
     python-pip python-xmlrunner \
     libprotobuf-dev protobuf-compiler libevent-dev libjemalloc-dev
 
 # Pip
 pip install unittest-xml-reporting
-
-# Install latest boost (1.55)
-add-apt-repository -y ppa:boost-latest/ppa
-apt-get -y --purge remove libboost-all-dev libboost-dev libboost-doc
-apt-get -y install -f
-dpkg --configure -a
-apt-get -y update
-apt-get install -y libboost1.55-tools-dev libboost1.55-dev libboost1.55-all-dev
