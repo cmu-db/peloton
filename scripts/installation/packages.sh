07ac78190dcb4470795d797dd135cb6b282779e4
Joy Arulraj <jarulraj@cs.cmu.edu>
2016-04-23 09:23:41 -0400

    Update packages.sh

--- scripts/installation/packages.sh
+++ scripts/installation/packages.sh
@@ -8,3 +8,8 @@ apt-get -y install git g++ autoconf pkg-config libtool libjson-spirit-dev librea
 
 # Pip
 pip install unittest-xml-reporting
+
+# Install boost 1.55
+add-apt-repository ppa:boost-latest/ppa
+apt-get -y update
+apt-get install -y libboost1.55-all-dev
