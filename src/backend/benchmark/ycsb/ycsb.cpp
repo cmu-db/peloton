9f893edf1e1049d04414fd41f520c04117df8d14
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-04-22 20:57:17 -0400

    add checkpointer to ycsb

--- src/backend/benchmark/ycsb/ycsb.cpp
+++ src/backend/benchmark/ycsb/ycsb.cpp
@@ -37,9 +37,9 @@ std::ofstream out("outputfile.summary");
 
 static void WriteOutput(double stat) {
   LOG_INFO("----------------------------------------------------------");
-  LOG_INFO("%lf %d %d %d %d %d :: %lf tps", state.update_ratio,
+  LOG_INFO("%lf %d %d %d %d %d %d :: %lf tps", state.update_ratio,
            state.scale_factor, state.column_count, state.logging_enabled,
-           state.sync_commit, state.file_size, stat);
+           state.sync_commit, state.file_size, state.checkpointer, stat);
 
   out << state.update_ratio << " ";
   out << state.scale_factor << " ";
@@ -48,6 +48,7 @@ static void WriteOutput(double stat) {
   out << state.sync_commit << " ";
   out << state.wait_timeout << " ";
   out << state.file_size << " ";
+  out << state.checkpointer << " ";
   out << stat << "\n";
   out.flush();
 }
@@ -55,8 +56,43 @@ static void WriteOutput(double stat) {
 inline void YCSBBootstrapLogger() {
   if (state.logging_enabled <= 0) return;
   peloton_logging_mode = LOGGING_TYPE_NVM_WAL;
-  peloton_wait_timeout = state.wait_timeout;
+
   auto& log_manager = peloton::logging::LogManager::GetInstance();
+  auto& checkpoint_manager = peloton::logging::CheckpointManager::GetInstance();
+
+  if (state.checkpointer != 0) {
+    peloton_checkpoint_mode = CHECKPOINT_TYPE_NORMAL;
+
+    // launch checkpoint thread
+    if (!checkpoint_manager.IsInCheckpointingMode()) {
+      // Wait for standby mode
+      std::thread(&peloton::logging::CheckpointManager::StartStandbyMode,
+                  &checkpoint_manager).detach();
+      checkpoint_manager.WaitForModeTransition(
+          peloton::CHECKPOINT_STATUS_STANDBY, true);
+
+      // Clean up table tile state before recovery from checkpoint
+      log_manager.PrepareRecovery();
+
+      // Do any recovery
+      checkpoint_manager.StartRecoveryMode();
+
+      // Wait for standby mode
+      checkpoint_manager.WaitForModeTransition(
+          peloton::CHECKPOINT_STATUS_DONE_RECOVERY, true);
+    }
+
+    // start checkpointing mode after recovery
+    if (peloton_checkpoint_mode != CHECKPOINT_TYPE_INVALID) {
+      if (!checkpoint_manager.IsInCheckpointingMode()) {
+        // Now, enter CHECKPOINTING mode
+        checkpoint_manager.SetCheckpointStatus(
+            peloton::CHECKPOINT_STATUS_CHECKPOINTING);
+      }
+    }
+  }
+
+  peloton_wait_timeout = state.wait_timeout;
   if (peloton_logging_mode != LOGGING_TYPE_INVALID) {
     // Launching a thread for logging
     if (!log_manager.IsInLoggingMode()) {
@@ -64,8 +100,8 @@ inline void YCSBBootstrapLogger() {
       if (state.sync_commit == 1) {
         log_manager.SetSyncCommit(true);
       }
-      log_manager.SetLogFileSizeLimit((unsigned int) state.file_size);
-      log_manager.SetLogBufferCapacity((unsigned int) state.log_buffer_size);
+      log_manager.SetLogFileSizeLimit((unsigned int)state.file_size);
+      log_manager.SetLogBufferCapacity((unsigned int)state.log_buffer_size);
 
       // Wait for standby mode
       std::thread(&peloton::logging::LogManager::StartStandbyMode, &log_manager)
@@ -91,7 +127,9 @@ inline void YCSBBootstrapLogger() {
 
 // Main Entry Point
 void RunBenchmark() {
+
   YCSBBootstrapLogger();
+
   // Create and load the user table
   CreateYCSBDatabase();
 
