a2d8e67610f818dd408b3245c470dc6f368601f7
Yingjun Wu <wu.yj0616@gmail.com>
2016-04-24 01:41:22 -0400

    fix concurrency bug

--- src/backend/catalog/manager.cpp
+++ src/backend/catalog/manager.cpp
@@ -38,7 +38,6 @@ Manager &Manager::GetInstance() {
 
 void Manager::AddTileGroup(
     const oid_t oid, const std::shared_ptr<storage::TileGroup> &location) {
-  std::shared_ptr<storage::TileGroup> old_location;
 
   {
     std::lock_guard<std::mutex> lock(locator_mutex);
