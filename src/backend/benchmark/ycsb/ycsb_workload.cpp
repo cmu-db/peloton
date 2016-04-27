b370db258f3be2579e8f9bd4f616ac4ad8b34ba4
Yingjun Wu <wu.yj0616@gmail.com>
2016-04-27 00:13:00 -0400

    fix benchmark bug

--- src/backend/benchmark/ycsb/ycsb_workload.cpp
+++ src/backend/benchmark/ycsb/ycsb_workload.cpp
@@ -345,12 +345,28 @@ bool RunRead() {
 
   ExecuteTest(executors);
 
-  Result result = txn_manager.CommitTransaction();
+  auto result = txn->GetResult();
+
+  // transaction passed execution.
   if (result == Result::RESULT_SUCCESS) {
-    return true;
-  } else {
+
+    result = txn_manager.CommitTransaction();
+
+    if (result == Result::RESULT_SUCCESS) {
+      // transaction passed commitment.
+      return true;
+    } else {
+      // transaction failed commitment.
+      assert(result == Result::RESULT_ABORTED ||
+             result == Result::RESULT_FAILURE);
+      return false;
+    }
+  }
+  // transaction aborted during execution.
+  else {
     assert(result == Result::RESULT_ABORTED ||
            result == Result::RESULT_FAILURE);
+    result = txn_manager.AbortTransaction();
     return false;
   }
 }
