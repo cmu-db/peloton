e9933ea6bb23325decdd87829b68427eade64f09
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-04-27 21:25:39 -0400

    Update script with new ycsb option

--- scripts/testing/logging/run_ycsb.sh
+++ scripts/testing/logging/run_ycsb.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
-SCALE=5
-#$WRITE=100
+SCALE=1
+WRITE=1.0
 rm -rf pl_log0/*.log
 rm -rf pl_checkpoint/*.log
 for backend in $(seq 1 1 20)
 do
-   for write in $(seq 0.50 0.40 0.990)
-   do
-        for size in $(seq 32 32 33)
-        do
-          for wait in $(seq 0 3 1)
-          do
-             for checkpoint in 0
-             do
-        #       ./rebuild.sh $pool
-                sh ./ycsb -k $SCALE -u $write -b $backend -l 1 -x 1 -f $size -w $wait -p $chechpoint;
-                sleep 1
-                cat outputfile.summary >> baseline.log
-                rm -rf pl_log0/*.log
-                rm -rf pl_checkpoint/*.log
 
+	filename="gc_on_sync_${WRITE}.log"
+  	sh ./ycsb -b ${backend} -l 1 -u $WRITE -x 1
+  	sleep 1
+	echo "-b $backend -l 1 -u ${WRITE} -x 1" >> $filename
+  	tail -n 1 outputfile.summary >> $filename
+        rm -rf pl_log0/*.log
+        rm -rf pl_checkpoint/*.log
 
-
-             done
-          done
-        done
-   done
-   # cleanup logs
-   rm -rf pl_log0/*.log
-   rm -rf pl_checkpoint/*.log
 done
