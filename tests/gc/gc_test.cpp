ecb8d22138e9647a99881cdfbdd035363bbd9a0f
runshenzhu <runshen.zhu@gmail.com>
2016-04-28 02:46:40 -0400

    gc test

--- tests/gc/gc_test.cpp
+++ tests/gc/gc_test.cpp
@@ -8,7 +8,7 @@
 
 #include "harness.h"
 #include "concurrency/transaction_tests_util.h"
-
+#include "backend/gc/gc_manager.h"
 namespace peloton {
 
 namespace test {
@@ -19,8 +19,7 @@ namespace test {
 
 class GCTest : public PelotonTest {};
 
-int StressDB(storage::DataTable *table, const int scale, const int num_key) {
-  const int num_txn = 16;
+int UpdateTable(storage::DataTable *table, const int scale, const int num_key, const int num_txn) {
   srand(15721);
 
 
@@ -45,21 +44,6 @@ int StressDB(storage::DataTable *table, const int scale, const int num_key) {
   }
   scheduler.Run();
 
-  // Read all values
-  TransactionScheduler scheduler2(1, table, &txn_manager);
-  for (int i = 0; i < num_key; i++) {
-    scheduler2.Txn(0).Read(i);
-  }
-  scheduler2.Txn(0).Commit();
-  scheduler2.Run();
-  // The sum should be zero
-  int sum = 0;
-  for (auto result : scheduler2.schedules[0].results) {
-    sum += result;
-  }
-
-  EXPECT_EQ(0, sum);
-
   // stats
   int nabort = 0;
   for (auto &schedule : scheduler.schedules) {
@@ -67,6 +51,19 @@ int StressDB(storage::DataTable *table, const int scale, const int num_key) {
   }
   LOG_INFO("Abort: %d out of %d", nabort, num_txn);
   return num_txn - nabort;
+}
+
+
+void ScanAndGC(storage::DataTable *table, const int num_key) {
+  auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+  TransactionScheduler scheduler(1, table, &txn_manager);
+  for (int i = 0; i < num_key; i++) {
+    scheduler.Txn(0).Read(i);
+  }
+  scheduler.Txn(0).Commit();
+  scheduler.Run();
+
+  EXPECT_TRUE(scheduler.schedules[0].txn_result == RESULT_SUCCESS);
 }
 
 int GarbageNum(storage::DataTable *table) {
@@ -94,22 +91,82 @@ int GarbageNum(storage::DataTable *table) {
   return old_num;
 }
 
+// get tuple recycled by GC
+int RecycledNum(storage::DataTable *table) {
+  int count = 0;
+  auto table_id = table->GetOid();
+  while(!gc::GCManagerFactory::GetInstance().ReturnFreeSlot(table_id).IsNull())
+    count++;
+
+  return count;
+}
+
+TEST_F(GCTest, SimpleTest) {
+
+  // create a table with only one key
+  const int num_key = 1;
+  std::unique_ptr<storage::DataTable> table(
+    TransactionTestsUtil::CreateTable(num_key, "TEST_TABLE", INVALID_OID, INVALID_OID, 1234, true));
+
+  // update this key 1 times, using only one thread
+  const int scale = 1;
+  const int thread_num = 1;
+  auto succ_num = UpdateTable(table.get(), scale, num_key, thread_num);
+
+  // transaction must success
+  EXPECT_EQ(succ_num, 1);
+
+  // count garbage num
+  auto old_num = GarbageNum(table.get());
+
+  // there should be only one garbage
+  // genereated by the last update
+  EXPECT_EQ(old_num, 1);
+
+  // index scan and gc
+  ScanAndGC(table.get(), num_key);
+
+  // there should be garbage
+  old_num = GarbageNum(table.get());
+  EXPECT_EQ(old_num, 0);
+
+  // sleep a while for gc to finish its job
+  std::this_thread::sleep_for(
+    3 * std::chrono::milliseconds(GC_PERIOD_MILLISECONDS));
+
+  // there should be 1 tuple recycled
+  EXPECT_EQ(1, RecycledNum(table.get()));
+}
+
+
 TEST_F(GCTest, StressTest) {
   const int num_key = 256;
-  const int scale = 4;
+  const int scale = 1;
   std::unique_ptr<storage::DataTable> table(
-    TransactionTestsUtil::CreateTable(num_key));
+    TransactionTestsUtil::CreateTable(num_key, "TEST_TABLE", INVALID_OID, INVALID_OID, 1234, true));
 
-  // firstly, stress db to create garbage
-  auto succ_num = StressDB(table.get(), scale, num_key);
+  // stress db to create garbage
+  auto succ_num = UpdateTable(table.get(), scale, num_key, 16);
 
   // count garbage number
   auto old_num = GarbageNum(table.get());
 
-  // garbage number should be scale * succ update time * how many tuples updated each time
-  EXPECT_EQ(scale * succ_num * 2, old_num);
+  // EXPECT_EQ(scale * succ_num * 2, old_num);
+  EXPECT_TRUE(old_num > 0);
+  ScanAndGC(table.get(), num_key);
 
+  old_num = GarbageNum(table.get());
+
+  EXPECT_EQ(old_num, 0);
+
+  // sleep a while to wait gc to finish its work
+  std::this_thread::sleep_for(
+    3 * std::chrono::milliseconds(GC_PERIOD_MILLISECONDS));
+
+  // garbage number should be scale * succ update time * how many tuples updated each time
+  EXPECT_EQ(scale * succ_num * 2, RecycledNum(table.get()));
 
 }
+
 }  // End test namespace
 }  // End peloton namespace
