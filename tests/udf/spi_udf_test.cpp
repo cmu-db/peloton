5e22947e075991e117c8566ab519af70db8db8e3
Seunghyun Lee <shlee0605@gmail.com>
2016-04-29 00:24:37 -0400

    finished to add item sales sum functions

--- tests/udf/spi_udf_test.cpp
+++ tests/udf/spi_udf_test.cpp
@@ -37,6 +37,7 @@ execq(text *sql)
     TupleDesc tupdesc = SPI_tuptable->tupdesc;
     SPITupleTable *tuptable = SPI_tuptable;
     char buf[8192];
+
     int i, j;
 
     for (j = 0; j < proc; j++)
@@ -57,6 +58,48 @@ execq(text *sql)
   return (proc);
 }
 
+
+PG_FUNCTION_INFO_V1(item_sales_sum_c);
+Datum
+item_sales_sum_c(PG_FUNCTION_ARGS) {
+  int ret;
+  int proc;
+  float8 result;
+  double val;
+  char * end;
+  char command[512];
+
+  int32 item_id = PG_GETARG_INT32(0);
+  sprintf(command, "select item.i_price from order_line,item where order_line.ol_i_id = %d and order_line.ol_i_id = item.i_id", item_id);
+
+  SPI_connect();
+
+  ret = SPI_exec(command, 0);
+
+  proc = SPI_processed;
+  result = 0.0;
+
+  if (ret > 0 && SPI_tuptable != NULL)
+  {
+    TupleDesc tupdesc = SPI_tuptable->tupdesc;
+    SPITupleTable *tuptable = SPI_tuptable;
+    char buf[8192];
+    int i, j;
+
+    for (j = 0; j < proc; j++)
+    {
+      HeapTuple tuple = tuptable->vals[j];
+
+      for (i = 1, buf[0] = 0; i <= tupdesc->natts; i++)
+        val = strtod(SPI_getvalue(tuple, tupdesc, i), &end);
+        result += val;
+    }
+  }
+
+  SPI_finish();
+
+  PG_RETURN_FLOAT8(result);
+}
 
 
 #ifdef __cplusplus
