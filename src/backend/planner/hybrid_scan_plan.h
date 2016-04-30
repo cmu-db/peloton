692e9ead4bef376d668c4b9a7bc321a01fe683ee
amaliujia <amaliujia@163.com>
2016-04-30 10:30:26 -0400

    add index_scan_part in hybrid scan plan

--- src/backend/planner/hybrid_scan_plan.h
+++ src/backend/planner/hybrid_scan_plan.h
@@ -5,13 +5,16 @@
 #pragma once
 
 #include "backend/planner/abstract_plan.h"
+#include "backend/planner/index_scan_plan.h"
 #include "backend/storage/data_table.h"
 #include "backend/index/index.h"
+#include "backend/common/types.h"
+#include "backend/expression/abstract_expression.h"
 
 namespace peloton {
 namespace planner{
 
-class HybridScanPlan : public AbstractPlan{
+class HybridScanPlan : public AbstractPlan {
 public:
   HybridScanPlan(const HybridScanPlan &) = delete;
   HybridScanPlan &operator=(const HybridScanPlan &) = delete;
@@ -23,6 +26,23 @@ public:
     : index_(index), table_(table) {}
 
 
+  HybridScanPlan(storage::DataTable *table, expression::AbstractExpression *predicate,
+      const std::vector<oid_t> &column_ids)
+      : AbstractScan(table, predicate, column_ids) {}
+
+  HybridScanPlan(storage::DataTable *table,
+                expression::AbstractExpression *predicate,
+                const std::vector<oid_t> &column_ids,
+                const IndexScanPlan::IndexScanDesc &index_scan_desc)
+                : AbstractScan(table, predicate, column_ids),
+                index_(index_scan_desc.index),
+                column_ids_(column_ids),
+                key_column_ids_(std::move(index_scan_desc.key_column_ids)),
+                expr_types_(std::move(index_scan_desc.expr_types)),
+                values_(std::move(index_scan_desc.values)),
+                runtime_keys_(std::move(index_scan_desc.runtime_keys)) {}
+
+
   index::Index *GetDataIndex() const {
     return this->index_;
   }
@@ -31,10 +51,32 @@ public:
     return this->table_;
   }
 
+  std::unique_ptr<AbstractPlan> Copy() const {
+    return std::unique_ptr<AbstractPlan>(nullptr);
+  }
+
 private:
   index::Index *index_ = nullptr;
 
   storage::DataTable *table_ = nullptr;
+
+  /** @brief Selection predicate. */
+  const std::unique_ptr<expression::AbstractExpression> predicate_;
+
+  /** @brief Columns from tile group to be added to logical tile output. */
+  std::vector<oid_t> column_ids_;
+
+
+  const std::vector<oid_t> column_ids_;
+
+  const std::vector<oid_t> key_column_ids_;
+
+  const std::vector<ExpressionType> expr_types_;
+
+  const std::vector<Value> values_;
+
+  const std::vector<expression::AbstractExpression *> runtime_keys_;
+};
 
 };
 
