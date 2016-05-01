02631f5aeabb77efcf543375e8cf423f4ba563cc
Xian Ran <xranthoar@gmail.com>
2016-05-01 01:13:43 -0400

    dirty

--- src/backend/benchmark/ycsb/ycsb_workload.cpp
+++ src/backend/benchmark/ycsb/ycsb_workload.cpp
@@ -74,13 +74,113 @@ namespace peloton {
 namespace benchmark {
 namespace ycsb {
 
+#ifndef __COMMON_FAST_RANDOM_H__
+#define __COMMON_FAST_RANDOM_H__
+
+#include <cstdint>
+#include <string>
+class fast_random {
+public:
+  fast_random(unsigned long seed)
+    : seed(0)
+  {
+    set_seed0(seed);
+  }
+
+  inline unsigned long
+    next()
+  {
+      return ((unsigned long)next(32) << 32) + next(32);
+    }
+
+  inline uint32_t
+    next_u32()
+  {
+      return next(32);
+    }
+
+  inline uint16_t
+    next_u16()
+  {
+      return (uint16_t)next(16);
+    }
+
+  /** [0.0, 1.0) */
+  inline double
+    next_uniform()
+  {
+      return (((unsigned long)next(26) << 27) + next(27)) / (double)(1L << 53);
+    }
+
+  inline char
+    next_char()
+  {
+      return next(8) % 256;
+    }
+
+  inline char
+    next_readable_char()
+  {
+      static const char readables[] = "0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+      return readables[next(6)];
+    }
+
+  inline std::string
+    next_string(size_t len)
+  {
+      std::string s(len, 0);
+      for (size_t i = 0; i < len; i++)
+        s[i] = next_char();
+      return s;
+    }
+
+  inline std::string
+    next_readable_string(size_t len)
+  {
+      std::string s(len, 0);
+      for (size_t i = 0; i < len; i++)
+        s[i] = next_readable_char();
+      return s;
+    }
+
+  inline unsigned long
+    get_seed()
+  {
+      return seed;
+    }
+
+  inline void
+    set_seed(unsigned long seed)
+  {
+      this->seed = seed;
+    }
+
+private:
+  inline void
+    set_seed0(unsigned long seed)
+  {
+      this->seed = (seed ^ 0x5DEECE66DL) & ((1L << 48) - 1);
+    }
+
+  inline unsigned long
+    next(unsigned int bits)
+  {
+      seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
+      return (unsigned long)(seed >> (48 - bits));
+    }
+
+  unsigned long seed;
+};
+
+#endif
+
 /////////////////////////////////////////////////////////
 // TRANSACTION TYPES
 /////////////////////////////////////////////////////////
 
 bool RunRead();
 
-bool RunUpdate();
+bool RunUpdate(fast_random &rng);
 
 /////////////////////////////////////////////////////////
 // WORKLOAD
@@ -109,6 +209,8 @@ void RunBackend(oid_t thread_id) {
   oid_t &execution_count_ref = abort_counts[thread_id];
   oid_t &transaction_count_ref = commit_counts[thread_id];
 
+  fast_random rng(rand());
+
   // Run these many transactions
   while (true) {
     if (is_running == false) {
@@ -117,7 +219,7 @@ void RunBackend(oid_t thread_id) {
     auto rng_val = generator.GetSample();
 
     if (rng_val < update_ratio) {
-      while (RunUpdate() == false) {
+      while (RunUpdate(rng) == false) {
         execution_count_ref++;
       }
     } else {
@@ -406,7 +508,7 @@ bool RunRead() {
   }
 }
 
-bool RunUpdate() {
+bool RunUpdate(fast_random &rng) {
   auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
 
   auto txn = txn_manager.BeginTransaction();
@@ -438,7 +540,7 @@ bool RunUpdate() {
     std::vector<expression::AbstractExpression *> runtime_keys;
 
     auto tuple_count = state.scale_factor * DEFAULT_TUPLES_PER_TILEGROUP;
-    auto lookup_key = rand() % tuple_count;
+    auto lookup_key = rng.next_u32() % tuple_count;
 
     key_column_ids.push_back(0);
     expr_types.push_back(ExpressionType::EXPRESSION_TYPE_COMPARE_EQUAL);
