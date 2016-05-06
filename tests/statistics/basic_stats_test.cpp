526ed9a78b609b0f3dc2fa5f504f544ea3165ece
aaron276h <aaron276h@yahoo.com>
2016-05-06 21:28:38 +0000

    slight update

--- tests/statistics/basic_stats_test.cpp
+++ tests/statistics/basic_stats_test.cpp
@@ -13,10 +13,15 @@
 #include "harness.h"
 
 #include "backend/statistics/stats_aggregator.h"
+#include "backend/statistics/backend_stats_context.h"
 #include "backend/storage/data_table.h"
 #include "backend/concurrency/transaction_manager_factory.h"
 #include "executor/executor_tests_util.h"
 #include "concurrency/transaction_tests_util.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
 
 namespace peloton {
   namespace test {
@@ -24,19 +29,32 @@ namespace peloton {
     class BasicStatsTest : public PelotonTest {};
     
     TEST_F(BasicStatsTest, CreateTest) {
-      peloton::stats::StatsAggregator * stats_ = new peloton::stats::StatsAggregator();
-      int tuple_count = 10;
+      /*
+       * Register to StatsAggregator
+       */
+      peloton::stats::BackendStatsContext* stats_ = peloton::stats::StatsAggregator::GetInstance().GetBackendStatsContext();
+      printf("have a stats context? %p\n", stats_);
+      
+      //int tuple_count = 10;
 
-      // Create a table and wrap it in logical tiles                                                                          
+      // Create a table and wrap it in logical tiles
+      /*
       auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
       auto txn = txn_manager.BeginTransaction();
       std::unique_ptr<storage::DataTable> data_table(
 						     ExecutorTestsUtil::CreateTable(tuple_count, false));
-      ExecutorTestsUtil::PopulateTable(txn, data_table.get(), tuple_count, false,
-				       false, true);
-      txn_manager.CommitTransaction();
 
-      delete stats_;
+      */
+       peloton::stats::StatsAggregator * stats_agg = new peloton::stats::StatsAggregator();
+       /*
+       ExecutorTestsUtil::PopulateTable(txn, data_table.get(), tuple_count, false,
+				       false, true);
+       txn_manager.CommitTransaction();
+       */
+
+      //printf("%s", stats_->aggregated_stats.ToString().c_str());
+
+       delete stats_agg;
     }
   }
 }
