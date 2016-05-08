c10bc9bbc0bab05926a8fe08eecac8d6ae104d66
Yingjun Wu <wu.yj0616@gmail.com>
2016-05-07 20:40:53 -0400

    refactor tpcc

---
+++ src/backend/benchmark/tpcc/tpcc_order_status.cpp
@@ -0,0 +1,96 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// workload.cpp
+//
+// Identification: benchmark/tpcc/workload.cpp
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <ctime>
+#include <cassert>
+#include <thread>
+#include <algorithm>
+#include <random>
+#include <cstddef>
+#include <limits>
+
+
+#include "backend/benchmark/tpcc/tpcc_workload.h"
+#include "backend/benchmark/tpcc/tpcc_configuration.h"
+#include "backend/benchmark/tpcc/tpcc_loader.h"
+
+#include "backend/catalog/manager.h"
+#include "backend/catalog/schema.h"
+
+#include "backend/common/types.h"
+#include "backend/common/value.h"
+#include "backend/common/value_factory.h"
+#include "backend/common/logger.h"
+#include "backend/common/timer.h"
+#include "backend/common/generator.h"
+
+#include "backend/concurrency/transaction.h"
+#include "backend/concurrency/transaction_manager_factory.h"
+
+#include "backend/executor/executor_context.h"
+#include "backend/executor/abstract_executor.h"
+#include "backend/executor/logical_tile.h"
+#include "backend/executor/logical_tile_factory.h"
+#include "backend/executor/materialization_executor.h"
+#include "backend/executor/update_executor.h"
+#include "backend/executor/index_scan_executor.h"
+#include "backend/executor/insert_executor.h"
+
+#include "backend/expression/abstract_expression.h"
+#include "backend/expression/constant_value_expression.h"
+#include "backend/expression/tuple_value_expression.h"
+#include "backend/expression/comparison_expression.h"
+#include "backend/expression/expression_util.h"
+#include "backend/expression/container_tuple.h"
+
+#include "backend/index/index_factory.h"
+
+#include "backend/logging/log_manager.h"
+
+#include "backend/planner/abstract_plan.h"
+#include "backend/planner/materialization_plan.h"
+#include "backend/planner/insert_plan.h"
+#include "backend/planner/update_plan.h"
+#include "backend/planner/index_scan_plan.h"
+
+#include "backend/storage/data_table.h"
+#include "backend/storage/table_factory.h"
+
+
+
+namespace peloton {
+namespace benchmark {
+namespace tpcc {
+
+bool RunOrderStatus(){
+  /*
+    "ORDER_STATUS": {
+    "getCustomerByCustomerId": "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_BALANCE FROM CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?", # w_id, d_id, c_id
+    "getCustomersByLastName": "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_BALANCE FROM CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_LAST = ? ORDER BY C_FIRST", # w_id, d_id, c_last
+    "getLastOrder": "SELECT O_ID, O_CARRIER_ID, O_ENTRY_D FROM ORDERS WHERE O_W_ID = ? AND O_D_ID = ? AND O_C_ID = ? ORDER BY O_ID DESC LIMIT 1", # w_id, d_id, c_id
+    "getOrderLines": "SELECT OL_SUPPLY_W_ID, OL_I_ID, OL_QUANTITY, OL_AMOUNT, OL_DELIVERY_D FROM ORDER_LINE WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?", # w_id, d_id, o_id
+    }
+   */
+    return true;
+}
+
+}
+}
+}
---
+++ src/backend/benchmark/tpcc/tpcc_stock_level.cpp
@@ -0,0 +1,94 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// workload.cpp
+//
+// Identification: benchmark/tpcc/workload.cpp
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <ctime>
+#include <cassert>
+#include <thread>
+#include <algorithm>
+#include <random>
+#include <cstddef>
+#include <limits>
+
+
+#include "backend/benchmark/tpcc/tpcc_workload.h"
+#include "backend/benchmark/tpcc/tpcc_configuration.h"
+#include "backend/benchmark/tpcc/tpcc_loader.h"
+
+#include "backend/catalog/manager.h"
+#include "backend/catalog/schema.h"
+
+#include "backend/common/types.h"
+#include "backend/common/value.h"
+#include "backend/common/value_factory.h"
+#include "backend/common/logger.h"
+#include "backend/common/timer.h"
+#include "backend/common/generator.h"
+
+#include "backend/concurrency/transaction.h"
+#include "backend/concurrency/transaction_manager_factory.h"
+
+#include "backend/executor/executor_context.h"
+#include "backend/executor/abstract_executor.h"
+#include "backend/executor/logical_tile.h"
+#include "backend/executor/logical_tile_factory.h"
+#include "backend/executor/materialization_executor.h"
+#include "backend/executor/update_executor.h"
+#include "backend/executor/index_scan_executor.h"
+#include "backend/executor/insert_executor.h"
+
+#include "backend/expression/abstract_expression.h"
+#include "backend/expression/constant_value_expression.h"
+#include "backend/expression/tuple_value_expression.h"
+#include "backend/expression/comparison_expression.h"
+#include "backend/expression/expression_util.h"
+#include "backend/expression/container_tuple.h"
+
+#include "backend/index/index_factory.h"
+
+#include "backend/logging/log_manager.h"
+
+#include "backend/planner/abstract_plan.h"
+#include "backend/planner/materialization_plan.h"
+#include "backend/planner/insert_plan.h"
+#include "backend/planner/update_plan.h"
+#include "backend/planner/index_scan_plan.h"
+
+#include "backend/storage/data_table.h"
+#include "backend/storage/table_factory.h"
+
+
+
+namespace peloton {
+namespace benchmark {
+namespace tpcc {
+
+bool RunStockLevel() {
+  /*
+     "STOCK_LEVEL": {
+     "getOId": "SELECT D_NEXT_O_ID FROM DISTRICT WHERE D_W_ID = ? AND D_ID = ?",
+     "getStockCount": "SELECT COUNT(DISTINCT(OL_I_ID)) FROM ORDER_LINE, STOCK  WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID < ? AND OL_O_ID >= ? AND S_W_ID = ? AND S_I_ID = OL_I_ID AND S_QUANTITY < ?
+     }
+   */
+     return true;
+}
+
+}
+}
+}
