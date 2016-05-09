a88828482e99e132c2985ca211f1c24eae192535
amaliujia <amaliujia@163.com>
2016-05-09 00:19:03 -0400

    update

--- tests/index/hybrid_index_test.cpp
+++ tests/index/hybrid_index_test.cpp
@@ -49,7 +49,7 @@ static int columncount = 4;
 static size_t tuples_per_tile_group = 10000;
 static size_t tile_group = 100;
 static float scalar = 0.9;
-static size_t iter = 300;
+static size_t iter = 400;
 
 void CreateTable(std::unique_ptr<storage::DataTable>& hyadapt_table, bool indexes) {
   oid_t column_count = projectivity * columncount;
@@ -449,7 +449,7 @@ void BuildIndex(index::Index *index, storage::DataTable *table) {
   }
 }
 
-/*TEST_F(HybridIndexTests, SeqScanTest) {
+TEST_F(HybridIndexTests, SeqScanTest) {
   std::unique_ptr<storage::DataTable> hyadapt_table;
   CreateTable(hyadapt_table, false);
   LoadTable(hyadapt_table);
@@ -466,7 +466,7 @@ TEST_F(HybridIndexTests, IndexScanTest) {
 
   for (size_t i = 0; i < iter; i++)
     LaunchIndexScan(hyadapt_table);
-}*/
+}
 
 TEST_F(HybridIndexTests, HybridScanTest) {
   std::unique_ptr<storage::DataTable> hyadapt_table;
