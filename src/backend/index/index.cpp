ba6da2390e269aaaa0d94963c5fe5b69f9026a00
runshenzhu <runshen.zhu@gmail.com>
2016-05-12 13:00:32 -0400

    clean

--- src/backend/index/index.cpp
+++ src/backend/index/index.cpp
@@ -59,7 +59,6 @@ bool Index::Compare(const AbstractTuple &index_key,
   // Go over each attribute in the list of comparison columns
   for (auto column_itr : key_column_ids) {
     key_column_itr++;
-    //(void) column_itr;
     const Value &rhs = values[key_column_itr];
     const Value &lhs = index_key.GetValue(id_map[column_itr]);
     const ExpressionType expr_type = expr_types[key_column_itr];
