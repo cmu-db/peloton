b84f583981ca6e1a26ae8d8a08623bfd5b63ab54
jarulraj <jarulraj@cs.cmu.edu>
2016-05-15 00:05:40 -0400

    Fixed validation

--- scripts/validators/validator.py
+++ scripts/validators/validator.py
@@ -10,6 +10,7 @@ import logging
 import os
 import re
 import sys
+import pprint
 
 ## ==============================================
 ## CONFIGURATION
@@ -34,12 +35,12 @@ DEFAULT_DIRS.append(PELOTON_TESTS_DIR)
 EXIT_SUCCESS = 0
 EXIT_FAILURE = -1
 
-VALIDATOR_PATTERNS = ["std\:\:cout", " printf\(", "cout", " malloc\(", " free\(", 
+VALIDATOR_PATTERNS = ["std\:\:cout", " printf\(", "cout", " malloc\(", " free\(",
                       " memset\(", " memcpy\(", " \:\:memset\(", " \:\:memcpy\(", " std\:\:memset\(", " std\:\:memcpy\(",
                       "\_\_attribute\_\_\(\(unused\)\)"]
 
 ## ==============================================
-## 			LOGGING CONFIGURATION
+##             LOGGING CONFIGURATION
 ## ==============================================
 
 LOG = logging.getLogger(__name__)
@@ -52,64 +53,75 @@ LOG_handler.setFormatter(LOG_formatter)
 LOG.addHandler(LOG_handler)
 LOG.setLevel(logging.INFO)
 
+SKIP_FILES_LIST = [
+    "src/backend/common/allocator.cpp",
+    "src/backend/common/macros.h"
+];
+
 ## ==============================================
-## 		  UTILITY FUNCTION DEFINITIONS
+##           UTILITY FUNCTION DEFINITIONS
 ## ==============================================
 
 #validate the file passed as argument
 def validate_file(file_path):
 
-	file_name = os.path.basename(file_path)
-	abs_path = os.path.abspath(file_path)
-	rel_path_from_peloton_dir = os.path.relpath(abs_path,PELOTON_DIR)
+    file_name = os.path.basename(file_path)
+    abs_path = os.path.abspath(file_path)
+    rel_path_from_peloton_dir = os.path.relpath(abs_path,PELOTON_DIR)
 
-	file = open(abs_path, "r")
+    # Skip some files
+    if rel_path_from_peloton_dir in SKIP_FILES_LIST:
+        return True
+    
+    file = open(abs_path, "r")
 
-	for line in file:
-		for validator_pattern in VALIDATOR_PATTERNS:
-			# Check for patterns one at a time
-			if re.search(validator_pattern, line):
-				LOG.info("Invalid pattern -- " + validator_pattern + " -- found in : " + file_path)
-				LOG.info("Line :: " + line)				
-				return False
-			
-	file.close()
-	return True
+    for line in file:
+        for validator_pattern in VALIDATOR_PATTERNS:
+            # Check for patterns one at a time
+            if re.search(validator_pattern, line):
+                LOG.info("Invalid pattern -- " + validator_pattern + " -- found in : " + file_path)
+                LOG.info("Line :: " + line)
+                return False
+
+    file.close()
+    return True
 
 #validate all the files in the dir passed as argument
 def validate_dir(dir_path):
 
-	for subdir, dirs, files in os.walk(dir_path):
-		for file in files:
-			file_path = subdir + os.path.sep + file
+    for subdir, dirs, files in os.walk(dir_path):
+        for file in files:
+            file_path = subdir + os.path.sep + file
 
-			if file_path.endswith(".h") or file_path.endswith(".cpp"):
-				status = validate_file(file_path)
-				if status == False:
-					return False
+            if file_path.endswith(".h") or file_path.endswith(".cpp"):
+                status = validate_file(file_path)
+                if status == False:
+                    return False
 
-			#END IF
-		#END FOR [file]
-	#END FOR [os.walk]
+            #END IF
+        #END FOR [file]
+    #END FOR [os.walk]
 #END ADD_HEADERS_DIR(DIR_PATH)
 
 ## ==============================================
-## 				Main Function
+##                 Main Function
 ## ==============================================
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(description='Check for some strings')
+    parser = argparse.ArgumentParser(description='Check for some strings')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	for dir in DEFAULT_DIRS:
-		LOG.info("Scanning : " + dir)
+    for dir in DEFAULT_DIRS:
+        LOG.info("Scanning : " + dir)
 
-		status = validate_dir(dir)
-		if status == False:
-			LOG.info("Validation not successful")
-			sys.exit(EXIT_FAILURE)
+        status = validate_dir(dir)
+        if status == False:
+            LOG.info("Validation not successful")
+            sys.exit(EXIT_FAILURE)
 
-	LOG.info("Validation successful")
-	sys.exit(EXIT_SUCCESS)
+    LOG.info("Validation successful")
+    sys.exit(EXIT_SUCCESS)
+            
+            
