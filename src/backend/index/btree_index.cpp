629a44ca44f9a864ab52ceefd04483a4b3811c35
amaliujia <amaliujia@163.com>
2016-05-18 14:17:06 -0400

    update

--- src/backend/index/btree_index.cpp
+++ src/backend/index/btree_index.cpp
@@ -401,6 +401,7 @@ void BTreeIndex<KeyType, ValueType, KeyComparator, KeyEqualityChecker>::FindMaxM
   std::map<oid_t, std::pair<Value, Value>>& non_leading_columns) {
 
   // find extreme nums on each column.
+  std::cout << "FindMinMax\t" << "leading column " << leading_column_id << std::endl;
   for (size_t i = 0; i < key_column_ids.size(); i++) {
     oid_t column_id = key_column_ids[i];
     if (column_id == leading_column_id) {
@@ -412,14 +413,20 @@ void BTreeIndex<KeyType, ValueType, KeyComparator, KeyEqualityChecker>::FindMaxM
       std::pair<Value, Value> range(Value::GetMaxValue(type), Value::GetMinValue(type));
       std::pair<oid_t, std::pair<Value, Value>> key_value(column_id, range);
       non_leading_columns.insert(key_value);
-    }
+      std::cout << "Insert a init kv\t" << non_leading_columns[column_id].first.GetInfo() << "\t" <<
+                              non_leading_columns[column_id].second.GetInfo() << std::endl;
+     }
 
     if (IfForwardExpression(expr_types[i]) || expr_types[i] == EXPRESSION_TYPE_COMPARE_EQUAL) {
+      std::cout << "min cur " << non_leading_columns[column_id].first.GetInfo() << " compare " << values[i].GetInfo() << std::endl;
       if (non_leading_columns[column_id].first.Compare(values[i]) == VALUE_COMPARE_GREATERTHAN) {
+        std::cout << "Update min " << std::endl;
         non_leading_columns[column_id].first = values[i];
       }
     } else if (IfBackwardExpression(expr_types[i]) || expr_types[i] == EXPRESSION_TYPE_COMPARE_EQUAL) {
+      std::cout << "max cur " << non_leading_columns[column_id].second.GetInfo() << " compare " << values[i].GetInfo() << std::endl;
       if (non_leading_columns[column_id].second.Compare(values[i]) == VALUE_COMPARE_LESSTHAN) {
+        std::cout << "Update max " << std::endl;
         non_leading_columns[column_id].second = values[i];
       }
     }
