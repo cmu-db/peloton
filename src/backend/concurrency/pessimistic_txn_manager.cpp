f551cf72863884b0198876e3c2ef59a38237a5bb
Yingjun Wu <wu.yj0616@gmail.com>
2016-05-29 21:58:46 -0400

    do not recycle invalid inserted tuples

--- src/backend/concurrency/pessimistic_txn_manager.cpp
+++ src/backend/concurrency/pessimistic_txn_manager.cpp
@@ -608,7 +608,7 @@ Result PessimisticTxnManager::AbortTransaction() {
         tile_group_header->SetTransactionId(tuple_slot, INVALID_TXN_ID);
 
         // GC recycle
-        RecycleInvalidTupleSlot(tile_group_id, tuple_slot);
+        //RecycleInvalidTupleSlot(tile_group_id, tuple_slot);
 
       } else if (tuple_entry.second == RW_TYPE_INS_DEL) {
         tile_group_header->SetEndCommitId(tuple_slot, MAX_CID);
