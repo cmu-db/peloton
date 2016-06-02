d0c1e97530ec49cd251f1d3a1ff3c7e37f5b886c
jarulraj <jarulraj@cs.cmu.edu>
2016-06-02 09:43:15 -0400

    Added cmake files

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,29 @@
+# This list is required for static linking and exported to PelotonConfig.cmake
+set(Peloton_LINKER_LIBS "")
+
+# ---[ Boost
+find_package(Boost 1.46 REQUIRED COMPONENTS system filesystem)
+include_directories(SYSTEM ${Boost_INCLUDE_DIR})
+list(APPEND Peloton_LINKER_LIBS ${Boost_LIBRARIES})
+
+# ---[ Threads
+#find_package(Threads REQUIRED)
+#list(APPEND Peloton_LINKER_LIBS ${CMAKE_THREAD_LIBS_INIT})
+
+# ---[ Google-glog
+include("cmake/External/glog.cmake")
+include_directories(SYSTEM ${GLOG_INCLUDE_DIRS})
+list(APPEND Peloton_LINKER_LIBS ${GLOG_LIBRARIES})
+
+# ---[ Google-gflags
+include("cmake/External/gflags.cmake")
+include_directories(SYSTEM ${GFLAGS_INCLUDE_DIRS})
+list(APPEND Peloton_LINKER_LIBS ${GFLAGS_LIBRARIES})
+
+# ---[ Google-protobuf
+include(cmake/ProtoBuf.cmake)
+
+# ---[ Doxygen
+if(BUILD_docs)
+  find_package(Doxygen)
+endif()
