a9e29c249d07d43219838506e11e6745569e7fc7
jarulraj <jarulraj@cs.cmu.edu>
2016-06-05 00:05:17 -0400

    Checkpoint

--- src/main/peloton/peloton.cpp
+++ src/main/peloton/peloton.cpp
@@ -14,7 +14,6 @@
 #include <iostream>
 
 #include "common/macros.h"
-#include "brain/sample.h"
 #include "wire/socket_base.h"
 #include "wire/wire.h"
 
@@ -22,17 +21,12 @@
  * Any Postgres server process begins execution here.
  */
 int main(UNUSED_ATTRIBUTE int argc, UNUSED_ATTRIBUTE char *argv[]) {
-  printf("foo\n");
-
-  peloton::brain::Sample sample(2);
-  std::cout << sample;
-
-  return 0;
 
   if (argc != 2) {
-    std::cout << "Usage: ./wire_server [port]" << std::endl;
+    std::cout << "Usage: ./peloton [port]" << std::endl;
     exit(EXIT_FAILURE);
   }
+
   peloton::wire::Server server(atoi(argv[1]), MAX_CONNECTIONS);
   peloton::wire::start_server(&server);
   peloton::wire::handle_connections<peloton::wire::PacketManager,
