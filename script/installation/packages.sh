d23ff72906379efae4f83acd6a0242f6fe3c79e1
jarulraj <jarulraj@cs.cmu.edu>
2016-06-06 10:53:17 -0400

    Added jsoncpp

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -4,7 +4,7 @@
 apt-get -y install git g++ autoconf pkg-config libtool libreadline-dev \
     libssl-dev valgrind libboost-dev libboost-filesystem-dev \
     python-pip python-xmlrunner \
-    libprotobuf-dev protobuf-compiler libevent-dev libjemalloc-dev
+    libprotobuf-dev protobuf-compiler libevent-dev libjemalloc-dev libjsoncpp-dev
 
 # Pip
 pip install unittest-xml-reporting
