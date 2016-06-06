d80a7f260591cfc4bf48e0eb828baf4e3337e3af
jarulraj <jarulraj@cs.cmu.edu>
2016-06-06 10:56:46 -0400

    Added more dependencies

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -3,8 +3,9 @@
 # Set up environment
 apt-get -y install git g++ autoconf pkg-config libtool libreadline-dev \
     libssl-dev valgrind libboost-dev libboost-filesystem-dev \
-    python-pip python-xmlrunner \
-    libprotobuf-dev protobuf-compiler libevent-dev libjemalloc-dev libjsoncpp-dev
+    python-pip python-xmlrunner bison flex \
+    libprotobuf-dev protobuf-compiler libevent-dev libjemalloc-dev libjsoncpp-dev \
+    libgflags-dev libgoogle-glog-dev
 
 # Pip
 pip install unittest-xml-reporting
