055b4b2cd87ad30c38ffc7753a36921d8db70e94
jarulraj <jarulraj@cs.cmu.edu>
2016-06-07 22:41:51 -0400

    Removed glog from dependency list

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -10,11 +10,6 @@ list(APPEND Peloton_LINKER_LIBS ${Boost_LIBRARIES})
 find_package(Threads REQUIRED)
 list(APPEND Peloton_LINKER_LIBS ${CMAKE_THREAD_LIBS_INIT})
 
-# ---[ Google-glog
-include("cmake/External/glog.cmake")
-include_directories(SYSTEM ${GLOG_INCLUDE_DIRS})
-list(APPEND Peloton_LINKER_LIBS ${GLOG_LIBRARIES})
-
 # ---[ Google-gflags
 include("cmake/External/gflags.cmake")
 include_directories(SYSTEM ${GFLAGS_INCLUDE_DIRS})
