1f90c6b35b422794225898c4624dff8284cb2c69
jarulraj <jarulraj@cs.cmu.edu>
2016-06-07 22:27:14 -0400

    travis

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -4,6 +4,4 @@
 apt-get -y install git g++ cmake \
     libboost-dev libboost-filesystem-dev \
     libprotobuf-dev protobuf-compiler libevent-dev libjemalloc-dev libjsoncpp-dev \
-    libgflags-dev libgoogle-glog-dev \
-    doxygen valgrind \
-    libsqlite3-dev
+    doxygen valgrind libsqlite3-dev
