a7e5200821df161df050ade89c14f83699140fa3
jarulraj <jarulraj@cs.cmu.edu>
2016-06-08 12:15:39 -0400

    Updated package list

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Set up environment
-apt-get --ignore-missing -y install \
+apt-get -y install \
     git g++ cmake \
     libprotobuf-dev protobuf-compiler libevent-dev \
     libboost-dev libboost-filesystem-dev \
-    libjemalloc-dev libjsoncpp-dev valgrind \
-    libunwind-dev libgflags-dev
+    libjemalloc-dev libjsoncpp-dev valgrind
+
+# libunwind-dev libgflags-dev
