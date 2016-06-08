7d71396bc41cce67b8bc97e185a4a64b1d476e93
jarulraj <jarulraj@cs.cmu.edu>
2016-06-07 22:32:54 -0400

    Updated the package list

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -4,4 +4,4 @@
 apt-get -y install git g++ cmake \
     libboost-dev libboost-filesystem-dev \
     libprotobuf-dev protobuf-compiler libevent-dev libjemalloc-dev libjsoncpp-dev \
-    doxygen valgrind libsqlite3-dev
+    valgrind libsqlite3-dev
