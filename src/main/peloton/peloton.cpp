ef254ee11d535daa8e6c477b2b41ee78a4dd0632
jarulraj <jarulraj@cs.cmu.edu>
2016-06-07 21:26:54 -0400

    Added peloton conf

--- src/main/peloton/peloton.cpp
+++ src/main/peloton/peloton.cpp
@@ -13,15 +13,25 @@
 #include <unistd.h>
 #include <iostream>
 
-#include "common/macros.h"
 #include "wire/socket_base.h"
 #include "wire/wire.h"
 
-/*
- * Any Postgres server process begins execution here.
- */
-int main(UNUSED_ATTRIBUTE int argc, UNUSED_ATTRIBUTE char *argv[]) {
-  peloton::wire::Server server(DEFAULT_PORT, MAX_CONNECTIONS);
+DECLARE_bool(help);
+DECLARE_bool(h);
+
+// Peloton process begins execution here.
+int main(int argc, char *argv[]) {
+
+  ::google::ParseCommandLineNonHelpFlags(&argc, &argv, true);
+
+  if (FLAGS_help || FLAGS_h) {
+    FLAGS_help = true;
+    ::google::SetUsageMessage("Usage Info: \n");
+    ::google::HandleCommandLineHelpFlags();
+  }
+
+  // Launch server
+  peloton::wire::Server server;
   peloton::wire::StartServer(&server);
   peloton::wire::HandleConnections<peloton::wire::PacketManager,
                                    peloton::wire::PktBuf>(&server);
