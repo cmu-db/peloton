b0f65bfd24b5067b860337bccfb6ab26a5a45004
jarulraj <jarulraj@cs.cmu.edu>
2016-06-07 21:54:47 -0400

    Updated package list

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Set up environment
-apt-get -y install git g++ pkg-config \
-    valgrind libboost-dev libboost-filesystem-dev \
+apt-get -y install git g++ cmake \
+    libboost-dev libboost-filesystem-dev \
     libprotobuf-dev protobuf-compiler libevent-dev libjemalloc-dev libjsoncpp-dev \
-    libgflags-dev libgoogle-glog-dev doxygen libsqlite3-dev
+    libgflags-dev libgoogle-glog-dev \
+    doxygen valgrind \
+    libsqlite3-dev
