60681944a1e5d5f57f7e35a86a7c25baabd12c75
jarulraj <jarulraj@cs.cmu.edu>
2016-06-09 00:38:44 -0400

    Refactored wire module

--- src/include/common/portal.h
+++ src/include/common/portal.h
@@ -20,29 +20,19 @@
 
 namespace peloton {
 
-typedef std::pair<std::vector<unsigned char>, std::vector<unsigned char>>
-    ResType;
-
-// fieldinfotype: field name, oid (data type), size
-typedef std::tuple<std::string, int, int> FieldInfoType;
-
 class Portal {
 
 public:
 
+  // Portal name
   std::string portal_name;
 
-  // logical name of prep stmt
-  std::string prep_stmt_name;
+  // Prepared statement
+  std::shared_ptr<PreparedStatement> prepared_statement;
 
-  // stores the attribute names
-  std::vector<FieldInfoType> tuple_desc;
+  // Group the parameter types and the parameters in this vector
+  std::vector<std::pair<int, std::string>> bind_parameters;
 
-  std::string query_string;
-
-  std::string query_type;
-
-  PreparedStatement *stmt;
 };
 
 }  // namespace peloton
