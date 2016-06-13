4a372fbf1ae2d2f5f2780a2783c3a21053f43c97
Saif AlHarthi <saif@namaa.co>
2016-06-13 14:14:14 -0400

    Added simple Drop Plan

---
+++ src/include/planner/drop_plan.h
@@ -0,0 +1,46 @@
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// drop_plan.h
+//
+// Identification: /peloton/src/include/planner/drop_plan.h
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "planner/abstract_plan.h"
+
+namespace peloton {
+
+namespace storage{
+class DataTable;
+}
+
+namespace planner {
+class DropPlan : AbstractPlan {
+ public:
+
+  DropPlan() = delete;
+  DropPlan(const DropPlan &) = delete;
+  DropPlan &operator=(const DropPlan &) = delete;
+  DropPlan(DropPlan &&) = delete;
+  DropPlan &operator=(DropPlan &&) = delete;
+
+  inline PlanNodeType GetPlanNodeType() const {
+    return PLAN_NODE_TYPE_DROP;
+  }
+
+  const std::string GetInfo() const {
+    return "DropPlan";
+  }
+
+ private:
+  storage::DataTable *target_table_ = nullptr;
+
+};
+}
+}
