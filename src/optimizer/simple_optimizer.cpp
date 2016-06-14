caacab696f70b7950cf186876df54ecb330a4e9c
Saif AlHarthi <saif@namaa.co>
2016-06-13 23:53:31 -0400

    Modified optimizer

--- src/optimizer/simple_optimizer.cpp
+++ src/optimizer/simple_optimizer.cpp
@@ -29,9 +29,9 @@ SimpleOptimizer::~SimpleOptimizer() {
 std::shared_ptr<planner::AbstractPlan> SimpleOptimizer::BuildPlanTree(
     const std::unique_ptr<parser::AbstractParse>& parse_tree) {
 
-  if (parse_tree.get() == nullptr)
-    return NULL;
   std::shared_ptr<planner::AbstractPlan> plan_tree;
+  if (parse_tree.get() == nullptr)
+     return plan_tree;
 
   std::unique_ptr<planner::AbstractPlan> child_plan;
 
@@ -44,16 +44,28 @@ std::shared_ptr<planner::AbstractPlan> SimpleOptimizer::BuildPlanTree(
     case PLAN_NODE_TYPE_DROP:
       child_plan = new planner::DropPlan();
       break;
+
     case PLAN_NODE_TYPE_SEQSCAN:
       child_plan = new planner::SeqScanPlan();
+      break;
+
+    default:
+      LOG_INFO("Unsupported Parse Node Type");
   }
+
+// Need to recurse and give base case. for every child in parse tree.
 
   if (child_plan != nullptr) {
       if (plan_tree != nullptr)
-        plan_tree->AddChild(child_plan);
+        plan_tree->AddChild(std::move(child_plan));
       else
         plan_tree = child_plan;
     }
+
+  auto child_parse = parse_tree->children_;
+  for(auto child : child_parse){
+    BuildPlanTree(child);
+  }
   return plan_tree;
 }
 
