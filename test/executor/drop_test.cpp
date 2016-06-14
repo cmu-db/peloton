2f3f23b0989effb35dce7c5cd786e11cdaedaaa5
Ahmed El-Roby <aelroby@andrew.cmu.edu>
2016-06-14 05:18:26 -0400

    Fixing drop_test

--- test/executor/drop_test.cpp
+++ test/executor/drop_test.cpp
@@ -32,15 +32,16 @@ TEST_F(DropTests, DroppingTable) {
 	auto &bootstrapper = catalog::Bootstrapper::GetInstance();
 	auto global_catalog = bootstrapper.bootstrap();
 
-	// Insert a table first
-	  auto id_column =
-		      catalog::Column(VALUE_TYPE_INTEGER, GetTypeSize(VALUE_TYPE_INTEGER),
-		                      "dept_id", true);
-	  auto name_column =
-	      catalog::Column(VALUE_TYPE_VARCHAR, 32,
-	                      "dept_name", false);
-	  std::unique_ptr<catalog::Schema> table_schema(new catalog::Schema({id_column, name_column}));
-  global_catalog->CreateTable("default_database", "department_table", table_schema);
+  // Insert a table first
+  auto id_column =
+		  catalog::Column(VALUE_TYPE_INTEGER, GetTypeSize(VALUE_TYPE_INTEGER),
+						  "dept_id", true);
+  auto name_column =
+	  catalog::Column(VALUE_TYPE_VARCHAR, 32,
+					  "dept_name", false);
+  std::unique_ptr<catalog::Schema> table_schema(new catalog::Schema({id_column, name_column}));
+  global_catalog->CreateDatabase("default_database");
+  global_catalog->CreateTable("default_database", "department_table", std::move(table_schema));
   EXPECT_EQ(global_catalog->GetDatabaseWithName("default_database")->GetTableCount(), 1);
 
   // Now dropping the table using the executer
@@ -50,6 +51,7 @@ TEST_F(DropTests, DroppingTable) {
       new executor::ExecutorContext(txn));
   planner::DropPlan node("department_table");
   executor::DropExecutor executor(&node, context.get());
+  executor.Init();
   executor.Execute();
   EXPECT_EQ(global_catalog->GetDatabaseWithName("default_database")->GetTableCount(), 0);
 
