16478fc8beb25b41aa3d3b569b106d998d0c0f31
Saif AlHarthi <saif@namaa.co>
2016-06-14 11:06:19 -0400

    fix drop_test

--- src/optimizer/simple_optimizer.cpp
+++ src/optimizer/simple_optimizer.cpp
@@ -12,8 +12,11 @@
 
 #include "optimizer/simple_optimizer.h"
 #include "parser/peloton/abstract_parse.h"
+#include "planner/abstract_plan.h"
 
 #include "common/logger.h"
+
+#include <memory>
 
 namespace peloton {
 namespace optimizer {
@@ -33,34 +36,34 @@ std::shared_ptr<planner::AbstractPlan> SimpleOptimizer::BuildPlanTree(
   if (parse_tree.get() == nullptr)
      return plan_tree;
 
-/*  std::unique_ptr<planner::AbstractPlan> child_plan;
-
-  // TODO: Transform the parse tree
-
-  // One to one Mapping
-  auto parse_item_node_type = parse_tree->GetParseNodeType();
-
-  switch(parse_item_node_type){
-    case PARSE_NODE_TYPE_DROP:
-      child_plan = new planner::DropPlan();
-      break;
-
-    case PARSE_NODE_TYPE_SCAN:
-      child_plan = new planner::SeqScanPlan();
-      break;
-
-    default:
-      LOG_INFO("Unsupported Parse Node Type");
-  }
-
-// Need to recurse and give base case. for every child in parse tree.
-
-  if (child_plan != nullptr) {
-      if (plan_tree != nullptr)
-        plan_tree->AddChild(std::move(child_plan));
-      else
-        plan_tree = child_plan;
-    }*/
+// std::unique_ptr<planner::AbstractPlan> child_plan;
+//
+//  // TODO: Transform the parse tree
+//
+//  // One to one Mapping
+//  auto parse_item_node_type = parse_tree->GetParseNodeType();
+//
+//  switch(parse_item_node_type){
+//    case PARSE_NODE_TYPE_DROP:
+//      child_plan = std::make_unique(new planner::DropPlan("department-table"));
+//      break;
+//
+//    case PARSE_NODE_TYPE_SCAN:
+//      child_plan = std::make_unique(new planner::SeqScanPlan());
+//      break;
+//
+//    default:
+//      LOG_INFO("Unsupported Parse Node Type");
+//  }
+//
+//// Need to recurse and give base case. for every child in parse tree.
+//
+//  if (child_plan != nullptr) {
+//      if (plan_tree != nullptr)
+//        plan_tree->AddChild(std::move(child_plan));
+//      else
+//        plan_tree = child_plan;
+//    }
 
   return plan_tree;
 }
