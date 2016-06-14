f29591ac3f2927c5c8caf858e49b622d659b9935
Saif AlHarthi <saif@namaa.co>
2016-06-14 14:44:58 -0400

    Additional fix

--- src/optimizer/simple_optimizer.cpp
+++ src/optimizer/simple_optimizer.cpp
@@ -78,7 +78,7 @@ std::shared_ptr<planner::AbstractPlan> SimpleOptimizer::BuildPlanTree(
 
   auto &children = parse_tree->GetChildren();
   for (auto &child : children) {
-    std::shared_ptr<planner::AbstractPlan> child_parse = std::move((BuildPlanTree(child)));
+    std::shared_ptr<planner::AbstractPlan> child_parse(BuildPlanTree(child));
     child_plan = std::move(child_parse);
   }
   return plan_tree;
