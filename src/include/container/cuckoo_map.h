6efc8e95cd4b0510f3acedad0ccb0627d2417a20
jarulraj <jarulraj@cs.cmu.edu>
2016-06-15 16:26:30 -0400

    Stash changes

--- src/include/container/cuckoo_map.h
+++ src/include/container/cuckoo_map.h
@@ -16,8 +16,7 @@
 #include <cstring>
 #include <cstdio>
 
-#include "libcds/cds/opt/hash.h"
-#include "libcds/cds/container/cuckoo_map.h"
+#include "libcuckoo/cuckoohash_map.hh"
 
 namespace peloton {
 
@@ -39,9 +38,7 @@ class CuckooMap {
   bool Insert(const KeyType &key, ValueType value);
 
   // Extracts item with high priority
-  std::pair<bool, bool> Update(const KeyType &key,
-                               ValueType value,
-                               bool bInsert=true);
+  bool Update(const KeyType &key, ValueType value);
 
   // Extracts the corresponding value
   bool Find(const KeyType &key, ValueType& value);
@@ -63,31 +60,8 @@ class CuckooMap {
 
  private:
 
-  struct hash1 {
-      size_t operator()(KeyType const& s) const {
-          return cds::opt::v::hash<KeyType>()(s);
-      }
-  };
-
-  struct hash2: private hash1 {
-      size_t operator()(KeyType const& s) const {
-          size_t h = ~( hash1::operator()(s));
-          return ~h + 0x9e3779b9 + (h << 6) + (h >> 2);
-      }
-  };
-
-  struct cuckoo_map_traits: public cds::container::cuckoo::traits {
-    typedef std::equal_to<KeyType> equal_to;
-    typedef cds::opt::hash_tuple< hash1, hash2 >  hash;
-
-    static bool const store_hash = true;
-  };
-
   // cuckoo map
-  typedef cds::container::CuckooMap<KeyType, ValueType, cuckoo_map_traits> cuckoo_map_t;
-
-  // map pair <key, value>
-  typedef typename cuckoo_map_t::value_type map_pair;
+  typedef cuckoohash_map<KeyType, ValueType> cuckoo_map_t;
 
   cuckoo_map_t cuckoo_map;
 };
