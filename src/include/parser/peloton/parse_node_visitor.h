07f5e4a9a44935e3b6b75b1a156dc8cbd26d1ac0
Lin Ma <malin1993ml@gmail.com>
2016-06-17 00:00:06 -0400

    Add parse_node_visitor. Working on transforming select. Print parse result in protocal.cpp for debug (need to be removed).

---
+++ src/include/parser/peloton/parse_node_visitor.h
@@ -0,0 +1,58 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// parse_node_visitor.h
+//
+// Identification: src/include/parser/peloton/parse_node_visitor.h
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+namespace peloton {
+namespace parser {
+
+class Variable;
+class Constant;
+class OperatorExpression;
+class AndOperator;
+class OrOperator;
+class NotOperator;
+class Attribute;
+class Table;
+class Join;
+class OrderBy;
+class Select;
+
+//===--------------------------------------------------------------------===//
+// Parse Node Visitor
+//===--------------------------------------------------------------------===//
+
+class ParseNodeVisitor {
+ public:
+  virtual ~ParseNodeVisitor() {};
+
+  /* TODO: implement them one by one later
+  virtual void visit(const Variable *) = 0;
+  virtual void visit(const Constant *) = 0;
+  virtual void visit(const OperatorExpression *) = 0;
+  virtual void visit(const AndOperator *) = 0;
+  virtual void visit(const OrOperator *) = 0;
+  virtual void visit(const NotOperator *) = 0;
+  virtual void visit(const Attribute *) = 0;
+  virtual void visit(const Table *) = 0;
+  virtual void visit(const Join *) = 0;
+  virtual void visit(const OrderBy *) = 0;
+  virtual void visit(const Select *) = 0;
+  */
+  virtual void visit(const CreateParse *) = 0;
+  virtual void visit(const DropParse *) = 0;
+  virtual void visit(const InsertParse *) = 0;
+  virtual void visit(const SelectParse *) = 0;
+};
+
+} /* namespace optimizer */
+} /* namespace peloton */
