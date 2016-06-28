d3d314ee096025b2406ff21061e0be63e0d54af7
Saif AlHarthi <saif@saifalharthi.me>
2016-06-28 10:34:58 -0400

    Added update parse with simple fixes

---
+++ src/include/parser/peloton/update_parse.h
@@ -0,0 +1,50 @@
+
+//===----------------------------------------------------------------------===//
+//
+//                         PelotonDB
+//
+// update_parse.h
+//
+// Identification: /peloton/src/include/parser/peloton/update_parse.h
+//
+// Copyright (c) 2015, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "parser/peloton/abstract_parse.h"
+
+namespace peloton {
+namespace parser {
+
+class UpdateParse : public AbstractParse {
+ public:
+	UpdateParse() = delete;
+	UpdateParse(const UpdateParse &) = delete;
+	UpdateParse &operator=(const UpdateParse &) = delete;
+	UpdateParse(UpdateParse &&) = delete;
+	UpdateParse &operator=(UpdateParse &&) = delete;
+
+  explicit UpdateParse(UpdateStmt *update_node);
+
+  inline ParseNodeType GetParseNodeType() const { return PARSE_NODE_TYPE_UPDATE; }
+
+  const std::string GetEntityName() { return entity_name; }
+
+  const std::string GetInfo() const { return "UpdateParse"; }
+
+  std::string GetTableName() { return entity_name; }
+
+
+ private:
+  // Type of entity
+  EntityType entity_type = ENTITY_TYPE_INVALID;
+
+  // Name of entity
+  std::string entity_name;
+
+};
+
+}  // namespace parser
+}  // namespace peloton
