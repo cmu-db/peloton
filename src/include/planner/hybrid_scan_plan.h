8da64cf4b477f45809daea222c03b32565336da0
Saif AlHarthi <saif@saifalharthi.me>
2016-06-30 18:21:18 -0400

    Fix check

--- src/include/planner/hybrid_scan_plan.h
+++ src/include/planner/hybrid_scan_plan.h
@@ -14,55 +14,26 @@
 
 #include "planner/abstract_scan_plan.h"
 #include "planner/index_scan_plan.h"
+#include "storage/data_table.h"
+#include "index/index.h"
 #include "common/types.h"
+#include "expression/abstract_expression.h"
 
 namespace peloton {
-
-class Value;
-
-namespace index{
-  class Index;
-}
 namespace planner {
 
 class HybridScanPlan : public AbstractScan {
-
-	enum HybridType { UNKNOWN, SEQ, INDEX, HYBRID };
-
  public:
   HybridScanPlan(const HybridScanPlan &) = delete;
   HybridScanPlan &operator=(const HybridScanPlan &) = delete;
   HybridScanPlan(HybridScanPlan &&) = delete;
   HybridScanPlan &operator=(HybridScanPlan &&) = delete;
 
-
-  HybridScanPlan(index::Index *index, storage::DataTable *table,
-                 expression::AbstractExpression *predicate,
-                 const std::vector<oid_t> &column_ids,
-                 const IndexScanPlan::IndexScanDesc &index_scan_desc);
-
-  HybridScanPlan(storage::DataTable *table,
-                 expression::AbstractExpression *predicate,
-                 const std::vector<oid_t> &column_ids);
-
-  HybridScanPlan(storage::DataTable *table,
-                 expression::AbstractExpression *predicate,
-                 const std::vector<oid_t> &column_ids,
-                 const IndexScanPlan::IndexScanDesc &index_scan_desc);
-
   HybridScanPlan(storage::DataTable *table,
                  expression::AbstractExpression *predicate,
                  const std::vector<oid_t> &column_ids,
                  const IndexScanPlan::IndexScanDesc &index_scan_desc,
-                 HybridScanType hybrid_scan_type)
-      : AbstractScan(table, predicate, column_ids),
-        type_(hybrid_scan_type),
-        column_ids_(column_ids),
-        key_column_ids_(std::move(index_scan_desc.key_column_ids)),
-        expr_types_(std::move(index_scan_desc.expr_types)),
-        values_(std::move(index_scan_desc.values)),
-        runtime_keys_(std::move(index_scan_desc.runtime_keys)),
-        index_(index_scan_desc.index){}
+                 HybridScanType hybrid_scan_type);
 
   index::Index *GetDataIndex() const { return this->index_; }
 
