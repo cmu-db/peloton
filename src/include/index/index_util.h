bb05251cb176e862a9f73d832c7d363bc3096cf5
jarulraj <jarulraj@cs.cmu.edu>
2016-07-08 02:55:51 -0400

    Adding index utils

---
+++ src/include/index/index_util.h
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// index_util.h
+//
+// Identification: src/include/index/index_util.h
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+
+#pragma once
+
+namespace peloton {
+namespace index {
+
+}  // End index namespace
+}  // End peloton namespace
