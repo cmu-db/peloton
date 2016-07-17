a45b5fa49bc72c09c3cc5b7aba19a36e576af48d
Ahmed El-Roby <aelroby@andrew.cmu.edu>
2016-07-16 23:29:53 -0400

    Update works now too

--- src/include/parser/statement_update.h
+++ src/include/parser/statement_update.h
@@ -30,6 +30,17 @@ class UpdateClause {
     free (column);
     delete value;
   }
+
+  UpdateClause* Copy(){
+	  UpdateClause* new_clause = new UpdateClause();
+      std::string str (column);
+	  char* new_cstr = new char [str.length()+1];
+	  std::strcpy (new_cstr, str.c_str());
+	  expression::AbstractExpression* new_expr = value->Copy();
+	  new_clause->column = new_cstr;
+	  new_clause->value = new_expr;
+	  return new_clause;
+  }
 };
 
 /**
