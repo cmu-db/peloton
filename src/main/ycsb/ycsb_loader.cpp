0e497f95859380624ea37f7d4dd2fdd47c2dd31a
jarulraj <jarulraj@cs.cmu.edu>
2016-07-20 02:17:08 -0400

    Revert changes

--- src/main/ycsb/ycsb_loader.cpp
+++ src/main/ycsb/ycsb_loader.cpp
@@ -79,7 +79,7 @@ void CreateYCSBDatabase() {
 
   for (oid_t col_itr = 1; col_itr < col_count; col_itr++) {
     auto column =
-        catalog::Column(VALUE_TYPE_INTEGER, GetTypeSize(VALUE_TYPE_INTEGER),
+        catalog::Column(VALUE_TYPE_VARCHAR, ycsb_field_length,
                         "FIELD" + std::to_string(col_itr), is_inlined);
     columns.push_back(column);
   }
@@ -146,10 +146,11 @@ void LoadYCSBDatabase() {
     std::unique_ptr<storage::Tuple> tuple(
         new storage::Tuple(table_schema, allocate));
     auto key_value = ValueFactory::GetIntegerValue(rowid);
+    auto field_value = ValueFactory::GetStringValue(field_raw_value);
 
     tuple->SetValue(0, key_value, nullptr);
     for (oid_t col_itr = 1; col_itr < col_count; col_itr++) {
-      tuple->SetValue(col_itr, key_value, nullptr);
+      tuple->SetValue(col_itr, field_value, pool.get());
     }
 
     planner::InsertPlan node(user_table, std::move(tuple));
