7fb8d6fdfaafb9c6a792bb8de136988941394302
Ahmed El-Roby <aelroby@andrew.cmu.edu>
2016-07-27 15:28:58 -0400

    Fix a bug - Added test cases for update/delete

--- test/planner/planner_test.cpp
+++ test/planner/planner_test.cpp
@@ -18,6 +18,7 @@
 #include "expression/comparison_expression.h"
 #include "parser/statements.h"
 #include "planner/delete_plan.h"
+#include "planner/update_plan.h"
 #include "executor/plan_executor.h"
 
 namespace peloton {
@@ -49,12 +50,12 @@ TEST_F(PlannerTests, DeletePlanTestParameter) {
 	std::unique_ptr<catalog::Schema> table_schema(new catalog::Schema({id_column, name_column}));
 	catalog::Bootstrapper::global_catalog->CreateTable(DEFAULT_DB_NAME, "department_table", std::move(table_schema));
 
-	// DELETE FROM department WHERE col_0 = $0
+	// DELETE FROM department_table WHERE id = $0
 	parser::DeleteStatement *delete_statement = new parser::DeleteStatement();
 	delete_statement->table_name = "department_table";
 	Value val = ValueFactory::GetNullValue(); // The value is not important at this point
 
-	// tuple[0].colum[0] = $0
+	// id = $0
 	auto parameter_expr = new expression::ParameterValueExpression(0, val);
 	auto tuple_expr = new expression::TupleValueExpression(VALUE_TYPE_INTEGER, 0, 0);
 	auto cmp_expr = new expression::ComparisonExpression<peloton::expression::CmpEq>(EXPRESSION_TYPE_COMPARE_EQUAL,
@@ -64,8 +65,77 @@ TEST_F(PlannerTests, DeletePlanTestParameter) {
 
 	auto del_plan = new planner::DeletePlan(delete_statement);
 
-
 	bridge::PlanExecutor::PrintPlan(del_plan, "Delete Plan");
+
+	auto values = new std::vector<Value>();
+
+	// id = 15
+	values->push_back(ValueFactory::GetIntegerValue(15));
+
+	// bind values to parameters in plan
+	del_plan->SetParameterValues(values);
+
+}
+
+TEST_F(PlannerTests, UpdatePlanTestParameter) {
+
+
+	// Bootstrapping peloton
+	catalog::Bootstrapper::bootstrap();
+	catalog::Bootstrapper::global_catalog->CreateDatabase(DEFAULT_DB_NAME);
+
+	// Create table
+	auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+	txn_manager.BeginTransaction();
+	auto id_column =
+	      catalog::Column(VALUE_TYPE_INTEGER, GetTypeSize(VALUE_TYPE_INTEGER),
+		                      "id", true);
+	auto name_column =
+	     catalog::Column(VALUE_TYPE_VARCHAR, 32,
+	                      "name", true);
+
+	std::unique_ptr<catalog::Schema> table_schema(new catalog::Schema({id_column, name_column}));
+	catalog::Bootstrapper::global_catalog->CreateTable(DEFAULT_DB_NAME, "department_table", std::move(table_schema));
+
+	// UPDATE department_table SET name = $0 WHERE id = $1
+	parser::UpdateStatement *update_statement = new parser::UpdateStatement();
+	parser::TableRef* table_ref = new parser::TableRef(peloton::TABLE_REFERENCE_TYPE_JOIN);
+	table_ref->name = "department_table";
+	update_statement->table = table_ref;
+	Value val = ValueFactory::GetNullValue(); // The value is not important at this point
+
+	// name = $0
+	auto update = new parser::UpdateClause();
+	update->column = "name";
+	auto parameter_expr = new expression::ParameterValueExpression(0, val);
+	auto tuple_expr = new expression::TupleValueExpression(VALUE_TYPE_INTEGER, 0, 1);
+	auto cmp_expr = new expression::ComparisonExpression<peloton::expression::CmpEq>(EXPRESSION_TYPE_COMPARE_EQUAL,
+			tuple_expr, parameter_expr);
+	update->value = cmp_expr;
+	auto updates = new std::vector<parser::UpdateClause*>();
+	updates->push_back(update);
+	update_statement->updates = updates;
+
+	// id = $1
+	parameter_expr = new expression::ParameterValueExpression(1, val);
+	tuple_expr = new expression::TupleValueExpression(VALUE_TYPE_INTEGER, 0, 0);
+	cmp_expr = new expression::ComparisonExpression<peloton::expression::CmpEq>(EXPRESSION_TYPE_COMPARE_EQUAL,
+			tuple_expr, parameter_expr);
+
+	update_statement->where = cmp_expr;
+
+	auto update_plan = new planner::UpdatePlan(update_statement);
+
+	bridge::PlanExecutor::PrintPlan(update_plan, "Update Plan");
+
+	auto values = new std::vector<Value>();
+
+	// name = CS, id = 1
+	values->push_back(ValueFactory::GetStringValue("CS"));
+	values->push_back(ValueFactory::GetIntegerValue(1));
+
+	// bind values to parameters in plan
+	update_plan->SetParameterValues(values);
 
 }
 
