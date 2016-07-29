14ba29cdab20c318bbcca595939ad351ef3ab975
Lin Ma <malin1993ml@gmail.com>
2016-07-29 13:11:34 -0400

    Some testing.

--- src/tcop/tcop.cpp
+++ src/tcop/tcop.cpp
@@ -10,7 +10,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-
 #include "tcop/tcop.h"
 
 #include "common/macros.h"
@@ -44,71 +43,70 @@ TrafficCop::~TrafficCop() {
   // Nothing to do here !
 }
 
-Result TrafficCop::ExecuteStatement(const std::string& query,
-                                    std::vector<ResultType> &result,
-                                    std::vector<FieldInfoType> &tuple_descriptor,
-                                    int &rows_changed,
-                                    std::string &error_message){
+Result TrafficCop::ExecuteStatement(
+    const std::string &query, std::vector<ResultType> &result,
+    std::vector<FieldInfoType> &tuple_descriptor, int &rows_changed,
+    std::string &error_message) {
   LOG_INFO("Received %s", query.c_str());
 
   // Prepare the statement
   std::string unnamed_statement = "unnamed";
   auto statement = PrepareStatement(unnamed_statement, query, error_message);
 
-  if(statement.get() == nullptr){
+  if (statement.get() == nullptr) {
     return Result::RESULT_FAILURE;
   }
 
   // Then, execute the statement
   bool unnamed = true;
-  auto status = ExecuteStatement(statement, unnamed,
-                                 result, rows_changed, error_message);
+  auto status =
+      ExecuteStatement(statement, unnamed, result, rows_changed, error_message);
 
-  if(status == Result::RESULT_SUCCESS) {
+  if (status == Result::RESULT_SUCCESS) {
     LOG_INFO("Execution succeeded!");
     tuple_descriptor = std::move(statement->GetTupleDescriptor());
-  }
-  else{
+  } else {
     LOG_INFO("Execution failed!");
   }
 
   return status;
 }
 
-Result TrafficCop::ExecuteStatement(UNUSED_ATTRIBUTE const std::shared_ptr<Statement>& statement,
-                                    UNUSED_ATTRIBUTE const bool unnamed,
-                                    UNUSED_ATTRIBUTE std::vector<ResultType> &result,
-                                    UNUSED_ATTRIBUTE int &rows_changed,
-                                    UNUSED_ATTRIBUTE std::string &error_message){
+Result TrafficCop::ExecuteStatement(
+    UNUSED_ATTRIBUTE const std::shared_ptr<Statement> &statement,
+    UNUSED_ATTRIBUTE const bool unnamed,
+    UNUSED_ATTRIBUTE std::vector<ResultType> &result,
+    UNUSED_ATTRIBUTE int &rows_changed,
+    UNUSED_ATTRIBUTE std::string &error_message) {
 
   LOG_INFO("Execute Statement %s", statement->GetStatementName().c_str());
   std::vector<Value> params;
   bridge::PlanExecutor::PrintPlan(statement->GetPlanTree().get(), "Plan");
-  bridge::peloton_status status = bridge::PlanExecutor::ExecutePlan(statement->GetPlanTree().get(), params);
+  bridge::peloton_status status =
+      bridge::PlanExecutor::ExecutePlan(statement->GetPlanTree().get(), params);
   LOG_INFO("Statement executed. Result: %d", status.m_result);
   return status.m_result;
-
 }
 
-std::shared_ptr<Statement> TrafficCop::PrepareStatement(const std::string& statement_name,
-                                                        const std::string& query_string,
-                                                        UNUSED_ATTRIBUTE std::string &error_message){
+std::shared_ptr<Statement> TrafficCop::PrepareStatement(
+    const std::string &statement_name, const std::string &query_string,
+    UNUSED_ATTRIBUTE std::string &error_message) {
   std::shared_ptr<Statement> statement;
 
   LOG_INFO("Prepare Statement %s", query_string.c_str());
 
   statement.reset(new Statement(statement_name, query_string));
 
-  auto& peloton_parser = parser::Parser::GetInstance();
+  auto &peloton_parser = parser::Parser::GetInstance();
   auto sql_stmt = peloton_parser.BuildParseTree(query_string);
 
-  statement->SetPlanTree(optimizer::SimpleOptimizer::BuildPelotonPlanTree(sql_stmt));
+  statement->SetPlanTree(
+      optimizer::SimpleOptimizer::BuildPelotonPlanTree(sql_stmt));
 
+  bridge::PlanExecutor::PrintPlan(statement->GetPlanTree().get(), "Plan");
   LOG_INFO("Statement Prepared!");
   return std::move(statement);
-
 }
 
-
 }  // End tcop namespace
-} // End peloton namespace
+}  // End peloton namespace
