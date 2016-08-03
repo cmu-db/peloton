b94aba81f4d88eb3ddb3b00f38323938f79406a7
Saif Alharthi <saif@saifalharthi.me>
2016-08-03 15:53:15 -0400

    Created Create Index test , some tests needs modification

--- test/executor/create_index_test.cpp
+++ test/executor/create_index_test.cpp
@@ -40,9 +40,9 @@ namespace test {
 // Catalog Tests
 //===--------------------------------------------------------------------===//
 
-class UpdateTests : public PelotonTest {};
+class CreateIndexTests : public PelotonTest {};
 
-TEST_F(UpdateTests, Updating) {
+TEST_F(CreateIndexTests, CreatingIndex) {
 
   LOG_INFO("Bootstrapping...");
   catalog::Bootstrapper::bootstrap();
@@ -50,16 +50,17 @@ TEST_F(UpdateTests, Updating) {
   LOG_INFO("Bootstrapping completed!");
 
 
+  // Create a table first
   auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
 
   txn_manager.BeginTransaction();
   LOG_INFO("Creating table");
-  LOG_INFO("Query: CREATE TABLE department_table(dept_id INT PRIMARY KEY, dept_name TEXT);");
+  LOG_INFO("Query: CREATE TABLE department_table(dept_id INT PRIMARY KEY,student_id INT, dept_name TEXT);");
   std::unique_ptr<Statement> statement;
-  statement.reset(new Statement("CREATE","CREATE TABLE department_table(dept_id INT PRIMARY KEY, dept_name TEXT);"));
+  statement.reset(new Statement("CREATE","CREATE TABLE department_table(dept_id INT PRIMARY KEY, student_id INT, dept_name TEXT);"));
   auto& peloton_parser = parser::Parser::GetInstance();
   LOG_INFO("Building parse tree...");
-  auto create_stmt = peloton_parser.BuildParseTree("CREATE TABLE department_table(dept_id INT PRIMARY KEY, dept_name TEXT);");
+  auto create_stmt = peloton_parser.BuildParseTree("CREATE TABLE department_table(dept_id INT PRIMARY KEY, student_id INT, dept_name TEXT);");
   LOG_INFO("Building parse tree completed!");
   LOG_INFO("Building plan tree...");
   statement->SetPlanTree(optimizer::SimpleOptimizer::BuildPelotonPlanTree(create_stmt));
@@ -76,10 +77,10 @@ TEST_F(UpdateTests, Updating) {
   // Inserting a tuple end-to-end
   txn_manager.BeginTransaction();
   LOG_INFO("Inserting a tuple...");
-  LOG_INFO("Query: INSERT INTO department_table(dept_id,dept_name) VALUES (1,'hello_1');");
-  statement.reset(new Statement("INSERT", "INSERT INTO department_table(dept_id,dept_name) VALUES (1,'hello_1');"));
+  LOG_INFO("Query: INSERT INTO department_table(dept_id,student_id ,dept_name) VALUES (1,52,'hello_1');");
+  statement.reset(new Statement("INSERT", "INSERT INTO department_table(dept_id, student_id, dept_name) VALUES (1,52,'hello_1');"));
   LOG_INFO("Building parse tree...");
-  auto insert_stmt = peloton_parser.BuildParseTree("INSERT INTO department_table(dept_id,dept_name) VALUES (1,'hello_1');");
+  auto insert_stmt = peloton_parser.BuildParseTree("INSERT INTO department_table(dept_id,student_id,dept_name) VALUES (1,52,'hello_1');");
   LOG_INFO("Building parse tree completed!");
   LOG_INFO("Building plan tree...");
   statement->SetPlanTree(optimizer::SimpleOptimizer::BuildPelotonPlanTree(insert_stmt));
@@ -93,11 +94,11 @@ TEST_F(UpdateTests, Updating) {
 
   // Now Updating end-to-end
   txn_manager.BeginTransaction();
-  LOG_INFO("Updating a tuple...");
-  LOG_INFO("Query: UPDATE department_table SET dept_name = 'CS' WHERE dept_id = 1");
-  statement.reset(new Statement("UPDATE", "UPDATE department_table SET dept_name = 'CS' WHERE dept_id = 1"));
+  LOG_INFO("Creating and Index");
+  LOG_INFO("Query: CREATE INDEX saif ON department_table (student_id);");
+  statement.reset(new Statement("CREATE", "CREATE INDEX saif ON department_table (student_id);"));
   LOG_INFO("Building parse tree...");
-  auto update_stmt = peloton_parser.BuildParseTree("UPDATE department_table SET dept_name = 'CS' WHERE dept_id = 1");
+  auto update_stmt = peloton_parser.BuildParseTree("CREATE INDEX saif ON department_table (student_id);");
   LOG_INFO("Building parse tree completed!");
   LOG_INFO("Building plan tree...");
   statement->SetPlanTree(optimizer::SimpleOptimizer::BuildPelotonPlanTree(update_stmt));
@@ -106,25 +107,9 @@ TEST_F(UpdateTests, Updating) {
   LOG_INFO("Executing plan...");
   status = bridge::PlanExecutor::ExecutePlan(statement->GetPlanTree().get(), params, result);
   LOG_INFO("Statement executed. Result: %d", status.m_result);
-  LOG_INFO("Tuple Updated!");
+  LOG_INFO("INDEX CREATED!");
   txn_manager.CommitTransaction();
 
-  txn_manager.BeginTransaction();
-  LOG_INFO("Deleting a tuple...");
-  LOG_INFO("Query: DELETE FROM department_table WHERE dept_name = 'CS'");
-  statement.reset(new Statement("DELETE", "DELETE FROM department_table WHERE dept_name = 'CS'"));
-  LOG_INFO("Building parse tree...");
-  auto delete_stmt = peloton_parser.BuildParseTree("DELETE FROM department_table WHERE dept_name = 'CS'");
-  LOG_INFO("Building parse tree completed!");
-  LOG_INFO("Building plan tree...");
-  statement->SetPlanTree(optimizer::SimpleOptimizer::BuildPelotonPlanTree(delete_stmt));
-  LOG_INFO("Building plan tree completed!");
-  bridge::PlanExecutor::PrintPlan(statement->GetPlanTree().get(), "Plan");
-  LOG_INFO("Executing plan...");
-  status = bridge::PlanExecutor::ExecutePlan(statement->GetPlanTree().get(), params, result);
-  LOG_INFO("Statement executed. Result: %d", status.m_result);
-  LOG_INFO("Tuple deleted!");
-  txn_manager.CommitTransaction();
 }
 
 }  // End test namespace
