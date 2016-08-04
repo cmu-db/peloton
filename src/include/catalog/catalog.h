d1393443bed827543ad2f0f7e568cb416dbf73ad
Saif Alharthi <saif@saifalharthi.me>
2016-08-04 11:04:34 -0400

    Added support for Unique INDEX

--- src/include/catalog/catalog.h
+++ src/include/catalog/catalog.h
@@ -58,7 +58,7 @@ class Catalog {
                             const std::string &table_name);
 
   Result CreateIndex(const std::string &database_name,
-                              const std::string &table_name,  std::vector<std::string> index_attr , std::string index_name);
+                              const std::string &table_name,  std::vector<std::string> index_attr , std::string index_name , bool unique);
 
   // Drop a database
   Result DropDatabase(std::string database_name);
