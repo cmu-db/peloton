fdf24b1d4b501983b53c107b277df84ade9f8342
Yingjun Wu <wu.yj0616@gmail.com>
2016-08-16 16:03:25 -0400

    support secondary index scan

--- src/executor/index_scan_executor.cpp
+++ src/executor/index_scan_executor.cpp
@@ -296,311 +296,178 @@ bool IndexScanExecutor::ExecPrimaryIndexLookup() {
 
 
 bool IndexScanExecutor::ExecSecondaryIndexLookup() {
-  return true;
-  // LOG_TRACE("ExecSecondaryIndexLookup");
-  // assert(!done_);
+  LOG_TRACE("ExecSecondaryIndexLookup");
+  PL_ASSERT(!done_);
 
-  // if (concurrency::TransactionManagerFactory::IsRB() == false &&
-  //     index::IndexFactory::GetSecondaryIndexType() == SECONDARY_INDEX_TYPE_TUPLE) {
-  //   return ExecTupleSecondaryIndexLookup();
-  // }
+  std::vector<ItemPointer *> tuple_location_ptrs;
 
-  // std::vector<ItemPointer> tuple_locations;
-  // std::vector<index::RBItemPointer> rb_tuple_locations;
-  // assert(index_->GetIndexType() != INDEX_CONSTRAINT_TYPE_PRIMARY_KEY);
+  // Grab info from plan node
+  const planner::IndexScanPlan &node = GetPlanNode<planner::IndexScanPlan>();
 
-  // if (0 == key_column_ids_.size()) {
-  //   if (concurrency::TransactionManagerFactory::IsRB()) {
-  //     if (index::IndexFactory::GetSecondaryIndexType() == SECONDARY_INDEX_TYPE_VERSION) {
-  //       index_->ScanAllKeys(rb_tuple_locations);
-  //       for (auto &rb_item_ptr : rb_tuple_locations) {
-  //         tuple_locations.push_back(rb_item_ptr.location);
-  //       }  
-  //     } else {
-  //       index_->ScanAllKeys(tuple_locations);
-  //     }
-  //   } else {
-  //     index_->ScanAllKeys(tuple_locations);  
-  //   }
-  // } else {
-  //   if (concurrency::TransactionManagerFactory::IsRB()) {
-  //     if (index::IndexFactory::GetSecondaryIndexType() == SECONDARY_INDEX_TYPE_VERSION) {
-  //       index_->Scan(values_, key_column_ids_, expr_types_, SCAN_DIRECTION_TYPE_FORWARD, rb_tuple_locations);  
-  //         for (auto &rb_item_ptr : rb_tuple_locations) {
-  //         tuple_locations.push_back(rb_item_ptr.location);
-  //       }
-  //     } else {
-  //       index_->Scan(values_, key_column_ids_, expr_types_, SCAN_DIRECTION_TYPE_FORWARD, tuple_locations);
-  //       RBVerifyVisible(tuple_locations);
-  //     }
-  //   } else {
-  //     index_->Scan(values_, key_column_ids_, expr_types_,SCAN_DIRECTION_TYPE_FORWARD, tuple_locations);  
-  //   }
-  // }
+  auto column_ids_ = node.GetColumnIds();
+  auto key_column_ids_ = node.GetKeyColumnIds();
+  auto expr_types_ = node.GetExprTypes();
 
-  // if (tuple_locations.size() == 0) return false;
+  PL_ASSERT(index_->GetIndexType() != INDEX_CONSTRAINT_TYPE_PRIMARY_KEY);
 
-  // auto &transaction_manager =
-  //     concurrency::TransactionManagerFactory::GetInstance();
+  if (0 == key_column_ids_.size()) {
+    index_->ScanAllKeys(tuple_location_ptrs);
+  } else {
+    index_->Scan(values_, key_column_ids_, expr_types_,
+                 SCAN_DIRECTION_TYPE_FORWARD, tuple_location_ptrs, &node.GetIndexPredicate().GetConjunctionList()[0]);
+  }
 
-  // std::map<oid_t, std::vector<oid_t>> visible_tuples;
-  // // for every tuple that is found in the index.
-  // for (auto tuple_location : tuple_locations) {
-  //   auto &manager = catalog::Manager::GetInstance();
-  //   auto tile_group = manager.GetTileGroup(tuple_location.block);
-  //   auto tile_group_header = tile_group.get()->GetHeader();
-  //   auto tile_group_id = tuple_location.block;
-  //   auto tuple_id = tuple_location.offset;
 
-  //   // if the tuple is visible.
-  //   auto visibility = transaction_manager.IsVisible(tile_group_header, tuple_id);
-  //   if (visibility  == VISIBILITY_OK) {
-  //     // perform predicate evaluation.
-  //     if (predicate_ == nullptr) {
-  //       visible_tuples[tile_group_id].push_back(tuple_id);
+  if (tuple_location_ptrs.size() == 0) {
+    LOG_TRACE("no tuple is retrieved from index.");
+    return false;
+  }
 
-  //       if (is_blind_write_ == false && concurrency::current_txn->IsStaticReadOnlyTxn() == false) {
-  //         auto res = transaction_manager.PerformRead(tuple_location);
-  //         if (!res) {
-  //           transaction_manager.SetTransactionResult(RESULT_FAILURE);
-  //           transaction_manager.AddOneReadAbort();
-  //           return res;
-  //         }
-  //       }
-  //     } else {
-  //       expression::ContainerTuple<storage::TileGroup> tuple(tile_group.get(),
-  //                                                            tuple_id);
-  //       auto eval =
-  //           predicate_->Evaluate(&tuple, nullptr, executor_context_).IsTrue();
-  //       if (eval == true) {
-  //         visible_tuples[tile_group_id].push_back(tuple_id);
+  auto &transaction_manager =
+    concurrency::TransactionManagerFactory::GetInstance();
+
+  std::map<oid_t, std::vector<oid_t>> visible_tuples;
+
+  for (auto tuple_location_ptr : tuple_location_ptrs) {
+    ItemPointer tuple_location = *tuple_location_ptr;
+
+    auto &manager = catalog::Manager::GetInstance();
+
+    auto tile_group = manager.GetTileGroup(tuple_location.block);
+    auto tile_group_header = tile_group.get()->GetHeader();
+
+    size_t chain_length = 0;
+
+    while (true) {
+      // check the version chain
+      ++chain_length;
+
+      auto visibility = transaction_manager.IsVisible(tile_group_header, tuple_location.offset);
+
+      if (visibility == VISIBILITY_DELETED) {
+        LOG_TRACE("encounter deleted tuple: %u, %u", tuple_location.block, tuple_location.offset);
+        break;
+      }
+      if (visibility == VISIBILITY_INVISIBLE) {
+        LOG_TRACE("Invisible read: %u, %u", tuple_location.block,
+                  tuple_location.offset);
+
+
+        // Break for new to old
+        if (tile_group_header->GetTransactionId(tuple_location.offset) == INITIAL_TXN_ID
+            && tile_group_header->GetEndCommitId(tuple_location.offset) <= concurrency::current_txn->GetBeginCommitId()) {
+          // See an invisible version that does not belong to any one in a new to old version chain
+          // Wire back
+          tuple_location = *(tile_group_header->GetIndirection(tuple_location.offset));
+          tile_group = manager.GetTileGroup(tuple_location.block);
+          tile_group_header = tile_group.get()->GetHeader();
+          //chain_length = 0;
+          continue;
+        }
+
+        ItemPointer old_item = tuple_location;
+        tuple_location = tile_group_header->GetNextItemPointer(old_item.offset);
+
+        // there must exist a visible version.
+
+        if(tuple_location.IsNull()) {
+          // FIXME:
+          // For an index scan on a version chain, the result should be one of the following:
+          //    (1) find a visible version
+          //    (2) find a deleted version
+          //    (3) find an aborted version with chain length equal to one
+          if (chain_length == 1) {
+            break;
+          }
+
+          // If we have traversed through the chain and still can not fulfill one of the above conditions,
+          // something wrong must happen.
+          // For speculative read, a transaction may incidentally miss a visible tuple due to a non-atomic
+          // timestamp update. In such case, we just return false and abort the txn.
+          transaction_manager.SetTransactionResult(RESULT_FAILURE);
+          return false;
+        }
+
+        tile_group = manager.GetTileGroup(tuple_location.block);
+        tile_group_header = tile_group.get()->GetHeader();
+      } else {
+        PL_ASSERT(visibility == VISIBILITY_OK);
+        LOG_TRACE("perform read: %u, %u", tuple_location.block,
+                  tuple_location.offset);
+
+        // Further check if the version has the secondary key
+        storage::Tuple key_tuple(index_->GetKeySchema(), true);
+        expression::ContainerTuple<storage::TileGroup> candidate_tuple(
+          tile_group.get(), tuple_location.offset
+        );
+        // Construct the key tuple
+        auto &indexed_columns = index_->GetKeySchema()->GetIndexedColumns();
+
+        oid_t this_col_itr = 0;
+        for (auto col : indexed_columns) {
+          key_tuple.SetValue(this_col_itr, candidate_tuple.GetValue(col), index_->GetPool());
+          this_col_itr++;
+        }
+
+        // Compare the key tuple and the key
+        if (index_->Compare(key_tuple, key_column_ids_, expr_types_, values_) == false) {
+          LOG_TRACE("Secondary key mismatch: %u, %u\n", tuple_location.block, tuple_location.offset);
+          break;
+        }
+
+        // perform predicate evaluation.
+        if (predicate_ == nullptr) {
+          visible_tuples[tuple_location.block].push_back(tuple_location.offset);
+
+          auto res = transaction_manager.PerformRead(tuple_location);
+          if (!res) {
+            transaction_manager.SetTransactionResult(RESULT_FAILURE);
+            return res;
+          }
           
-  //         if (is_blind_write_ == false && concurrency::current_txn->IsStaticReadOnlyTxn() == false) {
-  //           auto res = transaction_manager.PerformRead(tuple_location);
-  //           if (!res) {
-  //             transaction_manager.SetTransactionResult(RESULT_FAILURE);
-  //             transaction_manager.AddOneReadAbort();
-  //             return res;
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-  // // Construct a logical tile for each block
-  // for (auto tuples : visible_tuples) {
-  //   auto &manager = catalog::Manager::GetInstance();
-  //   auto tile_group = manager.GetTileGroup(tuples.first);
+        } else {
+          expression::ContainerTuple<storage::TileGroup> tuple(
+            tile_group.get(), tuple_location.offset);
+          auto eval =
+            predicate_->Evaluate(&tuple, nullptr, executor_context_).IsTrue();
+          if (eval == true) {
+            visible_tuples[tuple_location.block].push_back(tuple_location.offset);
 
-  //   std::unique_ptr<LogicalTile> logical_tile(LogicalTileFactory::GetTile());
-  //   // Add relevant columns to logical tile
-  //   logical_tile->AddColumns(tile_group, full_column_ids_);
-  //   logical_tile->AddPositionList(std::move(tuples.second));
-  //   if (column_ids_.size() != 0) {
-  //     logical_tile->ProjectColumns(full_column_ids_, column_ids_);
-  //   }
+            auto res = transaction_manager.PerformRead(tuple_location);
+            if (!res) {
+              transaction_manager.SetTransactionResult(RESULT_FAILURE);
+              return res;
+            }
+          }
+        }
+        break;
+      }
+    }
+    LOG_TRACE("Traverse length: %d\n", (int)chain_length);
+  }
 
-  //   result_.push_back(logical_tile.release());
-  // }
+  // Construct a logical tile for each block
+  for (auto tuples : visible_tuples) {
+    auto &manager = catalog::Manager::GetInstance();
+    auto tile_group = manager.GetTileGroup(tuples.first);
 
-  // done_ = true;
+    std::unique_ptr<LogicalTile> logical_tile(LogicalTileFactory::GetTile());
+    // Add relevant columns to logical tile
+    logical_tile->AddColumns(tile_group, full_column_ids_);
+    logical_tile->AddPositionList(std::move(tuples.second));
+    if (column_ids_.size() != 0) {
+      logical_tile->ProjectColumns(full_column_ids_, column_ids_);
+    }
 
-  // LOG_TRACE("Result tiles : %lu", result_.size());
+    result_.push_back(logical_tile.release());
+  }
 
-  // return true;
+  done_ = true;
+
+  LOG_TRACE("Result tiles : %lu", result_.size());
+
+  return true;
 }
-
-// bool IndexScanExecutor::ExecTupleSecondaryIndexLookup() {
-//   LOG_TRACE("ExecTupleSecondaryIndexLookup");
-//   PL_ASSERT(!done_);
-
-//   std::vector<ItemPointer *> tuple_location_ptrs;
-
-//   PL_ASSERT(index_->GetIndexType() != INDEX_CONSTRAINT_TYPE_PRIMARY_KEY);
-//   PL_ASSERT(index::IndexFactory::GetSecondaryIndexType() == SECONDARY_INDEX_TYPE_TUPLE);
-
-//   if (0 == key_column_ids_.size()) {
-//     index_->ScanAllKeys(tuple_location_ptrs);
-//   } else {
-//     index_->Scan(values_, key_column_ids_, expr_types_,
-//                  SCAN_DIRECTION_TYPE_FORWARD, tuple_location_ptrs);
-//   }
-
-//   if (tuple_location_ptrs.size() == 0) {
-//     LOG_TRACE("no tuple is retrieved from index.");
-//     return false;
-//   }
-
-//   //fprintf(stdout, "What the fuck are you looping on?\n");
-//   //fprintf(stdout, "Get result of size %d\n", (int) tuple_location_ptrs.size());
-
-//   auto &transaction_manager =
-//     concurrency::TransactionManagerFactory::GetInstance();
-
-//   std::map<oid_t, std::vector<oid_t>> visible_tuples;
-
-//   for (auto tuple_location_ptr : tuple_location_ptrs) {
-//     ItemPointer tuple_location = *tuple_location_ptr;
-
-//     auto &manager = catalog::Manager::GetInstance();
-
-//     auto tile_group = manager.GetTileGroup(tuple_location.block);
-//     auto tile_group_header = tile_group.get()->GetHeader();
-
-//     size_t chain_length = 0;
-
-//     while (true) {
-//       // check the version chain
-//       ++chain_length;
-
-//       auto visibility = transaction_manager.IsVisible(tile_group_header, tuple_location.offset);
-
-//       if (visibility == VISIBILITY_DELETED) {
-//         LOG_TRACE("encounter deleted tuple: %u, %u", tuple_location.block, tuple_location.offset);
-//         break;
-//       }
-//       if (visibility == VISIBILITY_INVISIBLE) {
-//         LOG_TRACE("Invisible read: %u, %u", tuple_location.block,
-//                   tuple_location.offset);
-
-//         // Break for new to old
-//         if (concurrency::TransactionManagerFactory::GetProtocol() == CONCURRENCY_TYPE_OCC_N2O
-//             && tile_group_header->GetTransactionId(tuple_location.offset) == INITIAL_TXN_ID
-//             && tile_group_header->GetEndCommitId(tuple_location.offset) <= concurrency::current_txn->GetBeginCommitId()) {
-//           // See an invisible version that does not belong to any one in a new to old version chain.
-//           // In such case, we assert that there should be either a deleted version or a newly updated version.
-//           // So we just wire back using the index head ptr stored in the reserve field.
-//           tuple_location = *((concurrency::OptimisticN2OTxnManager*)(&transaction_manager))->
-//             GetHeadPtr(tile_group_header, tuple_location.offset);
-//           tile_group = manager.GetTileGroup(tuple_location.block);
-//           tile_group_header = tile_group.get()->GetHeader();
-//           //chain_length = 0;
-//           continue;
-//         }
-
-//         // Break for new to old
-//         if (concurrency::TransactionManagerFactory::GetProtocol() == CONCURRENCY_TYPE_TO_N2O
-//             && tile_group_header->GetTransactionId(tuple_location.offset) == INITIAL_TXN_ID
-//             && tile_group_header->GetEndCommitId(tuple_location.offset) <= concurrency::current_txn->GetBeginCommitId()) {
-//           // See an invisible version that does not belong to any one in a new to old version chain
-//           // Wire back
-//           tuple_location = *((concurrency::TsOrderN2OTxnManager*)(&transaction_manager))->
-//             GetHeadPtr(tile_group_header, tuple_location.offset);
-//           tile_group = manager.GetTileGroup(tuple_location.block);
-//           tile_group_header = tile_group.get()->GetHeader();
-//           //chain_length = 0;
-//           continue;
-//         }
-
-//         ItemPointer old_item = tuple_location;
-//         tuple_location = tile_group_header->GetNextItemPointer(old_item.offset);
-
-//         // there must exist a visible version.
-
-//         if(tuple_location.IsNull()) {
-//           // FIXME:
-//           // For an index scan on a version chain, the result should be one of the following:
-//           //    (1) find a visible version
-//           //    (2) find a deleted version
-//           //    (3) find an aborted version with chain length equal to one
-//           if (chain_length == 1) {
-//             break;
-//           }
-
-//           // If we have traversed through the chain and still can not fulfill one of the above conditions,
-//           // something wrong must happen.
-//           // For speculative read, a transaction may incidentally miss a visible tuple due to a non-atomic
-//           // timestamp update. In such case, we just return false and abort the txn.
-//           transaction_manager.SetTransactionResult(RESULT_FAILURE);
-//           transaction_manager.AddOneReadAbort();
-//           return false;
-//         }
-
-//         tile_group = manager.GetTileGroup(tuple_location.block);
-//         tile_group_header = tile_group.get()->GetHeader();
-//       } else {
-//         PL_ASSERT(visibility == VISIBILITY_OK);
-//         LOG_TRACE("perform read: %u, %u", tuple_location.block,
-//                   tuple_location.offset);
-
-//         // Further check if the version has the secondary key
-//         storage::Tuple key_tuple(index_->GetKeySchema(), true);
-//         expression::ContainerTuple<storage::TileGroup> candidate_tuple(
-//           tile_group.get(), tuple_location.offset
-//         );
-//         // Construct the key tuple
-//         auto &indexed_columns = index_->GetKeySchema()->GetIndexedColumns();
-
-//         oid_t this_col_itr = 0;
-//         for (auto col : indexed_columns) {
-//           key_tuple.SetValue(this_col_itr, candidate_tuple.GetValue(col), index_->GetPool());
-//           this_col_itr++;
-//         }
-
-//         // Compare the key tuple and the key
-//         if (index_->Compare(key_tuple, key_column_ids_, expr_types_, values_) == false) {
-//           LOG_TRACE("Secondary key mismatch: %u, %u\n", tuple_location.block, tuple_location.offset);
-//           break;
-//         }
-
-//         // perform predicate evaluation.
-//         if (predicate_ == nullptr) {
-//           visible_tuples[tuple_location.block].push_back(tuple_location.offset);
-
-//           if (is_blind_write_ == false && concurrency::current_txn->IsStaticReadOnlyTxn() == false) {
-//             auto res = transaction_manager.PerformRead(tuple_location);
-//             if (!res) {
-//               transaction_manager.SetTransactionResult(RESULT_FAILURE);
-//               transaction_manager.AddOneReadAbort();
-//               return res;
-//             }
-//           }
-//         } else {
-//           expression::ContainerTuple<storage::TileGroup> tuple(
-//             tile_group.get(), tuple_location.offset);
-//           auto eval =
-//             predicate_->Evaluate(&tuple, nullptr, executor_context_).IsTrue();
-//           if (eval == true) {
-//             visible_tuples[tuple_location.block].push_back(tuple_location.offset);
-
-//             if (is_blind_write_ == false && concurrency::current_txn->IsStaticReadOnlyTxn() == false) {
-//               auto res = transaction_manager.PerformRead(tuple_location);
-//               if (!res) {
-//                 transaction_manager.SetTransactionResult(RESULT_FAILURE);
-//                 transaction_manager.AddOneReadAbort();
-//                 return res;
-//               }
-//             }
-//           }
-//         }
-//         break;
-//       }
-//     }
-//     LOG_TRACE("Traverse length: %d\n", (int)chain_length);
-//   }
-
-//   // Construct a logical tile for each block
-//   for (auto tuples : visible_tuples) {
-//     auto &manager = catalog::Manager::GetInstance();
-//     auto tile_group = manager.GetTileGroup(tuples.first);
-
-//     std::unique_ptr<LogicalTile> logical_tile(LogicalTileFactory::GetTile());
-//     // Add relevant columns to logical tile
-//     logical_tile->AddColumns(tile_group, full_column_ids_);
-//     logical_tile->AddPositionList(std::move(tuples.second));
-//     if (column_ids_.size() != 0) {
-//       logical_tile->ProjectColumns(full_column_ids_, column_ids_);
-//     }
-
-//     result_.push_back(logical_tile.release());
-//   }
-
-//   done_ = true;
-
-//   LOG_TRACE("Result tiles : %lu", result_.size());
-
-//   return true;
-// }
 
 
 }  // namespace executor
