ad38abf69f397dedd8431d10cfa96a0d5cd1ea24
Yingjun Wu <wu.yj0616@gmail.com>
2016-08-17 14:13:28 -0400

    add test case for thread pool

--- test/common/thread_pool_test.cpp
+++ test/common/thread_pool_test.cpp
@@ -25,7 +25,25 @@ class ThreadPoolTests : public PelotonTest {};
 
 
 TEST_F(ThreadPoolTests, BasicTest) {
+  ThreadPool thread_pool;
+  thread_pool.Initialize(2);
 
+  int var1 = 1;
+  int var2 = 2;
+  int var3 = 3;
+  int var4 = 4;
+  thread_pool.SubmitTask([](int *var){*var = *var + *var;}, &var1);
+  thread_pool.SubmitTask([](int *var){*var = *var - *var;}, &var2);
+  thread_pool.SubmitTask([](int *var){*var = *var * *var;}, &var3);
+  thread_pool.SubmitTask([](int *var){*var = *var / *var;}, &var4);
+
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  
+  EXPECT_EQ(2, var1);
+  EXPECT_EQ(0, var2);
+  EXPECT_EQ(9, var3);
+  EXPECT_EQ(1, var4);
 
 }
 
