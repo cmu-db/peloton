aa19c44bbef421437bd2fb8f0104814179a34b2b
Yingjun Wu <wu.yj0616@gmail.com>
2016-08-19 17:53:57 -0400

    remove duplicated bwtree

--- test/performance/index_performance_test.cpp
+++ test/performance/index_performance_test.cpp
@@ -390,7 +390,7 @@ static void TestIndexPerformance(const IndexType& index_type) {
 }
 
 TEST_F(IndexPerformanceTests, MultiThreadedTest) {
-  std::vector<IndexType> index_types = {INDEX_TYPE_BTREE, INDEX_TYPE_BWTREE, INDEX_TYPE_BWTREE};
+  std::vector<IndexType> index_types = {INDEX_TYPE_BTREE};
 
   // Run the test suite for each types of index
   for(auto index_type : index_types) {
