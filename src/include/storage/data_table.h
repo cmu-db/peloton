225bd696b7693dc246484771558b91d9124f447e
Yingjun Wu <wu.yj0616@gmail.com>
2016-08-21 20:54:46 -0400

    prepare api for staged version insertion

--- src/include/storage/data_table.h
+++ src/include/storage/data_table.h
@@ -105,6 +105,14 @@ class DataTable : public AbstractTable {
   // insert an version in table. designed for update operation.
   // as we implement logical-pointer indexing mechanism, targets_ptr is required.
   ItemPointer InsertVersion(const storage::Tuple *tuple, const TargetList *targets_ptr, ItemPointer *index_entry_ptr);
+
+  // these two functions are designed for reducing memory allocation by performing in-place update.
+  // in the update executor, we first acquire a version slot from the data table, and then
+  // copy the content into the version. after that, we need to check constraints and then install the version
+  // into all the corresponding indexes.
+  ItemPointer AcquireVersion();
+  bool InstallVersion(const storage::Tuple *tuple, const TargetList *targets_ptr, ItemPointer *index_entry_ptr);
+
   // insert tuple in table. the pointer to the index entry is returned as index_entry_ptr.
   ItemPointer InsertTuple(const Tuple *tuple, concurrency::Transaction *transaction, ItemPointer **index_entry_ptr = nullptr);
   // designed for tables without primary key. e.g., output table used by aggregate_executor.
