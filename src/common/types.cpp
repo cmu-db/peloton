c4c9dcb387153ccebaccd9a7f8e2fdca1dd7132e
Yingjun Wu <wu.yj0616@gmail.com>
2016-08-22 11:13:01 -0400

    resolve conflicts

--- src/common/types.cpp
+++ src/common/types.cpp
@@ -635,25 +635,10 @@ ExpressionType StringToExpressionType(const std::string& str) {
 
 std::string IndexTypeToString(IndexType type) {
   switch (type) {
-<<<<<<< HEAD
     case INDEX_TYPE_INVALID: { return "INVALID"; }
     case INDEX_TYPE_BTREE: { return "BTREE"; }
     case INDEX_TYPE_BWTREE: { return "BWTREE"; }
     case INDEX_TYPE_HASH: { return "HASH"; }
-=======
-    case INDEX_TYPE_INVALID: {
-      return "INVALID";
-    }
-    case INDEX_TYPE_BTREE: {
-      return "BTREE";
-    }
-    case INDEX_TYPE_BWTREE: {
-      return "BWTREE";
-    }
-    case INDEX_TYPE_HASH: {
-      return "HASH";
-    }
->>>>>>> master
   }
   return "INVALID";
 }
