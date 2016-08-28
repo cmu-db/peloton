50af172453f4f55c99362fdfbaf09b8a6ae01cf7
Yingjun Wu <wu.yj0616@gmail.com>
2016-08-28 00:22:36 -0400

    update tpcc benchmark

--- src/include/benchmark/tpcc/tpcc_workload.h
+++ src/include/benchmark/tpcc/tpcc_workload.h
@@ -16,6 +16,9 @@
 #include "common/macros.h"
 #include "benchmark/tpcc/tpcc_configuration.h"
 #include "benchmark/tpcc/tpcc_loader.h"
+#include "executor/update_executor.h"
+#include "executor/index_scan_executor.h"
+#include "executor/insert_executor.h"
 
 namespace peloton {
 
@@ -29,6 +32,26 @@ namespace tpcc {
 extern configuration state;
 
 void RunWorkload();
+
+bool RunNewOrder(const size_t &thread_id);
+
+bool RunPayment(const size_t &thread_id);
+
+bool RunDelivery(const size_t &thread_id);
+
+bool RunOrderStatus(const size_t &thread_id);
+
+bool RunStockLevel(const size_t &thread_id);
+
+size_t GenerateWarehouseId(const size_t &thread_id);
+
+/////////////////////////////////////////////////////////
+
+std::vector<std::vector<Value>> ExecuteRead(executor::AbstractExecutor* executor);
+
+void ExecuteUpdate(executor::AbstractExecutor* executor);
+
+void ExecuteDeleteTest(executor::AbstractExecutor* executor);
 
 }  // namespace tpcc
 }  // namespace benchmark
--- src/main/tpcc/tpcc.cpp
+++ src/main/tpcc/tpcc.cpp
@@ -1,68 +1,84 @@
-// //===----------------------------------------------------------------------===//
-// //
-// //                         Peloton
-// //
-// // tpcc.cpp
-// //
-// // Identification: src/main/tpcc/tpcc.cpp
-// //
-// // Copyright (c) 2015-16, Carnegie Mellon University Database Group
-// //
-// //===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// tpcc.cpp
+//
+// Identification: src/main/tpcc/tpcc.cpp
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
 
 
-// #include <iostream>
-// #include <fstream>
-// #include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
 
-// #include "benchmark/tpcc/tpcc_configuration.h"
-// #include "benchmark/tpcc/tpcc_loader.h"
-// #include "benchmark/tpcc/tpcc_workload.h"
+#include "benchmark/tpcc/tpcc_configuration.h"
+#include "benchmark/tpcc/tpcc_loader.h"
+#include "benchmark/tpcc/tpcc_workload.h"
 
-// #include "common/logger.h"
+#include "common/logger.h"
 
-// namespace peloton {
-// namespace benchmark {
-// namespace tpcc {
+namespace peloton {
+namespace benchmark {
+namespace tpcc {
 
-// configuration state;
+configuration state;
 
-// std::ofstream out("outputfile.summary");
+std::ofstream out("outputfile.summary");
 
-// static void WriteOutput(double stat) {
-//   LOG_INFO("----------------------------------------------------------");
-//   LOG_INFO("%d %d :: %lf", state.scale_factor, state.backend_count, stat);
+static void WriteOutput() {
+  LOG_INFO("----------------------------------------------------------");
+  LOG_INFO("%d %d %d :: %lf %lf",
+           state.scale_factor,
+           state.backend_count,
+           state.warehouse_count,
+           state.throughput,
+           state.abort_rate);
 
-//   out << state.backend_count << " ";
-//   out << stat << "\n";
-//   out.flush();
-//   out.close();
-// }
+  out << state.scale_factor << " ";
+  out << state.backend_count << " ";
+  out << state.warehouse_count << " ";
+  out << state.throughput << " ";
+  out << state.abort_rate << "\n";
+  for (size_t round_id = 0; round_id < state.profile_throughput.size();
+       ++round_id) {
+    out << "[" << std::setw(3) << std::left
+        << state.profile_duration * round_id << " - " << std::setw(3)
+        << std::left << state.profile_duration * (round_id + 1)
+        << " s]: " << state.profile_throughput[round_id] << " "
+        << state.profile_abort_rate[round_id] << "\n";
+  }
+  out.flush();
+  out.close();
+}
 
-// // Main Entry Point
-// void RunBenchmark() {
-//   // Create the database
-//   CreateTPCCDatabase();
+// Main Entry Point
+void RunBenchmark() {
+  // Create the database
+  CreateTPCCDatabase();
 
-//   // Load the database
-//   LoadTPCCDatabase();
+  // Load the database
+  LoadTPCCDatabase();
 
-//   // Run the workload
-//   RunWorkload();
+  // Run the workload
+  RunWorkload();
 
-//   // Emit throughput
-//   WriteOutput(state.throughput);
-// }
+  // Emit throughput
+  WriteOutput();
+}
 
-// }  // namespace tpcc
-// }  // namespace benchmark
-// }  // namespace peloton
+}  // namespace tpcc
+}  // namespace benchmark
+}  // namespace peloton
 
-// int main(int argc, char **argv) {
-//   peloton::benchmark::tpcc::ParseArguments(argc, argv,
-//                                            peloton::benchmark::tpcc::state);
+int main(int argc, char **argv) {
+  peloton::benchmark::tpcc::ParseArguments(argc, argv,
+                                           peloton::benchmark::tpcc::state);
 
-//   peloton::benchmark::tpcc::RunBenchmark();
+  peloton::benchmark::tpcc::RunBenchmark();
 
-//   return 0;
-// }
+  return 0;
+}
