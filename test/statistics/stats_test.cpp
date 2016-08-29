653df1ad91a3e8c1a122dc80df8aab120231f97f
Lin Ma <malin1993ml@gmail.com>
2016-08-29 11:11:03 -0400

    Some fixes for tests.

--- test/statistics/stats_test.cpp
+++ test/statistics/stats_test.cpp
@@ -10,23 +10,23 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "harness.h"
+#include "common/harness.h"
 
-#include "statistics/stats_tests_util.h"
 #include "statistics/stats_aggregator.h"
 #include "statistics/backend_stats_context.h"
 #include "storage/data_table.h"
 #include "concurrency/transaction_manager_factory.h"
-#include "executor_tests_util.h"
+#include "executor/executor_tests_util.h"
 #include "storage/tuple.h"
 #include "common/value.h"
+#include "common/config.h"
 #include "common/value_factory.h"
+
+#include "statistics/stats_tests_util.h"
 
 #include <iostream>
 using std::cout;
 using std::endl;
-
-extern StatsType peloton_stats_mode;
 
 namespace peloton {
 namespace test {
@@ -34,13 +34,12 @@ namespace test {
 class StatsTest : public PelotonTest {};
 
 TEST_F(StatsTest, PerThreadStatsTest) {
-  peloton_stats_mode = STATS_TYPE_ENABLE;
+  FLAGS_stats_mode = STATS_TYPE_ENABLE;
 
   /*
    * Register to StatsAggregator
    */
   peloton::stats::StatsAggregator::GetInstance(1000000);
-  auto stats_context = stats::BackendStatsContext::GetInstance();
 
   // int tuple_count = 10;
   int tups_per_tile_group = 100;
@@ -48,7 +47,7 @@ TEST_F(StatsTest, PerThreadStatsTest) {
 
   // Create a table and wrap it in logical tiles
   auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
-  __attribute__((unused)) auto txn = txn_manager.BeginTransaction();
+  auto txn = txn_manager.BeginTransaction();
   std::unique_ptr<storage::DataTable> data_table(
       ExecutorTestsUtil::CreateTable(tups_per_tile_group, false));
 
@@ -72,16 +71,19 @@ TEST_F(StatsTest, PerThreadStatsTest) {
     tuple_slot_ids.push_back(tuple_slot_id);
     EXPECT_TRUE(tuple_slot_id.block != INVALID_OID);
     EXPECT_TRUE(tuple_slot_id.offset != INVALID_OID);
-    txn_manager.PerformInsert(tuple_slot_id);
+    txn_manager.PerformInsert(txn, tuple_slot_id);
   }
-  txn_manager.CommitTransaction();
+  txn_manager.CommitTransaction(txn);
   oid_t database_id = data_table->GetDatabaseOid();
   oid_t table_id = data_table->GetOid();
 
   // Check: # transactions committed = 1, # table inserts = 10
-  int64_t txn_commited =
-      stats_context->GetDatabaseMetric(database_id)->GetTxnCommitted().GetCounter();
-  int64_t inserts = stats_context->GetTableMetric(database_id, table_id)
+  int64_t txn_commited = stats::BackendStatsContext::GetInstance()
+                             .GetDatabaseMetric(database_id)
+                             ->GetTxnCommitted()
+                             .GetCounter();
+  int64_t inserts = stats::BackendStatsContext::GetInstance()
+                        .GetTableMetric(database_id, table_id)
                         ->GetTableAccess()
                         .GetInserts();
   EXPECT_EQ(1, txn_commited);
@@ -90,17 +92,21 @@ TEST_F(StatsTest, PerThreadStatsTest) {
   // Read every other tuple
   txn = txn_manager.BeginTransaction();
   for (size_t i = 0; i < tuple_slot_ids.size(); i += 2) {
-    txn_manager.PerformRead(tuple_slot_ids[i]);
+    txn_manager.PerformRead(txn, tuple_slot_ids[i]);
   }
-  txn_manager.CommitTransaction();
+  txn_manager.CommitTransaction(txn);
 
   // Check: # transactions committed = 2, # inserts = 10, # reads = 5
-  txn_commited =
-      stats_context->GetDatabaseMetric(database_id)->GetTxnCommitted().GetCounter();
-  inserts = stats_context->GetTableMetric(database_id, table_id)
+  txn_commited = stats::BackendStatsContext::GetInstance()
+                     .GetDatabaseMetric(database_id)
+                     ->GetTxnCommitted()
+                     .GetCounter();
+  inserts = stats::BackendStatsContext::GetInstance()
+                .GetTableMetric(database_id, table_id)
                 ->GetTableAccess()
                 .GetInserts();
-  int64_t reads = stats_context->GetTableMetric(database_id, table_id)
+  int64_t reads = stats::BackendStatsContext::GetInstance()
+                      .GetTableMetric(database_id, table_id)
                       ->GetTableAccess()
                       .GetReads();
   EXPECT_EQ(2, txn_commited);
@@ -109,15 +115,20 @@ TEST_F(StatsTest, PerThreadStatsTest) {
 
   // Do a single read and abort
   txn = txn_manager.BeginTransaction();
-  txn_manager.PerformRead(tuple_slot_ids[0]);
-  txn_manager.AbortTransaction();
+  txn_manager.PerformRead(txn, tuple_slot_ids[0]);
+  txn_manager.AbortTransaction(txn);
 
   // Check: # txns committed = 2, # txns aborted = 1, # reads = 6
-  txn_commited =
-      stats_context->GetDatabaseMetric(database_id)->GetTxnCommitted().GetCounter();
-  int64_t txn_aborted =
-      stats_context->GetDatabaseMetric(database_id)->GetTxnAborted().GetCounter();
-  reads = stats_context->GetTableMetric(database_id, table_id)
+  txn_commited = stats::BackendStatsContext::GetInstance()
+                     .GetDatabaseMetric(database_id)
+                     ->GetTxnCommitted()
+                     .GetCounter();
+  int64_t txn_aborted = stats::BackendStatsContext::GetInstance()
+                            .GetDatabaseMetric(database_id)
+                            ->GetTxnAborted()
+                            .GetCounter();
+  reads = stats::BackendStatsContext::GetInstance()
+              .GetTableMetric(database_id, table_id)
               ->GetTableAccess()
               .GetReads();
   EXPECT_EQ(2, txn_commited);
@@ -130,17 +141,21 @@ TEST_F(StatsTest, PerThreadStatsTest) {
   ItemPointer new_tuple_slot_id = data_table->InsertTuple(&new_tuple);
   EXPECT_TRUE(new_tuple_slot_id.block != INVALID_OID);
   EXPECT_TRUE(new_tuple_slot_id.offset != INVALID_OID);
-  txn_manager.PerformUpdate(tuple_slot_ids[0], new_tuple_slot_id);
-  txn_manager.PerformRead(tuple_slot_ids[0]);
-  txn_manager.CommitTransaction();
+  txn_manager.PerformUpdate(txn, tuple_slot_ids[0], new_tuple_slot_id);
+  txn_manager.PerformRead(txn, tuple_slot_ids[0]);
+  txn_manager.CommitTransaction(txn);
 
   // Check: # txns committed = 3, # updates = 1, # reads = 7
-  txn_commited =
-      stats_context->GetDatabaseMetric(database_id)->GetTxnCommitted().GetCounter();
-  reads = stats_context->GetTableMetric(database_id, table_id)
+  txn_commited = stats::BackendStatsContext::GetInstance()
+                     .GetDatabaseMetric(database_id)
+                     ->GetTxnCommitted()
+                     .GetCounter();
+  reads = stats::BackendStatsContext::GetInstance()
+              .GetTableMetric(database_id, table_id)
               ->GetTableAccess()
               .GetReads();
-  int64_t updates = stats_context->GetTableMetric(database_id, table_id)
+  int64_t updates = stats::BackendStatsContext::GetInstance()
+                        .GetTableMetric(database_id, table_id)
                         ->GetTableAccess()
                         .GetUpdates();
   EXPECT_EQ(3, txn_commited);
@@ -149,21 +164,24 @@ TEST_F(StatsTest, PerThreadStatsTest) {
 
   // Delete the 6th tuple and read the 1st tuple
   txn = txn_manager.BeginTransaction();
-  storage::Tuple new_tuple2 = storage::Tuple(data_table->GetSchema(), true);
-  ItemPointer new_location = data_table->InsertEmptyVersion(&new_tuple2);
+  ItemPointer new_location = data_table->InsertEmptyVersion();
   EXPECT_TRUE(new_location.block != INVALID_OID);
   EXPECT_TRUE(new_location.offset != INVALID_OID);
-  txn_manager.PerformDelete(tuple_slot_ids[5], new_location);
-  txn_manager.PerformRead(tuple_slot_ids[1]);
-  txn_manager.CommitTransaction();
+  txn_manager.PerformDelete(txn, tuple_slot_ids[5], new_location);
+  txn_manager.PerformRead(txn, tuple_slot_ids[1]);
+  txn_manager.CommitTransaction(txn);
 
   // Check: # txns committed = 4, # deletes = 1, # reads = 8
-  txn_commited =
-      stats_context->GetDatabaseMetric(database_id)->GetTxnCommitted().GetCounter();
-  reads = stats_context->GetTableMetric(database_id, table_id)
+  txn_commited = stats::BackendStatsContext::GetInstance()
+                     .GetDatabaseMetric(database_id)
+                     ->GetTxnCommitted()
+                     .GetCounter();
+  reads = stats::BackendStatsContext::GetInstance()
+              .GetTableMetric(database_id, table_id)
               ->GetTableAccess()
               .GetReads();
-  int64_t deletes = stats_context->GetTableMetric(database_id, table_id)
+  int64_t deletes = stats::BackendStatsContext::GetInstance()
+                        .GetTableMetric(database_id, table_id)
                         ->GetTableAccess()
                         .GetDeletes();
   EXPECT_EQ(4, txn_commited);
--- test/statistics/stats_tests_util.cpp
+++ test/statistics/stats_tests_util.cpp
@@ -11,17 +11,17 @@
 //===----------------------------------------------------------------------===//
 
 #include "statistics/stats_tests_util.h"
-#include "backend/executor/executor_context.h"
-#include "backend/executor/delete_executor.h"
-#include "backend/executor/insert_executor.h"
-#include "backend/executor/seq_scan_executor.h"
-#include "backend/executor/update_executor.h"
-#include "backend/executor/logical_tile_factory.h"
-#include "backend/expression/expression_util.h"
-#include "backend/storage/tile.h"
+#include "executor/executor_context.h"
+#include "executor/delete_executor.h"
+#include "executor/insert_executor.h"
+#include "executor/seq_scan_executor.h"
+#include "executor/update_executor.h"
+#include "executor/logical_tile_factory.h"
+#include "expression/expression_util.h"
+#include "storage/tile.h"
 #include "executor/mock_executor.h"
-#include "backend/planner/delete_plan.h"
-#include "backend/planner/insert_plan.h"
+#include "planner/delete_plan.h"
+#include "planner/insert_plan.h"
 #include "executor/executor_tests_util.h"
 
 namespace peloton {
@@ -34,11 +34,10 @@ storage::Tuple StatsTestsUtil::PopulateTuple(const catalog::Schema *schema,
                                              int fourth_col_val) {
   auto testing_pool = TestingHarness::GetInstance().GetTestingPool();
   storage::Tuple tuple(schema, true);
-  tuple.SetValue(
-      0, ValueFactory::GetIntegerValue(first_col_val), testing_pool);
+  tuple.SetValue(0, ValueFactory::GetIntegerValue(first_col_val), testing_pool);
 
-  tuple.SetValue(
-      1, ValueFactory::GetIntegerValue(second_col_val), testing_pool);
+  tuple.SetValue(1, ValueFactory::GetIntegerValue(second_col_val),
+                 testing_pool);
 
   tuple.SetValue(2, ValueFactory::GetDoubleValue(third_col_val), testing_pool);
 
@@ -47,6 +46,5 @@ storage::Tuple StatsTestsUtil::PopulateTuple(const catalog::Schema *schema,
   tuple.SetValue(3, string_value, testing_pool);
   return tuple;
 }
-
 }
 }
