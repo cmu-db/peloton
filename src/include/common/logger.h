0db67ed7796e7c44dd735f0bc8d65f5476187fe9
Lin Ma <malin1993ml@gmail.com>
2016-09-10 16:04:47 -0400

    Fix the debug logger (#235)
    * Add multi-thread option to make commands.
    
    * Fix the LOG_LEVEL macros for debug information.
    
    * Replace printf with fprintf in logger.
    
    * Fix a memory leak in Tuple::IsNull().
    
    * Fix the memory leak in LogicalTile::GetInfo().
    
    * Fix the memory leak in ExecutorTestsUtil::PrintTileVector().

--- src/include/common/logger.h
+++ src/include/common/logger.h
@@ -14,137 +14,162 @@
 
 /**
  * Debug logging functions for EE. Unlike the performance counters,
- * these are just printf() turned on/off by LOG_LOG_LEVEL compile option.
+ * these are just fprintf() turned on/off by LOG_LEVEL compile option.
  * The main concern here is not to add any overhead on runtime performance
  * when the logging is turned off. Use LOG_XXX_ENABLED macros defined here to
  * eliminate all instructions in the final binary.
  * @author Hideaki
 */
 
-#include <string>
 #include <ctime>
+#include <string>
 
 namespace peloton {
 
 // Log levels.
-#define LOG_LEVEL_OFF    1000
-#define LOG_LEVEL_ERROR  500
-#define LOG_LEVEL_WARN   400
-#define LOG_LEVEL_INFO   300
-#define LOG_LEVEL_DEBUG  200
-#define LOG_LEVEL_TRACE  100
-#define LOG_LEVEL_ALL    0
+#define LOG_LEVEL_OFF 1000
+#define LOG_LEVEL_ERROR 500
+#define LOG_LEVEL_WARN 400
+#define LOG_LEVEL_INFO 300
+#define LOG_LEVEL_DEBUG 200
+#define LOG_LEVEL_TRACE 100
+#define LOG_LEVEL_ALL 0
 
 #define LOG_LOG_TIME_FORMAT "%Y-%m-%d %H:%M:%S"
+#define LOG_OUTPUT_STREAM stdout
 
 // Compile Option
-#ifndef LOG_LOG_LEVEL
-    // TODO : any way to use pragma message in GCC?
-    //#pragma message("Warning: LOG_LOG_LEVEL compile option was not explicitly given.")
-    #if defined(DEBUG) || defined(_DEBUG) || defined(_DEBUG_)
-        //#pragma message("LOG_LEVEL_DEBUG is used instead as DEBUG option is on.")
-        #define LOG_LOG_LEVEL LOG_LEVEL_DEBUG
-    #else
-        //#pragma message("LOG_LEVEL_WARN is used instead as DEBUG option is off.")
-        #define LOG_LOG_LEVEL LOG_LEVEL_WARN
-    #endif
-    //#pragma message("Give LOG_LOG_LEVEL compile option to overwrite the default level.")
+#ifndef LOG_LEVEL
+// TODO : any way to use pragma message in GCC?
+//#pragma message("Warning: LOG_LEVEL compile option was not explicitly
+// given.")
+#ifndef NDEBUG
+//#pragma message("LOG_LEVEL_DEBUG is used instead as DEBUG option is on.")
+#define LOG_LEVEL LOG_LEVEL_DEBUG
+#else
+//#pragma message("LOG_LEVEL_WARN is used instead as DEBUG option is off.")
+#define LOG_LEVEL LOG_LEVEL_WARN
 #endif
-
+//#pragma message("Give LOG_LEVEL compile option to overwrite the default
+// level.")
+#endif
 
 // For compilers which do not support __FUNCTION__
 #if !defined(__FUNCTION__) && !defined(__GNUC__)
-    #define __FUNCTION__ ""
+#define __FUNCTION__ ""
 #endif
 
 void outputLogHeader_(const char *file, int line, const char *func, int level);
 
 // Two convenient macros for debugging
 // 1. Logging macros.
-// 2. LOG_XXX_ENABLED macros. Use these to "eliminate" all the debug blocks from release binary.
+// 2. LOG_XXX_ENABLED macros. Use these to "eliminate" all the debug blocks from
+// release binary.
 #ifdef LOG_ERROR_ENABLED
-    #undef LOG_ERROR_ENABLED
+#undef LOG_ERROR_ENABLED
 #endif
-#if LOG_LOG_LEVEL<=LOG_LEVEL_ERROR
-    #define LOG_ERROR_ENABLED
-    //#pragma message("LOG_ERROR was enabled.")
-    #define LOG_ERROR(...) outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_ERROR);::printf(__VA_ARGS__);printf("\n");::fflush(stdout)
+#if LOG_LEVEL <= LOG_LEVEL_ERROR
+#define LOG_ERROR_ENABLED
+//#pragma message("LOG_ERROR was enabled.")
+#define LOG_ERROR(...)                                                 \
+  outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_ERROR); \
+  ::fprintf(LOG_OUTPUT_STREAM, __VA_ARGS__);                           \
+  fprintf(LOG_OUTPUT_STREAM, "\n");                                    \
+  ::fflush(stdout)
 #else
-    #define LOG_ERROR(...) ((void)0)
+#define LOG_ERROR(...) ((void)0)
 #endif
 
 #ifdef LOG_WARN_ENABLED
-    #undef LOG_WARN_ENABLED
+#undef LOG_WARN_ENABLED
 #endif
-#if LOG_LOG_LEVEL<=LOG_LEVEL_WARN
-    #define LOG_WARN_ENABLED
-    //#pragma message("LOG_WARN was enabled.")
-    #define LOG_WARN(...) outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_WARN);::printf(__VA_ARGS__);printf("\n");::fflush(stdout)
+#if LOG_LEVEL <= LOG_LEVEL_WARN
+#define LOG_WARN_ENABLED
+//#pragma message("LOG_WARN was enabled.")
+#define LOG_WARN(...)                                                 \
+  outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_WARN); \
+  ::fprintf(LOG_OUTPUT_STREAM, __VA_ARGS__);                          \
+  fprintf(LOG_OUTPUT_STREAM, "\n");                                   \
+  ::fflush(stdout)
 #else
-    #define LOG_WARN(...) ((void)0)
+#define LOG_WARN(...) ((void)0)
 #endif
 
 #ifdef LOG_INFO_ENABLED
-    #undef LOG_INFO_ENABLED
+#undef LOG_INFO_ENABLED
 #endif
-#if LOG_LOG_LEVEL<=LOG_LEVEL_INFO
-    #define LOG_INFO_ENABLED
-    //#pragma message("LOG_INFO was enabled.")
-    #define LOG_INFO(...) outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_INFO);::printf(__VA_ARGS__);printf("\n");::fflush(stdout)
+#if LOG_LEVEL <= LOG_LEVEL_INFO
+#define LOG_INFO_ENABLED
+//#pragma message("LOG_INFO was enabled.")
+#define LOG_INFO(...)                                                 \
+  outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_INFO); \
+  ::fprintf(LOG_OUTPUT_STREAM, __VA_ARGS__);                          \
+  fprintf(LOG_OUTPUT_STREAM, "\n");                                   \
+  ::fflush(stdout)
 #else
-    #define LOG_INFO(...) ((void)0)
+#define LOG_INFO(...) ((void)0)
 #endif
 
 #ifdef LOG_DEBUG_ENABLED
-    #undef LOG_DEBUG_ENABLED
+#undef LOG_DEBUG_ENABLED
 #endif
-#if LOG_LOG_LEVEL<=LOG_LEVEL_DEBUG
-    #define LOG_DEBUG_ENABLED
-    //#pragma message("LOG_DEBUG was enabled.")
-    #define LOG_DEBUG(...) outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_DEBUG);::printf(__VA_ARGS__);printf("\n");::fflush(stdout)
+#if LOG_LEVEL <= LOG_LEVEL_DEBUG
+#define LOG_DEBUG_ENABLED
+//#pragma message("LOG_DEBUG was enabled.")
+#define LOG_DEBUG(...)                                                 \
+  outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_DEBUG); \
+  ::fprintf(LOG_OUTPUT_STREAM, __VA_ARGS__);                           \
+  fprintf(LOG_OUTPUT_STREAM, "\n");                                    \
+  ::fflush(stdout)
 #else
-    #define LOG_DEBUG(...) ((void)0)
+#define LOG_DEBUG(...) ((void)0)
 #endif
 
 #ifdef LOG_TRACE_ENABLED
-    #undef LOG_TRACE_ENABLED
+#undef LOG_TRACE_ENABLED
 #endif
-#if LOG_LOG_LEVEL<=LOG_LEVEL_TRACE
-    #define LOG_TRACE_ENABLED
-    //#pragma message("LOG_TRACE was enabled.")
-    #define LOG_TRACE(...) outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_TRACE);::printf(__VA_ARGS__);printf("\n");::fflush(stdout)
+#if LOG_LEVEL <= LOG_LEVEL_TRACE
+#define LOG_TRACE_ENABLED
+//#pragma message("LOG_TRACE was enabled.")
+#define LOG_TRACE(...)                                                 \
+  outputLogHeader_(__FILE__, __LINE__, __FUNCTION__, LOG_LEVEL_TRACE); \
+  ::fprintf(LOG_OUTPUT_STREAM, __VA_ARGS__);                           \
+  fprintf(LOG_OUTPUT_STREAM, "\n");                                    \
+  ::fflush(stdout)
 #else
-    #define LOG_TRACE(...) ((void)0)
+#define LOG_TRACE(...) ((void)0)
 #endif
 
-// Output log message header in this format: [type] [file:line:function] time - 
-// ex: [ERROR] [somefile.cpp:123:doSome()] 2008/07/06 10:00:00 - 
-inline void outputLogHeader_(const char *file, int line, const char *func, int level) {
-    time_t t = ::time(NULL) ;
-    tm *curTime = localtime(&t);
-    char time_str[32]; // FIXME
-    ::strftime(time_str, 32, LOG_LOG_TIME_FORMAT, curTime);
-    const char* type;
-    switch (level) {
-        case LOG_LEVEL_ERROR:
-            type = "ERROR";
-            break;
-        case LOG_LEVEL_WARN:
-            type = "WARN ";
-            break;
-        case LOG_LEVEL_INFO:
-            type = "INFO ";
-            break;
-        case LOG_LEVEL_DEBUG:
-            type = "DEBUG";
-            break;
-        case LOG_LEVEL_TRACE:
-            type = "TRACE";
-            break;
-        default:
-            type = "UNKWN";
-    }
-    ::printf("[%s] [%s:%d:%s()] %s - ", type, file, line, func, time_str);
+// Output log message header in this format: [type] [file:line:function] time -
+// ex: [ERROR] [somefile.cpp:123:doSome()] 2008/07/06 10:00:00 -
+inline void outputLogHeader_(const char *file, int line, const char *func,
+                             int level) {
+  time_t t = ::time(NULL);
+  tm *curTime = localtime(&t);
+  char time_str[32];  // FIXME
+  ::strftime(time_str, 32, LOG_LOG_TIME_FORMAT, curTime);
+  const char *type;
+  switch (level) {
+    case LOG_LEVEL_ERROR:
+      type = "ERROR";
+      break;
+    case LOG_LEVEL_WARN:
+      type = "WARN ";
+      break;
+    case LOG_LEVEL_INFO:
+      type = "INFO ";
+      break;
+    case LOG_LEVEL_DEBUG:
+      type = "DEBUG";
+      break;
+    case LOG_LEVEL_TRACE:
+      type = "TRACE";
+      break;
+    default:
+      type = "UNKWN";
+  }
+  ::fprintf(LOG_OUTPUT_STREAM, "[%s] [%s:%d:%s()] %s - ", type, file, line,
+            func, time_str);
 }
 
 }  // End peloton namespace
