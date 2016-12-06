da8e68dcaf913979bd16575ef7c2e75dfab11c72
Lin Ma <malin1993ml@gmail.com>
2016-12-06 13:50:03 -0500

    [#374] Fix negative values update.

--- src/include/expression/abstract_expression.h
+++ src/include/expression/abstract_expression.h
@@ -10,17 +10,16 @@
 //
 //===----------------------------------------------------------------------===//
 
-
 #pragma once
 
 #include <string>
 
-#include "common/serializeio.h"
+#include "common/logger.h"
+#include "common/macros.h"
 #include "common/printable.h"
+#include "common/serializeio.h"
 #include "common/types.h"
 #include "common/value_factory.h"
-#include "common/macros.h"
-#include "common/logger.h"
 
 namespace peloton {
 
@@ -51,42 +50,39 @@ using namespace peloton::common;
 
 class AbstractExpression : public Printable {
  public:
-
   virtual Value Evaluate(const AbstractTuple *tuple1,
-                              const AbstractTuple *tuple2,
-                              executor::ExecutorContext *context) const = 0;
+                         const AbstractTuple *tuple2,
+                         executor::ExecutorContext *context) const = 0;
 
   /**
    * Return true if this expression or any descendent has a value that should be
    * substituted with a parameter.
    */
   virtual bool HasParameter() const {
-    for(auto &child : children_){
-      if (child->HasParameter()){
+    for (auto &child : children_) {
+      if (child->HasParameter()) {
         return true;
       }
     }
     return false;
   }
 
-  const AbstractExpression * GetChild(int index) const{
+  const AbstractExpression *GetChild(int index) const {
     return GetModifiableChild(index);
   }
 
-  size_t GetChildrenSize() const{
-    return children_.size();
-  }
+  size_t GetChildrenSize() const { return children_.size(); }
 
-  AbstractExpression * GetModifiableChild(int index) const{
-    if(index < 0 || index >= (int)children_.size()){
+  AbstractExpression *GetModifiableChild(int index) const {
+    if (index < 0 || index >= (int)children_.size()) {
       return nullptr;
     }
     return children_[index].get();
   }
 
-  void SetChild(int index, AbstractExpression* expr){
-    if(index >= (int)children_.size()){
-      children_.resize(index+1);
+  void SetChild(int index, AbstractExpression *expr) {
+    if (index >= (int)children_.size()) {
+      children_.resize(index + 1);
     }
     children_[index].reset(expr);
   }
@@ -104,8 +100,8 @@ class AbstractExpression : public Printable {
 
     os << "\tExpression :: "
        << " expression type = " << GetExpressionType() << ","
-       << " value type = " << Type::GetInstance(GetValueType())->ToString() << ","
-       << std::endl;
+       << " value type = " << Type::GetInstance(GetValueType())->ToString()
+       << "," << std::endl;
 
     return os.str();
   }
@@ -122,15 +118,13 @@ class AbstractExpression : public Printable {
   // Each sub-class will have to implement this function
   //===--------------------------------------------------------------------===//
 
-  //virtual bool SerializeTo(SerializeOutput &output) const {}
+  // virtual bool SerializeTo(SerializeOutput &output) const {}
 
-  //virtual bool DeserializeFrom(SerializeInput &input) const {}
+  // virtual bool DeserializeFrom(SerializeInput &input) const {}
 
   virtual int SerializeSize() { return 0; }
 
-  const char* GetExpressionName() const {
-    return expr_name_.c_str();
-  }
+  const char *GetExpressionName() const { return expr_name_.c_str(); }
 
   // Parser stuff
   int ival_ = 0;
@@ -145,16 +139,23 @@ class AbstractExpression : public Printable {
   AbstractExpression(ExpressionType exp_type, Type::TypeId return_value_type)
       : exp_type_(exp_type), return_value_type_(return_value_type) {}
   AbstractExpression(ExpressionType exp_type, Type::TypeId return_value_type,
-                     AbstractExpression *left,
-                     AbstractExpression *right)
+                     AbstractExpression *left, AbstractExpression *right)
       : exp_type_(exp_type), return_value_type_(return_value_type) {
     // Order of these is important!
-    children_.push_back(std::unique_ptr<AbstractExpression>(left));
-    children_.push_back(std::unique_ptr<AbstractExpression>(right));
+    if (left != nullptr)
+      children_.push_back(std::unique_ptr<AbstractExpression>(left));
+    // Sometimes there's no right child. E.g.: OperatorUnaryMinusExpression.
+    if (right != nullptr)
+      children_.push_back(std::unique_ptr<AbstractExpression>(right));
   }
-  AbstractExpression(const AbstractExpression &other) : ival_(other.ival_), expr_name_(other.expr_name_), distinct_(other.distinct_), exp_type_(other.exp_type_),
-      return_value_type_(other.return_value_type_), has_parameter_(other.has_parameter_){
-    for (auto &child : other.children_){
+  AbstractExpression(const AbstractExpression &other)
+      : ival_(other.ival_),
+        expr_name_(other.expr_name_),
+        distinct_(other.distinct_),
+        exp_type_(other.exp_type_),
+        return_value_type_(other.return_value_type_),
+        has_parameter_(other.has_parameter_) {
+    for (auto &child : other.children_) {
       children_.push_back(std::unique_ptr<AbstractExpression>(child->Copy()));
     }
   }
