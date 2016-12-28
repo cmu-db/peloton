f5331a3d4caf9a7a9b45a09e0bfc957d7915c70b
= <675973885@qq.com>
2016-12-27 22:36:09 -0500

    Changing IntsKey name

--- src/index/btree_index.cpp
+++ src/index/btree_index.cpp
@@ -295,13 +295,13 @@ std::string BTREE_TEMPLATE_TYPE::GetTypeName() const { return "Btree"; }
 
 // IMPORTANT: Make sure you don't exceed INTSKEY_MAX_SLOTS
 
-template class BTreeIndex<IntsKey<1>, ItemPointer *, IntsComparator<1>,
+template class BTreeIndex<CompactIntegerKey<1>, ItemPointer *, IntsComparator<1>,
                           IntsEqualityChecker<1>>;
-template class BTreeIndex<IntsKey<2>, ItemPointer *, IntsComparator<2>,
+template class BTreeIndex<CompactIntegerKey<2>, ItemPointer *, IntsComparator<2>,
                           IntsEqualityChecker<2>>;
-template class BTreeIndex<IntsKey<3>, ItemPointer *, IntsComparator<3>,
+template class BTreeIndex<CompactIntegerKey<3>, ItemPointer *, IntsComparator<3>,
                           IntsEqualityChecker<3>>;
-template class BTreeIndex<IntsKey<4>, ItemPointer *, IntsComparator<4>,
+template class BTreeIndex<CompactIntegerKey<4>, ItemPointer *, IntsComparator<4>,
                           IntsEqualityChecker<4>>;
 
 template class BTreeIndex<GenericKey<4>, ItemPointer *, GenericComparator<4>,
