2a27f98f61874a182530fdccb6b493d6d618343c
= <675973885@qq.com>
2017-01-04 22:33:21 -0500

    Adding ScanLimit() into cpp file

--- src/index/bwtree_index.cpp
+++ src/index/bwtree_index.cpp
@@ -188,6 +188,96 @@ void BWTREE_INDEX_TYPE::Scan(
     stats::BackendStatsContext::GetInstance()->IncrementIndexReads(
         result.size(), metadata);
   }
+  
+  return;
+}
+
+/*
+ * ScanLimit() - Scan the index with predicate and limit/offset
+ *
+ * This function scans the index using the given index optimizer's low key and
+ * high key. In addition to merely doing the scan, it checks scan direction
+ * and uses either begin() or end() iterator to scan the index, and stops
+ * after offset + limit elements are scanned, and limit elements are finally
+ * returned
+ */
+BWTREE_TEMPLATE_ARGUMENTS
+void BWTREE_INDEX_TYPE::ScanLimit(
+    UNUSED_ATTRIBUTE const std::vector<type::Value> &value_list,
+    UNUSED_ATTRIBUTE const std::vector<oid_t> &tuple_column_id_list,
+    UNUSED_ATTRIBUTE const std::vector<ExpressionType> &expr_list,
+    const ScanDirectionType &scan_direction, 
+    std::vector<ValueType> &result,
+    const ConjunctionScanPredicate *csp_p,
+    uint64_t limit,
+    uint64_t offset) {
+  // First make sure all three components of the scan predicate are
+  // of the same length
+  // Since there is a 1-to-1 correspondense between these three vectors
+  PL_ASSERT(tuple_column_id_list.size() == expr_list.size());
+  PL_ASSERT(tuple_column_id_list.size() == value_list.size());
+
+  // This is a hack - we do not support backward scan
+  if (scan_direction == SCAN_DIRECTION_TYPE_INVALID) {
+    throw Exception("Invalid scan direction \n");
+  }
+
+  LOG_TRACE("Point Query = %d; Full Scan = %d ", csp_p->IsPointQuery(),
+            csp_p->IsFullIndexScan());
+
+  if (csp_p->IsPointQuery() == true) {
+    LOG_TRACE("This is point query");
+    // For point query we construct the key and use equal_range
+
+    const storage::Tuple *point_query_key_p = csp_p->GetPointQueryKey();
+
+    KeyType point_query_key;
+    point_query_key.SetFromKey(point_query_key_p);
+
+    // Note: We could call ScanKey() to achieve better modularity
+    // (slightly less code), but since ScanKey() is a virtual function
+    // this would induce an overhead for point query, which must be highly
+    // optimized and super fast
+    container.GetValue(point_query_key, result);
+  } else if (csp_p->IsFullIndexScan() == true) {
+    // If it is a full index scan, then just do the scan
+    // until we have reached the end of the index by the same
+    // we take the snapshot of the last leaf node
+    for (auto scan_itr = container.Begin(); (scan_itr.IsEnd() == false);
+         scan_itr++) {
+      result.push_back(scan_itr->second);
+    }  // for it from begin() to end()
+  } else {
+    const storage::Tuple *low_key_p = csp_p->GetLowKey();
+    const storage::Tuple *high_key_p = csp_p->GetHighKey();
+
+    LOG_TRACE("Partial scan low key: %s\n high key: %s",
+              low_key_p->GetInfo().c_str(), high_key_p->GetInfo().c_str());
+
+    // Construct low key and high key in KeyType form, rather than
+    // the standard in-memory tuple
+    KeyType index_low_key;
+    KeyType index_high_key;
+    index_low_key.SetFromKey(low_key_p);
+    index_high_key.SetFromKey(high_key_p);
+
+    // We use bwtree Begin() to first reach the lower bound
+    // of the search key
+    // Also we keep scanning until we have reached the end of the index
+    // or we have seen a key higher than the high key
+    for (auto scan_itr = container.Begin(index_low_key);
+         (scan_itr.IsEnd() == false) &&
+             (container.KeyCmpLessEqual(scan_itr->first, index_high_key));
+         scan_itr++) {
+      result.push_back(scan_itr->second);
+    }
+  }  // if is full scan
+
+  if (FLAGS_stats_mode != STATS_TYPE_INVALID) {
+    stats::BackendStatsContext::GetInstance()->IncrementIndexReads(
+        result.size(), metadata);
+  }
+  
   return;
 }
 
