1cd4495f5474a94af983b8603cab906db2424694
= <675973885@qq.com>
2017-01-04 22:33:03 -0500

    Adding ScanLimit() into the header

--- src/include/index/bwtree_index.h
+++ src/include/index/bwtree_index.h
@@ -101,6 +101,15 @@ class BWTreeIndex : public Index {
             const ScanDirectionType &scan_direction,
             std::vector<ValueType> &result,
             const ConjunctionScanPredicate *csp_p);
+            
+  void ScanLimit(const std::vector<type::Value> &values,
+                 const std::vector<oid_t> &key_column_ids,
+                 const std::vector<ExpressionType> &expr_types,
+                 const ScanDirectionType &scan_direction,
+                 std::vector<ValueType> &result,
+                 const ConjunctionScanPredicate *csp_p,
+                 uint64_t limit,
+                 uint64_t offset);
 
   void ScanAllKeys(std::vector<ValueType> &result);
 
