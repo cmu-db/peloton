143abb4f6c4d35b35b562c879a9f39c625d94834
Andy Pavlo <pavlo@cs.brown.edu>
2017-01-24 22:14:22 -0500

    Added the remaining test cases for StringFunction #514

--- test/expression/string_functions_test.cpp
+++ test/expression/string_functions_test.cpp
@@ -80,6 +80,38 @@ TEST_F(StringFunctionsTests, ChrTest) {
   EXPECT_TRUE(result.IsNull());
 }
 
+TEST_F(StringFunctionsTests, SubstrTest) {
+  std::vector<std::string> words = { "Fuck", "yo", "couch" };
+  std::ostringstream os;
+  for (auto w : words) {
+    os << w;
+  }
+  int from = words[0].size() + 1;
+  int len = words[1].size();
+  const std::string expected = words[1];
+  std::vector<type::Value> args = {
+      type::ValueFactory::GetVarcharValue(os.str()),
+      type::ValueFactory::GetIntegerValue(from),
+      type::ValueFactory::GetIntegerValue(len),
+  };
+  auto result = expression::StringFunctions::Substr(args);
+  EXPECT_FALSE(result.IsNull());
+  EXPECT_EQ(expected, result.ToString());
+
+  // Use NULL for every argument and make sure that it always returns NULL.
+  for (int i = 0; i < 3; i++) {
+    std::vector <type::Value> nullargs = {
+        type::ValueFactory::GetVarcharValue("aaa"),
+        type::ValueFactory::GetVarcharValue("bbb"),
+        type::ValueFactory::GetVarcharValue("ccc"),
+    };
+    nullargs[i] = type::ValueFactory::GetNullValueByType(type::Type::VARCHAR);
+    auto result = expression::StringFunctions::Substr(nullargs);
+    EXPECT_TRUE(result.IsNull());
+  }
+
+}
+
 TEST_F(StringFunctionsTests, CharLengthTest) {
   const std::string str = "A";
   for (int i = 0; i < 100; i++) {
@@ -96,7 +128,7 @@ TEST_F(StringFunctionsTests, CharLengthTest) {
   std::vector<type::Value> args = {
       type::ValueFactory::GetNullValueByType(type::Type::VARCHAR)
   };
-  auto result = expression::StringFunctions::Ascii(args);
+  auto result = expression::StringFunctions::CharLength(args);
   EXPECT_TRUE(result.IsNull());
 }
 
@@ -116,11 +148,121 @@ TEST_F(StringFunctionsTests, RepeatTest) {
   }
   // NULL CHECK
   std::vector<type::Value> args = {
-      type::ValueFactory::GetNullValueByType(type::Type::VARCHAR)
+      type::ValueFactory::GetNullValueByType(type::Type::VARCHAR),
+      type::ValueFactory::GetVarcharValue(str),
   };
-  auto result = expression::StringFunctions::Ascii(args);
+  auto result = expression::StringFunctions::Repeat(args);
   EXPECT_TRUE(result.IsNull());
 }
+
+TEST_F(StringFunctionsTests, ReplaceTest) {
+  const std::string origChar = "A";
+  const std::string replaceChar = "X";
+  const std::string prefix = "**PAVLO**";
+  for (int i = 0; i < 100; i++) {
+    std::string expected = prefix + StringUtil::Repeat(origChar, i);
+    EXPECT_EQ(i + prefix.size(),  expected.size());
+    std::string input = prefix + StringUtil::Repeat(replaceChar, i);
+    EXPECT_EQ(i + prefix.size(),  expected.size());
+
+    std::vector<type::Value> args = {
+        type::ValueFactory::GetVarcharValue(input),
+        type::ValueFactory::GetVarcharValue(replaceChar),
+        type::ValueFactory::GetVarcharValue(origChar)
+    };
+
+    auto result = expression::StringFunctions::Replace(args);
+    EXPECT_FALSE(result.IsNull());
+    EXPECT_EQ(expected, result.ToString());
+  }
+  // Use NULL for every argument and make sure that it always returns NULL.
+  for (int i = 0; i < 3; i++) {
+    std::vector <type::Value> args = {
+        type::ValueFactory::GetVarcharValue("aaa"),
+        type::ValueFactory::GetVarcharValue("bbb"),
+        type::ValueFactory::GetVarcharValue("ccc"),
+    };
+    args[i] = type::ValueFactory::GetNullValueByType(type::Type::VARCHAR);
+    auto result = expression::StringFunctions::Replace(args);
+    EXPECT_TRUE(result.IsNull());
+  }
+}
+
+TEST_F(StringFunctionsTests, LTrimTest) {
+  const std::string message = "This is a string with spaces";
+  const std::string spaces = "    ";
+  const std::string origStr = spaces + message + spaces;
+  const std::string expected = message + spaces;
+  std::vector<type::Value> args = {
+    type::ValueFactory::GetVarcharValue(origStr),
+    type::ValueFactory::GetVarcharValue(" ")
+  };
+  auto result = expression::StringFunctions::LTrim(args);
+  EXPECT_FALSE(result.IsNull());
+  EXPECT_EQ(expected, result.ToString());
+
+  // Use NULL for every argument and make sure that it always returns NULL.
+  for (int i = 0; i < 2; i++) {
+    std::vector <type::Value> nullargs = {
+        type::ValueFactory::GetVarcharValue("aaa"),
+        type::ValueFactory::GetVarcharValue("bbb"),
+    };
+    nullargs[i] = type::ValueFactory::GetNullValueByType(type::Type::VARCHAR);
+    auto result = expression::StringFunctions::LTrim(nullargs);
+    EXPECT_TRUE(result.IsNull());
+  }
+}
+
+TEST_F(StringFunctionsTests, RTrimTest) {
+  const std::string message = "This is a string with spaces";
+  const std::string spaces = "    ";
+  const std::string origStr = spaces + message + spaces;
+  const std::string expected = spaces + message;
+  std::vector<type::Value> args = {
+      type::ValueFactory::GetVarcharValue(origStr),
+      type::ValueFactory::GetVarcharValue(" ")
+  };
+  auto result = expression::StringFunctions::RTrim(args);
+  EXPECT_FALSE(result.IsNull());
+  EXPECT_EQ(expected, result.ToString());
+
+  // Use NULL for every argument and make sure that it always returns NULL.
+  for (int i = 0; i < 2; i++) {
+    std::vector <type::Value> nullargs = {
+        type::ValueFactory::GetVarcharValue("aaa"),
+        type::ValueFactory::GetVarcharValue("bbb"),
+    };
+    nullargs[i] = type::ValueFactory::GetNullValueByType(type::Type::VARCHAR);
+    auto result = expression::StringFunctions::RTrim(nullargs);
+    EXPECT_TRUE(result.IsNull());
+  }
+}
+
+TEST_F(StringFunctionsTests, BTrimTest) {
+  const std::string message = "This is a string with spaces";
+  const std::string spaces = "    ";
+  const std::string origStr = spaces + message + spaces;
+  const std::string expected = message;
+  std::vector<type::Value> args = {
+      type::ValueFactory::GetVarcharValue(origStr),
+      type::ValueFactory::GetVarcharValue(" ")
+  };
+  auto result = expression::StringFunctions::BTrim(args);
+  EXPECT_FALSE(result.IsNull());
+  EXPECT_EQ(expected, result.ToString());
+
+  // Use NULL for every argument and make sure that it always returns NULL.
+  for (int i = 0; i < 2; i++) {
+    std::vector <type::Value> nullargs = {
+        type::ValueFactory::GetVarcharValue("aaa"),
+        type::ValueFactory::GetVarcharValue("bbb"),
+    };
+    nullargs[i] = type::ValueFactory::GetNullValueByType(type::Type::VARCHAR);
+    auto result = expression::StringFunctions::BTrim(nullargs);
+    EXPECT_TRUE(result.IsNull());
+  }
+}
+
 
 }  // namespace test
 }  // namespace peloton
