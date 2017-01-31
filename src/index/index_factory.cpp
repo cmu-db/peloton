907a6429a9455ac22e85d8443954903b4ca53bf1
Dana Van Aken <dvanaken@cs.cmu.edu>
2017-01-30 20:10:05 -0500

    Formatting and added SKIPLIST type to index_test.

--- src/index/index_factory.cpp
+++ src/index/index_factory.cpp
@@ -75,9 +75,9 @@ Index *IndexFactory::GetIndex(IndexMetadata *metadata) {
       index = IndexFactory::GetSkipListGenericKeyIndex(metadata);
     }
 
-    // -----------------------
-    // ERROR
-    // -----------------------
+  // -----------------------
+  // ERROR
+  // -----------------------
   } else {
     throw IndexException("Unsupported index scheme.");
   }
@@ -102,34 +102,38 @@ Index *IndexFactory::GetBwTreeIntsKeyIndex(IndexMetadata *metadata) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "CompactIntsKey<1>";
 #endif
-    index = new BWTreeIndex<
-        CompactIntsKey<1>, ItemPointer *, CompactIntsComparator<1>,
-        CompactIntsEqualityChecker<1>, CompactIntsHasher<1>,
-        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new BWTreeIndex<CompactIntsKey<1>, ItemPointer *,
+                        CompactIntsComparator<1>, CompactIntsEqualityChecker<1>,
+                        CompactIntsHasher<1>, ItemPointerComparator,
+                        ItemPointerHashFunc>(metadata);
   } else if (key_size <= sizeof(uint64_t) * 2) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "CompactIntsKey<2>";
 #endif
-    index = new BWTreeIndex<
-        CompactIntsKey<2>, ItemPointer *, CompactIntsComparator<2>,
-        CompactIntsEqualityChecker<2>, CompactIntsHasher<2>,
-        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new BWTreeIndex<CompactIntsKey<2>, ItemPointer *,
+                        CompactIntsComparator<2>, CompactIntsEqualityChecker<2>,
+                        CompactIntsHasher<2>, ItemPointerComparator,
+                        ItemPointerHashFunc>(metadata);
   } else if (key_size <= sizeof(uint64_t) * 3) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "CompactIntsKey<3>";
 #endif
-    index = new BWTreeIndex<
-        CompactIntsKey<3>, ItemPointer *, CompactIntsComparator<3>,
-        CompactIntsEqualityChecker<3>, CompactIntsHasher<3>,
-        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new BWTreeIndex<CompactIntsKey<3>, ItemPointer *,
+                        CompactIntsComparator<3>, CompactIntsEqualityChecker<3>,
+                        CompactIntsHasher<3>, ItemPointerComparator,
+                        ItemPointerHashFunc>(metadata);
   } else if (key_size <= sizeof(uint64_t) * 4) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "CompactIntsKey<4>";
 #endif
-    index = new BWTreeIndex<
-        CompactIntsKey<4>, ItemPointer *, CompactIntsComparator<4>,
-        CompactIntsEqualityChecker<4>, CompactIntsHasher<4>,
-        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new BWTreeIndex<CompactIntsKey<4>, ItemPointer *,
+                        CompactIntsComparator<4>, CompactIntsEqualityChecker<4>,
+                        CompactIntsHasher<4>, ItemPointerComparator,
+                        ItemPointerHashFunc>(metadata);
   } else {
     throw IndexException("Unsupported IntsKey scheme");
   }
@@ -172,33 +176,37 @@ Index *IndexFactory::GetBwTreeGenericKeyIndex(IndexMetadata *metadata) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "GenericKey<16>";
 #endif
-    index = new BWTreeIndex<
-        GenericKey<16>, ItemPointer *, FastGenericComparator<16>,
-        GenericEqualityChecker<16>, GenericHasher<16>, ItemPointerComparator,
-        ItemPointerHashFunc>(metadata);
+    index =
+        new BWTreeIndex<GenericKey<16>, ItemPointer *,
+                        FastGenericComparator<16>, GenericEqualityChecker<16>,
+                        GenericHasher<16>, ItemPointerComparator,
+                        ItemPointerHashFunc>(metadata);
   } else if (key_size <= 64) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "GenericKey<64>";
 #endif
-    index = new BWTreeIndex<
-        GenericKey<64>, ItemPointer *, FastGenericComparator<64>,
-        GenericEqualityChecker<64>, GenericHasher<64>, ItemPointerComparator,
-        ItemPointerHashFunc>(metadata);
+    index =
+        new BWTreeIndex<GenericKey<64>, ItemPointer *,
+                        FastGenericComparator<64>, GenericEqualityChecker<64>,
+                        GenericHasher<64>, ItemPointerComparator,
+                        ItemPointerHashFunc>(metadata);
   } else if (key_size <= 256) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "GenericKey<256>";
 #endif
-    index = new BWTreeIndex<
-        GenericKey<256>, ItemPointer *, FastGenericComparator<256>,
-        GenericEqualityChecker<256>, GenericHasher<256>, ItemPointerComparator,
-        ItemPointerHashFunc>(metadata);
+    index =
+        new BWTreeIndex<GenericKey<256>, ItemPointer *,
+                        FastGenericComparator<256>, GenericEqualityChecker<256>,
+                        GenericHasher<256>, ItemPointerComparator,
+                        ItemPointerHashFunc>(metadata);
   } else {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "TupleKey";
 #endif
-    index = new BWTreeIndex<
-        TupleKey, ItemPointer *, TupleKeyComparator, TupleKeyEqualityChecker,
-        TupleKeyHasher, ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new BWTreeIndex<TupleKey, ItemPointer *, TupleKeyComparator,
+                        TupleKeyEqualityChecker, TupleKeyHasher,
+                        ItemPointerComparator, ItemPointerHashFunc>(metadata);
   }
 
 #ifdef LOG_TRACE_ENABLED
@@ -223,34 +231,38 @@ Index *IndexFactory::GetSkipListIntsKeyIndex(IndexMetadata *metadata) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "CompactIntsKey<1>";
 #endif
-    index = new SkipListIndex<
-        CompactIntsKey<1>, ItemPointer *, CompactIntsComparator<1>,
-        CompactIntsEqualityChecker<1>, CompactIntsHasher<1>,
-        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new SkipListIndex<CompactIntsKey<1>, ItemPointer *,
+                          CompactIntsComparator<1>,
+                          CompactIntsEqualityChecker<1>, ItemPointerComparator>(
+            metadata);
   } else if (key_size <= sizeof(uint64_t) * 2) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "CompactIntsKey<2>";
 #endif
-    index = new SkipListIndex<
-        CompactIntsKey<2>, ItemPointer *, CompactIntsComparator<2>,
-        CompactIntsEqualityChecker<2>, CompactIntsHasher<2>,
-        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new SkipListIndex<CompactIntsKey<2>, ItemPointer *,
+                          CompactIntsComparator<2>,
+                          CompactIntsEqualityChecker<2>, ItemPointerComparator>(
+            metadata);
   } else if (key_size <= sizeof(uint64_t) * 3) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "CompactIntsKey<3>";
 #endif
-    index = new SkipListIndex<
-        CompactIntsKey<3>, ItemPointer *, CompactIntsComparator<3>,
-        CompactIntsEqualityChecker<3>, CompactIntsHasher<3>,
-        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new SkipListIndex<CompactIntsKey<3>, ItemPointer *,
+                          CompactIntsComparator<3>,
+                          CompactIntsEqualityChecker<3>, ItemPointerComparator>(
+            metadata);
   } else if (key_size <= sizeof(uint64_t) * 4) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "CompactIntsKey<4>";
 #endif
-    index = new SkipListIndex<
-        CompactIntsKey<4>, ItemPointer *, CompactIntsComparator<4>,
-        CompactIntsEqualityChecker<4>, CompactIntsHasher<4>,
-        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index =
+        new SkipListIndex<CompactIntsKey<4>, ItemPointer *,
+                          CompactIntsComparator<4>,
+                          CompactIntsEqualityChecker<4>, ItemPointerComparator>(
+            metadata);
   } else {
     throw IndexException("Unsupported IntsKey scheme");
   }
@@ -278,48 +290,49 @@ Index *IndexFactory::GetSkipListGenericKeyIndex(IndexMetadata *metadata) {
     comparatorType = "GenericKey<4>";
 #endif
     index =
-        new SkipListIndex<GenericKey<4>, ItemPointer *, FastGenericComparator<4>,
-                        GenericEqualityChecker<4>, GenericHasher<4>,
-                        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+        new SkipListIndex<GenericKey<4>, ItemPointer *,
+                          FastGenericComparator<4>, GenericEqualityChecker<4>,
+                          ItemPointerComparator>(metadata);
   } else if (key_size <= 8) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "GenericKey<8>";
 #endif
     index =
-        new SkipListIndex<GenericKey<8>, ItemPointer *, FastGenericComparator<8>,
-                        GenericEqualityChecker<8>, GenericHasher<8>,
-                        ItemPointerComparator, ItemPointerHashFunc>(metadata);
+        new SkipListIndex<GenericKey<8>, ItemPointer *,
+                          FastGenericComparator<8>, GenericEqualityChecker<8>,
+                          ItemPointerComparator>(metadata);
   } else if (key_size <= 16) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "GenericKey<16>";
 #endif
-    index = new SkipListIndex<
-        GenericKey<16>, ItemPointer *, FastGenericComparator<16>,
-        GenericEqualityChecker<16>, GenericHasher<16>, ItemPointerComparator,
-        ItemPointerHashFunc>(metadata);
+    index =
+        new SkipListIndex<GenericKey<16>, ItemPointer *,
+                          FastGenericComparator<16>, GenericEqualityChecker<16>,
+                          ItemPointerComparator>(metadata);
   } else if (key_size <= 64) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "GenericKey<64>";
 #endif
-    index = new SkipListIndex<
-        GenericKey<64>, ItemPointer *, FastGenericComparator<64>,
-        GenericEqualityChecker<64>, GenericHasher<64>, ItemPointerComparator,
-        ItemPointerHashFunc>(metadata);
+    index =
+        new SkipListIndex<GenericKey<64>, ItemPointer *,
+                          FastGenericComparator<64>, GenericEqualityChecker<64>,
+                          ItemPointerComparator>(metadata);
   } else if (key_size <= 256) {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "GenericKey<256>";
 #endif
-    index = new SkipListIndex<
-        GenericKey<256>, ItemPointer *, FastGenericComparator<256>,
-        GenericEqualityChecker<256>, GenericHasher<256>, ItemPointerComparator,
-        ItemPointerHashFunc>(metadata);
+    index =
+        new SkipListIndex<GenericKey<256>, ItemPointer *,
+                          FastGenericComparator<256>,
+                          GenericEqualityChecker<256>, ItemPointerComparator>(
+            metadata);
   } else {
 #ifdef LOG_TRACE_ENABLED
     comparatorType = "TupleKey";
 #endif
-    index = new SkipListIndex<
-        TupleKey, ItemPointer *, TupleKeyComparator, TupleKeyEqualityChecker,
-        TupleKeyHasher, ItemPointerComparator, ItemPointerHashFunc>(metadata);
+    index = new SkipListIndex<TupleKey, ItemPointer *, TupleKeyComparator,
+                              TupleKeyEqualityChecker, ItemPointerComparator>(
+        metadata);
   }
 
 #ifdef LOG_TRACE_ENABLED
