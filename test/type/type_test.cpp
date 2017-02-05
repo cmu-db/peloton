b8da3533b8eaf6c042e55b53eccf0e71825a9fc1
Andy Pavlo <pavlo@cs.brown.edu>
2017-02-05 08:37:31 -0500

    Added new test case for Invalid type. Yes, this is a blatant attempt to increase our test coverage

--- test/type/type_test.cpp
+++ test/type/type_test.cpp
@@ -13,6 +13,7 @@
 
 #include "common/harness.h"
 
+#include "common/exception.h"
 #include "storage/tuple.h"
 #include "type/type.h"
 
@@ -32,11 +33,63 @@ const std::vector<type::Type::TypeId> typeTestTypes = {
     // type::Type::VARCHAR
 };
 
+TEST_F(TypeTests, InvalidTypeTest) {
+  // First get the INVALID type instance
+  type::Type::TypeId type_id = type::Type::INVALID;
+  auto t = type::Type::GetInstance(type_id);
+  EXPECT_NE(nullptr, t);
+  EXPECT_EQ(type_id, t->GetTypeId());
+  EXPECT_FALSE(t->IsCoercableFrom(type_id));
+
+  // Then hit up all of the mofos methods
+  // They should all throw exceptions
+  EXPECT_THROW(type::Type::GetTypeSize(type_id), peloton::Exception);
+  EXPECT_THROW(type::Type::GetMinValue(type_id), peloton::Exception);
+  EXPECT_THROW(type::Type::GetMaxValue(type_id), peloton::Exception);
+
+  // All of the Compare methods for an invalid type should throw exceptions too
+  auto v0 = type::ValueFactory::GetNullValueByType(type::Type::INTEGER);
+  auto v1 = type::ValueFactory::GetNullValueByType(type::Type::INTEGER);
+  EXPECT_THROW(t->CompareEquals(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->CompareNotEquals(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->CompareLessThan(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->CompareLessThanEquals(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->CompareGreaterThan(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->CompareGreaterThanEquals(v0, v1), peloton::NotImplementedException);
+
+  // Same with all of the math functions
+  EXPECT_THROW(t->Add(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->Subtract(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->Multiply(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->Divide(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->Modulo(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->Min(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->Max(v0, v1), peloton::NotImplementedException);
+  EXPECT_THROW(t->OperateNull(v0, v1), peloton::NotImplementedException);
+
+  // Single argument methods. Same thing. They all drop exception bombs
+  EXPECT_THROW(t->IsZero(v0), peloton::NotImplementedException);
+  EXPECT_THROW(t->IsInlined(v0), peloton::NotImplementedException);
+  EXPECT_THROW(t->ToString(v0), peloton::NotImplementedException);
+  EXPECT_THROW(t->Hash(v0), peloton::NotImplementedException);
+  EXPECT_THROW(t->Copy(v0), peloton::NotImplementedException);
+  EXPECT_THROW(t->GetData(v0), peloton::NotImplementedException);
+  EXPECT_THROW(t->GetLength(v0), peloton::NotImplementedException);
+
+  // Ok here are the last ones
+  size_t size = static_cast<size_t>(123);
+  EXPECT_THROW(t->HashCombine(v0, size), peloton::NotImplementedException);
+  EXPECT_THROW(t->GetElementAt(v0, 0), peloton::NotImplementedException);
+  EXPECT_THROW(t->CastAs(v0, type_id), peloton::NotImplementedException);
+
+}
+
 TEST_F(TypeTests, GetInstanceTest) {
   for (auto col_type : typeTestTypes) {
     auto t = type::Type::GetInstance(col_type);
     EXPECT_NE(nullptr, t);
     EXPECT_EQ(col_type, t->GetTypeId());
+    EXPECT_TRUE(t->IsCoercableFrom(col_type));
   }
 }
 
@@ -53,7 +106,7 @@ TEST_F(TypeTests, MinValueTest) {
   for (auto col_type : typeTestTypes) {
     auto minVal = type::Type::GetMinValue(col_type);
     EXPECT_FALSE(minVal.IsNull());
-    // TODO: We should not be allowed to create a value that is greater than
+    // TODO: We should not be allowed to create a value that is less than
     // the min value.
   }
 }
