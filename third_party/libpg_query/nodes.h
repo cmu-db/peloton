be8e9ede8044eed0151908e274550b5159655ef2
Hao Jin <haoj@andrew.cmu.edu>
2017-02-18 22:15:14 -0500

    added typedef for bool

--- third_party/libpg_query/nodes.h
+++ third_party/libpg_query/nodes.h
@@ -14,6 +14,8 @@
 #ifndef NODES_H
 #define NODES_H
 
+typedef char bool;
+
 /*
  * The first field of every node is NodeTag. Each node created (with makeNode)
  * will have one of the following tags as the value of its first field.
