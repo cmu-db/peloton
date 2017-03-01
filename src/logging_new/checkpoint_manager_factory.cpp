ee9f099287e667bb426a5108edfda62a5bfc7480
Yingjun <yingjun@comp.nus.edu.sg>
2017-03-01 22:31:49 +0800

    start porting logging and checkpointing...

---
+++ src/logging_new/checkpoint_manager_factory.cpp
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// checkpoint_manager_factory.cpp
+//
+// Identification: src/logging/checkpoint_manager_factory.cpp
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+
+#include "logging_new/checkpoint_manager_factory.h"
+
+namespace peloton {
+namespace logging {
+
+CheckpointingType CheckpointManagerFactory::checkpointing_type_ = CheckpointingType::ON;
+
+int CheckpointingManagerFactory::checkpointing_thread_count_ = 1;
+
+}  // namespace gc
+}  // namespace peloton
