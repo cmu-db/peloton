7d3a4f70d6cd1c77c5c11a98f207118844604773
Yingjun <yingjun@comp.nus.edu.sg>
2017-03-03 20:13:02 +0800

    add framework for new logging

--- src/storage/storage_manager.cpp
+++ src/storage/storage_manager.cpp
@@ -85,9 +85,9 @@
 //  * intrinsic functions are not always available.  The intrinsic
 //  * functions are defined here in terms of asm statements for now.
 //  */
-// #define _mm_clflushopt(addr) \
+// #define _mm_clflushopt(addr) 
 //   asm volatile(".byte 0x66; clflush %0" : "+m"(*(volatile char *)addr));
-// #define _mm_clwb(addr) \
+// #define _mm_clwb(addr) 
 //   asm volatile(".byte 0x66; xsaveopt %0" : "+m"(*(volatile char *)addr));
 // #define _mm_pcommit() asm volatile(".byte 0x66, 0x0f, 0xae, 0xf8");
 
