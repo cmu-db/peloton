d1f26df3ed807f01422fae2d3ec424f217f764c3
Chen Luo <cluo1@andrew.cmu.edu>
2017-03-04 22:49:21 +0000

    Add LIMIT test for optimizer. Pass all tests

--- test/sql/optimizer_sql_test.cpp
+++ test/sql/optimizer_sql_test.cpp
@@ -20,7 +20,6 @@
 #include "optimizer/simple_optimizer.h"
 #include "planner/create_plan.h"
 
-
 namespace peloton {
 namespace test {
 
@@ -34,6 +33,8 @@ void CreateAndLoadTable() {
   // Insert tuples into table
   TestingSQLUtil::ExecuteSQLQuery("INSERT INTO test VALUES (1, 22, 333);");
   TestingSQLUtil::ExecuteSQLQuery("INSERT INTO test VALUES (2, 11, 000);");
+  TestingSQLUtil::ExecuteSQLQuery("INSERT INTO test VALUES (3, 33, 444);");
+  TestingSQLUtil::ExecuteSQLQuery("INSERT INTO test VALUES (4, 00, 555);");
 }
 
 TEST_F(OptimizerSQLTests, SimpleSelectTest) {
@@ -49,7 +50,8 @@ TEST_F(OptimizerSQLTests, SimpleSelectTest) {
       new optimizer::Optimizer());
 
   std::string query("SELECT * from test");
-  auto select_plan = TestingSQLUtil::GeneratePlanWithOptimizer(optimizer, query);
+  auto select_plan =
+      TestingSQLUtil::GeneratePlanWithOptimizer(optimizer, query);
   EXPECT_EQ(select_plan->GetPlanNodeType(), PlanNodeType::SEQSCAN);
 
   // test small int
@@ -93,7 +95,8 @@ TEST_F(OptimizerSQLTests, SelectProjectionTest) {
   std::string query("SELECT a * 5 + b, -1 + c from test");
 
   // check for plan node type
-  auto select_plan = TestingSQLUtil::GeneratePlanWithOptimizer(optimizer, query);
+  auto select_plan =
+      TestingSQLUtil::GeneratePlanWithOptimizer(optimizer, query);
   EXPECT_EQ(select_plan->GetPlanNodeType(), PlanNodeType::PROJECTION);
   EXPECT_EQ(select_plan->GetChildren()[0]->GetPlanNodeType(),
             PlanNodeType::SEQSCAN);
@@ -129,7 +132,8 @@ TEST_F(OptimizerSQLTests, SelectOrderByTest) {
   std::string query("SELECT b from test order by c");
 
   // check for plan node type
-  auto select_plan = TestingSQLUtil::GeneratePlanWithOptimizer(optimizer, query);
+  auto select_plan =
+      TestingSQLUtil::GeneratePlanWithOptimizer(optimizer, query);
   EXPECT_EQ(select_plan->GetPlanNodeType(), PlanNodeType::ORDERBY);
   EXPECT_EQ(select_plan->GetChildren()[0]->GetPlanNodeType(),
             PlanNodeType::SEQSCAN);
@@ -141,6 +145,43 @@ TEST_F(OptimizerSQLTests, SelectOrderByTest) {
   // Should be: 11, 22
   EXPECT_EQ("11", TestingSQLUtil::GetResultValueAsString(result, 0));
   EXPECT_EQ("22", TestingSQLUtil::GetResultValueAsString(result, 1));
+
+  // free the database just created
+  auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+  catalog::Catalog::GetInstance()->DropDatabaseWithName(DEFAULT_DB_NAME, txn);
+  txn_manager.CommitTransaction(txn);
+}
+
+TEST_F(OptimizerSQLTests, SelectLimitTest) {
+  catalog::Catalog::GetInstance()->CreateDatabase(DEFAULT_DB_NAME, nullptr);
+
+  CreateAndLoadTable();
+
+  std::vector<StatementResult> result;
+  std::vector<FieldInfo> tuple_descriptor;
+  std::string error_message;
+  int rows_changed;
+  std::unique_ptr<optimizer::AbstractOptimizer> optimizer(
+      new optimizer::Optimizer());
+
+  std::string query("SELECT b FROM test ORDER BY b LIMIT 2 OFFSET 2");
+
+  // check for plan node type
+  auto select_plan =
+      TestingSQLUtil::GeneratePlanWithOptimizer(optimizer, query);
+  EXPECT_EQ(select_plan->GetPlanNodeType(), PlanNodeType::LIMIT);
+  EXPECT_EQ(select_plan->GetChildren()[0]->GetPlanNodeType(),
+            PlanNodeType::ORDERBY);
+  EXPECT_EQ(select_plan->GetChildren()[0]->GetChildren()[0]->GetPlanNodeType(),
+            PlanNodeType::SEQSCAN);
+
+  TestingSQLUtil::ExecuteSQLQueryWithOptimizer(
+      optimizer, query, result, tuple_descriptor, rows_changed, error_message);
+
+  EXPECT_EQ(2, result.size());
+  EXPECT_EQ("22", TestingSQLUtil::GetResultValueAsString(result, 0));
+  EXPECT_EQ("33", TestingSQLUtil::GetResultValueAsString(result, 1));
 
   // free the database just created
   auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
