4dfedbfa1f813ff9a0b315ec09a0bc89cc10a699
mengranwo <mengranwo@gmail.com>
2017-03-18 19:41:28 -0400

    add GetOffsetByOid_Name

--- src/include/catalog/column_catalog.h
+++ src/include/catalog/column_catalog.h
@@ -30,12 +30,19 @@ class ColumnCatalog : public AbstractCatalog {
     return oid_++ | static_cast<oid_t>(type::CatalogType::TABLE);
   }
 
-  void DeleteTuple(oid_t id, concurrency::Transaction *txn);
+  // Read-only API
+  type::TypeId GetTypeByOid_Name(oid_t id, concurrency::Transaction *txn);
+  type::TypeId GetTypeByOid_Offset(std::string &name,
+                                   concurrency::Transaction *txn);
+  std::string GetNameByOid_Offset(oid_t id, concurrency::Transaction *txn);
+  oid_t GetOffsetByOid_Name(std::string &name, concurrency::Transaction *txn);
+
+  // Write related API
+  void DeleteTuple(oid_t id, std::string &column_name,
+                   concurrency::Transaction *txn);
 
   std::unique_ptr<storage::Tuple> GetColumnCatalogTuple(
-      oid_t table_id, std::string column_name, oid_t column_type,
-      oid_t column_offset, bool column_isPrimary, bool column_hasConstrain,
-      type::AbstractPool *pool);
+      oid_t table_id, std::string column_name, type::AbstractPool *pool);
 
  private:
   std::unique_ptr<catalog::Schema> InitializeColumnCatalogSchema();
