07ea1d4aac5fd623bef49daec817e813d04ed387
Xin Zhang <xinzhang1111@gmail.com>
2017-03-20 21:08:57 -0400

    add part

--- test/executor/testing_executor_util.cpp
+++ test/executor/testing_executor_util.cpp
@@ -119,11 +119,11 @@ catalog::Column TestingExecutorUtil::GetColumnInfo(int index) {
                                                not_null_constraint_name));
       return column;
     } break;
-	
-	case 4: {
-		auto column = catalog::Column(type::Type::INTEGER, type::Type::GetTypeSize(type::Type::INTEGER), "COL_E", is_inlined);
-		column.AddConstraint(catalog::Constraint(ConstraintType::DEFAULT, not_null_constraint_name));
-	} break;
+    
+    case 4: {
+        auto column = catalog::Column(type::Type::INTEGER, type::Type::GetTypeSize(type::Type::INTEGER), "COL_E", is_inlined);
+        column.AddConstraint(catalog::Constraint(ConstraintType::DEFAULT, not_null_constraint_name));
+    } break;
 
     case 115: {
       auto column = catalog::Column(
