bdfbb08ebe5b642b61afa9dd61fcadb5e36e60ec
Jungmin Seo <seojungmin@gmail.com>
2017-03-25 01:42:00 -0400

    Added more support for the legacy parser

--- src/include/parser/delete_statement.h
+++ src/include/parser/delete_statement.h
@@ -2,11 +2,11 @@
 //
 //                         Peloton
 //
-// statement_delete.h
+// delete_statement.h
 //
-// Identification: src/include/parser/statement_delete.h
+// Identification: src/include/parser/delete_statement.h
 //
-// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+// Copyright (c) 2015-17, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
 
@@ -31,14 +31,32 @@ struct DeleteStatement : SQLStatement {
         table_ref(nullptr), expr(nullptr) {};
 
   virtual ~DeleteStatement() {
-    delete table_ref;
+    // FIXME The following line should be removed
+    //       when the old parser gets obsolete
     delete table_info_;
+
+    delete table_ref;
     delete expr;
   }
 
-  std::string GetTableName() const { return table_ref->GetTableName(); }
+  std::string GetTableName() const {
+    // FIXME The following two lines should be removed
+    //       when the old parser gets obsolete
+    if (table_info_ != nullptr)
+      return table_info_->table_name;
+
+    return table_ref->GetTableName();
+  }
 
   std::string GetDatabaseName() const {
+    // FIXME The following four lines should be removed
+    //       when the old parser gets obsolete
+    if (table_info_ != nullptr) {
+      if (table_info_->database_name == nullptr)
+        return DEFAULT_DB_NAME;
+      return table_info_->database_name;
+    }
+
     return table_ref->GetDatabaseName();
   }
 
@@ -46,8 +64,11 @@ struct DeleteStatement : SQLStatement {
     v->Visit(this);
   }
 
-  parser::TableRef* table_ref;
+  // FIXME The following line should be removed
+  //       when the old parser gets obsolete
   parser::TableInfo* table_info_ = nullptr;
+
+  parser::TableRef* table_ref;
   expression::AbstractExpression* expr;
 };
 
