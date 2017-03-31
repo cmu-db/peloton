cdc343a9b5e5257ea780c65a6b44c41a8ee6edbd
Xin Zhang <xinzhang1111@gmail.com>
2017-03-30 20:26:53 -0400

    Make check constrain more faster

--- src/include/storage/data_table.h
+++ src/include/storage/data_table.h
@@ -276,7 +276,9 @@ class DataTable : public AbstractTable {
                 std::pair<ExpressionType, type::Value> exp) const;
   bool CheckUnique(const storage::Tuple *tuple, oid_t column_idx) const;
 
-  bool CheckExp(const storage::Tuple *tupe) const;
+  bool CheckNulls(const storage::Tuple *tuple, oid_t column_idx) const;
+
+  bool CheckExp(const storage::Tuple *tuple, oid_t column_idx) const;
 
   bool CheckConstraints(const storage::Tuple *tuple) const;
 
