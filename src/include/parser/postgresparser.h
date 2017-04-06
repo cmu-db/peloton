19b3f4d76677dc74a7945dd1b647bc9baa2f2227
Tao Lin <nblintao@gmail.com>
2017-04-06 00:50:38 -0400

    Transform trigger's when clause to expression

--- src/include/parser/postgresparser.h
+++ src/include/parser/postgresparser.h
@@ -91,6 +91,9 @@ class PostgresParser {
   // transform helper for where clauses
   static expression::AbstractExpression* WhereTransform(Node* root);
 
+  // transform helper for when clauses
+  static expression::AbstractExpression* WhenTransform(Node* root);
+
   // transform helper for column refs
   static expression::AbstractExpression* ColumnRefTransform(ColumnRef* root);
 
