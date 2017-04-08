835970adcaf2a19c16b6dee7d51448d51a3cb31e
root <root@node2.node2.cmu15721.narwhal.pdl.cmu.edu>
2017-04-08 19:44:10 -0400

    Add foreign key test case for single and multi columns

--- src/include/storage/data_table.h
+++ src/include/storage/data_table.h
@@ -235,6 +235,8 @@ class DataTable : public AbstractTable {
 
   inline oid_t GetDatabaseOid() const { return (database_oid); }
 
+  inline oid_t GetTableOid() const { return (table_oid); }
+
   bool HasPrimaryKey() const { return (has_primary_key_); }
 
   bool HasUniqueConstraints() const { return (unique_constraint_count_ > 0); }
@@ -326,6 +328,7 @@ class DataTable : public AbstractTable {
 
   oid_t database_oid;
   std::string table_name;
+  oid_t table_oid;
 
   // number of tuples allocated per tilegroup
   size_t tuples_per_tilegroup_;
