6377fc6b6f41fae40798613aded44d1f996dac7b
Yang <queerforwood@hotmail.com>
2017-04-09 09:01:33 -0400

    * thread pool wrapper takes concrete function type

--- src/include/codegen/query_thread_pool.h
+++ src/include/codegen/query_thread_pool.h
@@ -20,8 +20,7 @@ class QueryThreadPool : public ThreadPool {
  public:
 
   // submit a specialized query task to the thread pool
-  template <typename FunctionType>
-  void SubmitQueryTask(FunctionType &&func, int64_t start, int64_t end) {
+  void SubmitQueryTask(int64_t (&&func)(int64_t *, int64_t *), int64_t start, int64_t end) {
     // add task to thread pool.
     SubmitTask(func, &start, &end);
   }
