6336cacc79fc4f478440ae2bde8ab8f2893b162e
rohit-cmu <ragrawa1@andrew.cmu.edu>
2017-04-09 01:52:15 -0400

    adding compression_sql_test.cpp

---
+++ test/sql/compression_sql_test.cpp
@@ -0,0 +1,37 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// Compression Testing Script
+//===----------------------------------------------------------------------===//
+
+#include <memory>
+
+#include "sql/testing_sql_util.h"
+#include "catalog/catalog.h"
+#include "common/harness.h"
+#include "executor/create_executor.h"
+#include "optimizer/simple_optimizer.h"
+#include "planner/create_plan.h"
+
+
+namespace peloton {
+namespace test {
+
+class CompressionTest : public PelotonTest {};
+
+TEST_F(CompressionTest, BasicInsertionTest) {
+  catalog::Catalog::GetInstance()->CreateDatabase(DEFAULT_DB_NAME, nullptr);
+
+  // Create a table first
+  TestingSQLUtil::ExecuteSQLQuery("CREATE TABLE foo(id integer, year integer);");
+  int i;
+  for (i = 0; i < 100; i++) {
+    std::ostringstream os;
+    os << "insert into foo values("<< i << ", " << i*100 <<");";
+    TestingSQLUtil::ExecuteSQLQuery(os.str());
+  }
+    EXPECT_EQ(i, 100);
+  }
+}
+}
