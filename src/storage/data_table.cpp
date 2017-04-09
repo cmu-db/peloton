fb6548dd1d61a0ab125a932392e301c4b1ff5673
Xin Zhang <xinzhang1111@gmail.com>
2017-04-09 17:45:45 -0400

    Fix memory leak problem

--- src/storage/data_table.cpp
+++ src/storage/data_table.cpp
@@ -560,11 +560,8 @@ bool DataTable::CheckConstraints(const storage::Tuple *tuple) const {
 // and the argument cannot be set to nullptr.
 ItemPointer DataTable::GetEmptyTupleSlot(const storage::Tuple *tuple,
                                          bool check_constraint) {
-  // assert(tuple);
-  if (check_constraint && tuple != nullptr) {
-    if (CheckConstraints(tuple) == false)
-      return INVALID_ITEMPOINTER;
-  }
+  if (tuple && check_constraint && CheckConstraints(tuple) == false)
+    return INVALID_ITEMPOINTER;
   //=============== garbage collection==================
   // check if there are recycled tuple slots
   auto &gc_manager = gc::GCManagerFactory::GetInstance();
