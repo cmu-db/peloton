9282a59a340742872bd0e25613fc8c6e4d81fc19
bechob <a2ab2ab@gmail.com>
2017-04-12 18:22:01 -0400

    cache singleton

--- src/include/codegen/query_cache.h
+++ src/include/codegen/query_cache.h
@@ -1,16 +1,46 @@
+//===----------------------------------------------------------------------===//
 //
-// Created by bechob on 4/11/17.
+//                         Peloton
 //
+// value_peeker_proxy.h
+//
+// Identification: src/include/codegen/cache.h
+//
+// Copyright (c) 2015-17, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+namespace peloton {
+namespace codegen {
+class QueryCache {
+public:
+  static QueryCache& Instance() {
+    static  QueryCache instance;
+    return instance;
+  }
+  bool FindPlan(const planner::AbstractPlan & key) {
+    auto it = cache.find(key);
+    if (it != cache.end())
+      return false;
+    return true;
+  }
 
-#ifndef PELOTON_CACHE_H
-#define PELOTON_CACHE_H
+  Query GetQuery(const planenr::AbstractPlan & key) {
+    return cache.find(key)->second;
+  }
 
+  void InsertPlan(const planner::AbstractPlan & key, const Query & val) {
+    cache.insert(key, val);
+  }
 
-
-class cache {
-
+private:
+  struct ComparePlan {
+    bool operator()(const  planner::AbstractPlan & a, const planner::AbstractPlan & b) const {
+      return (codegen::PlanComparator::Compare(A, B) < 0);
+    }
+  };
+  std::map<planner::AbstractPlan, Query, ComparePlan> cache;
+  QueryCache() {}
 };
-
-
-
-#endif //PELOTON_CACHE_H
+}
+}
