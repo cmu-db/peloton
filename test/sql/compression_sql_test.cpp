32892cbef647a05cc98de55eb962f4e75596396d
Pooja Nilangekar <poojanilangekar@github.com>
2017-04-12 02:20:25 +0000

    updated compression_sql_test

--- test/sql/compression_sql_test.cpp
+++ test/sql/compression_sql_test.cpp
@@ -24,15 +24,32 @@ TEST_F(CompressionTest, BasicInsertionTest) {
   catalog::Catalog::GetInstance()->CreateDatabase(DEFAULT_DB_NAME, nullptr);
 
   // Create a table first
-  TestingSQLUtil::ExecuteSQLQuery("CREATE TABLE foo(id integer primary key, year integer);");
+  TestingSQLUtil::ExecuteSQLQuery("CREATE TABLE foo(id integer, year integer);");
   int i;
 
-  for (i = 0; i < 20; i++) {
+  for (i = 0; i < 25; i++) {
     std::ostringstream os;
     os << "insert into foo values("<< i << ", " << i*100 <<");";
     TestingSQLUtil::ExecuteSQLQuery(os.str());
   }
-    EXPECT_EQ(i, 20);
+    EXPECT_EQ(i, 25);
+
+    std::vector<StatementResult> result;
+    std::vector<FieldInfo> tuple_descriptor;
+    std::string error_message;
+    int rows_affected;
+    std::ostringstream os;
+
+    os<< "select * from foo;";
+    
+    TestingSQLUtil::ExecuteSQLQuery(os.str(),result, tuple_descriptor,
+                                  rows_affected, error_message);
+    for (i = 0; i<25; i++) {
+      std::string resultStr(TestingSQLUtil::GetResultValueAsString(result, (2*i)) + "\t" + TestingSQLUtil::GetResultValueAsString(result, (2*i)+1));
+      std::string expectedStr(std::to_string(i) + "\t" + std::to_string(i*100));
+      EXPECT_EQ(resultStr,expectedStr);
+    }
+    
   }
 }
 }
