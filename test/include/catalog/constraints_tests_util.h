0a3684621f945fd0de11e2e7333d5983599483c7
Andy Pavlo <pavlo@cs.brown.edu>
2017-04-13 15:56:39 -0400

    Last std::cout.

--- test/include/catalog/constraints_tests_util.h
+++ test/include/catalog/constraints_tests_util.h
@@ -92,7 +92,7 @@ class ConstraintsTestsUtil {
   /*
   static storage::DataTable *CreateTable(
       int tuples_per_tilegroup_count = TESTS_TUPLES_PER_TILEGROUP,
-      bool indexes = true) {std::cout << tuples_per_tilegroup_count << indexes;
+      bool indexes = true) {
   return NULL;};
     */
 
