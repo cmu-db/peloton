e9d306c44dfd0e592bc0b599527ed5cdd138ce6d
Hao Li <lihao98722@gmail.com>
2017-04-18 02:34:18 +0000

    added QueryThreadPool::GetInstance() and related proxy; succeed.

--- src/include/codegen/query_thread_pool.h
+++ src/include/codegen/query_thread_pool.h
@@ -13,31 +13,23 @@
 #pragma once
 
 #include "codegen/codegen.h"
+#include "codegen/runtime_state.h"
 #include "codegen/multi_thread_context.h"
 #include "common/thread_pool.h"
 
 namespace peloton {
 namespace codegen {
 
-// a wrapper for boost worker thread pool.
 class QueryThreadPool {
  public:
 
-  static QueryThreadPool *GetInstance() {
-    int thread_count = std::thread::hardware_concurrency() + 3;
-    static std::unique_ptr<QueryThreadPool> global_query_thread_pool(new QueryThreadPool(thread_count));
-    return global_query_thread_pool.get();
-  }
+  static QueryThreadPool *GetInstance();
 
   ~QueryThreadPool() {
     pool.Shutdown();
   }
 
-  // submit a specialized query task to the thread pool
-  // TODO(tq5124): the thread func should be: void (RuntimeState *, MultiThreadContext).
-  void SubmitQueryTask(MultiThreadContext context, void (*produce_func)(MultiThreadContext)) {
-    pool.SubmitTask(produce_func, std::move(context));
-  }
+  void SubmitQueryTask(RuntimeState *runtime_state, MultiThreadContext *multi_thread_context, void (*target_func)(RuntimeState*,MultiThreadContext*));
 
  private:
 
