984b3454abea80123086b5362cdde5d259da1b17
Patrick Huang <hzxa21@126.com>
2017-04-22 17:45:19 -0400

    Automerge errors fix

--- test/sql/optimizer_sql_test.cpp
+++ test/sql/optimizer_sql_test.cpp
@@ -55,7 +55,10 @@ class OptimizerSQLTests : public PelotonTest {
   /*** Helper functions **/
   void CreateAndLoadTable() {
     // Create database
-    catalog::Catalog::GetInstance()->CreateDatabase(DEFAULT_DB_NAME, nullptr);
+    auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+    auto txn = txn_manager.BeginTransaction();
+    catalog::Catalog::GetInstance()->CreateDatabase(DEFAULT_DB_NAME, txn);
+    txn_manager.CommitTransaction(txn);
 
     // Create a table first
     TestingSQLUtil::ExecuteSQLQuery(
