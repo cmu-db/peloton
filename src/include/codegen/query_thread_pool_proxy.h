7554747e5cf773c9f96142773d5d19ab8ca0a89d
tq5124 <tq5124.web@gmail.com>
2017-04-24 18:00:45 -0400

    Change to make llvm submit multi threads work
    Test is still pass because it is still single thread now. Need barrier
    to make multi threads version work

--- src/include/codegen/query_thread_pool_proxy.h
+++ src/include/codegen/query_thread_pool_proxy.h
@@ -13,6 +13,7 @@
 #pragma once
 
 #include "codegen/codegen.h"
+#include "codegen/runtime_state.h"
 
 namespace peloton {
 namespace codegen {
@@ -21,7 +22,7 @@ class QueryThreadPoolProxy {
  public:
   static llvm::Type *GetType(CodeGen &codegen);
 
-  static llvm::Function *GetSubmitQueryTaskFunction(CodeGen &codegen);
+  static llvm::Function *GetSubmitQueryTaskFunction(CodeGen &codegen, RuntimeState *runtime_state);
   static llvm::Function *GetGetIntanceFunction(CodeGen &codegen);
 };
 
