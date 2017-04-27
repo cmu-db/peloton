f7856314327b4aef2ecc1c5c7e2d01b828c8914a
YileiCHU <ychu1@andrew.cmu.edu>
2017-04-27 14:15:10 -0400

    Resolve rebase comflict for commit Add libevent server start and close function

--- src/include/wire/libevent_server.h
+++ src/include/wire/libevent_server.h
@@ -228,9 +228,8 @@ struct LibeventServer {
 
   uint64_t port_;           // port number
   size_t max_connections_;  // maximum number of connections
-  //struct event_base *base;  // libevent event_base
+  struct event_base *base;  // libevent event_base
   struct event *evstop;     // libevent stop event
-  struct event *ev_timeout;
   std::shared_ptr<LibeventThread> master_thread;
   // std::shared_ptr<LibeventThread> master_thread(
   //    new LibeventMasterThread(CONNECTION_THREAD_COUNT, base));
