e2944d1a40bca4dbe07c29ffa23747255842220b
YileiCHU <ychu1@andrew.cmu.edu>
2017-04-27 14:27:08 -0400

    Resolve rebase conflict for commit Split packet_manager_test into simple_query_test and prepare_stmt_test

--- test/wire/simple_query_test.cpp
+++ test/wire/simple_query_test.cpp
@@ -2,9 +2,9 @@
 //
 //                         Peloton
 //
-// packet_manager_test.cpp
+// simple_query_test.cpp
 //
-// Identification: test/wire/packet_manager_test.cpp
+// Identification: test/wire/simple_query_test.cpp
 //
 // Copyright (c) 2016-17, Carnegie Mellon University Database Group
 //
@@ -22,10 +22,10 @@ namespace peloton {
 namespace test {
 
 //===--------------------------------------------------------------------===//
-// Packet Manager Tests
+// Simple Query Tests
 //===--------------------------------------------------------------------===//
 
-class PacketManagerTests : public PelotonTest {};
+class SimpleQueryTests : public PelotonTest {};
 
 static void *LaunchServer(peloton::wire::LibeventServer libeventserver) {
   try {
@@ -73,51 +73,6 @@ void *SimpleQueryTest(void *) {
   }
 
   LOG_INFO("[SimpleQueryTest] Client has closed");
-  return NULL;
-}
-
-/**
- * named prepare statement without parameters
- * TODO: add prepare's parameters when parser team fix the bug
- */
-
-void *PrepareStatementTest(void *) {
-  try {
-    pqxx::connection C(
-        "host=127.0.0.1 port=15721 user=postgres sslmode=disable");
-    LOG_INFO("[PrepareStatementTest] Connected to %s", C.dbname());
-    pqxx::work W(C);
-    
-    peloton::wire::LibeventSocket *conn =
-        peloton::wire::LibeventServer::GetConn(
-            peloton::wire::LibeventServer::recent_connfd);
-
-    // create table and insert some data
-    W.exec("DROP TABLE IF EXISTS employee;");
-    W.exec("CREATE TABLE employee(id INT, name VARCHAR(100));");
-    W.commit();
-    
-    pqxx::work W1(C);
-    W1.exec("INSERT INTO employee VALUES (1, 'Han LI');");
-    W1.exec("INSERT INTO employee VALUES (2, 'Shaokun ZOU');");
-    W1.exec("INSERT INTO employee VALUES (3, 'Yilei CHU');");
-
-    // test prepare statement
-    C.prepare("searchstmt","SELECT name FROM employee WHERE id=$1;");
-    // invocation as in variable binding
-    pqxx::result R = W1.prepared("searchstmt")(1).exec();
-    W1.commit();
-
-    // test prepared statement already in statement cache
-//    LOG_INFO("[Prepare statement cache] %d",conn->pkt_manager.ExistCachedStatement("searchstmt"));
-    EXPECT_TRUE(conn->pkt_manager.ExistCachedStatement("searchstmt"));
-
-    LOG_INFO("Prepare statement search result:%lu",R.size());
-  } catch (const std::exception &e) {
-    LOG_INFO("[PrepareStatementTest] Exception occurred");
-  }
-
-  LOG_INFO("[PrepareStatementTest] Client has closed");
   return NULL;
 }
 
@@ -199,7 +154,7 @@ TEST_F(PacketManagerTests, RollbackTest) {
  * Simple query test to guarantee both sides run correctly
  * Callback method to close server after client finishes
  */
-TEST_F(PacketManagerTests, SimpleQueryTest) {
+TEST_F(SimpleQueryTests, SimpleQueryTest) {
   peloton::PelotonInit::Initialize();
   LOG_INFO("Server initialized");
   peloton::wire::LibeventServer libeventserver;
@@ -218,24 +173,43 @@ TEST_F(PacketManagerTests, SimpleQueryTest) {
   LOG_INFO("Peloton has shut down\n");
 }
 
-
-TEST_F(PacketManagerTests, RollbackTest) {
-  peloton::PelotonInit::Initialize();
-  LOG_INFO("Server initialized");
-  peloton::wire::LibeventServer libeventserver;
-  int port = 15721;
-  std::thread serverThread(LaunchServer, libeventserver,port);
-  while (!libeventserver.GetIsStarted()) {
-    sleep(1);
-  }
-
-  RollbackTest(NULL);
-
-  libeventserver.CloseServer();
-  serverThread.join();
-  peloton::PelotonInit::Shutdown();
-  LOG_INFO("Peloton has shut down\n");
-}
+///**
+// * Scalability test
+// * Open 2 servers in threads concurrently
+// * Both conduct simple query job
+// */
+//TEST_F(PacketManagerTests, ScalabilityTest) {
+//  peloton::PelotonInit::Initialize();
+//
+//  /* launch 2 libevent servers in different port */
+//  // first server
+//  int port1 = 15721;
+//  peloton::wire::LibeventServer libeventserver1;
+//  std::thread serverThread1(LaunchServer, libeventserver1,port1);
+//
+//  // second server
+//  int port2 = 15722;
+//  peloton::wire::LibeventServer libeventserver2;
+//  std::thread serverThread2(LaunchServer, libeventserver2,port2);
+//
+//  while (!libeventserver1.is_started || !libeventserver2.is_started) {
+//    sleep(1);
+//  }
+//
+//  /* launch 2 clients to do simple query separately */
+//  SimpleQueryTest(port1);
+//  SimpleQueryTest(port2);
+//
+//  libeventserver1.CloseServer();
+//  libeventserver2.CloseServer();
+//
+//  serverThread1.join();
+//  serverThread2.join();
+//
+//  LOG_INFO("[ScalabilityTest] Threads have joined");
+//  peloton::PelotonInit::Shutdown();
+//  LOG_INFO("[ScalabilityTest] Peloton has shut down");
+//}
 
 }  // End test namespace
 }  // End peloton namespace
