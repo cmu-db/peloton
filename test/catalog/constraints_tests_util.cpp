e9896890986b93d52105b0456515b2ca7f68754b
Prashanth Menon <pmenon@cs.cmu.edu>
2017-05-03 08:31:50 -0400

    Fix constraints test utility class for #651

--- test/catalog/constraints_tests_util.cpp
+++ test/catalog/constraints_tests_util.cpp
@@ -27,12 +27,15 @@
 #include "concurrency/transaction_manager_factory.h"
 #include "executor/abstract_executor.h"
 #include "executor/logical_tile.h"
+#include "expression/abstract_expression.h"
+#include "expression/expression_util.h"
 #include "storage/tile_group.h"
 #include "storage/tile_group_factory.h"
 #include "storage/tuple.h"
 #include "storage/data_table.h"
 #include "storage/table_factory.h"
 #include "index/index_factory.h"
+#include "planner/attribute_info.h"
 #include "planner/delete_plan.h"
 #include "planner/insert_plan.h"
 #include "executor/executor_context.h"
@@ -60,9 +63,9 @@ catalog::Column ConstraintsTestsUtil::GetColumnInfo(int index) {
 
   switch (index) {
     case 0: {
-      auto column =
-          catalog::Column(type::Type::INTEGER, type::Type::GetTypeSize(type::Type::INTEGER),
-                          "COL_A", is_inlined);
+      auto column = catalog::Column(
+          type::Type::INTEGER, type::Type::GetTypeSize(type::Type::INTEGER),
+          "COL_A", is_inlined);
 
       column.AddConstraint(catalog::Constraint(ConstraintType::NOTNULL,
                                                not_null_constraint_name));
@@ -70,9 +73,9 @@ catalog::Column ConstraintsTestsUtil::GetColumnInfo(int index) {
     } break;
 
     case 1: {
-      auto column =
-          catalog::Column(type::Type::INTEGER, type::Type::GetTypeSize(type::Type::INTEGER),
-                          "COL_B", is_inlined);
+      auto column = catalog::Column(
+          type::Type::INTEGER, type::Type::GetTypeSize(type::Type::INTEGER),
+          "COL_B", is_inlined);
 
       column.AddConstraint(catalog::Constraint(ConstraintType::NOTNULL,
                                                not_null_constraint_name));
@@ -80,9 +83,9 @@ catalog::Column ConstraintsTestsUtil::GetColumnInfo(int index) {
     } break;
 
     case 2: {
-      auto column =
-          catalog::Column(type::Type::DECIMAL, type::Type::GetTypeSize(type::Type::DECIMAL),
-                          "COL_C", is_inlined);
+      auto column = catalog::Column(
+          type::Type::DECIMAL, type::Type::GetTypeSize(type::Type::DECIMAL),
+          "COL_C", is_inlined);
 
       column.AddConstraint(catalog::Constraint(ConstraintType::NOTNULL,
                                                not_null_constraint_name));
@@ -127,12 +130,15 @@ void ConstraintsTestsUtil::PopulateTable(concurrency::Transaction *transaction,
     int populate_value = rowid;
 
     // First column is unique in this case
-    auto col1 = type::ValueFactory::GetIntegerValue(PopulatedValue(populate_value, 0));
+    auto col1 =
+        type::ValueFactory::GetIntegerValue(PopulatedValue(populate_value, 0));
 
     // In case of random, make sure this column has duplicated values
-    auto col2 = type::ValueFactory::GetIntegerValue(PopulatedValue(populate_value, 1));
+    auto col2 =
+        type::ValueFactory::GetIntegerValue(PopulatedValue(populate_value, 1));
 
-    auto col3 = type::ValueFactory::GetDecimalValue(PopulatedValue(populate_value, 2));
+    auto col3 =
+        type::ValueFactory::GetDecimalValue(PopulatedValue(populate_value, 2));
 
     // In case of random, make sure this column has duplicated values
     auto col4 = type::ValueFactory::GetVarcharValue(
@@ -149,9 +155,13 @@ std::unique_ptr<const planner::ProjectInfo> MakeProjectInfoFromTuple(
   DirectMapList direct_map_list;
 
   for (oid_t col_id = START_OID; col_id < tuple->GetColumnCount(); col_id++) {
-    std::unique_ptr<type::Value> value(tuple->GetValue(col_id));
-    auto expression = expression::ExpressionUtil::ConstantValueFactory(*value);
-    target_list.emplace_back(col_id, expression);
+    auto *expression = expression::ExpressionUtil::ConstantValueFactory(
+        tuple->GetValue(col_id));
+
+    planner::DerivedAttribute attribute;
+    attribute.expr = expression;
+    attribute.attribute_info.type = attribute.expr->GetValueType();
+    target_list.emplace_back(col_id, attribute);
   }
 
   return std::unique_ptr<const planner::ProjectInfo>(new planner::ProjectInfo(
@@ -160,8 +170,10 @@ std::unique_ptr<const planner::ProjectInfo> MakeProjectInfoFromTuple(
 
 bool ConstraintsTestsUtil::ExecuteInsert(concurrency::Transaction *transaction,
                                          storage::DataTable *table,
-                                         const Value &col1, const Value &col2,
-                                         const Value &col3, const Value &col4) {
+                                         const type::Value &col1,
+                                         const type::Value &col2,
+                                         const type::Value &col3,
+                                         const type::Value &col4) {
   std::unique_ptr<executor::ExecutorContext> context(
       new executor::ExecutorContext(transaction));
 
