a1c7dd21df6518d64921c2a1c3ddc17ea7ee1872
vanryan <zvonryan@gmail.com>
2017-05-06 04:08:31 +0000

    Equality selectivity test enabled.

--- test/optimizer/selectivity_test.cpp
+++ test/optimizer/selectivity_test.cpp
@@ -148,7 +148,7 @@ TEST_F(SelectivityTests, LikeSelectivityTest) {
   EXPECT_EQ(like_than_sel_4, DEFAULT_SELECTIVITY);
 }
 
-/*
+
 TEST_F(SelectivityTests, EqualSelectivityTest) {
   auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
   auto txn = txn_manager.BeginTransaction();
@@ -169,11 +169,13 @@ TEST_F(SelectivityTests, EqualSelectivityTest) {
   auto table = catalog->GetTableWithName(DEFAULT_DB_NAME, TEST_TABLE_NAME);
   oid_t db_id = database->GetOid();
   oid_t table_id = table->GetOid();
+  oid_t column_id1 = 1;
 
   type::Value value1 = type::ValueFactory::GetDecimalValue(1.0);
 
   // Check for default selectivity when table stats does not exist.
-  double sel = Selectivity::GetEqualSelectivity(db_id, table_id, 1, value1);
+  SelParam param1{db_id, table_id, column_id1, value1};
+  double sel = Selectivity::Equal(&param1);
   EXPECT_EQ(sel, DEFAULT_SELECTIVITY);
 
   // Run analyze
@@ -181,10 +183,8 @@ TEST_F(SelectivityTests, EqualSelectivityTest) {
 
   // Check selectivity
   // equal, in mcv
-  double eq_sel_in_mcv = Selectivity::GetEqualSelectivity(db_id, table_id, 0,
-value1);
-  double neq_sel_in_mcv = Selectivity::GetNotEqualSelectivity(db_id, table_id,
-0, value1);
+  double eq_sel_in_mcv = Selectivity::Equal(&param1);
+  double neq_sel_in_mcv = Selectivity::NotEqual(&param1);
   ExpectSelectivityEqual(eq_sel_in_mcv, 0.33);
   ExpectSelectivityEqual(neq_sel_in_mcv, 0.67);
 
@@ -192,16 +192,14 @@ value1);
   // default top_k == 10, so add another 10 - 3 = 7 elements (4-10)
   for (int i = 1; i <= nrow; i++) {
     std::ostringstream os;
-    os << "INSERT INTO test VALUES (" << i + 1000 << ", " << i % 7 + 4 << ",
-1.21);";
+    os << "INSERT INTO test VALUES (" << i + 1000 << ", " << i % 7 + 4 << ", 1.21);";
     //os << "INSERT INTO test VALUES (" << i << ", 1.1, 1.21);";
     TestingSQLUtil::ExecuteSQLQuery(os.str());
   }
   // these elements will not be in mcv
   for (int i = 1; i <= nrow; i++) {
     std::ostringstream os;
-    os << "INSERT INTO test VALUES (" << i + 2000 << ", " << i % 50 + 11 << ",
-1.21);";
+    os << "INSERT INTO test VALUES (" << i + 2000 << ", " << i % 50 + 11 << ", 1.21);";
     //os << "INSERT INTO test VALUES (" << i << ", 1.1, 1.21);";
     TestingSQLUtil::ExecuteSQLQuery(os.str());
   }
@@ -211,12 +209,12 @@ value1);
 
   // Check selectivity
   // equal, not in mcv
+  oid_t column_id2 = 0;
   type::Value value2 = type::ValueFactory::GetDecimalValue(20.0);
+  SelParam param2{db_id, table_id, column_id2, value2};
 
-  double eq_sel_nin_mcv = Selectivity::GetEqualSelectivity(db_id, table_id, 1,
-value2);
-  double neq_sel_nin_mcv = Selectivity::GetNotEqualSelectivity(db_id, table_id,
-1, value2);
+  double eq_sel_nin_mcv = Selectivity::Equal(&param2);
+  double neq_sel_nin_mcv = Selectivity::NotEqual(&param2);
   // (1 - 2/3) / (3 + 7 + 50 - 10) = 1 / 150 = 0.01667
   ExpectSelectivityEqual(eq_sel_nin_mcv, 0.0066, 0.01);
   ExpectSelectivityEqual(neq_sel_nin_mcv, 0.9933, 0.01);
@@ -226,6 +224,7 @@ value2);
   catalog::Catalog::GetInstance()->DropDatabaseWithName(DEFAULT_DB_NAME, txn);
   txn_manager.CommitTransaction(txn);
 }
-*/
+
 } /* namespace test */
 } /* namespace peloton */
+
