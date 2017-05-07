01360a24a4484fbc1b12169ae1f0fc3a6656d9e9
Haoran Wang <hrwangchengdu@hotmail.com>
2017-05-07 02:37:40 +0000

    Add UDF Test

---
+++ test/sql/udf_test.cpp
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// order_by_sql_test.cpp
+//
+// Identification: test/sql/order_by_sql_test.cpp
+//
+// Copyright (c) 2015-17, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include <memory>
+
+#include "sql/testing_sql_util.h"
+#include "catalog/catalog.h"
+#include "common/harness.h"
+#include "executor/create_executor.h"
+#include "optimizer/simple_optimizer.h"
+#include "planner/create_plan.h"
+
+namespace peloton {
+namespace test {
+
+class UDFTests : public PelotonTest {};
+
+TEST_F(UDFTests, ExecuteTest) {
+  auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+  catalog::Catalog::GetInstance()->CreateDatabase(DEFAULT_DB_NAME, txn);
+  txn_manager.CommitTransaction(txn);
+  TestingSQLUtil::ExecuteSQLQuery("CREATE FUNCTION c_overpaid(integer, integer) RETURNS boolean AS 'DIRECTORY/funcs', 'c_overpaid' LANGUAGE C STRICT;");
+}
+
+}  // namespace test
+}  // namespace peloton
