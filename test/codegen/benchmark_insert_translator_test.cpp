d48008b2638ad0fb24dab6ae20a33a1ee6ae0fd9
Wei Cui <cuiwei.exe@gmail.com>
2017-05-08 18:44:55 -0400

    remove print code

--- test/codegen/benchmark_insert_translator_test.cpp
+++ test/codegen/benchmark_insert_translator_test.cpp
@@ -16,7 +16,7 @@
 #include "codegen/insert/insert_tuples_translator.h"
 #include "codegen/codegen_test_util.h"
 
-#define NUM_OF_INSERT_ROWS 100000
+#define NUM_OF_INSERT_ROWS 10000
 
 namespace peloton {
 namespace test {
@@ -61,72 +61,68 @@ class InsertTranslatorTest : public PelotonCodeGenTest {
 };
 
 
-//TEST_F(InsertTranslatorTest, InsertTuples) {
-//  auto table = &this->GetTestTable(this->TestTable1Id());
-//  auto table2 = &this->GetTestTable(this->TestTable2Id());
-//
-//  LOG_INFO("Before Insert: #tuples in table = %zu", table->GetTupleCount());
-//
-//  auto testing_pool = TestingHarness::GetInstance().GetTestingPool();
-//
-//  Timer<std::ratio<1, 1000>> timer;
-//  double compileTotalTime = 0;
-//  double executeTotalTime = 0;
-//
-//  for (int i = 0; i < NUM_OF_INSERT_ROWS; i++) {
-//
-//    if (i % 500 == 0) {
-//      LOG_INFO("Current i: %d", i);
-//    }
-//    std::unique_ptr<storage::Tuple> tuple(new storage::Tuple(table->GetSchema(), true));
-//    tuple->SetValue(0, type::ValueFactory::GetIntegerValue(10 * i));
-//    tuple->SetValue(1, type::ValueFactory::GetIntegerValue(10 * i + 1));
-//    tuple->SetValue(2, type::ValueFactory::GetIntegerValue(10 * i + 2));
-//    tuple->SetValue(3, type::ValueFactory::GetVarcharValue("hello world", true), testing_pool);
-//
-//    std::unique_ptr<planner::InsertPlan> insert_plan(new planner::InsertPlan(table, std::move(tuple)));
-//
-//    planner::BindingContext context;
-//    insert_plan->PerformBinding(context);
-//    codegen::BufferingConsumer buffer{{0, 1}, context};
-//
-//    timer.Start();
-//    CompileAndExecute(*insert_plan, buffer,
-//                      reinterpret_cast<char *>(buffer.GetState()));
-//    timer.Stop();
-//    compileTotalTime += timer.GetDuration();
-//    timer.Reset();
-//
-//    std::unique_ptr<storage::Tuple> tuple2(new storage::Tuple(table2->GetSchema(), true));
-//    tuple2->SetValue(0, type::ValueFactory::GetIntegerValue(10 * i + 5));
-//    tuple2->SetValue(1, type::ValueFactory::GetIntegerValue(10 * i + 6));
-//    tuple2->SetValue(2, type::ValueFactory::GetIntegerValue(10 * i + 7));
-//    tuple2->SetValue(3, type::ValueFactory::GetVarcharValue("Advanced DB", true), testing_pool);
-//
-//    std::unique_ptr<planner::InsertPlan> insert_plan_2(new planner::InsertPlan(table2, std::move(tuple2)));
-//    auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
-//    concurrency::Transaction *txn = txn_manager.BeginTransaction();
-//    std::unique_ptr<executor::ExecutorContext> executor_context(
-//        new executor::ExecutorContext(txn)
-//    );
-//
-//    std::unique_ptr<executor::InsertExecutor> insert_executor =
-//        std::make_unique<executor::InsertExecutor>(
-//            insert_plan_2.get(), executor_context.get()
-//        );
-//
-//    timer.Start();
-//    while (insert_executor->Execute()) {}
-//    txn_manager.CommitTransaction(txn);
-//    timer.Stop();
-//    executeTotalTime += timer.GetDuration();
-//    timer.Reset();
-//  }
-//  ASSERT_EQ(table->GetTupleCount(), NUM_OF_INSERT_ROWS);
-//  ASSERT_EQ(table2->GetTupleCount(), NUM_OF_INSERT_ROWS);
-//  LOG_INFO("compiled insert finished in: %.2f ms\n", compileTotalTime);
-//  LOG_INFO("executor insert finished in: %.2f ms\n", executeTotalTime);
-//}
+TEST_F(InsertTranslatorTest, InsertTuples) {
+  auto table = &this->GetTestTable(this->TestTable1Id());
+  auto table2 = &this->GetTestTable(this->TestTable2Id());
+
+  LOG_INFO("Before Insert: #tuples in table = %zu", table->GetTupleCount());
+
+  auto testing_pool = TestingHarness::GetInstance().GetTestingPool();
+
+  Timer<std::ratio<1, 1000>> timer;
+  double compileTotalTime = 0;
+  double executeTotalTime = 0;
+
+  for (int i = 0; i < NUM_OF_INSERT_ROWS; i++) {
+    std::unique_ptr<storage::Tuple> tuple(new storage::Tuple(table->GetSchema(), true));
+    tuple->SetValue(0, type::ValueFactory::GetIntegerValue(10 * i));
+    tuple->SetValue(1, type::ValueFactory::GetIntegerValue(10 * i + 1));
+    tuple->SetValue(2, type::ValueFactory::GetIntegerValue(10 * i + 2));
+    tuple->SetValue(3, type::ValueFactory::GetVarcharValue("hello world", true), testing_pool);
+
+    std::unique_ptr<planner::InsertPlan> insert_plan(new planner::InsertPlan(table, std::move(tuple)));
+
+    planner::BindingContext context;
+    insert_plan->PerformBinding(context);
+    codegen::BufferingConsumer buffer{{0, 1}, context};
+
+    timer.Start();
+    CompileAndExecute(*insert_plan, buffer,
+                      reinterpret_cast<char *>(buffer.GetState()));
+    timer.Stop();
+    compileTotalTime += timer.GetDuration();
+    timer.Reset();
+
+    std::unique_ptr<storage::Tuple> tuple2(new storage::Tuple(table2->GetSchema(), true));
+    tuple2->SetValue(0, type::ValueFactory::GetIntegerValue(10 * i + 5));
+    tuple2->SetValue(1, type::ValueFactory::GetIntegerValue(10 * i + 6));
+    tuple2->SetValue(2, type::ValueFactory::GetIntegerValue(10 * i + 7));
+    tuple2->SetValue(3, type::ValueFactory::GetVarcharValue("Advanced DB", true), testing_pool);
+
+    std::unique_ptr<planner::InsertPlan> insert_plan_2(new planner::InsertPlan(table2, std::move(tuple2)));
+    auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+    concurrency::Transaction *txn = txn_manager.BeginTransaction();
+    std::unique_ptr<executor::ExecutorContext> executor_context(
+        new executor::ExecutorContext(txn)
+    );
+
+    std::unique_ptr<executor::InsertExecutor> insert_executor =
+        std::make_unique<executor::InsertExecutor>(
+            insert_plan_2.get(), executor_context.get()
+        );
+
+    timer.Start();
+    while (insert_executor->Execute()) {}
+    txn_manager.CommitTransaction(txn);
+    timer.Stop();
+    executeTotalTime += timer.GetDuration();
+    timer.Reset();
+  }
+  ASSERT_EQ(table->GetTupleCount(), NUM_OF_INSERT_ROWS);
+  ASSERT_EQ(table2->GetTupleCount(), NUM_OF_INSERT_ROWS);
+  LOG_INFO("compiled insert finished in: %.2f ms\n", compileTotalTime);
+  LOG_INFO("executor insert finished in: %.2f ms\n", executeTotalTime);
+}
 
 /**
  * @brief Insert all tuples in table4 into table3.
@@ -175,8 +171,8 @@ TEST_F(InsertTranslatorTest, InsertScanExecutor) {
 
   txn_manager.CommitTransaction(txn);
 
-  LOG_DEBUG("Table 3 has %zu tuples", table3->GetTupleCount());
-  LOG_DEBUG("Table 4 has %zu tuples", table4->GetTupleCount());
+  LOG_INFO("Table 3 has %zu tuples", table3->GetTupleCount());
+  LOG_INFO("Table 4 has %zu tuples", table4->GetTupleCount());
 }
 
 /**
@@ -188,8 +184,8 @@ TEST_F(InsertTranslatorTest, InsertScanTranslator) {
   auto table3 = &this->GetTestTable(this->TestTable3Id());
   auto table4 = &this->GetTestTable(this->TestTable4Id());
 
-  LOG_DEBUG("Table 3 has %zu tuples", table3->GetTupleCount());
-  LOG_DEBUG("Table 4 has %zu tuples", table4->GetTupleCount());
+  LOG_INFO("Table 3 has %zu tuples", table3->GetTupleCount());
+  LOG_INFO("Table 4 has %zu tuples", table4->GetTupleCount());
 
   // Insert into table3
   std::unique_ptr<planner::InsertPlan> insert_plan(
@@ -219,8 +215,8 @@ TEST_F(InsertTranslatorTest, InsertScanTranslator) {
   (void)results;
 
   PL_ASSERT(table3->GetTupleCount() == table4->GetTupleCount());
-  LOG_DEBUG("Table 3 has %zu tuples", table3->GetTupleCount());
-  LOG_DEBUG("Table 4 has %zu tuples", table4->GetTupleCount());
+  LOG_INFO("Table 3 has %zu tuples", table3->GetTupleCount());
+  LOG_INFO("Table 4 has %zu tuples", table4->GetTupleCount());
 }
 
 }  // namespace test
