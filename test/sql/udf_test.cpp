ae56f011ccf8a415835ca2cf4b2b21b958965687
nasrin.jaleel.93 <nasrin.jaleel.93@gmail.com>
2017-05-09 03:13:20 +0000

    modified table invocation test

--- test/sql/udf_test.cpp
+++ test/sql/udf_test.cpp
@@ -53,7 +53,7 @@ TEST_F(UDFTests, PLPGSQLTest) {
                                   tuple_descriptor, rows_affected,
                                   error_message);
 
-  //TestingSQLUtil::ShowTable("pg_catalog","pg_proc");
+ // TestingSQLUtil::ShowTable("pg_catalog","pg_proc");
   LOG_DEBUG("Statement executed. Result: %s",
            ResultTypeToString(status).c_str());
  
@@ -76,7 +76,7 @@ TEST_F(UDFTests, PLPGSQLInvocationTest){
 
 }
 
-TEST_F(UDFTests, TABLE) {
+TEST_F(UDFTests, TableInvocationTest) {
   auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
   auto txn = txn_manager.BeginTransaction();
   catalog::Catalog::GetInstance()->CreateDatabase(DEFAULT_DB_NAME, txn);
@@ -86,7 +86,18 @@ TEST_F(UDFTests, TABLE) {
   TestingSQLUtil::ExecuteSQLQuery("INSERT INTO test VALUES (0, 1);");
   TestingSQLUtil::ExecuteSQLQuery("INSERT INTO test VALUES (1, 2);");
   TestingSQLUtil::ExecuteSQLQuery("INSERT INTO test VALUES (2, 3);");
-  TestingSQLUtil::ExecuteSQLQuery("SELECT a + 1 from test;");
+
+  std::vector<StatementResult> result;
+  std::vector<FieldInfo> tuple_descriptor;
+  std::string error_message;
+  int rows_affected;
+
+  TestingSQLUtil::ExecuteSQLQuery("SELECT increment(a) from test;", result, tuple_descriptor,rows_affected, error_message);
+  
+  EXPECT_EQ('1', result[0].second[0]);
+  EXPECT_EQ('2', result[1].second[0]);
+  EXPECT_EQ('3', result[2].second[0]);
+  
 
   // free the database just created
   txn = txn_manager.BeginTransaction();
