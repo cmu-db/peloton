c585b62dca17894accd93f057f8c4f9a07986fef
Tao Lin <nblintao@gmail.com>
2017-05-08 23:14:21 -0400

    Unit test for dropping trigger

--- test/executor/drop_test.cpp
+++ test/executor/drop_test.cpp
@@ -18,6 +18,12 @@
 #include "common/harness.h"
 #include "concurrency/transaction_manager_factory.h"
 #include "executor/drop_executor.h"
+#include "common/logger.h"
+#include "executor/create_executor.h"
+#include "executor/drop_executor.h"
+#include "parser/postgresparser.h"
+#include "planner/drop_plan.h"
+#include "planner/plan_util.h"
 
 namespace peloton {
 namespace test {
@@ -65,6 +71,91 @@ TEST_F(DropTests, DroppingTable) {
   catalog->DropTable(DEFAULT_DB_NAME, "department_table", txn);
   txn_manager.CommitTransaction(txn);
   EXPECT_EQ(catalog->GetDatabaseWithName(DEFAULT_DB_NAME)->GetTableCount(), 1);
+
+  // free the database just created
+  txn = txn_manager.BeginTransaction();
+  catalog->DropDatabaseWithName(DEFAULT_DB_NAME, txn);
+  txn_manager.CommitTransaction(txn);
+}
+
+TEST_F(DropTests, DroppingTrigger) {
+  auto catalog = catalog::Catalog::GetInstance();
+
+  auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+
+  // Create a table first
+  auto id_column = catalog::Column(type::Type::INTEGER,
+                                   type::Type::GetTypeSize(type::Type::INTEGER),
+                                   "dept_id", true);
+  auto name_column =
+    catalog::Column(type::Type::VARCHAR, 32, "dept_name", false);
+
+  std::unique_ptr<catalog::Schema> table_schema(
+    new catalog::Schema({id_column, name_column}));
+
+  catalog->CreateDatabase(DEFAULT_DB_NAME, txn);
+  txn_manager.CommitTransaction(txn);
+
+  txn = txn_manager.BeginTransaction();
+  catalog->CreateTable(DEFAULT_DB_NAME, "department_table",
+                       std::move(table_schema), txn);
+  txn_manager.CommitTransaction(txn);
+
+
+  // Create a trigger
+  auto parser = parser::PostgresParser::GetInstance();
+  std::string query =
+    "CREATE TRIGGER update_dept_name "
+      "BEFORE UPDATE OF dept_name ON department_table "
+      "EXECUTE PROCEDURE log_update_dept_name();";
+  std::unique_ptr<parser::SQLStatementList> stmt_list(parser.BuildParseTree(query).release());
+  EXPECT_TRUE(stmt_list->is_valid);
+  EXPECT_EQ(StatementType::CREATE, stmt_list->GetStatement(0)->GetType());
+  auto create_trigger_stmt =
+    static_cast<parser::CreateStatement *>(stmt_list->GetStatement(0));
+  // Create plans
+  planner::CreatePlan plan(create_trigger_stmt);
+  // Execute the create trigger
+  txn = txn_manager.BeginTransaction();
+  std::unique_ptr<executor::ExecutorContext> context(
+    new executor::ExecutorContext(txn));
+  executor::CreateExecutor createTriggerExecutor(&plan, context.get());
+  createTriggerExecutor.Init();
+  createTriggerExecutor.Execute();
+  txn_manager.CommitTransaction(txn);
+
+  // Check the effect of creation
+  storage::DataTable *target_table =
+    catalog::Catalog::GetInstance()->GetTableWithName(DEFAULT_DB_NAME,
+                                                      "department_table");
+  EXPECT_EQ(1, target_table->GetTriggerNumber());
+  commands::Trigger *new_trigger = target_table->GetTriggerByIndex(0);
+  EXPECT_EQ(new_trigger->GetTriggerName(), "update_dept_name");
+
+
+  // Drop statement and drop plan
+  parser::DropStatement drop_statement(parser::DropStatement::EntityType::kTrigger, "department_table", "update_dept_name");
+  planner::DropPlan drop_plan(&drop_statement);
+
+  // Execute the create trigger
+  txn = txn_manager.BeginTransaction();
+  std::unique_ptr<executor::ExecutorContext> context2(
+    new executor::ExecutorContext(txn));
+  executor::DropExecutor drop_executor(&drop_plan, context2.get());
+  drop_executor.Init();
+  drop_executor.Execute();
+  txn_manager.CommitTransaction(txn);
+
+  // Check the effect of drop
+  // Most major check in this test case
+  EXPECT_EQ(0, target_table->GetTriggerNumber());
+
+  // Now dropping the table using the executer
+  txn = txn_manager.BeginTransaction();
+  catalog->DropTable(DEFAULT_DB_NAME, "department_table", txn);
+  txn_manager.CommitTransaction(txn);
+  EXPECT_EQ(0, catalog->GetDatabaseWithName(DEFAULT_DB_NAME)->GetTableCount());
 
   // free the database just created
   txn = txn_manager.BeginTransaction();
