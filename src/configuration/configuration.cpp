696bcad75302e6f7f5701a6323ac9dfa76135a4b
rohit-cmu <ragrawa1@andrew.cmu.edu>
2017-05-09 17:14:32 -0400

    fsanitize flag fix

--- src/configuration/configuration.cpp
+++ src/configuration/configuration.cpp
@@ -112,7 +112,7 @@ DEFINE_bool(layout_tuner,
 //===----------------------------------------------------------------------===//
 
 DEFINE_bool(codegen,
-            false,
+            true,
             "Enable code-generation for query execution (default: true)");
 
 // Layout mode
