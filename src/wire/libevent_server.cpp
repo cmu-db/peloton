974fef99568b0047aa738df76a30bf197c9e06fb
zoushaokun <zoushaokun11@outlook.com>
2017-05-16 11:43:28 -0400

    remove err call

--- src/wire/libevent_server.cpp
+++ src/wire/libevent_server.cpp
@@ -134,7 +134,6 @@ void LibeventServer::StartServer() {
                                     SSL_FILETYPE_PEM) == 0)
     {
       SSL_CTX_free(ssl_context);
-      ERR_print_errors_fp(stderr);
       throw ConnectionException("Error associating private key.\n");
     }
     LOG_INFO("certificate file path %s", certificate_file_.c_str());
@@ -143,7 +142,6 @@ void LibeventServer::StartServer() {
                                      SSL_FILETYPE_PEM) == 0)
     {
       SSL_CTX_free(ssl_context);
-      ERR_print_errors_fp(stderr);
       throw ConnectionException("Error associating certificate.\n");
     }
 
