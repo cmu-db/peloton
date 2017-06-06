a7f966fee1c16542f6512c15991c6435d4972c0e
Yingjun <yingjun@comp.nus.edu.sg>
2017-06-06 11:05:01 +0800

    fix typo

--- test/executor/copy_test.cpp
+++ test/executor/copy_test.cpp
@@ -1,142 +1,143 @@
-// //===----------------------------------------------------------------------===//
-// //
-// //                         Peloton
-// //
-// // copy_test.cpp
-// //
-// // Identification: test/executor/copy_test.cpp
-// //
-// // Copyright (c) 2015-16, Carnegie Mellon University Database Group
-// //
-// //===----------------------------------------------------------------------===//
 
-// #include <cstdio>
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// copy_test.cpp
+//
+// Identification: test/executor/copy_test.cpp
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
 
-// #include "catalog/catalog.h"
-// #include "common/harness.h"
-// #include "common/logger.h"
-// #include "common/statement.h"
-// #include "executor/copy_executor.h"
-// #include "executor/seq_scan_executor.h"
-// #include "optimizer/simple_optimizer.h"
-// #include "parser/postgresparser.h"
-// #include "planner/seq_scan_plan.h"
-// #include "tcop/tcop.h"
+#include <cstdio>
 
-// #include "gtest/gtest.h"
-// #include "statistics/testing_stats_util.h"
+#include "catalog/catalog.h"
+#include "common/harness.h"
+#include "common/logger.h"
+#include "common/statement.h"
+#include "executor/copy_executor.h"
+#include "executor/seq_scan_executor.h"
+#include "optimizer/simple_optimizer.h"
+#include "parser/postgresparser.h"
+#include "planner/seq_scan_plan.h"
+#include "tcop/tcop.h"
 
-// namespace peloton {
-// namespace test {
+#include "gtest/gtest.h"
+#include "statistics/testing_stats_util.h"
 
-// //===--------------------------------------------------------------------===//
-// // Copy Tests
-// //===--------------------------------------------------------------------===//
+namespace peloton {
+namespace test {
 
-// class CopyTests : public PelotonTest {};
+//===--------------------------------------------------------------------===//
+// Copy Tests
+//===--------------------------------------------------------------------===//
 
-// TEST_F(CopyTests, Copying) {
-//   auto catalog = catalog::Catalog::GetInstance();
-//   auto& txn_manager = concurrency::TransactionManagerFactory::GetInstance();
-//   auto txn = txn_manager.BeginTransaction();
-//   catalog->CreateDatabase("emp_db", txn);
-//   txn_manager.CommitTransaction(txn);
+class CopyTests : public PelotonTest {};
 
-//   optimizer::SimpleOptimizer optimizer;
-//   auto& traffic_cop = tcop::TrafficCop::GetInstance();
+TEST_F(CopyTests, Copying) {
+  auto catalog = catalog::Catalog::GetInstance();
+  auto& txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+  catalog->CreateDatabase("emp_db", txn);
+  txn_manager.CommitTransaction(txn);
 
-//   // Create a table without primary key
-//   TestingStatsUtil::CreateTable(false);
-//   txn = txn_manager.BeginTransaction();
-//   std::string short_string = "eeeeeeeeee";
-//   std::string long_string =
-//       short_string + short_string + short_string + short_string + short_string +
-//       short_string + short_string + short_string + short_string + short_string +
-//       short_string + short_string + short_string + short_string + short_string +
-//       short_string + short_string + short_string;
-//   std::string escape_string = "eeeeeee,eeeeee,eeeeeee,";
+  optimizer::SimpleOptimizer optimizer;
+  auto& traffic_cop = tcop::TrafficCop::GetInstance();
 
-//   // Inserting tuples end-to-end
-//   int num_tuples = 100;
-//   size_t num_bytes_to_write = 0;
-//   size_t integer_len = 5;
-//   size_t default_delimiter_len = 2;
-//   size_t extra_delimiter_len = 6;
-//   for (int i = 0; i < num_tuples; i++) {
-//     // Choose a string and calculate the number of bytes to write
-//     std::string insert_str;
-//     if (i % 3 == 0) {
-//       insert_str = short_string;
-//     } else if (i % 3 == 1) {
-//       insert_str = long_string;
-//     } else {
-//       insert_str = escape_string;
-//       num_bytes_to_write += extra_delimiter_len;
-//     }
-//     // Including the byte to write delimiters
-//     num_bytes_to_write +=
-//         insert_str.length() + integer_len + default_delimiter_len;
+  // Create a table without primary key
+  TestingStatsUtil::CreateTable(false);
+  txn = txn_manager.BeginTransaction();
+  std::string short_string = "eeeeeeeeee";
+  std::string long_string =
+      short_string + short_string + short_string + short_string + short_string +
+      short_string + short_string + short_string + short_string + short_string +
+      short_string + short_string + short_string + short_string + short_string +
+      short_string + short_string + short_string;
+  std::string escape_string = "eeeeeee,eeeeee,eeeeeee,";
 
-//     // Execute insert
-//     auto statement = TestingStatsUtil::GetInsertStmt(12345, insert_str);
-//     std::vector<type::Value> params;
-//     std::vector<int> result_format(statement->GetTupleDescriptor().size(), 0);
-//     std::vector<StatementResult> result;
-//     executor::ExecuteResult status = traffic_cop.ExecuteStatementPlan(
-//         statement->GetPlanTree().get(), params, result, result_format);
-//     EXPECT_EQ(status.m_result, peloton::ResultType::SUCCESS);
-//     LOG_TRACE("Statement executed. Result: %s",
-//               ResultTypeToString(status.m_result).c_str());
-//   }
-//   LOG_INFO("Tuples inserted!");
-//   txn_manager.CommitTransaction(txn);
+  // Inserting tuples end-to-end
+  int num_tuples = 100;
+  size_t num_bytes_to_write = 0;
+  size_t integer_len = 5;
+  size_t default_delimiter_len = 2;
+  size_t extra_delimiter_len = 6;
+  for (int i = 0; i < num_tuples; i++) {
+    // Choose a string and calculate the number of bytes to write
+    std::string insert_str;
+    if (i % 3 == 0) {
+      insert_str = short_string;
+    } else if (i % 3 == 1) {
+      insert_str = long_string;
+    } else {
+      insert_str = escape_string;
+      num_bytes_to_write += extra_delimiter_len;
+    }
+    // Including the byte to write delimiters
+    num_bytes_to_write +=
+        insert_str.length() + integer_len + default_delimiter_len;
 
-//   // Now Copying end-to-end
-//   LOG_INFO("Copying a table...");
-//   std::string copy_sql =
-//       "COPY emp_db.department_table TO './copy_output.csv' DELIMITER ',';";
-//   txn = txn_manager.BeginTransaction();
-//   LOG_INFO("Query: %s", copy_sql.c_str());
-//   std::unique_ptr<Statement> statement(new Statement("COPY", copy_sql));
+    // Execute insert
+    auto statement = TestingStatsUtil::GetInsertStmt(12345, insert_str);
+    std::vector<type::Value> params;
+    std::vector<int> result_format(statement->GetTupleDescriptor().size(), 0);
+    std::vector<StatementResult> result;
+    executor::ExecuteResult status = traffic_cop.ExecuteStatementPlan(
+        statement->GetPlanTree().get(), params, result, result_format);
+    EXPECT_EQ(status.m_result, peloton::ResultType::SUCCESS);
+    LOG_TRACE("Statement executed. Result: %s",
+              ResultTypeToString(status.m_result).c_str());
+  }
+  LOG_INFO("Tuples inserted!");
+  txn_manager.CommitTransaction(txn);
 
-//   LOG_INFO("Building parse tree...");
-//   auto& peloton_parser = parser::PostgresParser::GetInstance();
-//   auto copy_stmt = peloton_parser.BuildParseTree(copy_sql);
+  // Now Copying end-to-end
+  LOG_INFO("Copying a table...");
+  std::string copy_sql =
+      "COPY emp_db.department_table TO './copy_output.csv' DELIMITER ',';";
+  txn = txn_manager.BeginTransaction();
+  LOG_INFO("Query: %s", copy_sql.c_str());
+  std::unique_ptr<Statement> statement(new Statement("COPY", copy_sql));
 
-//   LOG_INFO("Building plan tree...");
-//   auto copy_plan = optimizer.BuildPelotonPlanTree(copy_stmt);
-//   statement->SetPlanTree(copy_plan);
+  LOG_INFO("Building parse tree...");
+  auto& peloton_parser = parser::PostgresParser::GetInstance();
+  auto copy_stmt = peloton_parser.BuildParseTree(copy_sql);
 
-//   LOG_INFO("Building executor tree...");
-//   // Build executor tree
-//   std::unique_ptr<executor::ExecutorContext> context(
-//       new executor::ExecutorContext(txn));
-//   auto copy_executor =
-//       new executor::CopyExecutor(copy_plan.get(), context.get());
-//   std::unique_ptr<executor::AbstractExecutor> root_executor(copy_executor);
-//   std::unique_ptr<executor::AbstractExecutor> seq_scan_executor(
-//       new executor::SeqScanExecutor(copy_plan->GetChildren()[0].get(),
-//                                     context.get()));
-//   copy_executor->AddChild(seq_scan_executor.get());
+  LOG_INFO("Building plan tree...");
+  auto copy_plan = optimizer.BuildPelotonPlanTree(copy_stmt);
+  statement->SetPlanTree(copy_plan);
 
-//   LOG_INFO("Executing plan...");
-//   // Initialize the executor tree
-//   auto status = root_executor->Init();
-//   EXPECT_EQ(status, true);
-//   // Execute the tree until we get result tiles from root node
-//   while (status == true) {
-//     status = root_executor->Execute();
-//   }
+  LOG_INFO("Building executor tree...");
+  // Build executor tree
+  std::unique_ptr<executor::ExecutorContext> context(
+      new executor::ExecutorContext(txn));
+  auto copy_executor =
+      new executor::CopyExecutor(copy_plan.get(), context.get());
+  std::unique_ptr<executor::AbstractExecutor> root_executor(copy_executor);
+  std::unique_ptr<executor::AbstractExecutor> seq_scan_executor(
+      new executor::SeqScanExecutor(copy_plan->GetChildren()[0].get(),
+                                    context.get()));
+  copy_executor->AddChild(seq_scan_executor.get());
 
-//   // Check the number of bypes written
-//   EXPECT_EQ(copy_executor->GetTotalBytesWritten(), num_bytes_to_write);
-//   txn_manager.CommitTransaction(txn);
+  LOG_INFO("Executing plan...");
+  // Initialize the executor tree
+  auto status = root_executor->Init();
+  EXPECT_EQ(status, true);
+  // Execute the tree until we get result tiles from root node
+  while (status == true) {
+    status = root_executor->Execute();
+  }
 
-//   // free the database just created
-//   txn = txn_manager.BeginTransaction();
-//   catalog->DropDatabaseWithName("emp_db", txn);
-//   txn_manager.CommitTransaction(txn);
-// }
+  // Check the number of bytes written
+  EXPECT_EQ(copy_executor->GetTotalBytesWritten(), num_bytes_to_write);
+  txn_manager.CommitTransaction(txn);
 
-// }  // End test namespace
-// }  // End peloton namespace
+  // free the database just created
+  txn = txn_manager.BeginTransaction();
+  catalog->DropDatabaseWithName("emp_db", txn);
+  txn_manager.CommitTransaction(txn);
+}
+
+}  // End test namespace
+}  // End peloton namespace
