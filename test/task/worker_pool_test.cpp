5478b2abd1ec2c1330275e406585072576d3b1f6
Yuchen Deng <ydeng1@andrew.cmu.edu>
2017-07-10 14:18:49 -0400

    add test cases

--- test/task/worker_pool_test.cpp
+++ test/task/worker_pool_test.cpp
@@ -23,34 +23,100 @@ namespace peloton {
 namespace test {
 
 class WorkerPoolTests : public PelotonTest {};
+/*
+class MasterThread {
+ public:
+  MasterThread(const size_t queue_size, const size_t pool_size):
+};*/
 
-void func1(std::vector<void *> args) {
+void printString(std::vector<void *> args) {
   std::string *sp = static_cast<std::string *>(*args.begin());
   std::string str = *sp;
   std::thread::id id = std::this_thread::get_id();
-  std::cout << "thread " << id << ", get " << str.c_str() << std::endl;
+  LOG_INFO("thread %x get %s", id, str.c_str());
 }
 
-void hear() {
-  std::cout << "master activate";
+void masterCallback() {
+  LOG_INFO("master activate");
 }
 
 TEST_F(WorkerPoolTests, OneWorkerTest) {
-  std::cout << "master, start" <<
-  std::endl;
+  LOG_INFO("master starts");
   const size_t sz = 10;
   peloton::task::TaskQueue tq(sz);
   peloton::task::WorkerPool wp(1, &tq);
 
   std::vector<void *> params1;
-  std::string p1 = "monday";
+  std::string p1 = "string1";
   params1.push_back((void*)&p1);
 
+  std::vector<void *> params2;
+  std::string p2 = "string2";
+  params2.push_back((void *)&p2);
+
+  std::vector<shared_ptr<Task *>> task_v;
   peloton::task::Task task1(func1, params1);
-  tq.SubmitTask(shared_ptr<Task>(task1));
-  std::cout << "master, finish" <<std::endl;
+  task_v.push_back(shared_ptr<Task>(task1));
+  peloton::task::Task task2(func1, params2);
+  task_v.push_back(shared_ptr<Task>(task2));
+
+  tq.SubmitTaskBatch(task_v);
+
+  LOG_INFO("master finishes");
   wp.Shutdown();
 }
+
+TEST_F(WorkerPoolTests, MultiWorkerTest) {
+  LOG_INFO("master starts");
+  const size_t sz = 10;
+  peloton::task::TaskQueue tq(sz);
+  peloton::task::WorkerPool wp(1, &tq);
+
+  std::vector<void *> params1;
+  std::string p1 = "string1";
+  params1.push_back((void*)&p1);
+
+  std::vector<void *> params2;
+  std::string p2 = "string2";
+  params2.push_back((void *)&p2);
+
+  std::vector<shared_ptr<Task> task_v;
+  peloton::task::Task task1(func1, params1);
+  task_v.push_back(shared_ptr<Task>(task1));
+  peloton::task::Task task2(func1, params2);
+  task_v.push_back(shared_ptr<Task>(task2));
+
+  tq.SubmitTaskBatch(task_v);
+
+  LOG_INFO("master finishes");
+  wp.Shutdown();
+}
+/*
+TEST_F(WorkerPoolTests, ActivateTest) {
+  LOG_INFO("master starts");
+  const size_t sz = 10;
+  peloton::task::TaskQueue tq(sz);
+  peloton::task::WorkerPool wp(1, &tq);
+
+  std::vector<void *> params1;
+  std::string p1 = "func1";
+  params1.push_back((void*)&p1);
+
+  std::vector<void *> params2;
+  std::string p2 = "func2";
+  params2.push_back((void *)&p2);
+
+  std::vector<shared_ptr<Task>> task_v;
+  peloton::task::Task task1(func1, params1);
+  task_v.push_back(shared_ptr<Task>(task1));
+  peloton::task::Task task2(func1, params2);
+  task_v.push_back(shared_ptr<Task>(task2));
+
+  tq.SubmitTaskBatch(task_v);
+
+  LOG_INFO("master finishes");
+  wp.Shutdown();
+}*/
 
 } // end of test
 } // end of peloton
