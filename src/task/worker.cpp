17514df20ace8c200554cab1e60d57d5a2800d06
Yuchen Deng <ydeng1@andrew.cmu.edu>
2017-07-10 16:54:08 -0400

    changes in test cases

--- src/task/worker.cpp
+++ src/task/worker.cpp
@@ -54,7 +54,7 @@ WorkerPool::WorkerPool(size_t num_threads, TaskQueue *task_queue) {
   this->task_queue_ = task_queue;
   for (size_t thread_id = 0; thread_id < num_threads; thread_id++){
     // start thread on construction
-    std::unique_ptr<Worker> worker(new Worker());
+    std::unique_ptr<Worker> worker = std::make_unique<Worker>();
     worker->StartThread(this);
     worker_threads_.push_back(std::move(worker));
   }
