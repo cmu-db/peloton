fd71f268da11e45ce271adcb4a7fb812e67dcdc4
Andy Pavlo <pavlo@cs.brown.edu>
2017-07-15 11:26:40 -0400

    Added MultiConstraint class. This is not used anywhere just yet. Still porting more code

--- src/include/catalog/schema.h
+++ src/include/catalog/schema.h
@@ -14,6 +14,7 @@
 
 #include <memory>
 #include "catalog/column.h"
+#include "catalog/multi_constraint.h"
 #include "common/printable.h"
 #include "type/type.h"
 #include "boost/algorithm/string.hpp"
@@ -209,6 +210,14 @@ class Schema : public Printable {
     }
   }
 
+  inline void AddMultiConstraints(const catalog::MultiConstraint &mc) {
+    multi_constraints.push_back(mc);
+  }
+
+  inline std::vector<MultiConstraint> GetMultiConstraints() {
+    return multi_constraints;
+  }
+
   // Get a string representation for debugging
   const std::string GetInfo() const;
 
@@ -221,6 +230,9 @@ class Schema : public Printable {
 
   // keeps track of unlined columns
   std::vector<oid_t> uninlined_columns;
+
+  // keeps multi_constraints
+  std::vector<MultiConstraint> multi_constraints;
 
   // keep these in sync with the vectors above
   oid_t column_count = INVALID_OID;
