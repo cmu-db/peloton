d894e64f3ac34aa8598252d161b26976df7b92bf
Tianyi Chen <tianyic1@andrew.cmu.edu>
2017-08-04 12:29:59 -0700

    rename networking to distributed

---
+++ src/include/distributed/rpc_type.h
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// rpc_type.h
+//
+// Identification: src/include/distributed/rpc_type.h
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+
+#pragma once
+
+namespace peloton {
+namespace networking {
+
+enum MessageType { MSG_TYPE_INVALID = 0, MSG_TYPE_REQ, MSG_TYPE_REP };
+
+}  // namespace message
+}  // namespace peloton
---
+++ test/distributed/rpc_queryplan_test.cpp
@@ -0,0 +1,37 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// rpc_queryplan_test.cpp
+//
+// Identification: test/distributed/rpc_queryplan_test.cpp
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+
+#include "common/harness.h"
+#include "networking/rpc_server.h"
+#include "networking/peloton_service.h"
+#include "planner/seq_scan_plan.h"
+
+namespace peloton {
+namespace test {
+
+class RpcQueryPlanTests : public PelotonTest {};
+
+TEST_F(RpcQueryPlanTests, BasicTest) {
+  peloton::planner::SeqScanPlan mapped_plan_ptr;
+
+  const peloton::PlanNodeType type = mapped_plan_ptr.GetPlanNodeType();
+  peloton::networking::QueryPlanExecRequest request;
+  request.set_plan_type(static_cast<int>(type));
+
+  peloton::CopySerializeOutput output_plan;
+  bool serialize = mapped_plan_ptr.SerializeTo(output_plan);
+  // Becuase the plan is not completed, so it is false
+  EXPECT_EQ(serialize, false);
+}
+}
+}
