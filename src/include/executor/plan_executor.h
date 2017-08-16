ef5409528ec3f2af1f37ac5fac6228500aab8bc8
huahaoxiang <huahaoxiang@gmail.com>
2017-08-16 15:41:17 -0400

    rebase master

--- src/include/executor/plan_executor.h
+++ src/include/executor/plan_executor.h
@@ -72,11 +72,11 @@ class PlanExecutor {
    * pass value list directly rather than passing Postgres's ParamListInfo
    */
   static void ExecutePlan(std::shared_ptr<planner::AbstractPlan> plan,
-                                    concurrency::Transaction* txn,
-                                    const std::vector<type::Value> &params,
-                                    std::vector<StatementResult> &result,
-                                    const std::vector<int> &result_format,
-                                    executor::ExecuteResult &p_status);
+                                   concurrency::Transaction* txn,
+                                   const std::vector<type::Value> &params,
+                                   std::vector<StatementResult> &result,
+                                   const std::vector<int> &result_format,
+                                   executor::ExecuteResult &p_status);
 
   /*
    * @brief When a peloton node recvs a query plan, this function is invoked
