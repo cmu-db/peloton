e4560a514897e1a8f74a097bc66767ce01a9cf55
paulo <pauloamora1111@gmail.com>
2017-09-21 18:27:27 +0000

    Fixing tests

--- test/logging/logging_test.cpp
+++ test/logging/logging_test.cpp
@@ -50,7 +50,7 @@ class LoggingTests : public PelotonTest {};
     auto &log_manager = logging::DurabilityFactory::GetLoggerInstance();
     log_manager.SetDirectories({"/tmp/test"});
     log_manager.StartLoggers();
-    log_manager.LogInsert(ItemPointer(table->GetTileGroup(0)->GetTileGroupId(),0), 5);
+    log_manager.LogInsert(ItemPointer(table->GetTileGroup(0)->GetTileGroupId(),0), 5,1);
     log_manager.StopLoggers();
 
 
