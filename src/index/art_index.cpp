ccac3b2178dcecb94135ed4cf06a12a93a6f5f92
Min Huang <huangminchn@yahoo.com>
2017-09-26 18:10:53 -0400

    skeloton for range scan (missing expected result length)

--- src/index/art_index.cpp
+++ src/index/art_index.cpp
@@ -13,17 +13,26 @@
 namespace peloton {
 namespace index {
 
-void loadKey(TID tid, Key &key) {
+void loadKey(UNUSED_ATTRIBUTE TID tid, UNUSED_ATTRIBUTE Key &key) {
   // Store the key of the tuple into the key vector
   // Implementation is database specific
-  key.setKeyLen(sizeof(tid));
-  reinterpret_cast<uint64_t *>(&key[0])[0] = __builtin_bswap64(tid);
+
+  // TODO: recover key from tuple_pointer (recover a storage::Tuple from ItemPointer)
+//  auto &manager = catalog::Manager::GetInstance();
+//  ItemPointer *tuple_pointer = (ItemPointer *) tid;
+//  ItemPointer tuple_location = *tuple_pointer;
+//  auto tile_group = manager.GetTileGroup(tuple_location.block);
+//  auto tile_group_header = tile_group.get()->GetHeader();
+//  size_t chain_length = 0;
+//
+//
+//  key.setKeyLen(sizeof(tid));
+//  reinterpret_cast<uint64_t *>(&key[0])[0] = __builtin_bswap64(tid);
 }
 
 ArtIndex::ArtIndex(IndexMetadata *metadata)
   :  // Base class
   Index{metadata}, artTree(loadKey) {
-
   return;
 }
 
@@ -128,7 +137,36 @@ void ArtIndex::Scan(
   UNUSED_ATTRIBUTE ScanDirectionType scan_direction,
   UNUSED_ATTRIBUTE std::vector<ItemPointer *> &result,
   UNUSED_ATTRIBUTE const ConjunctionScanPredicate *csp_p) {
-  // TODO: Add your implementation here
+
+  if (csp_p->IsPointQuery() == true) {
+    const storage::Tuple *point_query_key_p = csp_p->GetPointQueryKey();
+    ScanKey(point_query_key_p, result);
+  } else if (csp_p->IsFullIndexScan() == true) {
+    if (scan_direction == ScanDirectionType::FORWARD) {
+      ScanAllKeys(result);
+    } else if (scan_direction == ScanDirectionType::BACKWARD) {
+      // TODO
+    }
+  } else {
+    // range scan
+    const storage::Tuple *low_key_p = csp_p->GetLowKey();
+    const storage::Tuple *high_key_p = csp_p->GetHighKey();
+
+    Key index_low_key, index_high_key, continue_key;
+    index_low_key.set(low_key_p->GetData(), low_key_p->GetLength());
+    index_low_key.setKeyLen(low_key_p->GetLength());
+    index_high_key.set(high_key_p->GetData(), high_key_p->GetLength());
+    index_high_key.setKeyLen(high_key_p->GetLength());
+
+    // TODO: how do I know the result length before scanning?
+//    std::size_t expected_result_length = -1;
+//    std::size_t actual_result_length = 0;
+//
+//    auto t = artTree.getThreadInfo();
+//    artTree.lookupRange(index_low_key, index_high_key, continue_key, result, expected_result_length,
+//      actual_result_length, t);
+
+  }
   return;
 }
 
