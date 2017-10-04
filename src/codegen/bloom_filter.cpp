a6f4641ff7c0462fe5d570b9e6ee36fb7c722e90
Chen Luo <luochen@umich.edu>
2017-10-04 17:53:59 -0400

    Change bloom filter to be configurable

--- src/codegen/bloom_filter.cpp
+++ src/codegen/bloom_filter.cpp
@@ -13,20 +13,35 @@
 #include "codegen/bloom_filter.h"
 #include "codegen/proxy/bloom_filter_storage_proxy.h"
 
+#include <cmath>
+
 namespace peloton {
 namespace codegen {
 //===----------------------------------------------------------------------===//
 // Static Members
 //===----------------------------------------------------------------------===//
-std::vector<Hash::HashMethod> BloomFilter::kHashFunctions = {
+const Hash::HashMethod BloomFilter::kSeedHashFuncs[2] = {
     Hash::HashMethod::Murmur3, Hash::HashMethod::Crc32};
+
+const double BloomFilter::false_positive_rate = 0.1;
 
 //===----------------------------------------------------------------------===//
 // Member Functions
 //===----------------------------------------------------------------------===//
 
-void BloomFilter::Init(CodeGen &codegen, llvm::Value *bloom_filter) {
-  codegen.Call(BloomFilterStorageProxy::Init, {bloom_filter});
+void BloomFilter::Init(CodeGen &codegen, llvm::Value *bloom_filter,
+                       uint64_t estimated_num_tuples) {
+  // Calculate the size of bloom filter and number of hashes to use based on
+  // estimated cardinalities and target false positive rate. Formula is from
+  // http://blog.michaelschmatz.com/2016/04/11/how-to-write-a-bloom-filter-cpp/
+  uint64_t num_bits = -(double)estimated_num_tuples * log(false_positive_rate) /
+                      (log(2) * log(2));
+  num_hash_funcs_ = (double)num_bits * log(2) / (double)estimated_num_tuples;
+  LOG_DEBUG("BloomFilter num_bits: %lu num_hash_funcs: %d", num_bits,
+            num_hash_funcs_);
+  PL_ASSERT(num_hash_funcs_ > 0);
+  codegen.Call(BloomFilterStorageProxy::Init,
+               {bloom_filter, codegen.Const64(num_bits)});
 }
 
 void BloomFilter::Destroy(CodeGen &codegen, llvm::Value *bloom_filter) {
@@ -35,7 +50,7 @@ void BloomFilter::Destroy(CodeGen &codegen, llvm::Value *bloom_filter) {
 
 void BloomFilter::Add(CodeGen &codegen, llvm::Value *bloom_filter,
                       const std::vector<codegen::Value> &key) const {
-  llvm::Value *num_hashes = codegen.Const32(kHashFunctions.size());
+  llvm::Value *num_hashes = codegen.Const32(num_hash_funcs_);
   llvm::Value *hashes = CalculateHashes(codegen, key);
   // Pass the hashes to the underlying bloom filter storage to mark the bits
   codegen.Call(BloomFilterStorageProxy::Add,
@@ -45,7 +60,7 @@ void BloomFilter::Add(CodeGen &codegen, llvm::Value *bloom_filter,
 llvm::Value *BloomFilter::Contains(
     CodeGen &codegen, llvm::Value *bloom_filter,
     const std::vector<codegen::Value> &key) const {
-  llvm::Value *num_hashes = codegen.Const32(kHashFunctions.size());
+  llvm::Value *num_hashes = codegen.Const32(num_hash_funcs_);
   llvm::Value *hashes = CalculateHashes(codegen, key);
   // Pass the hashes to the underlying bloom filter storage to mark the bits
   return codegen.Call(BloomFilterStorageProxy::Contains,
@@ -55,10 +70,25 @@ llvm::Value *BloomFilter::Contains(
 llvm::Value *BloomFilter::CalculateHashes(
     CodeGen &codegen, const std::vector<codegen::Value> &key) const {
   // Alloca space on stack to store the hashes
-  llvm::Value *hashes = codegen->CreateAlloca(
-      codegen.Int64Type(), codegen.Const32(kHashFunctions.size()));
-  for (unsigned i = 0; i < kHashFunctions.size(); i++) {
-    llvm::Value *hash = Hash::HashValues(codegen, key, kHashFunctions[i]);
+  llvm::Value *hashes = codegen->CreateAlloca(codegen.Int64Type(),
+                                              codegen.Const32(num_hash_funcs_));
+  std::vector<llvm::Value *> seed_hashes(2);
+  for (int i = 0; i < num_hash_funcs_; i++) {
+    llvm::Value *hash;
+    if (i <= 1) {
+      // The first two hashes are calculated using the seed hash functions.
+      hash = Hash::HashValues(codegen, key, kSeedHashFuncs[i]);
+      seed_hashes[i] = hash;
+    } else {
+      // For later hashes, we use double hashing to efficient generate hash.
+      // hash[i] = hash[0] + i * hash[1];
+      // More details can be found http://spyced.blogspot.com/2009/01/all-you-
+      // ever-wanted-to-know-about.html
+      hash = codegen->CreateAdd(
+          seed_hashes[0],
+          codegen->CreateMul(codegen.Const64(i), seed_hashes[1]));
+    }
+
     // hashes[i] = hash
     codegen->CreateStore(hash,
                          codegen->CreateInBoundsGEP(codegen.Int64Type(), hashes,
