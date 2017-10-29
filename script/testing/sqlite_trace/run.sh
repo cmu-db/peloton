23cf8e751526a0226050df51ea418c0c107830c5
Tianyu Li <tli2@andrew.cmu.edu>
2017-10-29 13:38:55 -0400

    Modify sqlite testing script to work with jenkins configuration and fetch a built version of the testing framework instead of the source code.

--- script/testing/sqlite_trace/run.sh
+++ script/testing/sqlite_trace/run.sh
@@ -2,8 +2,7 @@
 
 ./clear.sh
 
-git clone https://github.com/lixupeng/peloton-test.git
-
+wget -O- -q "https://github.com/cmu-db/peloton-test/blob/master/deploy/peloton-test.tar?raw=true" | tar x
 wget -O- -q http://pelotondb.io/files/data/sqlite-traces.tar.gz | tar xz
 
 python trace-replay.py
