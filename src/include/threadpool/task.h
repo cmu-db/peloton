946b1c95c8b257209b55b4b6aed08b941c54e8f8
Jungmin Seo <seojungmin@gmail.com>
2017-10-30 15:07:11 -0400

    Tidy up threadpool

--- src/include/threadpool/task.h
+++ src/include/threadpool/task.h
@@ -12,14 +12,6 @@
 
 #pragma once
 
-#include <mutex>
-#include <condition_variable>
-#include <vector>
-#include <memory>
-
-#include "container/lock_free_queue.h"
-#include "event.h"
-
 namespace peloton {
 namespace threadpool {
 /**
@@ -31,37 +23,28 @@ class Task {
   friend class Worker; // grant access to ExecuteTask
 
  public:
-  inline Task() {};
-  inline Task(void(*task_ptr)(void*), void* task_arg, void(*task_callback_ptr)(void*), void* task_callback_arg) :
-      task_ptr_(task_ptr), task_arg_(task_arg), task_callback_ptr_(task_callback_ptr), task_callback_arg_(task_callback_arg) {};
+  Task(void (*task_ptr)(void *), void *task_arg,
+       void (*callback_ptr)(void *), void *callback_arg) :
+      task_ptr_(task_ptr), task_arg_(task_arg),
+      callback_ptr_(callback_ptr), callback_arg_(callback_arg) {};
 
  private:
-  // Functions
-  void ExecuteTask();
+  // Run
+  void Run() {
+    task_ptr_(task_arg_);
+
+    // then call the TaskCallBack
+    callback_ptr_(callback_arg_);
+  }
 
   // Instance variables
+  void (*task_ptr_)(void *);
+  void *task_arg_;
+
   // Callback function
-  void(*task_ptr_)(void *);
-  void * task_arg_;
-  void(*task_callback_ptr_)(void *);
-  void * task_callback_arg_;
+  void (*callback_ptr_)(void *);
+  void *callback_arg_;
 };
 
-/**
- * @class TaskQueue
- * @brief A queue for user to submit task and for user to poll tasks
- */
-class TaskQueue {
- public:
-  inline TaskQueue(const size_t sz) : task_queue_(sz) {};
-void ExecutePlanWrapper(void *arg_ptr);
-  bool PollTask(std::shared_ptr<Task> &task);
-  bool IsEmpty();
-  void EnqueueTask(void(*task_ptr)(void*), void* task_arg, void(*task_callback_ptr)(void*), void* task_callback_arg);
-
- private:
-  peloton::LockFreeQueue<std::shared_ptr<Task>> task_queue_;
-};
-
-} // namespace threadpool
-} // namespace peloton
+}  // namespace threadpool
+}  // namespace peloton
