2ac396750eecf83337bf148f72aaff9ce0c6cdf8
haoxianghua <huahaoxiang@gmail.com>
2017-10-29 17:12:58 -0700

    refactor ssl and move it to protocal_handler

--- src/include/network/network_connection.h
+++ src/include/network/network_connection.h
@@ -113,15 +113,6 @@ class NetworkConnection {
   // Extracts the header of a Postgres start up packet from the read socket buffer
   static bool ReadStartupPacketHeader(Buffer &rbuf, InputPacket &rpkt);
 
-  /* Routine to deal with the first packet from the client */
-  bool ProcessInitialPacket(InputPacket* pkt);
-
-  /* Routine to deal with general Startup message */
-  bool ProcessStartupPacket(InputPacket* pkt, int32_t proto_version);
-
-  /* Routine to deal with SSL request message */
-  bool ProcessSSLRequestPacket(InputPacket *pkt);
-
   // Writes a packet's header (type, size) into the write buffer
   WriteState BufferWriteBytesHeader(OutputPacket *pkt);
 
