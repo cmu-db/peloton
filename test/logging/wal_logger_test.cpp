d4ab3e4d7c7872d43a4d604031400ace70ba7596
PauloAmora <pauloamora1111@gmail.com>
2017-11-01 14:35:33 -0300

    Begin to add tests to logger

---
+++ test/logging/wal_logger_test.cpp
@@ -0,0 +1,43 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// new_logging_test.cpp
+//
+// Identification: test/logging/wal_logger_test.cpp
+//
+// Copyright (c) 2015-17, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "common/harness.h"
+#include "logging/wal_logger.h"
+#include "catalog/catalog.h"
+#include "util/file_util.h"
+
+namespace peloton {
+namespace test {
+
+//===--------------------------------------------------------------------===//
+// Logging Tests
+//===--------------------------------------------------------------------===//
+
+class WalLoggerTests : public PelotonTest {};
+
+TEST_F(WalLoggerTests, LogWrittenTest) {
+  logging::WalLogger* logger = new logging::WalLogger(1, "/tmp/log");
+  catalog::Catalog::GetInstance();
+
+  std::vector<logging::LogRecord> rs;
+  logging::LogRecord r = logging::LogRecordFactory::CreateTupleRecord(LogRecordType::TUPLE_INSERT,ItemPointer(2,5),1,1);
+  rs.push_back(r);
+  logger->WriteTransaction(rs);
+  delete logger;
+  //log_manager.Reset();
+
+  EXPECT_TRUE(FileUtil::Exists("/tmp/log/log_1_0"));
+
+}
+
+}
+}
