4c99e6b306eb542e5043f589dd4b7287306d0190
Min Huang <huangminchn@yahoo.com>
2017-11-03 20:34:11 -0400

    remove TBB dependency in linker

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -47,7 +47,6 @@ list(APPEND Peloton_LINKER_LIBS ${PQXX_LIBRARIES})
 
 # --[ Open SSL
 list(APPEND Peloton_LINKER_LIBS "-lssl")
-list(APPEND Peloton_LINKER_LIBS "-ltbb")
 
 # --[ LLVM 3.7+
 find_package(LLVM 3.7 REQUIRED CONFIG)
