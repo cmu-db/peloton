a27582c63150d7860405dbfb3b59d6f328ec8982
mengranwo <mengranwo@gmail.com>
2017-11-10 14:21:11 -0500

    register builtin function and add operator id

--- src/include/type/types.h
+++ src/include/type/types.h
@@ -1046,7 +1046,7 @@ enum class OperatorId : uint32_t {
   Floor,
 
   // Add more operators here, before the last "Invalid" entry
-
+  like,
   Invalid
 };
 std::string OperatorIdToString(OperatorId op_id);
