8a9a0336c6574095be07e4d984ead4916985fe70
mengranwo <mengranwo@gmail.com>
2017-11-20 21:31:09 -0500

    using libunwind to print out stack trace info when throw out exception

--- script/installation/packages.sh
+++ script/installation/packages.sh
@@ -90,7 +90,7 @@ elif [[ "$DISTRO" == *"REDHAT"* ]] && [[ "${DISTRO_VER%.*}" == "7" ]]; then
             echo "The download path is required."
             exit 1
         fi
-    
+
         pushd $TMPDIR
         wget -nc --no-check-certificate "$1"
         tpath=$(basename "$1")
