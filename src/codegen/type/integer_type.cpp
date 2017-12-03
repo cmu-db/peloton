2fdee057cf1db154cfc826e8cc09a144d4ab7891
Prashanth Menon <pmenon@users.noreply.github.com>
2017-12-03 13:13:44 -0500

    Fixed unused attribute in Integer type

--- src/codegen/type/integer_type.cpp
+++ src/codegen/type/integer_type.cpp
@@ -182,10 +182,9 @@ struct Ceil : public TypeSystem::UnaryOperator {
     return Type{Integer::Instance()};
   }
 
-  Value DoWork(CodeGen __attribute__((unused))&codegen, const Value &val) const override {
-      PL_ASSERT(SupportsType(val.GetType()));
-
-    // Return result
+  Value DoWork(UNUSED_ATTRIBUTE CodeGen &codegen,
+               const Value &val) const override {
+    PL_ASSERT(SupportsType(val.GetType()));
     return val;
   }
 };
