6e61eb5795352c33d118d32cb3b96a05f7e0f48f
PauloAmora <pauloamora1111@gmail.com>
2017-12-05 13:41:18 -0300

    Adding more tests on logger

--- test/logging/wal_logger_test.cpp
+++ test/logging/wal_logger_test.cpp
@@ -29,7 +29,7 @@ namespace test {
 
 class WalLoggerTests : public PelotonTest {};
 
-TEST_F(WalLoggerTests, LogWrittenTest) {
+TEST_F(WalLoggerTests, LogInsertTest) {
   logging::WalLogger* logger = new logging::WalLogger(1, "/tmp");
   catalog::Catalog::GetInstance();
   std::vector<logging::LogRecord> rs;
@@ -56,6 +56,75 @@ TEST_F(WalLoggerTests, LogWrittenTest) {
   EXPECT_EQ(1, record_decode.ReadLong());
   EXPECT_EQ(CATALOG_DATABASE_OID, record_decode.ReadLong());
   EXPECT_EQ(COLUMN_CATALOG_OID, record_decode.ReadLong());
+  EXPECT_EQ(2, record_decode.ReadLong());
+  EXPECT_EQ(5, record_decode.ReadLong());
+
+
+}
+
+TEST_F(WalLoggerTests, LogDeleteTest) {
+  logging::WalLogger* logger = new logging::WalLogger(2, "/tmp");
+  catalog::Catalog::GetInstance();
+  std::vector<logging::LogRecord> rs;
+  logging::LogRecord r = logging::LogRecordFactory::CreateTupleRecord(
+      LogRecordType::TUPLE_DELETE, ItemPointer(2, 5), 1, 3);
+  rs.push_back(r);
+  logger->WriteTransaction(rs);
+  delete logger;
+  EXPECT_TRUE(FileUtil::Exists("/tmp/logfile_2"));
+  FileHandle f;
+  size_t buf_size = 4096;
+  std::unique_ptr<char[]> buffer(new char[buf_size]);
+  char length_buf[sizeof(int32_t)];
+  logging::LoggingUtil::OpenFile("/tmp/logfile_2", "rb", f);
+  logging::LoggingUtil::ReadNBytesFromFile(f,(void *)length_buf, 4);
+  CopySerializeInput length_decode((const void *)&length_buf, 4);
+  int length = length_decode.ReadInt();
+  logging::LoggingUtil::ReadNBytesFromFile(f, (void *)buffer.get(),
+                                          length);
+  CopySerializeInput record_decode((const void *)buffer.get(), length);
+    LogRecordType log_record_type = (LogRecordType)record_decode.ReadEnumInSingleByte();
+  EXPECT_EQ(LogRecordType::TUPLE_DELETE, log_record_type);
+  EXPECT_EQ(3, record_decode.ReadLong());
+  EXPECT_EQ(1, record_decode.ReadLong());
+  EXPECT_EQ(CATALOG_DATABASE_OID, record_decode.ReadLong());
+  EXPECT_EQ(COLUMN_CATALOG_OID, record_decode.ReadLong());
+  EXPECT_EQ(2, record_decode.ReadLong());
+  EXPECT_EQ(5, record_decode.ReadLong());
+
+
+}
+
+TEST_F(WalLoggerTests, LogUpdateTest) {
+  logging::WalLogger* logger = new logging::WalLogger(3, "/tmp");
+  catalog::Catalog::GetInstance();
+  std::vector<logging::LogRecord> rs;
+  logging::LogRecord r = logging::LogRecordFactory::CreateTupleRecord(
+      LogRecordType::TUPLE_UPDATE, ItemPointer(2, 5), 1, 3);
+  r.SetOldItemPointer(ItemPointer(2,4));
+  rs.push_back(r);
+  logger->WriteTransaction(rs);
+  delete logger;
+  EXPECT_TRUE(FileUtil::Exists("/tmp/logfile_3"));
+  FileHandle f;
+  size_t buf_size = 4096;
+  std::unique_ptr<char[]> buffer(new char[buf_size]);
+  char length_buf[sizeof(int32_t)];
+  logging::LoggingUtil::OpenFile("/tmp/logfile_3", "rb", f);
+  logging::LoggingUtil::ReadNBytesFromFile(f,(void *)length_buf, 4);
+  CopySerializeInput length_decode((const void *)&length_buf, 4);
+  int length = length_decode.ReadInt();
+  logging::LoggingUtil::ReadNBytesFromFile(f, (void *)buffer.get(),
+                                          length);
+  CopySerializeInput record_decode((const void *)buffer.get(), length);
+    LogRecordType log_record_type = (LogRecordType)record_decode.ReadEnumInSingleByte();
+  EXPECT_EQ(LogRecordType::TUPLE_UPDATE, log_record_type);
+  EXPECT_EQ(3, record_decode.ReadLong());
+  EXPECT_EQ(1, record_decode.ReadLong());
+  EXPECT_EQ(CATALOG_DATABASE_OID, record_decode.ReadLong());
+  EXPECT_EQ(COLUMN_CATALOG_OID, record_decode.ReadLong());
+  EXPECT_EQ(2, record_decode.ReadLong());
+  EXPECT_EQ(4, record_decode.ReadLong());
   EXPECT_EQ(2, record_decode.ReadLong());
   EXPECT_EQ(5, record_decode.ReadLong());
 
