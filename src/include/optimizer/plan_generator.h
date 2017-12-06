62415fe92d128e8ddbbdd76e5cb7c939ccbe8cd4
chenboy <411468452@qq.com>
2017-12-06 01:55:01 +0000

    Add interface for plan generation

---
+++ src/include/optimizer/plan_generator.h
@@ -0,0 +1,107 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// operator_to_plan_transformer.h
+//
+// Identification: src/include/optimizer/operator_to_plan_transformer.h
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "optimizer/operator_visitor.h"
+#include "properties.h"
+
+namespace peloton {
+
+namespace planner {
+class AbstractPlan;
+class HashJoinPlan;
+class NestedLoopJoinPlan;
+class ProjectionPlan;
+class SeqScanPlan;
+class AggregatePlan;
+}
+
+namespace optimizer {
+class OperatorExpression;
+}
+
+namespace optimizer {
+
+class PlanGenerator : public OperatorVisitor {
+ public:
+  PlanGenerator();
+
+  std::unique_ptr<planner::AbstractPlan> ConvertOpExpression(
+      std::shared_ptr<OperatorExpression> op,
+      std::shared_ptr<PropertySet> required_props,
+      std::vector<expression::AbstractExpression *> required_cols,
+      std::vector<expression::AbstractExpression *> output_cols,
+      std::vector<std::unique_ptr<planner::AbstractPlan>> &children_plans,
+      std::vector<ExprMap *> children_expr_map);
+
+  void Visit(const DummyScan *) override;
+
+  void Visit(const PhysicalSeqScan *) override;
+
+  void Visit(const PhysicalIndexScan *) override;
+
+  void Visit(const QueryDerivedScan *) override;
+
+  void Visit(const PhysicalProject *) override;
+
+  void Visit(const PhysicalOrderBy *) override;
+
+  void Visit(const PhysicalLimit *) override;
+
+  void Visit(const PhysicalFilter *) override;
+
+  void Visit(const PhysicalInnerNLJoin *) override;
+
+  void Visit(const PhysicalLeftNLJoin *) override;
+
+  void Visit(const PhysicalRightNLJoin *) override;
+
+  void Visit(const PhysicalOuterNLJoin *) override;
+
+  void Visit(const PhysicalInnerHashJoin *) override;
+
+  void Visit(const PhysicalLeftHashJoin *) override;
+
+  void Visit(const PhysicalRightHashJoin *) override;
+
+  void Visit(const PhysicalOuterHashJoin *) override;
+
+  void Visit(const PhysicalInsert *) override;
+
+  void Visit(const PhysicalInsertSelect *) override;
+
+  void Visit(const PhysicalDelete *) override;
+
+  void Visit(const PhysicalUpdate *) override;
+
+  void Visit(const PhysicalHashGroupBy *) override;
+
+  void Visit(const PhysicalSortGroupBy *) override;
+
+  void Visit(const PhysicalDistinct *) override;
+
+  void Visit(const PhysicalAggregate *) override;
+
+ private:
+  std::vector<std::shared_ptr<PropertySet>> *required_input_props_;
+
+  std::shared_ptr<PropertySet> required_props_;
+  std::vector<expression::AbstractExpression *> required_cols_;
+  std::vector<expression::AbstractExpression *> output_cols_;
+  std::vector<std::unique_ptr<planner::AbstractPlan>> children_plans_;
+  std::vector<ExprMap *> children_expr_map_;
+  std::unique_ptr<planner::AbstractPlan> output_plan_;
+};
+
+}  // namespace optimizer
+}  // namespace peloton
---
+++ src/optimizer/plan_generator.cpp
@@ -0,0 +1,116 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// operator_to_plan_transformer.cpp
+//
+// Identification: src/optimizer/operator_to_plan_transformer.cpp
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "optimizer/plan_generator.h"
+
+#include "optimizer/operator_expression.h"
+#include "planner/aggregate_plan.h"
+#include "planner/delete_plan.h"
+#include "planner/hash_join_plan.h"
+#include "planner/hash_plan.h"
+#include "planner/index_scan_plan.h"
+#include "planner/insert_plan.h"
+#include "planner/limit_plan.h"
+#include "planner/nested_loop_join_plan.h"
+#include "planner/order_by_plan.h"
+#include "planner/projection_plan.h"
+#include "planner/seq_scan_plan.h"
+#include "planner/update_plan.h"
+#include "settings/settings_manager.h"
+#include "storage/data_table.h"
+
+using std::vector;
+using std::make_pair;
+using std::string;
+using std::to_string;
+using std::unique_ptr;
+using std::shared_ptr;
+using std::move;
+using std::make_tuple;
+using std::make_pair;
+using std::pair;
+
+namespace peloton {
+namespace optimizer {
+
+PlanGenerator::PlanGenerator() {}
+
+unique_ptr<planner::AbstractPlan> PlanGenerator::ConvertOpExpression(
+    shared_ptr<OperatorExpression> op, shared_ptr<PropertySet> required_props,
+    vector<expression::AbstractExpression *> required_cols,
+    vector<expression::AbstractExpression *> output_cols,
+    vector<unique_ptr<planner::AbstractPlan>> &children_plans,
+    vector<ExprMap *> children_expr_map) {
+  required_props_ = required_props;
+  required_cols_ = required_cols;
+  output_cols_ = output_cols;
+  children_plans_ = move(children_plans);
+  children_expr_map_ = children_expr_map;
+  op->Op().Accept(this);
+  return move(output_plan_);
+}
+
+void PlanGenerator::Visit(const DummyScan *) {
+  // DummyScan is used in case of SELECT without FROM so that enforcer
+  // can enforce a PhysicalProjection on top of DummyScan to generate correct
+  // result. But here, no need to translate DummyScan to any physical plan.
+  output_plan_ = nullptr;
+}
+
+void PlanGenerator::Visit(const PhysicalSeqScan *) {}
+
+void PlanGenerator::Visit(const PhysicalIndexScan *) {}
+
+void PlanGenerator::Visit(const QueryDerivedScan *) {}
+
+void PlanGenerator::Visit(const PhysicalProject *) {}
+
+void PlanGenerator::Visit(const PhysicalLimit *) {}
+
+void PlanGenerator::Visit(const PhysicalOrderBy *) {}
+
+void PlanGenerator::Visit(const PhysicalHashGroupBy *) {}
+
+void PlanGenerator::Visit(const PhysicalSortGroupBy *) {}
+
+void PlanGenerator::Visit(const PhysicalAggregate *) {}
+
+void PlanGenerator::Visit(const PhysicalDistinct *) {}
+
+void PlanGenerator::Visit(const PhysicalFilter *) {}
+
+void PlanGenerator::Visit(const PhysicalInnerNLJoin *) {}
+
+void PlanGenerator::Visit(const PhysicalLeftNLJoin *) {}
+
+void PlanGenerator::Visit(const PhysicalRightNLJoin *) {}
+
+void PlanGenerator::Visit(const PhysicalOuterNLJoin *) {}
+
+void PlanGenerator::Visit(const PhysicalInnerHashJoin *) {}
+
+void PlanGenerator::Visit(const PhysicalLeftHashJoin *) {}
+
+void PlanGenerator::Visit(const PhysicalRightHashJoin *) {}
+
+void PlanGenerator::Visit(const PhysicalOuterHashJoin *) {}
+
+void PlanGenerator::Visit(const PhysicalInsert *) {}
+
+void PlanGenerator::Visit(const PhysicalInsertSelect *) {}
+
+void PlanGenerator::Visit(const PhysicalDelete *) {}
+
+void PlanGenerator::Visit(const PhysicalUpdate *) {}
+
+}  // namespace optimizer
+}  // namespace peloton
