91767f24bf526329cead7fea5115e70e5edd70b8
Tianyi Chen <tianyic1@andrew.cmu.edu>
2017-12-13 01:08:54 -0500

    implement statement cache and statement cache manager

--- src/include/common/statement_cache_manager.h
+++ src/include/common/statement_cache_manager.h
@@ -13,18 +13,19 @@
 #pragma once
 
 #include "container/cuckoo_map.h"
-#include "statement_cache.h"
+#include "common/statement_cache.h"
 
 namespace peloton {
 class StatementCacheManager {
+  typedef std::shared_ptr<StatementCache> StmtCachePtr;
  public:
   // Register a statement cache to this statement manager
   // Called by protocol handler when building up a connection
-  void RegisterStatementCache(StatementCache &stmt_cache);
+  void RegisterStatementCache(StmtCachePtr stmt_cache);
 
   // Remove the statement cache from this statement manager
   // Called by protocol handler when tearing down a connection
-  void UnRegisterStatementCache(StatementCache &stmt_cache);
+  void UnRegisterStatementCache(StmtCachePtr stmt_cache);
 
   // Notify the manager that the table schema or index 
   // is changed now.
@@ -32,13 +33,13 @@ class StatementCacheManager {
   // WARNING: 
   // Be conservative to call this function as it would block
   // the whole statement caches map and prevent them from register
-  // and unregister to the manager. It eould therefore block new 
+  // and unregister to the manager. It would therefore block new 
   // connections coming in and old connection tearing down. 
-  // However, current connection can still retrieve updated plans from their plan cache.
+  // However, current connections can still retrieve updated plans from their plan cache.
   void InvalidateTableOid(oid_t table_id);
 
 private:
   // A lock free hash map
-  CuckooMap<StatementCache, nullptr> statement_caches;
-}
+  CuckooMap<StmtCachePtr, StmtCachePtr> statement_caches_;
+};
 }  // namespace peloton
