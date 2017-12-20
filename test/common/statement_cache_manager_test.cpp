0edad37f215221041e6766609082faf99262e949
Tianyi Chen <tianyic1@andrew.cmu.edu>
2017-12-21 02:19:17 +0800

    add test case for statement cache manager

--- test/common/statement_cache_manager_test.cpp
+++ test/common/statement_cache_manager_test.cpp
@@ -22,5 +22,36 @@ namespace test {
 // Tests for both statement cache
 class StatementCacheTests : public PelotonTest {};
   
+  TEST_F(StatementCacheTests, CorrectnessTest) {
+    // Register the statement cache to the statement cache manager
+    auto statement_cache_manager = std::make_shared<StatementCacheManager>();
+    auto statement_cache = std::make_shared<StatementCache>();
+    statement_cache_manager->RegisterStatementCache(statement_cache);
+    std::set<oid_t> ref_table = {0};
+
+    std::string string_name = "foo";
+    std::string query = "bar";
+    auto statement = std::make_shared<Statement>(string_name, query);
+    statement->SetReferencedTables(ref_table);
+
+    EXPECT_TRUE(!statement->GetNeedsPlan());
+    statement_cache->AddStatement(statement);
+
+    // Invalidate table oid 0
+    statement_cache_manager->InvalidateTableOid(0);
+
+    // The plan need to replan now
+    statement = statement_cache->GetStatement(string_name);
+    EXPECT_TRUE(statement->GetNeedsPlan());
+
+    // Unregister the statement cache and invalidate again
+    statement->SetNeedsPlan(false);
+    statement_cache_manager->UnRegisterStatementCache(statement_cache);
+    statement_cache_manager->InvalidateTableOid(0);
+    statement = statement_cache->GetStatement(string_name);
+    // This one should not be affected, since the cache is already un-registered
+    EXPECT_TRUE(!statement->GetNeedsPlan());
+  }
+
 }
 }
