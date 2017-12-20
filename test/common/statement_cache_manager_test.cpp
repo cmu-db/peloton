b4ac4e6af65f61a380016afce421e44870b56c16
Tianyi Chen <tianyic1@andrew.cmu.edu>
2017-12-20 11:31:11 +0800

    add statement cache manager file

---
+++ test/common/statement_cache_manager_test.cpp
@@ -0,0 +1,26 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// statement_cache_test.cpp
+//
+// Identification: test/common/statement_cache_manager_test.cpp
+//
+// Copyright (c) 2015-17, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "common/statement_cache_manager.h"
+#include "common/statement_cache.h"
+#include "common/statement.h"
+
+#include "common/harness.h"
+
+
+namespace peloton {
+namespace test {
+// Tests for both statement cache
+class StatementCacheTests : public PelotonTest {};
+  
+}
+}
