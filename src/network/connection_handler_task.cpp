a47638572a839867b59f857ee0f0e9d03c5076ec
Tianyu Li <tli2@andrew.cmu.edu>
2017-12-21 12:33:21 -0500

    Add documentation for tasks

--- src/network/connection_handler_task.cpp
+++ src/network/connection_handler_task.cpp
@@ -2,9 +2,9 @@
 //
 //                         Peloton
 //
-// network_worker_thread.cpp
+// connection_handler_task.cpp
 //
-// Identification: src/network/network_worker_thread.cpp
+// Identification: src/network/connection_handler_task.cpp
 //
 // Copyright (c) 2015-17, Carnegie Mellon University Database Group
 //
@@ -16,18 +16,13 @@
 namespace peloton {
 namespace network {
 
-/*
-* The worker thread creates a pipe for master-worker communication on
-* constructor.
-*/
 ConnectionHandlerTask::ConnectionHandlerTask(const int task_id)
-    : NotifiableTask(task_id), new_conn_queue(QUEUE_SIZE) {
+    : NotifiableTask(task_id), new_conn_queue_(QUEUE_SIZE) {
   int fds[2];
   if (pipe(fds)) {
     LOG_ERROR("Can't create notify pipe to accept connections");
     exit(1);
   }
-  new_conn_receive_fd = fds[0];
   new_conn_send_fd_ = fds[1];
   RegisterEvent(fds[0], EV_READ|EV_PERSIST, CallbackUtil::OnNewConnectionDispatch, this);
   struct timeval one_second = {1, 0};
@@ -35,9 +30,7 @@ ConnectionHandlerTask::ConnectionHandlerTask(const int task_id)
 }
 
 void ConnectionHandlerTask::Notify(int conn_fd) {
-  std::shared_ptr<NewConnQueueItem> item =
-      std::make_shared<NewConnQueueItem>(conn_fd, EV_READ|EV_PERSIST, ConnState::CONN_READ);
-  new_conn_queue.Enqueue(item);
+  new_conn_queue_.Enqueue(conn_fd);
   char buf[1];
   buf[0] = 'c';
   if (write(new_conn_send_fd_, buf, 1) != 1) {
