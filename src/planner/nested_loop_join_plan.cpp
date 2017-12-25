5defbb807873a767025231d4d8e8f71aed34c6a6
Prashanth Menon <pmenon@cs.cmu.edu>
2017-12-25 11:45:32 -0500

    Removed unused NLJ plan constructor

--- src/planner/nested_loop_join_plan.cpp
+++ src/planner/nested_loop_join_plan.cpp
@@ -11,14 +11,9 @@
 //===----------------------------------------------------------------------===//
 
 #include <memory>
-#include <string>
 #include <vector>
 
 #include "planner/nested_loop_join_plan.h"
-
-#include "common/internal_types.h"
-#include "expression/abstract_expression.h"
-#include "planner/project_info.h"
 
 namespace peloton {
 namespace planner {
@@ -27,21 +22,31 @@ NestedLoopJoinPlan::NestedLoopJoinPlan(
     JoinType join_type,
     std::unique_ptr<const expression::AbstractExpression> &&predicate,
     std::unique_ptr<const ProjectInfo> &&proj_info,
-    std::shared_ptr<const catalog::Schema> &proj_schema)
-    : AbstractJoinPlan(join_type, std::move(predicate), std::move(proj_info),
-                       proj_schema) {}
-
-NestedLoopJoinPlan::NestedLoopJoinPlan(
-    JoinType join_type,
-    std::unique_ptr<const expression::AbstractExpression> &&predicate,
-    std::unique_ptr<const ProjectInfo> &&proj_info,
     std::shared_ptr<const catalog::Schema> &proj_schema,
-    std::vector<oid_t> &join_column_ids_left,
-    std::vector<oid_t> &join_column_ids_right)
+    const std::vector<oid_t> &join_column_ids_left,
+    const std::vector<oid_t> &join_column_ids_right)
     : AbstractJoinPlan(join_type, std::move(predicate), std::move(proj_info),
                        proj_schema) {
   join_column_ids_left_ = join_column_ids_left;
   join_column_ids_right_ = join_column_ids_right;
+}
+
+void NestedLoopJoinPlan::HandleSubplanBinding(
+    UNUSED_ATTRIBUTE bool from_left,
+    UNUSED_ATTRIBUTE const BindingContext &ctx) {}
+
+std::unique_ptr<AbstractPlan> NestedLoopJoinPlan::Copy() const {
+  std::unique_ptr<const expression::AbstractExpression> predicate_copy(
+      GetPredicate()->Copy());
+
+  std::shared_ptr<const catalog::Schema> schema_copy(
+      catalog::Schema::CopySchema(GetSchema()));
+
+  NestedLoopJoinPlan *new_plan = new NestedLoopJoinPlan(
+      GetJoinType(), std::move(predicate_copy), GetProjInfo()->Copy(),
+      schema_copy, join_column_ids_left_, join_column_ids_right_);
+
+  return std::unique_ptr<AbstractPlan>(new_plan);
 }
 
 }  // namespace planner
