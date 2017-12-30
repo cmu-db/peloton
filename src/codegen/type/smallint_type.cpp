0e7f6460582d0a40dc9d34f8d0d2fe7b8a6b2373
saatviks <saatviks@andrew.cmu.edu>
2017-12-30 03:28:25 -0800

    Minor type related fixes/additions to Floor function

--- src/codegen/type/smallint_type.cpp
+++ src/codegen/type/smallint_type.cpp
@@ -187,6 +187,23 @@ struct Negate : public TypeSystem::UnaryOperatorHandleNull {
   }
 };
 
+// Floor
+struct Floor : public TypeSystem::UnaryOperatorHandleNull {
+  bool SupportsType(const Type &type) const override {
+    return type.GetSqlType() == SmallInt::Instance();
+  }
+
+  Type ResultType(UNUSED_ATTRIBUTE const Type &val_type) const override {
+    return Type{SmallInt::Instance()};
+  }
+
+  Value Impl(UNUSED_ATTRIBUTE CodeGen &codegen,
+             const Value &val) const override {
+    PL_ASSERT(SupportsType(val.GetType()));
+    return val;
+  }
+};
+
 // Ceiling
 struct Ceil : public TypeSystem::UnaryOperatorHandleNull {
   bool SupportsType(const Type &type) const override {
@@ -448,9 +465,11 @@ static std::vector<TypeSystem::ComparisonInfo> kComparisonTable = {
 // Unary operators
 static Negate kNegOp;
 static Ceil kCeilOp;
+static Floor kFloorOp;
 static std::vector<TypeSystem::UnaryOpInfo> kUnaryOperatorTable = {
     {OperatorId::Negation, kNegOp},
-    {OperatorId::Ceil, kCeilOp}};
+    {OperatorId::Ceil, kCeilOp},
+    {OperatorId::Floor, kFloorOp}};
 
 // Binary operations
 static Add kAddOp;
