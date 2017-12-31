a04adc5835d27ae79482176c214965851074f97a
Prashanth Menon <pmenon@cs.cmu.edu>
2017-12-31 14:22:15 +0530

    Use placement new for values to avoid calling destructor and copy

--- src/codegen/values_runtime.cpp
+++ src/codegen/values_runtime.cpp
@@ -12,6 +12,7 @@
 
 #include "codegen/values_runtime.h"
 
+#include "type/value.h"
 #include "type/type_util.h"
 #include "type/value_factory.h"
 #include "type/value_peeker.h"
@@ -19,62 +20,72 @@
 namespace peloton {
 namespace codegen {
 
+namespace {
+
+inline void SetValue(type::Value *val_ptr, type::Value &&val) {
+  new (val_ptr) type::Value(val);
+}
+
+}  // namespace
+
 void ValuesRuntime::OutputBoolean(char *values, uint32_t idx, bool val,
                                   bool is_null) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
+  auto *vals = reinterpret_cast<type::Value *>(values);
   if (is_null) {
-    vals[idx] = type::ValueFactory::GetNullValueByType(type::TypeId::BOOLEAN);
+    SetValue(&vals[idx],
+             type::ValueFactory::GetNullValueByType(type::TypeId::BOOLEAN));
   } else {
-    vals[idx] = type::ValueFactory::GetBooleanValue(val);
+    SetValue(&vals[idx], type::ValueFactory::GetBooleanValue(val));
   }
 }
 
 void ValuesRuntime::OutputTinyInt(char *values, uint32_t idx, int8_t val) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  vals[idx] = type::ValueFactory::GetTinyIntValue(val);
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  SetValue(&vals[idx], type::ValueFactory::GetTinyIntValue(val));
 }
 
 void ValuesRuntime::OutputSmallInt(char *values, uint32_t idx, int16_t val) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  vals[idx] = type::ValueFactory::GetSmallIntValue(val);
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  SetValue(&vals[idx], type::ValueFactory::GetSmallIntValue(val));
 }
 
 void ValuesRuntime::OutputInteger(char *values, uint32_t idx, int32_t val) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  vals[idx] = type::ValueFactory::GetIntegerValue(val);
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  SetValue(&vals[idx], type::ValueFactory::GetIntegerValue(val));
 }
 
 void ValuesRuntime::OutputBigInt(char *values, uint32_t idx, int64_t val) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  vals[idx] = type::ValueFactory::GetBigIntValue(val);
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  SetValue(&vals[idx], type::ValueFactory::GetBigIntValue(val));
 }
 
 void ValuesRuntime::OutputDate(char *values, uint32_t idx, int32_t val) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  vals[idx] = type::ValueFactory::GetDateValue(val);
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  SetValue(&vals[idx], type::ValueFactory::GetDateValue(val));
 }
 
 void ValuesRuntime::OutputTimestamp(char *values, uint32_t idx, int64_t val) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  vals[idx] = type::ValueFactory::GetTimestampValue(val);
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  SetValue(&vals[idx], type::ValueFactory::GetTimestampValue(val));
 }
 
 void ValuesRuntime::OutputDecimal(char *values, uint32_t idx, double val) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  vals[idx] = type::ValueFactory::GetDecimalValue(val);
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  SetValue(&vals[idx], type::ValueFactory::GetDecimalValue(val));
 }
 
-void ValuesRuntime::OutputVarchar(char *values, uint32_t idx, char *str,
-                                  UNUSED_ATTRIBUTE uint32_t len) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  vals[idx] = type::ValueFactory::GetVarcharValue(str, len, false);
+void ValuesRuntime::OutputVarchar(char *values, uint32_t idx, const char *str,
+                                  uint32_t len) {
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  SetValue(&vals[idx], type::ValueFactory::GetVarcharValue(str, len, false));
 }
 
-void ValuesRuntime::OutputVarbinary(char *values, uint32_t idx, char *ptr,
+void ValuesRuntime::OutputVarbinary(char *values, uint32_t idx, const char *ptr,
                                     uint32_t len) {
-  type::Value *vals = reinterpret_cast<type::Value *>(values);
-  const auto *bin_ptr = reinterpret_cast<unsigned char *>(ptr);
-  vals[idx] = type::ValueFactory::GetVarbinaryValue(bin_ptr, len, false);
+  auto *vals = reinterpret_cast<type::Value *>(values);
+  const auto *bin_ptr = reinterpret_cast<const unsigned char *>(ptr);
+  SetValue(&vals[idx],
+           type::ValueFactory::GetVarbinaryValue(bin_ptr, len, false));
 }
 
 int32_t ValuesRuntime::CompareStrings(const char *str1, uint32_t len1,
