cmu-db/peloton

commit eccc8b474687cb60abe3bf74c4878a1bbe8da5c1

Fix packages.sh building on Ubuntu 15.x and 16.x

script/installation/packages.sh
@@ -31,11 +31,25 @@ if [ "$DISTRO" = "UBUNTU" ]; then
         sudo apt-get update -qq
     fi
 
-    sudo apt-get -qq --force-yes --ignore-missing -y install \
+    # Fix for cmake3 name change in Ubuntu 15.x and 16.x plus --force-yes deprecation 
+    CMAKE_NAME="cmake3"
+    FORCE_Y="--force-yes"
+    MAJOR_VERSION=$(echo "$DISTRO_VER" | cut -d '.' -f 1)
+    for version in "15" "16"
+    do
+       if [ "$MAJOR_VER" = "$version" ]
+       then
+          FORCE_Y=""
+          CMAKE_NAME="cmake"
+          break
+       fi
+    done
+
+    sudo apt-get -qq $FORCE_Y --ignore-missing -y install \
         git \
         g++ \
         clang-3.7 \
-        cmake3 \
+        $CMAKE_NAME \
         libgflags-dev \
         libprotobuf-dev \
         protobuf-compiler \

commit d85192fb7e2747dace080a9f457c4b4ee79fe4c6

Fix typo introduced by commit eccc8b47468

script/installation/packages.sh
@@ -34,7 +34,7 @@ if [ "$DISTRO" = "UBUNTU" ]; then
     # Fix for cmake3 name change in Ubuntu 15.x and 16.x plus --force-yes deprecation 
     CMAKE_NAME="cmake3"
     FORCE_Y="--force-yes"
-    MAJOR_VERSION=$(echo "$DISTRO_VER" | cut -d '.' -f 1)
+    MAJOR_VER=$(echo "$DISTRO_VER" | cut -d '.' -f 1)
     for version in "15" "16"
     do
        if [ "$MAJOR_VER" = "$version" ]