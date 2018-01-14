5f6a23c1e456f56635a8576dcb3649b5b07e6727
Sivaprasad Sudhir <sivaprasad2626@gmail.com>
2018-01-14 11:29:34 -0800

    Minor fixes in the test cases

--- test/sql/timestamp_functions_sql_test.cpp
+++ test/sql/timestamp_functions_sql_test.cpp
@@ -39,46 +39,36 @@ TEST_F(TimestampFunctionsSQLTest, DateTruncTest) {
 
   txn_manager.CommitTransaction(txn);
 
-  // Fetch values from the table
+  std::string test_query;
+  std::vector<std::string> expected;
   std::vector<ResultValue> result;
-  std::vector<FieldInfo> tuple_descriptor;
-  std::string error_message;
-  int rows_affected;
 
   // wrong argument type
-  std::string test_query = "select date_trunc(123, value) from foo;";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  EXPECT_EQ(result.size(), 0);
+  test_query = "select date_trunc(123, value) from foo;";
+  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result);
+  EXPECT_EQ(0, result.size());
 
   // wrong DatePartType
   test_query = "select date_trunc('abc', value) from foo;";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  EXPECT_EQ(result.size(), 0);
+  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result);
+  EXPECT_EQ(0, result.size());
 
   // Test a few end-to-end DatePartType strings. The correctness of the function
   // is already tested in the unit tests.
   test_query = "select date_trunc('minute', value) from foo;";
-  std::string expected = "2016-12-07 13:26:00.000000-05";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  auto query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
+  expected = {"2016-12-07 13:26:00.000000-05"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   test_query = "select date_trunc('DAY', value) from foo;";
-  expected = "2016-12-07 00:00:00.000000-05";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
+  expected = {"2016-12-07 00:00:00.000000-05"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   test_query = "select date_trunc('CenTuRy', value) from foo;";
-  expected = "2001-01-01 00:00:00.000000-05";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
+  expected = {"2001-01-01 00:00:00.000000-05"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   // free the database just created
   txn = txn_manager.BeginTransaction();
@@ -103,74 +93,55 @@ TEST_F(TimestampFunctionsSQLTest, DatePartTest) {
 
   txn_manager.CommitTransaction(txn);
 
-  // Fetch values from the table
+  std::string test_query;
+  std::vector<std::string> expected;
   std::vector<ResultValue> result;
-  std::vector<FieldInfo> tuple_descriptor;
-  std::string error_message;
-  int rows_affected;
 
   // wrong argument type
-  std::string test_query = "select date_part('123', value) from foo;";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  EXPECT_EQ(result.size(), 0);
+  test_query = "select date_part('123', value) from foo;";
+  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result);
+  EXPECT_EQ(0, result.size());
 
   // wrong DatePartType
   test_query = "select date_part('abc', value) from foo;";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  EXPECT_EQ(result.size(), 0);
+  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result);
+  EXPECT_EQ(0, result.size());
 
   test_query = "select extract(abc from value) from foo;";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  EXPECT_EQ(result.size(), 0);
+  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result);
+  EXPECT_EQ(0, result.size());
 
   // Test a few end-to-end DatePartType strings. The correctness of the function
   // is already tested in the unit tests.
   test_query = "select date_part('minute', value) from foo;";
-  std::string expected = "26";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  auto query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
+  expected = {"26"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   test_query = "select extract(minute from value) from foo;";
-  expected = "26";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
-
+  expected = {"26"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   test_query = "select date_part('DAY', value) from foo;";
-  expected = "7";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
+  expected = {"7"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   test_query = "select extract(DAY from value) from foo;";
-  expected = "7";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
-
+  expected = {"7"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   test_query = "select date_part('CenTuRy', value) from foo;";
-  expected = "21";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
+  expected = {"21"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   test_query = "select extract(CenTuRy from value) from foo;";
-  expected = "21";
-  TestingSQLUtil::ExecuteSQLQuery(test_query.c_str(), result, tuple_descriptor,
-                                  rows_affected, error_message);
-  query_result = TestingSQLUtil::GetResultValueAsString(result, 0);
-  EXPECT_EQ(query_result, expected);
+  expected = {"21"};
+  TestingSQLUtil::ExecuteSQLQueryAndCheckResult(test_query.c_str(), expected,
+                                                false);
 
   // free the database just created
   txn = txn_manager.BeginTransaction();
