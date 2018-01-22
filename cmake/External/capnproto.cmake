fb2b1efd3d6e17aa74371d52fc878c821ce99940
Pranav Kant <pranav913@gmail.com>
2018-01-22 23:47:36 +0530

    Fix superfluous and incorrect capnp cmake exports
    Some of these variables are already exported by cmake files in capnproto
    in third_party. And correctly.
    
    The problem I see in my machine is that capnproto writes the static libs
    to prefix/lib64/ but hardcoded CAPNP_LIBRARIES says that they are in the
    prefix/lib/ directory. This makes peloton fail to link with capnp static
    libs.
    
    Let capnp cmake files in third_party handle it correctly.

--- cmake/External/capnproto.cmake
+++ cmake/External/capnproto.cmake
@@ -44,21 +44,6 @@ if (NOT __CAPNP_INCLUDED) # guard against multiple includes
       LOG_INSTALL 1
       )
 
-    set(CAPNP_FOUND TRUE)
-    set(CAPNP_EXECUTABLE ${capnp_INSTALL}/bin/capnp)
-    set(CAPNPC_CXX_EXECUTABLE ${capnp_INSTALL}/bin/capnpc-c++)
-    set(CAPNP_INCLUDE_DIRS ${capnp_INSTALL}/include)
-    set(CAPNP_INCLUDE_DIRECTORY ${capnp_INSTALL}/include)
-    set(CAPNP_LIBRARIES
-      ${capnp_INSTALL}/lib/libcapnp-rpc.a
-      ${capnp_INSTALL}/lib/libcapnp.a
-      ${capnp_INSTALL}/lib/libkj-async.a
-      ${capnp_INSTALL}/lib/libkj.a
-      ${CMAKE_THREAD_LIBS_INIT}
-      )
-    set(CAPNP_LIBRARY_DIRS ${capnp_INSTALL}/lib)
-    set(CAPNP_EXTERNAL TRUE)
-
     list(APPEND external_project_dependencies capnp)
 
   endif()
