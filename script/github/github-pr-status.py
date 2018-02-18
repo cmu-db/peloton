3368151f0eb76abecc6b100d7cf8ecfa637828ab
Andy Pavlo <pavlo@cs.brown.edu>
2018-02-17 19:45:37 -0500

    Updated Github PR checker script to support sending reviewer reminder emails

--- script/github/github-pr-status.py
+++ script/github/github-pr-status.py
@@ -15,6 +15,9 @@ import json
 import logging
 import requests
 import csv
+import time
+import pytz
+import dateutil.parser
 from StringIO import StringIO
 from datetime import datetime
 from pprint import pprint
@@ -42,19 +45,174 @@ LOG.setLevel(logging.INFO)
 
 GITHUB_USER = 'cmu-db'
 GITHUB_REPO = 'peloton'
+GITHUB_REPO_API_URL = "https://api.github.com/repos/%s/%s" % (GITHUB_USER, GITHUB_REPO)
 
 CACHE_FILEPATH = "/tmp/github-cache/%s.cache"
 
 EMAIL_FROM = "no-reply@pelotondb.io"
+EMAIL_PREFIX = "[Peloton] "
+
+FIRST_COMMIT = '35823950d500314811212282bd68c101e34b9a06'
+
+
+EMAILS_TO_IGNORE = [
+    "noreply.github.com",
+    "macbook-pro.local",
+    "cc.cmu.edu",
+    "vagrant.vm",
+]
 
 # ==============================================
 # pr_format
 # ==============================================
 def pr_format(pr):
     assert not pr is None
+    
     ret = "%5s - %s [%s]\n" % ('#'+str(pr.number), pr.title, pr.user["login"])
+    ret += "        Last Updated: %s\n" % pr.updated_at.strftime("%Y-%m-%d")
     ret += "        %s" % (pr.html_url)
+    
     return (ret)
+## DEF
+
+# ==============================================
+# get_url
+# ==============================================
+def get_url(url, name, read_cache=False):
+    data = None
+    cache_file = CACHE_FILEPATH % name
+    if read_cache and os.path.exists(cache_file):
+        LOG.debug("Read Cached File '%s' for '%s'" % (cache_file, url))
+        with open(cache_file, "r") as fd:
+            data = fd.read()
+    else:
+        LOG.debug("Downloading data from %s" % url)
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise Exception("Failed to download '%s'" % url)
+        data = response.content
+        LOG.debug("Creating cached file '%s'" % cache_file)
+        with open(cache_file, "w") as fd:
+            fd.write(data)
+    ## IF
+    return (data)
+## DEF
+
+# ==============================================
+# get_user_emails
+# ==============================================
+def get_user_emails(gh, email_file, cache=False):
+    users = { }
+    newest_commit = None
+    load_all = True
+    
+    if os.path.exists(email_file):
+        LOG.debug("Reading Email Mapping '%s'" % (email_file))
+        with open(email_file, "r") as fd:
+            for row in csv.reader(fd):
+                # The first line is the date of the newest
+                # commit that we have seen. If we end up seeing
+                # a commit that is older than this, then we can
+                # stop fetching things from Github
+                if newest_commit is None:
+                    newest_commit = datetime.fromtimestamp(int(row[0]))
+                    newest_commit = newest_commit.replace(tzinfo=pytz.UTC)
+                    
+                # Otherwise then each line in the CSV
+                # will contain the Github login in the first
+                # column and then a variable-length number of 
+                # columns for all the emails that the account 
+                # has that we found.
+                else:
+                    users[row[0]] = row[1:]
+        ## WITH
+        assert not newest_commit is None
+        load_all = False
+    ## IF
+
+    # Loop through until we get nothing or find a commit that
+    # is older than our newest_commit
+    page = 1
+    missing_author = 0
+    stop = False
+    while stop == False:
+        url = GITHUB_REPO_API_URL + "/commits?page=%d" % page
+        try:
+            contents = get_url(url, 'commits%d'%page, read_cache=cache)
+        except:
+            stop = True
+            break
+            
+        if len(contents) > 0:
+            #contents = json.loads(contents)
+            #url = GITHUB_REPO_API_URL + "/compare/" + FIRST_COMMIT + "..." + contents["object"]["sha"]
+            #contents = get_url(url, 'compare', read_cache=cache)
+            github_data = json.loads(contents)
+            #pprint (github_data)
+            
+            for c in github_data:
+                if c is None: continue
+                #pprint(c)
+                #sys.exit(0)
+            
+                if not "author" in c or c["author"] is None:
+                    #pprint(c)
+                    #sys.exit(1)
+                    missing_author = missing_author + 1
+                    continue
+                if not "login" in c["author"]:
+                    #pprint(c)
+                    #sys.exit(1)
+                    missing_author = missing_author + 1
+                    continue
+                
+                login = c["author"]["login"]
+                email = c["commit"]["author"]["email"]
+                
+                # Get the commit timestamp
+                commit_ts = dateutil.parser.parse(c["commit"]["author"]["date"])
+                if newest_commit is None or newest_commit < commit_ts:
+                    newest_commit = commit_ts
+                    LOG.debug("Newest Commit TS: %s" % str(commit_ts))
+                elif load_all == False and newest_commit > commit_ts:
+                    LOG.debug("Stop gathering commits [commit:%s > newest:%s]" % (str(commit_ts), str(newest_commit)))
+                    stop = True
+                    break
+                ## IF
+                
+                ignore = False
+                for e in EMAILS_TO_IGNORE:
+                    if email.find(e) != -1:
+                        LOG.debug("Ignoring email '%s'" % email)
+                        ignore = True
+                        break
+                if ignore or email.find("@") == -1: continue
+                
+                if not login in users:
+                    users[login] = [ ]
+                if not email in users[login]:
+                    users[login].append(email)
+        ## IF
+        page = page + 1
+    ## WHILE
+    
+    LOG.debug("Saving Email Mapping '%s'" % (email_file))
+    with open(email_file, "w") as fd:
+        writer = csv.writer(fd, quoting=csv.QUOTE_ALL)
+        
+        # Write out the newest timestamp
+        if not newest_commit is None:
+            writer.writerow([int(time.mktime(newest_commit.timetuple()))])
+        else:
+            writer.writerow([0])
+        
+        for login in sorted(users.keys()):
+            if len(users[login]) == 0: continue
+            writer.writerow([ login ] + users[login])
+    ## WITH
+    
+    LOG.debug("Total Commits Missing Authors: %d" % missing_author)
+    return (users)
 ## DEF
 
 # ==============================================
@@ -62,22 +220,7 @@ def pr_format(pr):
 # ==============================================
 def get_current_czar(url, cache=False):
 
-    data = None
-    cache_file = CACHE_FILEPATH % "schedule"
-    if cache and os.path.exists(cache_file):
-        LOG.debug("Cached Schedule '%s'" % cache_file)
-        with open(cache_file, "r") as fd:
-            data = fd.read()
-    else:
-        LOG.debug("Downloading schedule from %s" % url)
-        response = requests.get(url)
-        assert response.status_code == 200, \
-            "Failed to download schedule '%s'" % url
-        data = response.content
-        LOG.debug("Creating schedule cached file '%s'" % cache_file)
-        with open(cache_file, "w") as fd:
-            fd.write(data)
-    ## IF
+    data = get_url(url, "schedule", read_cache=cache)
 
     # The CSV file should have two columns:
     #   (1) The date "Month Year"
@@ -100,6 +243,30 @@ def get_current_czar(url, cache=False):
     return (current_czar)
 ## DEF
 
+# ==============================================
+# send_email
+# ==============================================
+def send_email(args, subject, contents, send_from, send_to, send_cc=[]):
+    msg = MIMEText(contents)
+    msg['Subject'] = EMAIL_PREFIX + subject
+    msg['From'] = send_from
+    msg['To'] = send_to
+    if len(send_cc) > 0: msg['CC'] = ",".join(send_cc)
+    msg['Reply-To'] = "no-reply@db.cs.cmu.edu"
+
+    server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server_ssl.ehlo() # optional, called by login()
+    server_ssl.login(args["gmail_user"], args["gmail_pass"])
+    server_ssl.sendmail(msg['From'], [msg['To']], msg.as_string())
+    server_ssl.close()
+    
+    #print "Subject:", msg['Subject']
+    #print "From:", msg['From']
+    #print "To:", msg['To']
+    #if 'CC' in msg: print "CC:", msg['CC']
+    #print contents
+## DEF
+
 
 # ==============================================
 # main
@@ -108,8 +275,10 @@ if __name__ == '__main__':
     aparser = argparse.ArgumentParser()
     aparser.add_argument('token', type=str, help='Github API Token')
     aparser.add_argument('schedule', type=str, help='Schedule CSV URL')
-    aparser.add_argument('--send', action='store_true', help='Send status report to current czar')
-    aparser.add_argument('--send-to', type=str, help='Send status report to given email')
+    aparser.add_argument('emails', type=str, help='Mapping file (Github->email)')
+    aparser.add_argument('--send-status', action='store_true', help='Send status report to current czar')
+    aparser.add_argument('--send-reminders', action='store_true', help='Send PR review reminders')
+    aparser.add_argument('--override', type=str, help='All emails will go to this account')
     aparser.add_argument('--gmail-user', type=str, help='Gmail username')
     aparser.add_argument('--gmail-pass', type=str,help='Gmail password')
     aparser.add_argument("--cache", action='store_true', help="Enable caching of raw Github requests (for development only)")
@@ -131,11 +300,16 @@ if __name__ == '__main__':
 
     # Download the schedule CSV and figure out who is the czar this month
     current_czar = get_current_czar(args['schedule'], cache=args['cache'])
-
+    
     ## ----------------------------------------------
 
     gh = Github(token=args['token'])
-    r = gh.repos.get(user=GITHUB_USER, repo='peloton')
+    r = gh.repos.get(user=GITHUB_USER, repo=GITHUB_REPO)
+
+    ## ----------------------------------------------
+    
+    # Get the mapping of Github acctnames to emails
+    user_emails = get_user_emails(gh, args["emails"], cache=args["cache"])
 
     ## --------------------------------
     ## PR MONITOR
@@ -248,7 +422,7 @@ if __name__ == '__main__':
     linebreak = "-"*60 + "\n\n"
 
     ## NO LABELS
-    content += "*NO LABELS*\n\n"
+    content += "=== NO LABELS ===\n\n"
     if len(status['NoLabels']) == 0:
         content += "*NONE*\n\n"
     else:
@@ -257,7 +431,7 @@ if __name__ == '__main__':
     content += linebreak
 
     ## READY TO MERGE
-    content += "*READY TO MERGE*\n\n"
+    content += "=== READY TO MERGE ===\n\n"
     if len(status['ReadyToMerge']) == 0:
         content += "*NONE*\n\n"
     else:
@@ -266,7 +440,7 @@ if __name__ == '__main__':
     content += linebreak
 
     ## READY FOR REVIEW, NO ASSIGNMENT
-    content += "*READY FOR REVIEW WITHOUT ASSIGNMENT*\n\n"
+    content += "=== READY FOR REVIEW WITHOUT ASSIGNMENT ===\n\n"
     if len(status['NeedReviewers']) == 0:
         content += "*NONE*\n\n"
     else:
@@ -275,7 +449,7 @@ if __name__ == '__main__':
     content += linebreak
 
     ## MISSING REVIEWS
-    content += "*WAITING FOR REVIEW*\n\n"
+    content += "=== WAITING FOR REVIEW ===\n\n"
     if len(status['ReviewMissing']) == 0:
         content += "*NONE*\n\n"
     else:
@@ -290,30 +464,47 @@ if __name__ == '__main__':
             content += "        Reviews Provided: %s\n\n" % (",".join(all_recieved_reviews[pr_num]))
     #content += linebreak
 
-
-    if "send" in args and args["send"]:
+    if "send_status" in args and args["send_status"]:
         send_to = current_czar
-        if "send_to" in args and args["send_to"]:
-            send_to = args["send_to"]
-
-        LOG.debug("Sending status report to '%s'" % send_to)
-        msg = MIMEText(content)
-        msg['Subject'] = "%s PR Status Report (%s)" % (GITHUB_REPO.title(), datetime.now().strftime("%Y-%m-%d"))
-        msg['From'] = EMAIL_FROM
-        msg['To'] = send_to
-        msg['Reply-To'] = "no-reply@db.cs.cmu.edu"
-
-        server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server_ssl.ehlo() # optional, called by login()
-        server_ssl.login(args["gmail_user"], args["gmail_pass"])
-        # ssl server doesn't support or need tls, so don't call server_ssl.starttls()
-        server_ssl.sendmail(msg['From'], [msg['To']], msg.as_string())
-        #server_ssl.quit()
-        server_ssl.close()
-
-        LOG.info("Status email sent to '%s'" % send_to)
+        if "override" in args and args["override"]:
+            send_to = args["override"]
+        subject = "%s PR Status Report (%s)" % (GITHUB_REPO.title(), datetime.now().strftime("%Y-%m-%d"))
+        
+        send_email(args, subject, contents, EMAIL_FROM, send_to)
+        LOG.info("Sent status email to '%s'" % send_to)
     else:
         print content
+    ## IF
+    
+    # Send reminder emails for missing PRs
+    if "send_reminders" in args and args["send_reminders"]:
+        if "override" in args and args["override"]:
+            master_sender = args["override"]
+        else:
+            master_sender = current_czar
+        
+        for pr_num in sorted(status['ReviewMissing']):
+            for reviewer in all_reviewers[pr_num]:
+                if reviewer in all_recieved_reviews[pr_num]:
+                    continue
+                if not reviewer in user_emails:
+                    LOG.warn("No email available for login '%s'" % reviewer)
+                    continue
+                
+                subject = "Missing Review for PR #%d" % pr_num
+                if "override" in args and args["override"]:
+                    send_to = args["override"]
+                else:
+                    send_to = ",".join(user_emails[reviewer])
+                send_cc = [ master_sender ] 
+                
+                contents = "Please add your review for the following PR:\n\n" + \
+                            pr_format(open_pulls[pr_num][0])
+                            
+                send_email(args, subject, contents, EMAIL_FROM, send_to, send_cc)
+                LOG.debug("Sent reminder email to '%s'" % send_to)
+            ## FOR
+        ## FOR
     ## IF
 
 ## MAIN
