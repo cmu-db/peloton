4a7a7929bac537038d32fc599595c13dc56fa42f
Tianyu Li <tli2@andrew.cmu.edu>
2018-03-04 13:09:07 -0500

    Move notifiable_task to common and split into .h and .cpp

---
+++ src/include/brain/scheduler_task.h
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// scheduler_task.h
+//
+// Identification: src/include/brain/scheduler_task.h
+//
+// Copyright (c) 2015-2018, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "common/notifiable_task.h"
+
+namespace peloton {
+namespace brain {
+
+}
+}
