d18033d5afabd5af237abd9e944f821eab3ca324
pbollimp <pbollimp@andrew.cmu.edu>
2018-03-29 19:41:13 -0400

    added the files for cost evaluation

---
+++ src/include/brain/cost_evaluation.h
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// cost_evaluation.h
+//
+// Identification: src/include/brain/cost_evaluation.h
+//
+// Copyright (c) 2015-2018, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <string>
+
+#include "parser/pg_query.h"
+
+namespace peloton {
+namespace brain {
+
+
+
+
+
+}  // namespace brain
+}  // namespace peloton
