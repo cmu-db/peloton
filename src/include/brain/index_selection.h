b930b675ff151811ac34ed3fdab3d55f576c6155
Sivaprasad Sudhir <sivaprasad2626@gmail.com>
2018-03-29 20:00:26 -0400

    Basic classes

---
+++ src/include/brain/index_selection.h
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// index_selection.h
+//
+// Identification: src/include/brain/index_selection.h
+//
+// Copyright (c) 2015-2018, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+namespace peloton {
+namespace brain {
+
+//===--------------------------------------------------------------------===//
+// IndexSelection
+//===--------------------------------------------------------------------===//
+
+class IndexSelection {
+ public:
+  /**
+   * @brief Constructor
+   */
+  IndexSelection() {}
+
+ private:
+
+};
+
+}  // namespace brain
+}  // namespace peloton
