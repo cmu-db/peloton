371fd38af57f17c6431af5a6ed5058af4b363917
Sivaprasad Sudhir <sivaprasad2626@gmail.com>
2018-04-09 18:14:01 -0400

    adding cost model classes

---
+++ src/brain/cost_model.cpp
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// cost_model.cpp
+//
+// Identification: src/brain/cost_model.cpp
+//
+// Copyright (c) 2015-2018, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "brain/cost_model.h"
+#include "common/logger.h"
+
+namespace peloton {
+namespace brain {
+
+}  // namespace brain
+}  // namespace peloton
