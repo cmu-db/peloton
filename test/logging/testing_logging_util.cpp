7553a08036f3a48fbe5a9dcf9a2b3e706179e90d
Aaron Tian <aatt@cmu.edu>
2018-04-12 15:03:45 -0400

    Create logging testing utility files

---
+++ test/logging/testing_logging_util.cpp
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// testing_logging_util.cpp
+//
+// Identification: test/index/testing_logging_util.cpp
+//
+// Copyright (c) 2015-2018, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "logging/testing_logging_util.h"
+
+namespace peloton {
+  namespace test {
+
+  }  // namespace test
+}  // namespace peloton
