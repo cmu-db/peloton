7e59cd8f79a342b7dd49e789d101884b7fabfb51
Aaron Tian <aatt@cmu.edu>
2018-04-13 11:07:00 -0400

    Add tile group for the log_buffer_test

--- test/logging/log_buffer_test.cpp
+++ test/logging/log_buffer_test.cpp
@@ -10,6 +10,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "storage/tile_group_factory.h"
+#include "catalog/schema.h"
+#include "catalog/column.h"
 #include "logging/log_buffer.h"
 #include "common/harness.h"
 
@@ -23,13 +26,44 @@ namespace test {
 class LogBufferTests : public PelotonTest {};
 
 TEST_F(LogBufferTests, LogBufferTest) {
-  ItemPointer old_location(1, 8);
-  ItemPointer location(1, 16);
-  ItemPointer new_location(1, 24);
+  oid_t block = 1;
+  ItemPointer old_location(block, 8);
+  ItemPointer location(block, 16);
+  ItemPointer new_location(block, 24);
   eid_t epoch_id = 3;
   txn_id_t txn_id = 99;
   cid_t commit_id = 98;
-  logging::LogBuffer log_buffer(3);
+  oid_t database_id = 10;
+  oid_t table_id = 20;
+  oid_t tile_group_id = 30;
+
+  logging::LogBuffer *log_buffer;
+  log_buffer = new logging::LogBuffer(logging::LogManager::GetInstance().GetTransactionBufferSize());
+
+  // TILES
+  std::vector<std::string> tile_column_names;
+  std::vector<std::vector<std::string>> column_names;
+
+  tile_column_names.push_back("INTEGER COL");
+  column_names.push_back(tile_column_names);
+
+  std::vector<catalog::Schema> schemas;
+  std::vector<catalog::Column> columns;
+
+  // SCHEMA
+  catalog::Column column1(type::TypeId::INTEGER, type::Type::GetTypeSize(type::TypeId::INTEGER),
+                          "A", true);
+  columns.push_back(column1);
+
+  std::unique_ptr<catalog::Schema> schema1(new catalog::Schema(columns));
+  schemas.push_back(*schema1);
+
+  std::map<oid_t, std::pair<oid_t, oid_t>> column_map;
+  column_map[0] = std::make_pair(0, 0);
+
+  std::shared_ptr<storage::TileGroup> tile_group(storage::TileGroupFactory::GetTileGroup(
+          database_id, table_id, tile_group_id, nullptr, schemas, column_map, 3));
+  catalog::Manager::GetInstance().AddTileGroup(block, tile_group);
 
   logging::LogRecord insert_record = logging::LogRecordFactory::CreateTupleRecord(
           LogRecordType::TUPLE_INSERT, location, epoch_id, txn_id, commit_id);
@@ -39,18 +73,19 @@ TEST_F(LogBufferTests, LogBufferTest) {
   logging::LogRecord delete_record = logging::LogRecordFactory::CreateTupleRecord(
           LogRecordType::TUPLE_DELETE, old_location, epoch_id, txn_id, commit_id);
 
-  EXPECT_EQ(log_buffer.GetThreshold(), 3);
-//  log_buffer.WriteRecord(insert_record);
-//  std::cout << log_buffer.GetSize() << std::endl;
-//  std::cout << log_buffer.GetData() << std::endl;
-//
-//  log_buffer.WriteRecord(update_record);
-//  std::cout << log_buffer.GetSize() << std::endl;
-//  std::cout << log_buffer.GetData() << std::endl;
-//
-//  log_buffer.WriteRecord(delete_record);
-//  std::cout << log_buffer.GetSize() << std::endl;
-//  std::cout << log_buffer.GetData() << std::endl;
+  EXPECT_EQ(log_buffer->GetThreshold(), logging::LogManager::GetInstance().GetTransactionBufferSize());
+  log_buffer->WriteRecord(insert_record);
+
+//  std::cout << log_buffer->GetSize() << std::endl;
+//  std::cout << log_buffer->GetData() << std::endl;
+
+//  log_buffer->WriteRecord(update_record);
+//  std::cout << log_buffer->GetSize() << std::endl;
+//  std::cout << log_buffer->GetData() << std::endl;
+
+//  log_buffer->WriteRecord(delete_record);
+//  std::cout << log_buffer->GetSize() << std::endl;
+//  std::cout << log_buffer->GetData() << std::endl;
 }
 }
 }
