196e60ca4467c087694c7d7edf93d71f46594448
Aaron Tian <aatt@cmu.edu>
2018-04-16 11:05:39 -0400

    Complete update record tests

--- test/logging/log_buffer_test.cpp
+++ test/logging/log_buffer_test.cpp
@@ -26,10 +26,11 @@ namespace test {
 class LogBufferTests : public PelotonTest {};
 
 TEST_F(LogBufferTests, LogBufferTest) {
-  oid_t block = 1, offset = 16;
-  ItemPointer old_location(block, 8);
+  oid_t block = 1, offset = 16, old_block = 1, old_offset = 8, new_block = 1,
+        new_offset = 24;
+  ItemPointer old_location(old_block, old_offset);
   ItemPointer location(block, offset);
-  ItemPointer new_location(block, 24);
+  ItemPointer new_location(new_block, new_offset);
   eid_t epoch_id = 3;
   txn_id_t txn_id = 99;
   cid_t commit_id = 98;
@@ -37,6 +38,7 @@ TEST_F(LogBufferTests, LogBufferTest) {
   oid_t table_id = 20;
   oid_t tile_group_id = 30;
   double value1 = 9.1, value2 = 9.2, value3 = 9.3;
+  oid_t target_oid1 = 2, target_oid2 = 4, target_oid3 = 5;
 
   logging::LogBuffer *log_buffer;
   log_buffer = new logging::LogBuffer(logging::LogManager::GetInstance().GetTransactionBufferSize());
@@ -85,11 +87,29 @@ TEST_F(LogBufferTests, LogBufferTest) {
   values.push_back(val3);
   insert_record.SetValuesArray(reinterpret_cast<char *>(values.data()), static_cast<uint32_t>(values.size()));
   log_buffer->WriteRecord(insert_record);
-  size_t buffer_size = sizeof(int32_t) + sizeof(int8_t) + sizeof(int64_t) * 3 + sizeof(int64_t) * 4 +
+  size_t buffer_size1 = sizeof(int32_t) + sizeof(int8_t) + sizeof(int64_t) * 3 + sizeof(int64_t) * 4 +
                        sizeof(double) * 3;
-  EXPECT_EQ(buffer_size, log_buffer->GetSize());
+  EXPECT_EQ(buffer_size1, log_buffer->GetSize());
+
+  expression::AbstractExpression *expr = nullptr;
+  planner::DerivedAttribute tmp_att(expr);
+  Target target1 = std::make_pair(target_oid1, tmp_att);
+  Target target2 = std::make_pair(target_oid2, tmp_att);
+  Target target3 = std::make_pair(target_oid3, tmp_att);
+  TargetList *target_list = new TargetList;
+  target_list->push_back(target1);
+  target_list->push_back(target2);
+  target_list->push_back(target3);
+  update_record.SetOffsetsArray(target_list);
+  update_record.SetValuesArray(reinterpret_cast<char *>(values.data()), static_cast<uint32_t>(values.size()));
+  log_buffer->WriteRecord(update_record);
+  size_t buffer_size2 = sizeof(int32_t) + sizeof(int8_t) + sizeof(int64_t) * 3 + sizeof(int64_t) * 7 +
+                         (sizeof(int32_t) + sizeof(double)) * 3;
+  EXPECT_EQ(buffer_size2, log_buffer->GetSize() - buffer_size1);
+
   ReferenceSerializeInput log_buffer_input(log_buffer->GetData(), log_buffer->GetSize());
-  EXPECT_EQ(log_buffer->GetSize() - sizeof(int32_t), log_buffer_input.ReadInt());
+  // Insert record
+  EXPECT_EQ(buffer_size1 - sizeof(int32_t), log_buffer_input.ReadInt());
   EXPECT_EQ(static_cast<int>(LogRecordType::TUPLE_INSERT), log_buffer_input.ReadEnumInSingleByte());
   EXPECT_EQ(epoch_id, log_buffer_input.ReadLong());
   EXPECT_EQ(txn_id, log_buffer_input.ReadLong());
@@ -100,6 +120,26 @@ TEST_F(LogBufferTests, LogBufferTest) {
   EXPECT_EQ(offset, log_buffer_input.ReadLong());
   EXPECT_EQ(value1, log_buffer_input.ReadDouble());
   EXPECT_EQ(value2, log_buffer_input.ReadDouble());
+  EXPECT_EQ(value3, log_buffer_input.ReadDouble());
+
+  // Update Record
+  EXPECT_EQ(buffer_size2 - sizeof(int32_t), log_buffer_input.ReadInt());
+  EXPECT_EQ(static_cast<int>(LogRecordType::TUPLE_UPDATE), log_buffer_input.ReadEnumInSingleByte());
+  EXPECT_EQ(epoch_id, log_buffer_input.ReadLong());
+  EXPECT_EQ(txn_id, log_buffer_input.ReadLong());
+  EXPECT_EQ(commit_id, log_buffer_input.ReadLong());
+  EXPECT_EQ(database_id, log_buffer_input.ReadLong());
+  EXPECT_EQ(table_id, log_buffer_input.ReadLong());
+  EXPECT_EQ(block, log_buffer_input.ReadLong());
+  EXPECT_EQ(offset, log_buffer_input.ReadLong());
+  EXPECT_EQ(new_block, log_buffer_input.ReadLong());
+  EXPECT_EQ(new_offset, log_buffer_input.ReadLong());
+  EXPECT_EQ(update_record.GetNumValues(), log_buffer_input.ReadLong());
+  EXPECT_EQ(target_oid1, log_buffer_input.ReadInt());
+  EXPECT_EQ(value1, log_buffer_input.ReadDouble());
+  EXPECT_EQ(target_oid2, log_buffer_input.ReadInt());
+  EXPECT_EQ(value2, log_buffer_input.ReadDouble());
+  EXPECT_EQ(target_oid3, log_buffer_input.ReadInt());
   EXPECT_EQ(value3, log_buffer_input.ReadDouble());
 
 //  log_buffer->WriteRecord(update_record);
