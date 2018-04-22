5d020383b5409f5769c29102e690e82ab51944da
David Gershuni <dgershun@usc.edu>
2018-04-22 15:44:26 -0400

    Fixed bug where tuple slots are not reclaimed when insertions fail. Added function RecycleUnusedTupleSlot() TransactionLevelGCManager.

--- src/include/gc/gc_manager.h
+++ src/include/gc/gc_manager.h
@@ -65,13 +65,11 @@ class GCManager {
 
   virtual void StopGC() {}
 
-  virtual ItemPointer GetRecycledTupleSlot(const oid_t &table_id
-                                               UNUSED_ATTRIBUTE) {
+  virtual ItemPointer GetRecycledTupleSlot(const oid_t &table_id UNUSED_ATTRIBUTE) {
     return INVALID_ITEMPOINTER;
   }
 
-  virtual void RecycleUnusedTupleSlot(const ItemPointer &location
-                                          UNUSED_ATTRIBUTE) {}
+  virtual void RecycleUnusedTupleSlot(const ItemPointer &location UNUSED_ATTRIBUTE) {}
 
   virtual void RegisterTable(const oid_t &table_id UNUSED_ATTRIBUTE) {}
 
