f68eafd8e0953a94fd38e208f702c651a956be60
Justin <justinwang.stl@gmail.com>
2018-04-22 16:06:16 -0400

    begin new stats api

---
+++ src/include/statistics/abstract_metric_new.h
@@ -0,0 +1,58 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// abstract_metric.h
+//
+// Identification: src/statistics/abstract_metric.h
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <string>
+
+#include "common/internal_types.h"
+#include "common/printable.h"
+
+namespace peloton {
+namespace stats {
+
+/**
+ * Abstract class for metrics
+ * A metric should be able to:
+ * (1) identify its type;
+ * (2) print itself (ToString);
+ * (3) reset itself;
+ * (4) aggregate itself with another source
+ *     of the same type.
+ */
+class AbstractMetric : public Printable {
+ public:
+  AbstractMetric(MetricType type_);
+  virtual ~AbstractMetric();
+
+  const inline MetricType& GetType() const { return type_; }
+
+  virtual void Reset() = 0;
+
+  virtual const std::string GetInfo() const = 0;
+
+  // only called by aggregator thread, read-only on source
+  virtual void Aggregate(AbstractMetric& source) = 0;
+
+  // log a metric (eg increment, record latency)
+  virtual void Log() = 0;
+
+  // do any metric logging setup (eg nothing for point metrics, start a timer)
+  virtual void Init() = 0;
+
+ private:
+  // The type this metric belongs to
+  MetricType type_;
+};
+
+}  // namespace stats
+}  // namespace peloton
