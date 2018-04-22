dd66fd15960c4cee41deb0f3513a24585874146d
Weichen Li <bladelee@outlook.com>
2018-04-22 19:53:52 -0400

    added util functions for the test

--- test/brain/rl_framework_test.cpp
+++ test/brain/rl_framework_test.cpp
@@ -10,7 +10,11 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "brain/index_selection.h"
+#include "catalog/index_catalog.h"
 #include "common/harness.h"
+#include "concurrency/transaction_manager_factory.h"
+#include "sql/testing_sql_util.h"
 #include "util/file_util.h"
 
 namespace peloton {
@@ -20,7 +24,48 @@ namespace test {
 // RL Framework Tests
 //===--------------------------------------------------------------------===//
 
-class RLFrameworkTest : public PelotonTest {};
+class RLFrameworkTest : public PelotonTest {
+ private:
+  std::string database_name;
+
+ public:
+  RLFrameworkTest() {}
+
+  // Create a new database
+  void CreateDatabase(const std::string &db_name) {
+    database_name = db_name;
+    std::string create_db_str = "CREATE DATABASE " + db_name + ";";
+    TestingSQLUtil::ExecuteSQLQuery(create_db_str);
+  }
+
+  // Create a new table with schema (a INT, b INT, c INT).
+  void CreateTable(const std::string &table_name) {
+    std::string create_str =
+        "CREATE TABLE " + table_name + "(a INT, b INT, c INT);";
+    TestingSQLUtil::ExecuteSQLQuery(create_str);
+  }
+
+  void DropTable(const std::string &table_name) {
+    std::string create_str = "DROP TABLE " + table_name + ";";
+    TestingSQLUtil::ExecuteSQLQuery(create_str);
+  }
+
+  void DropDatabase(const std::string &db_name) {
+    std::string create_str = "DROP DATABASE " + db_name + ";";
+    TestingSQLUtil::ExecuteSQLQuery(create_str);
+  }
+
+  // Inserts a given number of tuples with increasing values into the table.
+  void InsertIntoTable(const std::string &table_name, int no_of_tuples) {
+    // Insert tuples into table
+    for (int i = 0; i < no_of_tuples; i++) {
+      std::ostringstream oss;
+      oss << "INSERT INTO " << table_name << " VALUES (" << i << "," << i + 1
+          << "," << i + 2 << ");";
+      TestingSQLUtil::ExecuteSQLQuery(oss.str());
+    }
+  }
+};
 
 TEST_F(RLFrameworkTest, BasicTest) { EXPECT_TRUE(1); }
 
