1373bb2de5950add86c30377ffeb0ec00057e2e0
mbutrovich <mbutrovich@gmail.com>
2018-04-23 13:57:47 -0400

    Reenabled 4 disabled tests because we still want to test recycle slots. Disabled the index checks in those tests until that issue is resolved.

--- test/gc/transaction_level_gc_manager_test.cpp
+++ test/gc/transaction_level_gc_manager_test.cpp
@@ -476,7 +476,7 @@ TEST_F(TransactionLevelGCManagerTests, AbortUpAdateSecondaryKeyTest) {
 // Assert RQ.size = 0
 // Assert old tuple in 1 index (primary key)
 // Assert new tuple in 2 indexes
-TEST_F(TransactionLevelGCManagerTests, DISABLED_CommitInsertUpdateTest) {
+TEST_F(TransactionLevelGCManagerTests, CommitInsertUpdateTest) {
   std::string test_name = "CommitInsertUpdate";
   uint64_t current_epoch = 0;
   auto &epoch_manager = concurrency::EpochManagerFactory::GetInstance();
@@ -513,10 +513,12 @@ TEST_F(TransactionLevelGCManagerTests, DISABLED_CommitInsertUpdateTest) {
 
   EXPECT_EQ(0, GetNumRecycledTuples(table.get()));
 
-  EXPECT_EQ(0, CountOccurrencesInIndex(table.get(), 1, 0, 1));
-
-  EXPECT_EQ(1, CountOccurrencesInIndex(table.get(), 0, 0, 2));
-  EXPECT_EQ(1, CountOccurrencesInIndex(table.get(), 1, 0, 2));
+  // TODO: Enable these once we figure out how to handle reused tuple slots with
+  // indexes
+  //  EXPECT_EQ(0, CountOccurrencesInIndex(table.get(), 1, 0, 1));
+  //
+  //  EXPECT_EQ(1, CountOccurrencesInIndex(table.get(), 0, 0, 2));
+  //  EXPECT_EQ(1, CountOccurrencesInIndex(table.get(), 1, 0, 2));
 
   table.release();
   TestingExecutorUtil::DeleteDatabase(test_name + "DB");
@@ -532,7 +534,7 @@ TEST_F(TransactionLevelGCManagerTests, DISABLED_CommitInsertUpdateTest) {
 // Assert RQ.size = 1 or 2?
 // Assert inserted tuple in 0 indexes
 // Assert updated tuple in 0 indexes
-TEST_F(TransactionLevelGCManagerTests, DISABLED_AbortInsertUpdateTest) {
+TEST_F(TransactionLevelGCManagerTests, AbortInsertUpdateTest) {
   std::string test_name = "AbortInsertUpdate";
   uint64_t current_epoch = 0;
   auto &epoch_manager = concurrency::EpochManagerFactory::GetInstance();
@@ -567,8 +569,10 @@ TEST_F(TransactionLevelGCManagerTests, DISABLED_AbortInsertUpdateTest) {
   gc_manager.ClearGarbage(0);
 
   EXPECT_EQ(1, GetNumRecycledTuples(table.get()));
-  EXPECT_EQ(0, CountOccurrencesInAllIndexes(table.get(), 0, 1));
-  EXPECT_EQ(0, CountOccurrencesInAllIndexes(table.get(), 0, 2));
+  // TODO: Enable these once we figure out how to handle reused tuple slots with
+  // indexes
+  //  EXPECT_EQ(0, CountOccurrencesInAllIndexes(table.get(), 0, 1));
+  //  EXPECT_EQ(0, CountOccurrencesInAllIndexes(table.get(), 0, 2));
 
   table.release();
   TestingExecutorUtil::DeleteDatabase(test_name + "DB");
@@ -792,7 +796,7 @@ TEST_F(TransactionLevelGCManagerTests, AbortInsertDeleteTest) {
 // Assert RQ.size = 2
 // Assert old tuple in 0 indexes
 // Assert new tuple in 0 indexes
-TEST_F(TransactionLevelGCManagerTests, DISABLED_CommitUpdateDeleteTest) {
+TEST_F(TransactionLevelGCManagerTests, CommitUpdateDeleteTest) {
   std::string test_name = "CommitUpdateDelete";
   uint64_t current_epoch = 0;
   auto &epoch_manager = concurrency::EpochManagerFactory::GetInstance();
@@ -829,8 +833,10 @@ TEST_F(TransactionLevelGCManagerTests, DISABLED_CommitUpdateDeleteTest) {
   gc_manager.ClearGarbage(0);
 
   EXPECT_EQ(2, GetNumRecycledTuples(table.get()));
-  EXPECT_EQ(0, CountOccurrencesInAllIndexes(table.get(), 0, 1));
-  EXPECT_EQ(0, CountOccurrencesInAllIndexes(table.get(), 0, 2));
+  // TODO: Enable these once we figure out how to handle reused tuple slots with
+  // indexes
+  //  EXPECT_EQ(0, CountOccurrencesInAllIndexes(table.get(), 0, 1));
+  //  EXPECT_EQ(0, CountOccurrencesInAllIndexes(table.get(), 0, 2));
 
   table.release();
   TestingExecutorUtil::DeleteDatabase(test_name + "DB");
@@ -848,7 +854,7 @@ TEST_F(TransactionLevelGCManagerTests, DISABLED_CommitUpdateDeleteTest) {
 // Assert RQ size = 2
 // Assert old tuple in 2 indexes
 // Assert new tuple in 1 index (primary key)
-TEST_F(TransactionLevelGCManagerTests, DISABLED_AbortUpdateDeleteTest) {
+TEST_F(TransactionLevelGCManagerTests, AbortUpdateDeleteTest) {
   std::string test_name = "AbortUpdateDelete";
   uint64_t current_epoch = 0;
   auto &epoch_manager = concurrency::EpochManagerFactory::GetInstance();
@@ -887,8 +893,10 @@ TEST_F(TransactionLevelGCManagerTests, DISABLED_AbortUpdateDeleteTest) {
 
   EXPECT_EQ(1, GetNumRecycledTuples(table.get()));
 
-  EXPECT_EQ(2, CountOccurrencesInAllIndexes(table.get(), 0, 1));
-  EXPECT_EQ(0, CountOccurrencesInIndex(table.get(), 1, 0, 2));
+  // TODO: Enable these once we figure out how to handle reused tuple slots with
+  // indexes
+  //  EXPECT_EQ(2, CountOccurrencesInAllIndexes(table.get(), 0, 1));
+  //  EXPECT_EQ(0, CountOccurrencesInIndex(table.get(), 1, 0, 2));
 
   table.release();
   TestingExecutorUtil::DeleteDatabase(test_name + "DB");
