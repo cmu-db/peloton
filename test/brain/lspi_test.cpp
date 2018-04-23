60a65a820fa280e33e21879ab104d58cbfd052ae
saatviks <saatviks@andrew.cmu.edu>
2018-04-23 00:25:57 -0400

    RLSE model + test

---
+++ test/brain/lspi_test.cpp
@@ -0,0 +1,53 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// tensorflow_test.cpp
+//
+// Identification: test/brain/tensorflow_test.cpp
+//
+// Copyright (c) 2015-2018, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "brain/indextune/lspi/rlse_lm.h"
+#include "brain/util/eigen_util.h"
+#include "common/harness.h"
+
+namespace peloton {
+namespace test {
+
+//===--------------------------------------------------------------------===//
+// Tensorflow Tests
+//===--------------------------------------------------------------------===//
+
+class LSPITests : public PelotonTest {};
+
+TEST_F(LSPITests, RLSETest) {
+  // Attempt to fit y = m*x
+  int NUM_SAMPLES = 500;
+  int LOG_INTERVAL = 100;
+  int m = 3;
+  vector_eig data_in = vector_eig::LinSpaced(NUM_SAMPLES, 0, NUM_SAMPLES - 1);
+  vector_eig data_out = data_in.array()*m;
+  vector_eig loss_vector = vector_eig::Zero(LOG_INTERVAL);
+  float prev_loss = std::numeric_limits<float>::max();
+  auto model = brain::RLSEModel(1);
+  for(int i = 0; i < NUM_SAMPLES; i++) {
+    vector_eig feat_vec = data_in.segment(i, 1);
+    double value_true = data_out(i);
+    double value_pred = model.Predict(feat_vec);
+    double loss = fabs(value_pred - value_true);
+    loss_vector(i % LOG_INTERVAL) = loss;
+    model.Update(feat_vec, value_true);
+    if((i+1) % LOG_INTERVAL == 0) {
+      float curr_loss = loss_vector.array().mean();
+      LOG_DEBUG("Loss at %d: %.5f", i, curr_loss);
+      EXPECT_LE(curr_loss, prev_loss);
+      prev_loss = curr_loss;
+    }
+  }
+}
+
+}  // namespace test
+}  // namespace peloton
