e98228bc48178923c989da08d3342600eb3b5376
Weichen Li <bladelee@outlook.com>
2018-04-22 20:41:58 -0400

    using txn for util functions

--- test/brain/rl_framework_test.cpp
+++ test/brain/rl_framework_test.cpp
@@ -11,10 +11,10 @@
 //===----------------------------------------------------------------------===//
 
 #include "brain/index_selection.h"
+#include "catalog/catalog.h"
 #include "catalog/index_catalog.h"
 #include "common/harness.h"
 #include "concurrency/transaction_manager_factory.h"
-#include "sql/testing_sql_util.h"
 #include "util/file_util.h"
 
 namespace peloton {
@@ -26,48 +26,68 @@ namespace test {
 
 class RLFrameworkTest : public PelotonTest {
  private:
-  std::string database_name;
+  std::string database_name_;
+  catalog::Catalog *catalog_;
+  concurrency::TransactionManager *txn_manager_;
 
  public:
-  RLFrameworkTest() {}
+  RLFrameworkTest()
+      : catalog_{catalog::Catalog::GetInstance()},
+        txn_manager_(&concurrency::TransactionManagerFactory::GetInstance()) {
+    catalog_->Bootstrap();
+  }
 
   // Create a new database
   void CreateDatabase(const std::string &db_name) {
-    database_name = db_name;
-    std::string create_db_str = "CREATE DATABASE " + db_name + ";";
-    TestingSQLUtil::ExecuteSQLQuery(create_db_str);
+    database_name_ = db_name;
+
+    auto txn = txn_manager_->BeginTransaction();
+    catalog_->CreateDatabase(database_name_, txn);
+    txn_manager_->CommitTransaction(txn);
   }
 
   // Create a new table with schema (a INT, b INT, c INT).
   void CreateTable(const std::string &table_name) {
-    std::string create_str =
-        "CREATE TABLE " + table_name + "(a INT, b INT, c INT);";
-    TestingSQLUtil::ExecuteSQLQuery(create_str);
+    auto a_column = catalog::Column(
+        type::TypeId::INTEGER, type::Type::GetTypeSize(type::TypeId::INTEGER),
+        "a", true);
+    auto b_column = catalog::Column(
+        type::TypeId::INTEGER, type::Type::GetTypeSize(type::TypeId::INTEGER),
+        "b", true);
+    auto c_column = catalog::Column(
+        type::TypeId::INTEGER, type::Type::GetTypeSize(type::TypeId::INTEGER),
+        "c", true);
+    std::unique_ptr<catalog::Schema> table_schema(
+        new catalog::Schema({a_column, b_column, c_column}));
+
+    auto txn = txn_manager_->BeginTransaction();
+    catalog_->CreateTable(database_name_, table_name, std::move(table_schema),
+                          txn);
+    txn_manager_->CommitTransaction(txn);
   }
 
   void DropTable(const std::string &table_name) {
-    std::string create_str = "DROP TABLE " + table_name + ";";
-    TestingSQLUtil::ExecuteSQLQuery(create_str);
+    auto txn = txn_manager_->BeginTransaction();
+    catalog_->DropTable(database_name_, table_name, txn);
+    txn_manager_->CommitTransaction(txn);
   }
 
-  void DropDatabase(const std::string &db_name) {
-    std::string create_str = "DROP DATABASE " + db_name + ";";
-    TestingSQLUtil::ExecuteSQLQuery(create_str);
-  }
-
-  // Inserts a given number of tuples with increasing values into the table.
-  void InsertIntoTable(const std::string &table_name, int no_of_tuples) {
-    // Insert tuples into table
-    for (int i = 0; i < no_of_tuples; i++) {
-      std::ostringstream oss;
-      oss << "INSERT INTO " << table_name << " VALUES (" << i << "," << i + 1
-          << "," << i + 2 << ");";
-      TestingSQLUtil::ExecuteSQLQuery(oss.str());
-    }
+  void DropDatabase() {
+    auto txn = txn_manager_->BeginTransaction();
+    catalog_->DropDatabaseWithName(database_name_, txn);
+    txn_manager_->CommitTransaction(txn);
   }
 };
 
-TEST_F(RLFrameworkTest, BasicTest) { EXPECT_TRUE(1); }
+TEST_F(RLFrameworkTest, BasicTest) {
+  std::string database_name = DEFAULT_DB_NAME;
+  std::string table_name_1 = "dummy_table_1";
+  std::string table_name_2 = "dummy_table_2";
+
+  CreateDatabase(database_name);
+  CreateTable(table_name_1);
+  CreateTable(table_name_2);
+}
 
 }  // namespace test
 }  // namespace peloton
