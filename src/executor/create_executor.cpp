b3cf7b4248ef3120f44dfb63dad69624e4eef2bf
Xingyu Jin <xyj@XingyudeMBP.wv.cc.cmu.edu>
2018-05-09 16:03:50 -0400

    add logs

--- src/executor/create_executor.cpp
+++ src/executor/create_executor.cpp
@@ -93,12 +93,15 @@ bool CreateExecutor::DExecute() {
 }
 
 bool CreateExecutor::CreateDatabase(const planner::CreatePlan &node) {
-  LOG_TRACE("node dbname = %s", node.GetDatabaseName().c_str());
+  LOG_TRACE("begin create db");
   auto txn = context_->GetTransaction();
+  LOG_TRACE("get txn");
   auto database_name = node.GetDatabaseName();
+  LOG_TRACE("node dbname = %s", node.GetDatabaseName().c_str());
   // invoke logic within catalog.cpp
   ResultType result =
       catalog::Catalog::GetInstance()->CreateDatabase(database_name, txn);
+  LOG_TRACE("get result");
   txn->SetResult(result);
   LOG_TRACE("Result is: %s", ResultTypeToString(txn->GetResult()).c_str());
   return (true);
