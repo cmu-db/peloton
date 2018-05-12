c7da1a1273b7ef6bf8934f41bae29b152e80bfec
Tianyi Chen <tianyic1@andrew.cmu.edu>
2018-05-12 00:52:47 -0400

    add test metric and testing code, add desctructor of Abstract Metric to avoid memory leak

---
+++ src/include/statistics/test_metric.h
@@ -0,0 +1,8 @@
+//
+// Created by tianyi on 5/11/18.
+//
+
+#ifndef PELOTON_TEST_METRIC_H
+#define PELOTON_TEST_METRIC_H
+
+#endif //PELOTON_TEST_METRIC_H
---
+++ test/statistics/stats_framework_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tianyi on 5/11/18.
+//
+
+#include "stats_framework_test.h"
