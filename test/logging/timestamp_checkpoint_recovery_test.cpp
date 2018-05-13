a068a97ae56f933c24f230f0f1c553e83599fa83
ksaito7 <new.wahiro@gmail.com>
2018-05-13 18:04:49 -0400

    Check where the travis error occurs

--- test/logging/timestamp_checkpoint_recovery_test.cpp
+++ test/logging/timestamp_checkpoint_recovery_test.cpp
@@ -63,9 +63,8 @@ TEST_F(TimestampCheckpointRecoveryTests, CheckpointRecoveryTest) {
     LOG_INFO("Check the table %d %s\n%s", table_catalog->GetTableOid(),
              table_name.c_str(), table->GetInfo().c_str());
 
-    /*
     // check the basic information of columns
-    if (table_name == "checkpoint_table_test") {
+    /*if (table_name == "checkpoint_table_test") {
       for (auto column_pair : table_catalog->GetColumnObjects()) {
         auto column_catalog = column_pair.second;
         auto column =
@@ -105,7 +104,9 @@ TEST_F(TimestampCheckpointRecoveryTests, CheckpointRecoveryTest) {
     // end: check the basic information of columns
 
     // check the index recovery
-    else if (table_name == "checkpoint_index_test") {
+    else
+    	*/
+			if (table_name == "checkpoint_index_test") {
       for (auto index_pair : table_catalog->GetIndexObjects()) {
         auto index_catalog = index_pair.second;
 
@@ -423,7 +424,6 @@ TEST_F(TimestampCheckpointRecoveryTests, CheckpointRecoveryTest) {
     } else {
       LOG_ERROR("Unexpected table is found: %s", table_name.c_str());
     }
-    */
   }  // table loop end
 
   // finish the low level test
