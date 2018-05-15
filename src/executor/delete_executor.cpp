e5e7138a37f347336abb2820c6dba6a73f377902
alphalzh <lzhalpha@gmail.com>
2018-05-15 00:45:49 -0400

    Update delete_executor.cpp

--- src/executor/delete_executor.cpp
+++ src/executor/delete_executor.cpp
@@ -83,7 +83,7 @@ bool DeleteExecutor::DExecute() {
       (void *)lm, table_oid);
   bool lock_success = lm->LockShared(table_oid);
   if (!lock_success) {
-    LOG_WARN("Cannot obtain lock for the table, abort!");
+    LOG_TRACE("Cannot obtain lock for the table, abort!");
   } else {
     current_txn->AddLockShared(table_oid);
   }
