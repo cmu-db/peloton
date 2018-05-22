19881558f3cacaddfadb2bb993909171093774bf
mbutrovich <mbutrovich@gmail.com>
2018-05-22 10:52:54 -0400

    Putting back the TileGroupIterator for future enhancement.

--- src/include/storage/tile_group_iterator.h
+++ src/include/storage/tile_group_iterator.h
@@ -1,66 +1,66 @@
-////===----------------------------------------------------------------------===//
-////
-////                         Peloton
-////
-//// tile_group_iterator.h
-////
-//// Identification: src/include/storage/tile_group_iterator.h
-////
-//// Copyright (c) 2015-16, Carnegie Mellon University Database Group
-////
-////===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
+//                         Peloton
 //
-//#pragma once
+// tile_group_iterator.h
 //
-//#include <memory>
+// Identification: src/include/storage/tile_group_iterator.h
 //
-//#include "common/internal_types.h"
-//#include "common/iterator.h"
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
 //
-//namespace peloton {
-//namespace storage {
-//
-////===--------------------------------------------------------------------===//
-//// TileGroup Iterator
-////===--------------------------------------------------------------------===//
-//
-//class DataTable;
-//class TileGroup;
-//
-///**
-// * Iterator for table which goes over all active tiles.
-// * FIXME: This is not thread-safe or transactional!
-// **/
-//class TileGroupIterator : public Iterator<std::shared_ptr<TileGroup>> {
-//  TileGroupIterator() = delete;
-//
-// public:
-//  TileGroupIterator(const DataTable *table)
-//      : table_(table), tile_group_itr_(0) {
-//    // More Wu Tang!
-//  }
-//
-//  TileGroupIterator(const TileGroupIterator &other)
-//      : table_(other.table_), tile_group_itr_(other.tile_group_itr_) {
-//    // More Wu Tang!
-//  }
-//
-//  /**
-//   * Updates the given tile so that it points to the next tile in the table.
-//   * @return true if succeeded. false if no more tuples are there.
-//   */
-//  bool Next(std::shared_ptr<TileGroup> &tileGroup);
-//
-//  bool HasNext();
-//
-// private:
-//  // Table
-//  const DataTable *table_;
-//
-//  // Tile group iterator
-//  oid_t tile_group_itr_;
-//};
-//
-//}  // namespace storage
-//}  // namespace peloton
+//===----------------------------------------------------------------------===//
+
+
+#pragma once
+
+#include <memory>
+
+#include "common/internal_types.h"
+#include "common/iterator.h"
+
+namespace peloton {
+namespace storage {
+
+//===--------------------------------------------------------------------===//
+// TileGroup Iterator
+//===--------------------------------------------------------------------===//
+
+class DataTable;
+class TileGroup;
+
+/**
+ * Iterator for table which goes over all active tiles.
+ * FIXME: This is not thread-safe or transactional!
+ **/
+class TileGroupIterator : public Iterator<std::shared_ptr<TileGroup>> {
+  TileGroupIterator() = delete;
+
+ public:
+  TileGroupIterator(const DataTable *table)
+      : table_(table), tile_group_itr_(0) {
+    // More Wu Tang!
+  }
+
+  TileGroupIterator(const TileGroupIterator &other)
+      : table_(other.table_), tile_group_itr_(other.tile_group_itr_) {
+    // More Wu Tang!
+  }
+
+  /**
+   * Updates the given tile so that it points to the next tile in the table.
+   * @return true if succeeded. false if no more tuples are there.
+   */
+  bool Next(std::shared_ptr<TileGroup> &tileGroup);
+
+  bool HasNext();
+
+ private:
+  // Table
+  const DataTable *table_;
+
+  // Tile group iterator
+  oid_t tile_group_itr_;
+};
+
+}  // namespace storage
+}  // namespace peloton
---
+++ test/storage/tile_group_iterator_test.cpp
@@ -0,0 +1,62 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// tile_group_iterator_test.cpp
+//
+// Identification: test/storage/tile_group_iterator_test.cpp
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+
+#include <memory>
+
+#include "common/harness.h"
+
+#include "storage/data_table.h"
+#include "storage/tile_group.h"
+#include "storage/tile_group_iterator.h"
+
+#include "executor/testing_executor_util.h"
+#include "concurrency/transaction_manager_factory.h"
+
+namespace peloton {
+namespace test {
+
+//===--------------------------------------------------------------------===//
+// TileGroupIterator Tests
+//===--------------------------------------------------------------------===//
+
+class TileGroupIteratorTests : public PelotonTest {};
+
+TEST_F(TileGroupIteratorTests, BasicTest) {
+  const int tuples_per_tilegroup = TESTS_TUPLES_PER_TILEGROUP;
+  const int expected_tilegroup_count = 5;
+  const int allocated_tilegroup_count = 6;
+  const int tuple_count = tuples_per_tilegroup * expected_tilegroup_count;
+
+  // Create a table and wrap it in logical tiles
+  auto &txn_manager = concurrency::TransactionManagerFactory::GetInstance();
+  auto txn = txn_manager.BeginTransaction();
+  std::unique_ptr<storage::DataTable> data_table(
+      TestingExecutorUtil::CreateTable(tuples_per_tilegroup, false));
+  TestingExecutorUtil::PopulateTable(data_table.get(), tuple_count, false, false,
+                                   true, txn);
+  txn_manager.CommitTransaction(txn);
+
+  storage::TileGroupIterator tile_group_itr(data_table.get());
+  std::shared_ptr<storage::TileGroup> tile_group_ptr;
+  int actual_tile_group_count = 0;
+  while (tile_group_itr.Next(tile_group_ptr)) {
+    if (tile_group_ptr.get() != nullptr) {
+      actual_tile_group_count += 1;
+    }
+  }  // WHILE
+
+  EXPECT_EQ(allocated_tilegroup_count, actual_tile_group_count);
+}
+
+}  // namespace test
+}  // namespace peloton
