0343478959561d121a5d32db0ea66fcda753155c
mbutrovich <mbutrovich@gmail.com>
2018-06-25 13:56:12 -0400

    Cherry-pick GC fixes from #1349, first part.

--- src/concurrency/timestamp_ordering_transaction_manager.cpp
+++ src/concurrency/timestamp_ordering_transaction_manager.cpp
@@ -709,6 +709,9 @@ ResultType TimestampOrderingTransactionManager::CommitTransaction(
       gc_set->operator[](tile_group_id)[tuple_slot] =
           GCVersionType::COMMIT_DELETE;
 
+      gc_set->operator[](new_version.block)[new_version.offset] =
+          GCVersionType::TOMBSTONE;
+
       log_manager.LogDelete(ItemPointer(tile_group_id, tuple_slot));
 
     } else if (tuple_entry.second == RWType::INSERT) {
@@ -827,9 +830,11 @@ ResultType TimestampOrderingTransactionManager::AbortTransaction(
       // before we unlink the aborted version from version list
       ItemPointer *index_entry_ptr =
           tile_group_header->GetIndirection(tuple_slot);
-      UNUSED_ATTRIBUTE auto res = AtomicUpdateItemPointer(
-          index_entry_ptr, ItemPointer(tile_group_id, tuple_slot));
-      PELOTON_ASSERT(res == true);
+      if (index_entry_ptr) {
+        UNUSED_ATTRIBUTE auto res = AtomicUpdateItemPointer(
+            index_entry_ptr, ItemPointer(tile_group_id, tuple_slot));
+        PELOTON_ASSERT(res == true);
+      }
       //////////////////////////////////////////////////
 
       // we should set the version before releasing the lock.
@@ -875,9 +880,11 @@ ResultType TimestampOrderingTransactionManager::AbortTransaction(
       // before we unlink the aborted version from version list
       ItemPointer *index_entry_ptr =
           tile_group_header->GetIndirection(tuple_slot);
-      UNUSED_ATTRIBUTE auto res = AtomicUpdateItemPointer(
-          index_entry_ptr, ItemPointer(tile_group_id, tuple_slot));
-      PELOTON_ASSERT(res == true);
+      if (index_entry_ptr) {
+        UNUSED_ATTRIBUTE auto res = AtomicUpdateItemPointer(
+            index_entry_ptr, ItemPointer(tile_group_id, tuple_slot));
+        PELOTON_ASSERT(res == true);
+      }
       //////////////////////////////////////////////////
 
       // we should set the version before releasing the lock.
@@ -895,7 +902,7 @@ ResultType TimestampOrderingTransactionManager::AbortTransaction(
 
       // add the version to gc set.
       gc_set->operator[](new_version.block)[new_version.offset] =
-          GCVersionType::ABORT_DELETE;
+          GCVersionType::TOMBSTONE;
 
     } else if (tuple_entry.second == RWType::INSERT) {
       tile_group_header->SetBeginCommitId(tuple_slot, MAX_CID);
