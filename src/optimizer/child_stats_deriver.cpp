1fc8b5586162afb7a2f5607256abed149f74a665
Gustavo Angulo <GustavoAngulo@users.noreply.github.com>
2018-09-18 14:11:41 -0400

    Add cardinality estimate to AbstractPlan object (#1475)
    * Clarified variable name
    
    * Added cardinality and test
    
    * Add optimizer testing class
    
    * Remove debugging code
    
    * Revert default estimate to fix broken test
    
    * Formatting
    
    * Removed unecessary override in Cardinality test
    
    * More comments and clean up tests

--- src/optimizer/child_stats_deriver.cpp
+++ src/optimizer/child_stats_deriver.cpp
@@ -2,11 +2,11 @@
 //
 //                         Peloton
 //
-// cost_and_stats_calculator.h
+// child_stats_deriver.cpp
 //
-// Identification: src/optimizer/stats_calculator.cpp
+// Identification: src/optimizer/child_stats_deriver.cpp
 //
-// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+// Copyright (c) 2015-2018, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
 
@@ -67,7 +67,7 @@ void ChildStatsDeriver::PassDownColumn(expression::AbstractExpression *col) {
     auto child_group = memo_->GetGroupByID(gexpr_->GetChildGroupId(idx));
     if (child_group->GetTableAliases().count(tv_expr->GetTableName()) &&
         // If we have not derived the column stats yet
-        child_group->HasColumnStats(tv_expr->GetColFullName())) {
+        !child_group->HasColumnStats(tv_expr->GetColFullName())) {
       output_[idx].insert(col);
       break;
     }
