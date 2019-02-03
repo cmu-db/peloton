1c4e4b30d12ce8aee6d747e64d8969832d2d6728
GustavoAngulo <gangulo@andrew.cmu.edu>
2019-02-02 23:55:01 -0500

    Add test cases for trivial cost model

--- test/optimizer/plan_test.cpp
+++ test/optimizer/plan_test.cpp
@@ -48,7 +48,6 @@ TEST_F(PlanTest, PlanEqualityTest) {
 
   OptimizerTestUtil::PrintPlan(plan1);
   OptimizerTestUtil::PrintPlan(plan2);
-
 }
 
 
@@ -87,6 +86,94 @@ TEST_F(PlanTest, PostgresTwoJoinOrderTestSmall) {
   EXPECT_EQ(1, plan->GetChildren()[1]->GetChildren().size());
   auto right_scan =
       dynamic_cast<peloton::planner::AbstractScan *>(plan->GetChildren()[1]->GetChildren()[0].get());
+  EXPECT_EQ(PlanNodeType::SEQSCAN, right_scan->GetPlanNodeType());
+
+  // Check we build hash table on larger table (right table), and probe with smaller table (left table)
+  EXPECT_EQ(test1_table_name, left_scan->GetTable()->GetName().c_str());
+  EXPECT_EQ(test2_table_name, right_scan->GetTable()->GetName().c_str());
+}
+
+// With trivial model, ordering of tables in join should be reversed as both orderings have the same cost, however
+// test2 x test1 is explored after test1 x test2, so we pick the most previously explored one.
+TEST_F(PlanTest, TrivialTwoJoinOrderTestSmall) {
+
+  // Set cost model to postgres cost model
+  OptimizerTestUtil::SetCostModel(optimizer::CostModels::TRIVIAL);
+
+  // Populate Tables and run Analyze
+  std::string test1_table_name = "test1";
+  std::string test2_table_name = "test2";
+  int test1_table_size = 10;
+  int test2_table_size = 100;
+  OptimizerTestUtil::CreateTable(test1_table_name, test1_table_size);
+  OptimizerTestUtil::CreateTable(test2_table_name, test2_table_size);
+  OptimizerTestUtil::AnalyzeTable(test1_table_name);
+  OptimizerTestUtil::AnalyzeTable(test2_table_name);
+
+
+  // Generate query
+  auto query = OptimizerTestUtil::CreateTwoWayJoinQuery(test2_table_name, test1_table_name, "a", "a");
+
+  auto plan = OptimizerTestUtil::GeneratePlan(query);
+
+  EXPECT_EQ(PlanNodeType::HASHJOIN, plan->GetPlanNodeType());
+  EXPECT_EQ(2, plan->GetChildren().size());
+
+
+  // Get Left Scan
+  EXPECT_EQ(PlanNodeType::SEQSCAN, plan->GetChildren()[0]->GetPlanNodeType());
+  EXPECT_EQ(0, plan->GetChildren()[0]->GetChildren().size());
+  auto left_scan = dynamic_cast<peloton::planner::AbstractScan *>(plan->GetChildren()[0].get());
+
+  // Get Right Scan
+  EXPECT_EQ(PlanNodeType::HASH, plan->GetChildren()[1]->GetPlanNodeType());
+  EXPECT_EQ(1, plan->GetChildren()[1]->GetChildren().size());
+  auto right_scan =
+      dynamic_cast<peloton::planner::AbstractScan *>(plan->GetChildren()[1]->GetChildren()[0].get());
+  EXPECT_EQ(PlanNodeType::SEQSCAN, right_scan->GetPlanNodeType());
+
+  // Check we build hash table on larger table (right table), and probe with smaller table (left table)
+  EXPECT_EQ(test1_table_name, left_scan->GetTable()->GetName().c_str());
+  EXPECT_EQ(test2_table_name, right_scan->GetTable()->GetName().c_str());
+}
+
+
+// Tests that since the left table is a single tuple, we just use nested loop
+TEST_F(PlanTest, TrivialTwoJoinOrderTestSmall2) {
+
+  // Set cost model to postgres cost model
+  OptimizerTestUtil::SetCostModel(optimizer::CostModels::TRIVIAL);
+
+  // Populate Tables and run Analyze
+  std::string test1_table_name = "test1";
+  std::string test2_table_name = "test2";
+  int test1_table_size = 1;
+  int test2_table_size = 100;
+  OptimizerTestUtil::CreateTable(test1_table_name, test1_table_size);
+  OptimizerTestUtil::CreateTable(test2_table_name, test2_table_size);
+  OptimizerTestUtil::AnalyzeTable(test1_table_name);
+  OptimizerTestUtil::AnalyzeTable(test2_table_name);
+
+
+  // Generate query
+  auto query = OptimizerTestUtil::CreateTwoWayJoinQuery(test1_table_name, test2_table_name, "a", "a");
+
+  auto plan = OptimizerTestUtil::GeneratePlan(query);
+
+  EXPECT_EQ(PlanNodeType::NESTLOOP, plan->GetPlanNodeType());
+  EXPECT_EQ(2, plan->GetChildren().size());
+
+
+  // Get Left Scan
+  EXPECT_EQ(PlanNodeType::SEQSCAN, plan->GetChildren()[0]->GetPlanNodeType());
+  EXPECT_EQ(0, plan->GetChildren()[0]->GetChildren().size());
+  auto left_scan = dynamic_cast<peloton::planner::AbstractScan *>(plan->GetChildren()[0].get());
+
+  // Get Right Scan
+  EXPECT_EQ(PlanNodeType::SEQSCAN, plan->GetChildren()[1]->GetPlanNodeType());
+  EXPECT_EQ(0, plan->GetChildren()[1]->GetChildren().size());
+  auto right_scan =
+      dynamic_cast<peloton::planner::AbstractScan *>(plan->GetChildren()[1].get());
   EXPECT_EQ(PlanNodeType::SEQSCAN, right_scan->GetPlanNodeType());
 
   // Check we build hash table on larger table (right table), and probe with smaller table (left table)
